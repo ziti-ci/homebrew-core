854df9fdf2ee37078d31677dd37c10999f280495
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-06 17:28:44 +0000

    nom: update 2.13.0 bottle.

--- Formula/n/nom.rb
+++ Formula/n/nom.rb
@@ -7,13 +7,13 @@ class Nom < Formula
   head "https://github.com/guyfedwards/nom.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a43c3eb4fedc376b1aa0ac63a9879c7ab7fe0b7f39cace0490207aa9ec7ef072"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "af2f54ec86b5ab3307e1b76e14ddafdff5a9836560c396ac73d8c02d41b002c8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e7344965f4b7fd8f5434ac3adb8fcac0b58c10acec26eea75408528e77d0ed29"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a239a78f7216f75546e98895c25b5dd394def5f6d6a9b3abc79b3fa244b270e4"
-    sha256 cellar: :any_skip_relocation, ventura:       "ac30bcd8ccfaef9de1b3376e022ed47402d8b8f39dd92ffc04cfaf30d8225877"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3ee12b71e50cc3f0fa83562d598e4adc57fb9b59c48f5c1aae1222570fc31216"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "614fa0d3ca6a67781b6107d7d9752f863c499b6516ba808322865e574d33afc6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8d1b81a28d305679df6b4c6f8f9b1f5b2e016d77c25136d7087b3ea0e536c589"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5619529273fe8290353f9e2164058a7b30f2967467a2cda707306a5a963503bb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "916db36547c31f1e10ae3795cac922483fbf6705fdc08f12fb8f08865c48304c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "701a66638ec7d58ab364b69835ddaf5f0efffb636da8e5bde1a1464b85ed35ab"
+    sha256 cellar: :any_skip_relocation, ventura:       "def9204bc4135bf8b12774662a25e8366f9e098fe44a43db73c2b931082d8c3f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a75418aff236e43fc9960322d4db8643ae886697929d40cda0e7798f2bedb73f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "57f04d38b81d7db2d4ecc66aa6f347785246aa1ca103ebcc2c0c3191bf3decf3"
   end
 
   depends_on "go" => :build

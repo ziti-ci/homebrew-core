338865af41e8105e5d0e6ef59817e9c65404f655
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-06 15:01:03 +0000

    nom: update 2.12.0 bottle.

--- Formula/n/nom.rb
+++ Formula/n/nom.rb
@@ -7,13 +7,13 @@ class Nom < Formula
   head "https://github.com/guyfedwards/nom.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "32685f1285a43cf42db6412ec9f87cf202cbc4dbdfde0f617c5eed012da86e57"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a035f9d49babd58c38430e995afcb4b78b16a5da56fa4e9a994ee3acfdcbd1a8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "5adf707a510f8798513b197bf4b1053001a67ea5925686d360623f706dd20560"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bc856944947d1d4fe3b55b358304f6d805336c590f82d5686b30f640dfedc125"
-    sha256 cellar: :any_skip_relocation, ventura:       "6dc764d333bd8ceb3e20cf42b64b560f46bbe3b4bae26884fd82e67debbf44f8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f3728386a10006f45c1ed7577322f1760852e33e700e23e2cbd09668aecd10ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7545e7fb930cc40fc60e2c5ee251b33c6bce1cce653711911c99edfe9cd8d3c8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a43c3eb4fedc376b1aa0ac63a9879c7ab7fe0b7f39cace0490207aa9ec7ef072"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "af2f54ec86b5ab3307e1b76e14ddafdff5a9836560c396ac73d8c02d41b002c8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "e7344965f4b7fd8f5434ac3adb8fcac0b58c10acec26eea75408528e77d0ed29"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a239a78f7216f75546e98895c25b5dd394def5f6d6a9b3abc79b3fa244b270e4"
+    sha256 cellar: :any_skip_relocation, ventura:       "ac30bcd8ccfaef9de1b3376e022ed47402d8b8f39dd92ffc04cfaf30d8225877"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3ee12b71e50cc3f0fa83562d598e4adc57fb9b59c48f5c1aae1222570fc31216"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "614fa0d3ca6a67781b6107d7d9752f863c499b6516ba808322865e574d33afc6"
   end
 
   depends_on "go" => :build

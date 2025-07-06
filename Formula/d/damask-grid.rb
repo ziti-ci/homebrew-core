d9b2f6d8dfb59e16cbc848bee276bcdc16935be9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-06 13:46:01 +0900

    damask-grid: update 3.0.2 bottle.
    Co-authored-by: Daeho Ro <40587651+daeho-ro@users.noreply.github.com>

--- Formula/d/damask-grid.rb
+++ Formula/d/damask-grid.rb
@@ -5,6 +5,15 @@ class DamaskGrid < Formula
   sha256 "82f9b3aefde87193c12a7c908f42b711b278438f6cad650918989e37fb6dbde4"
   license "AGPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "0b18ca183c5b10361923e36624cd0dd48200735b9e294da7fcb669c835a58d99"
+    sha256 cellar: :any,                 arm64_sonoma:  "638f168e4c065d244b6d6fa8b060651058c17a87e6cc1af946cd6d97064a4c5c"
+    sha256 cellar: :any,                 arm64_ventura: "8b17bcdfc98a445fa66706bea7dcb980004b6445619d8db416579fc31bd2510f"
+    sha256 cellar: :any,                 sonoma:        "407856987aa7e2facc13e48dafa1422427b3e641204e5086e359082de837723f"
+    sha256 cellar: :any,                 ventura:       "899688e732738e9bfa9f1002b34deaf379c899e87c3561bf779fef5baf83c6af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "44b4d4d3d26e0f059b2d5a7a1e31086929d9183f365b45fb9894623d022d2a9c"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "petsc"

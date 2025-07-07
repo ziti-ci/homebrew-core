86d18a6804576c4a1ea3718aa0b938c47118821d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-07 17:02:52 +0000

    lunarml: add 0.2.1 bottle.

--- Formula/l/lunarml.rb
+++ Formula/l/lunarml.rb
@@ -6,6 +6,15 @@ class Lunarml < Formula
   license "MIT"
   head "https://github.com/minoki/LunarML.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "99d7c42200ec770420de1ee83f8e2a2948510dc5585df1eb3a7fa3a8c4952224"
+    sha256 cellar: :any,                 arm64_sonoma:  "8ce3a6d12a55a1977c4c39491f52742814da9dd6b1d007271c81d666ef62c912"
+    sha256 cellar: :any,                 arm64_ventura: "b9e15d9780dc25de4e2fd6292365fad7aae3e0250b2ee15e7fbcc827e861ffdf"
+    sha256 cellar: :any,                 sonoma:        "bfe37be13b868f9cc63813d9fed1fe3018e291816f45ba8eaf5c59876b36fb2c"
+    sha256 cellar: :any,                 ventura:       "95588395c2dcfa6363cc5e5d9c2fa8d1241cdc85a2e47baddce0e4cb4b1dd9b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6e0c9969d0590e2023d69b3ff8a4cfcad0392ce07aafe299882648b76373c43c"
+  end
+
   depends_on "mlton" => :build
   depends_on "lua" => :test
   depends_on "node" => :test

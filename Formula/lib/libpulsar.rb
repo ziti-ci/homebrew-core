bf60649f13d472e4c3c01a98d336a60a6c279780
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-07 05:08:26 +0000

    libpulsar 3.7.2
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/lib/libpulsar.rb
+++ Formula/lib/libpulsar.rb
@@ -1,11 +1,10 @@
 class Libpulsar < Formula
   desc "Apache Pulsar C++ library"
   homepage "https://pulsar.apache.org/"
-  url "https://dlcdn.apache.org/pulsar/pulsar-client-cpp-3.7.0/apache-pulsar-client-cpp-3.7.0.tar.gz"
-  mirror "https://archive.apache.org/dist/pulsar/pulsar-client-cpp-3.7.0/apache-pulsar-client-cpp-3.7.0.tar.gz"
-  sha256 "3223cfeda484ab7b580f4a8768b5a85739cc064005c765c06cde67c3238639c9"
+  url "https://dlcdn.apache.org/pulsar/pulsar-client-cpp-3.7.2/apache-pulsar-client-cpp-3.7.2.tar.gz"
+  mirror "https://archive.apache.org/dist/pulsar/pulsar-client-cpp-3.7.2/apache-pulsar-client-cpp-3.7.2.tar.gz"
+  sha256 "e4eee34cfa3d5838c08f20ac70f5b28239cb137bb59c75199f809141070620dd"
   license "Apache-2.0"
-  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "17409dae5bc4dfeae755a2e7ebc4d0e255a56f12f6c8d658a8f631cdbfe35bec"
@@ -17,7 +16,6 @@ class Libpulsar < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "2438a72ed70e421eae87a6e3fa2c4db58d3f4e7ec8b1bce6893421546c533d95"
   end
 
-  depends_on "asio" => :build # FIXME: Not compatible with Boost.Asio 1.87+
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
@@ -31,12 +29,18 @@ class Libpulsar < Formula
   uses_from_macos "curl"
   uses_from_macos "zlib"
 
+  # Backport of https://github.com/apache/pulsar-client-cpp/pull/477
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/93a4bb54004417c3742ca0e41183c662d9f417f5/libpulsar/asio.patch"
+    sha256 "519ecb20d3721575a916f45e7e0d382ae61de38ceaee23b53b97c7b4fcdbc019"
+  end
+
   def install
     args = %W[
       -DBUILD_TESTS=OFF
       -DCMAKE_CXX_STANDARD=17
       -DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}
-      -DUSE_ASIO=ON
+      -DUSE_ASIO=OFF
     ]
 
     system "cmake", "-S", ".", "build", *args, *std_cmake_args
@@ -49,7 +53,7 @@ class Libpulsar < Formula
       #include <pulsar/Client.h>
 
       int main (int argc, char **argv) {
-        pulsar::Client client("pulsar://localhost:6650");
+        pulsar::Client client("pulsar://localhost:#{free_port}");
         return 0;
       }
     CPP

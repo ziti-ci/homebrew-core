d267d02817db19ab4b70c195c1219ef0b6815092
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-07 17:20:47 +0000

    samba 4.22.3

--- Formula/s/samba.rb
+++ Formula/s/samba.rb
@@ -4,8 +4,8 @@ class Samba < Formula
   # option. The shared folder appears in the guest as "\\10.0.2.4\qemu".
   desc "SMB/CIFS file, print, and login server for UNIX"
   homepage "https://www.samba.org/"
-  url "https://download.samba.org/pub/samba/stable/samba-4.22.2.tar.gz"
-  sha256 "d9ac8e224a200159e62c651cf42307dc162212ec25d04eb6800b9a7ccfbcc3c1"
+  url "https://download.samba.org/pub/samba/stable/samba-4.22.3.tar.gz"
+  sha256 "8fd7092629a3596d935cd7567d934979f94272918ec3affd0cc807934ecf22ba"
   license "GPL-3.0-or-later"
 
   livecheck do

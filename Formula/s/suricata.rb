821d6121b6de49df953d6e4ed146a06f3090bc1a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-08 14:33:22 +0000

    suricata 8.0.0

--- Formula/s/suricata.rb
+++ Formula/s/suricata.rb
@@ -3,8 +3,8 @@ class Suricata < Formula
 
   desc "Network IDS, IPS, and security monitoring engine"
   homepage "https://suricata.io"
-  url "https://www.openinfosecfoundation.org/download/suricata-7.0.10.tar.gz"
-  sha256 "197f925ea701bdcb4a15aca024b06546b002674cd958b58958f29a5bb214d759"
+  url "https://www.openinfosecfoundation.org/download/suricata-8.0.0.tar.gz"
+  sha256 "51f36ef492cbee8779d6018e4f18b98a08e677525851251279c1f851654f451f"
   license "GPL-2.0-only"
 
   livecheck do

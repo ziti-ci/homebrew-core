a01dad139b2b0b8788808b2e4833c30d4954bc9d
botantony <antonsm21@gmail.com>
2025-07-08 02:36:23 +0200

    tkdiff 6.0
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/t/tkdiff.rb
+++ Formula/t/tkdiff.rb
@@ -1,11 +1,10 @@
 class Tkdiff < Formula
   desc "Graphical side by side diff utility"
   homepage "https://tkdiff.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.7/tkdiff-5-7.zip"
-  version "5.7"
-  sha256 "e2dec98e4c2f7c79a1e31290d3deaaa5915f53c8220c05728f282336bb2e405d"
+  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/6.0/tkdiff-6-0.zip"
+  version "6.0"
+  sha256 "4fa27c87846c1d6635da5beaa90ce4561638ee25a9169e455175afcf5288e453"
   license "GPL-2.0-only"
-  revision 1
 
   livecheck do
     url :stable

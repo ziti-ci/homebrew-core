a7268c8cbc78fbc413bb9cc99a6bc8d46faa710c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-08 00:57:45 +0000

    tkdiff: update 6.0 bottle.

--- Formula/t/tkdiff.rb
+++ Formula/t/tkdiff.rb
@@ -14,7 +14,7 @@ class Tkdiff < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f9df1848b67170c556bedec9313ed342304d7170d544c98840847dd2a7517bcf"
+    sha256 cellar: :any_skip_relocation, all: "e36f04c9c518b1d76ff97fab80dc92c59d158f9f83bdf6a87c17a98bbb20bba5"
   end
 
   # upstream bug report about running with system tcl-tk, https://sourceforge.net/p/tkdiff/bugs/98/

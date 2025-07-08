b60e910d4ca01c6d86abd24cae0926257c918543
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-08 23:04:00 +0000

    helm 3.18.4

--- Formula/h/helm.rb
+++ Formula/h/helm.rb
@@ -2,8 +2,8 @@ class Helm < Formula
   desc "Kubernetes package manager"
   homepage "https://helm.sh/"
   url "https://github.com/helm/helm.git",
-      tag:      "v3.18.3",
-      revision: "6838ebcf265a3842d1433956e8a622e3290cf324"
+      tag:      "v3.18.4",
+      revision: "d80839cf37d860c8aa9a0503fe463278f26cd5e2"
   license "Apache-2.0"
   head "https://github.com/helm/helm.git", branch: "main"
 

613675a25f61cd9ed4759c088065a035d2f3cc44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-09 09:40:26 +0000

    lefthook: update 1.12.1 bottle.

--- Formula/l/lefthook.rb
+++ Formula/l/lefthook.rb
@@ -9,12 +9,12 @@ class Lefthook < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78b0690e1c425577d1dabbff4719933fbb04c34e2527afd8bd33b80727033878"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "78b0690e1c425577d1dabbff4719933fbb04c34e2527afd8bd33b80727033878"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "78b0690e1c425577d1dabbff4719933fbb04c34e2527afd8bd33b80727033878"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a62c8724c5f44885638652310a87bd192154ab62087c824723c2812ebeb3a9a8"
-    sha256 cellar: :any_skip_relocation, ventura:       "a62c8724c5f44885638652310a87bd192154ab62087c824723c2812ebeb3a9a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed2dd93cbeb3bedf08d2fac5f81233aeebaa4ce638b42495a88d7d833f210fd6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "224a1bba3fa885c0d03329442a9c3f5a5c6bf438b03ce1dc41688e81e630335e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "224a1bba3fa885c0d03329442a9c3f5a5c6bf438b03ce1dc41688e81e630335e"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "224a1bba3fa885c0d03329442a9c3f5a5c6bf438b03ce1dc41688e81e630335e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a9da0566f808f16a45201c4fe35ccfb6b2af13abf278b7793fc5295e3e60fe33"
+    sha256 cellar: :any_skip_relocation, ventura:       "a9da0566f808f16a45201c4fe35ccfb6b2af13abf278b7793fc5295e3e60fe33"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "250a7c27fb73e00b1ab02a98f43d8912d00b56d67416a76435c70d11818834da"
   end
 
   depends_on "go" => :build

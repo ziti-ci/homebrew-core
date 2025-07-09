f0200d4d00d05b524cc84e28d7e30f47b90e1d47
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-09 20:16:54 +0000

    sprocket 0.14.0
    sprocket: update sha
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sprocket.rb
+++ Formula/s/sprocket.rb
@@ -1,8 +1,8 @@
 class Sprocket < Formula
   desc "Bioinformatics workflow engine built on the Workflow Description Language (WDL)"
   homepage "https://sprocket.bio"
-  url "https://github.com/stjude-rust-labs/sprocket/archive/refs/tags/v0.13.0.tar.gz"
-  sha256 "bb8364ae4d323d2ab1c3de60aa479fce2e6c922f7500e1462ca85671fc856433"
+  url "https://github.com/stjude-rust-labs/sprocket/archive/refs/tags/v0.14.0.tar.gz"
+  sha256 "98db53c7a23a20e4cc392a1dad7200b18674ccb1a50c93c5bb6654d2d63d7212"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/stjude-rust-labs/sprocket.git", branch: "main"
 

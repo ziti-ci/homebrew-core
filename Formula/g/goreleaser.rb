5c86be981b0348054d404e28441bdb32b5c47485
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-09 03:07:55 +0000

    goreleaser: update 2.11.0 bottle.

--- Formula/g/goreleaser.rb
+++ Formula/g/goreleaser.rb
@@ -8,12 +8,12 @@ class Goreleaser < Formula
   head "https://github.com/goreleaser/goreleaser.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "58831d56af4ea603e0f8b872167734516f4bfddb4cf3d31d4ee418079cf5a612"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "58831d56af4ea603e0f8b872167734516f4bfddb4cf3d31d4ee418079cf5a612"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "58831d56af4ea603e0f8b872167734516f4bfddb4cf3d31d4ee418079cf5a612"
-    sha256 cellar: :any_skip_relocation, sonoma:        "45faddc59352036449b64900d8d8170c68c58ebb56208b9972362449ea751295"
-    sha256 cellar: :any_skip_relocation, ventura:       "598872b8403f42408ca27b83e797c26f90894b341048d6cd25611b937b72a0d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8eaac9950a8c375717f0df643fe5d4fc2e03ec7309172f52893f1b6c3b819155"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37243367d2ce12016dc11a8a2f3d09a21a6b7238754a9e125679a9e48b190df0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "37243367d2ce12016dc11a8a2f3d09a21a6b7238754a9e125679a9e48b190df0"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "37243367d2ce12016dc11a8a2f3d09a21a6b7238754a9e125679a9e48b190df0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "66990302e0621b4b8e59c4efebf6f9d65c35bef96f938ab5435008de78e43af2"
+    sha256 cellar: :any_skip_relocation, ventura:       "43e5fa658e257418b46357938666e20e0b687724eb9a5c76dce22a9b652fed15"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bbb654237e7b55308dec9da6da22491c649ecdc641fe712cc954290170b39e7e"
   end
 
   depends_on "go" => :build

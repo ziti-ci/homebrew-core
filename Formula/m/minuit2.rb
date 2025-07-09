a4ec3a12a368b325de54170f716ba5ceebc3250c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-09 20:06:33 +0000

    minuit2 6.36.02

--- Formula/m/minuit2.rb
+++ Formula/m/minuit2.rb
@@ -1,8 +1,8 @@
 class Minuit2 < Formula
   desc "Physics analysis tool for function minimization"
   homepage "https://root.cern.ch/doc/master/Minuit2Page.html"
-  url "https://root.cern.ch/download/root_v6.36.00.source.tar.gz"
-  sha256 "94afc8def92842679a130a27521be66e2abdaa37620888e61d828a43fc4b01a2"
+  url "https://root.cern.ch/download/root_v6.36.02.source.tar.gz"
+  sha256 "510d677b33ac7ca48aa0d712bdb88d835a1ff6a374ef86f1a1e168fa279eb470"
   license "LGPL-2.1-or-later"
   head "https://github.com/root-project/root.git", branch: "master"
 

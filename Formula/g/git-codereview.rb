c50a6c24595b0ce7b147695a13bdf5e9916e8bf1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-09 19:59:12 +0000

    git-codereview 1.16.0

--- Formula/g/git-codereview.rb
+++ Formula/g/git-codereview.rb
@@ -1,8 +1,8 @@
 class GitCodereview < Formula
   desc "Tool for working with Gerrit code reviews"
   homepage "https://pkg.go.dev/golang.org/x/review/git-codereview"
-  url "https://github.com/golang/review/archive/refs/tags/v1.15.0.tar.gz"
-  sha256 "0a8d3ce0124e5bbf394607723a42942b9af91aaa7c03e0037c26bd462519a3b1"
+  url "https://github.com/golang/review/archive/refs/tags/v1.16.0.tar.gz"
+  sha256 "7e9d47d8025f1569c0a53c6030602e6eb049818d25c5fd0cad777efd21eeca20"
   license "BSD-3-Clause"
   head "https://github.com/golang/review.git", branch: "master"
 

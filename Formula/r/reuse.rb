21dc94cab5ba65cf7cb41a197cb9509852ac455b
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    reuse: Generate Bash completions

--- Formula/r/reuse.rb
+++ Formula/r/reuse.rb
@@ -79,7 +79,7 @@ class Reuse < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"reuse", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"reuse", shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

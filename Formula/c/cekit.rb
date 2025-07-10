4f475d4a2b8c349f24aac9cc6cfa352566392529
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    cekit: Generate Bash completions

--- Formula/c/cekit.rb
+++ Formula/c/cekit.rb
@@ -78,7 +78,7 @@ class Cekit < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"cekit", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"cekit", shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

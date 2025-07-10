ffd4118de988f5775edbd93c93dedcb7e920a9eb
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    localstack: Generate Bash completions

--- Formula/l/localstack.rb
+++ Formula/l/localstack.rb
@@ -171,7 +171,8 @@ class Localstack < Formula
     virtualenv_install_with_resources
     bin.install_symlink libexec/"bin/localstack"
 
-    generate_completions_from_executable(bin/"localstack", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"localstack",
+                                         shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

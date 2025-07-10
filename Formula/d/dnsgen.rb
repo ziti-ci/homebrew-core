39218ab95b9b77faf9cf36dea670d16b3945f5ac
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    dnsgen: Generate Bash completions

--- Formula/d/dnsgen.rb
+++ Formula/d/dnsgen.rb
@@ -59,7 +59,7 @@ class Dnsgen < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"dnsgen", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"dnsgen", shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

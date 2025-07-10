6f064c19bab8a9ea0c9203aa33ed65c604164da4
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:52 +0300

    uvicorn: Generate Bash completions

--- Formula/u/uvicorn.rb
+++ Formula/u/uvicorn.rb
@@ -80,7 +80,7 @@ class Uvicorn < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"uvicorn", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"uvicorn", shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

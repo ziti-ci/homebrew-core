d1d9f483fdfa6673b1bddf9d8528e9ef8dfe988e
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    posting: Generate Bash completions

--- Formula/p/posting.rb
+++ Formula/p/posting.rb
@@ -186,7 +186,7 @@ class Posting < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"posting", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"posting", shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

4c01e2603336b20fc2eed49972842d578b4ff827
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    gptme: Generate Bash completions

--- Formula/g/gptme.rb
+++ Formula/g/gptme.rb
@@ -279,7 +279,7 @@ class Gptme < Formula
 
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"gptme", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"gptme", shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

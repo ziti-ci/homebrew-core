f75fbe600f84963795894b72b4225842db40aa9a
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    bump-my-version: Generate Bash completions

--- Formula/b/bump-my-version.rb
+++ Formula/b/bump-my-version.rb
@@ -154,7 +154,8 @@ class BumpMyVersion < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"bump-my-version", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"bump-my-version",
+                                         shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

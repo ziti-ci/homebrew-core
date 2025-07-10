7aad7f1e33467b7ae3b4df2c3f9bae541acc39e7
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    notifiers: Generate Bash completions

--- Formula/n/notifiers.rb
+++ Formula/n/notifiers.rb
@@ -85,7 +85,8 @@ class Notifiers < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"notifiers", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"notifiers",
+                                         shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

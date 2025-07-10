d441c2774a393489337c27f48e042631a9e9f610
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    fiona: Generate Bash completions

--- Formula/f/fiona.rb
+++ Formula/f/fiona.rb
@@ -47,7 +47,7 @@ class Fiona < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"fio", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"fio", shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

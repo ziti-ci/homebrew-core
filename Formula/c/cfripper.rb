58452fa14042651392f5ebf3a661102164d081dc
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-07-10 19:33:51 +0300

    cfripper: Generate Bash completions

--- Formula/c/cfripper.rb
+++ Formula/c/cfripper.rb
@@ -115,7 +115,7 @@ class Cfripper < Formula
   def install
     virtualenv_install_with_resources
 
-    generate_completions_from_executable(bin/"cfripper", shells: [:fish, :zsh], shell_parameter_format: :click)
+    generate_completions_from_executable(bin/"cfripper", shells: [:bash, :fish, :zsh], shell_parameter_format: :click)
   end
 
   test do

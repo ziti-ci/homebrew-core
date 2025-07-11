3bfcf51228f7e60649c1317158aece2c850f2260
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-11 17:09:34 +0000

    ki18n 6.16.0

--- Formula/k/ki18n.rb
+++ Formula/k/ki18n.rb
@@ -1,8 +1,8 @@
 class Ki18n < Formula
   desc "KDE Gettext-based UI text internationalization"
   homepage "https://api.kde.org/frameworks/ki18n/html/index.html"
-  url "https://download.kde.org/stable/frameworks/6.15/ki18n-6.15.0.tar.xz"
-  sha256 "1897755d9fde5a1bea6f7d71244c2662b9911b4b8894cc53740bfe38b4d5d91d"
+  url "https://download.kde.org/stable/frameworks/6.16/ki18n-6.16.0.tar.xz"
+  sha256 "c52ad10b14365e60c4a219cb66706d114476645297cb2a9f15498747cf4421d7"
   license all_of: [
     "BSD-3-Clause",
     "LGPL-2.0-or-later",

80a7006bc915fdbab0c1947772eff57d5264e41d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-11 20:20:09 +0000

    zx 8.7.0

--- Formula/z/zx.rb
+++ Formula/z/zx.rb
@@ -1,8 +1,8 @@
 class Zx < Formula
   desc "Tool for writing better scripts"
   homepage "https://google.github.io/zx/"
-  url "https://registry.npmjs.org/zx/-/zx-8.6.2.tgz"
-  sha256 "172e5db48d7754640004cfc38bc1ba25c41680386cc7186e13bdd06bd38a62f4"
+  url "https://registry.npmjs.org/zx/-/zx-8.7.0.tgz"
+  sha256 "99317c9d4997e54b567cfd1b5bb4f034e84a8d136094708608656baafc42eae4"
   license "Apache-2.0"
 
   bottle do

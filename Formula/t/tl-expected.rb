fcaa72504d94c6cf03a79162e269679b7ab95057
botantony <antonsm21@gmail.com>
2025-07-12 22:33:18 +0200

    tl-expected 1.2.0
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/t/tl-expected.rb
+++ Formula/t/tl-expected.rb
@@ -1,11 +1,9 @@
 class TlExpected < Formula
   desc "C++11/14/17 std::expected with functional-style extensions"
   homepage "https://tl.tartanllama.xyz/en/latest/"
-  url "https://github.com/TartanLlama/expected/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "1db357f46dd2b24447156aaf970c4c40a793ef12a8a9c2ad9e096d9801368df6"
+  url "https://github.com/TartanLlama/expected/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "f5424f5fc74e79157b9981ba2578a28e0285ac6ec2a8f075e86c41226fe33386"
   license "CC0-1.0"
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     rebuild 1

26f37af7e3d6f6c42e19ea52856d0cdee2158d59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-12 10:52:18 +0000

    cargo-expand 1.0.113

--- Formula/c/cargo-expand.rb
+++ Formula/c/cargo-expand.rb
@@ -1,8 +1,8 @@
 class CargoExpand < Formula
   desc "Show what Rust code looks like with macros expanded"
   homepage "https://github.com/dtolnay/cargo-expand"
-  url "https://github.com/dtolnay/cargo-expand/archive/refs/tags/1.0.112.tar.gz"
-  sha256 "cc11dafb0a13f5c71468667c239fb0f75e1ef97ca12c75946126b9943f327349"
+  url "https://github.com/dtolnay/cargo-expand/archive/refs/tags/1.0.113.tar.gz"
+  sha256 "8d2571214b19cb47a9068ad2e81ae819a04798f5c7508fae6d51ada052bda5d0"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/dtolnay/cargo-expand.git", branch: "master"
 

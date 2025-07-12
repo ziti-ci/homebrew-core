1b8289095440f9bd7eed3a23fb80416e400a3a7e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-12 09:35:33 +0000

    tflint: update 0.58.1 bottle.

--- Formula/t/tflint.rb
+++ Formula/t/tflint.rb
@@ -9,12 +9,12 @@ class Tflint < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f334670b74f8d6ec47e6cb8e21784753fba04f87a11b6d3743190463eff27615"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f334670b74f8d6ec47e6cb8e21784753fba04f87a11b6d3743190463eff27615"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f334670b74f8d6ec47e6cb8e21784753fba04f87a11b6d3743190463eff27615"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6766f0bb9933eece28ca9b795587baaf14e9894488709892fc84980f39dd2bee"
-    sha256 cellar: :any_skip_relocation, ventura:       "e14cdcd73610c1a3ccd977dbd843b4926d683793b91c0b115fcc04873b37e66c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2ce462994f711becd98a8fdd25766fe30605dcb07663d971144b163c148d956"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "24339a7eb6991d6fbe6109fc1e92db9228d2519a2f72c1bb621bc841557cdffd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "24339a7eb6991d6fbe6109fc1e92db9228d2519a2f72c1bb621bc841557cdffd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "24339a7eb6991d6fbe6109fc1e92db9228d2519a2f72c1bb621bc841557cdffd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a48a5b625e1089a2a982ba9fc78f486bb7c6c4868c10deb103f0dfe0c9e8f790"
+    sha256 cellar: :any_skip_relocation, ventura:       "73a38207dc2e8397cc59971e5533a3237cdda184e5ff6c24cb9a7a7747f69f54"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a71d83a6b89aae23bdee2f4b0b295d0e9eb26a5f319afff8479558300aba75c"
   end
 
   depends_on "go" => :build

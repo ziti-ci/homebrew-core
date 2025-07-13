62930aba70c5ef4cfdd98c2bc89d62df25beb8ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-13 14:00:50 +0000

    imagemagick@6 6.9.13-26

--- Formula/i/imagemagick@6.rb
+++ Formula/i/imagemagick@6.rb
@@ -1,8 +1,8 @@
 class ImagemagickAT6 < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://legacy.imagemagick.org/"
-  url "https://imagemagick.org/archive/releases/ImageMagick-6.9.13-25.tar.xz"
-  sha256 "17ba5ee0e0ce4a2248db5115d3683dd7c24e82eec96515da028997c9f926a121"
+  url "https://imagemagick.org/archive/releases/ImageMagick-6.9.13-26.tar.xz"
+  sha256 "c286a233340a98d5ca4e86054d1da45f6b4abf5523f92e2402b86f7d8ef51bb1"
   license "ImageMagick"
   head "https://github.com/imagemagick/imagemagick6.git", branch: "main"
 

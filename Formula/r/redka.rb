d927393d8e84d87ef5ff4a79e7f21a3e1d15435d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-13 20:15:38 +0000

    redka 0.6.0

--- Formula/r/redka.rb
+++ Formula/r/redka.rb
@@ -1,8 +1,8 @@
 class Redka < Formula
   desc "Redis re-implemented with SQLite"
   homepage "https://github.com/nalgeon/redka"
-  url "https://github.com/nalgeon/redka/archive/refs/tags/v0.5.3.tar.gz"
-  sha256 "c5b1746f5c1af905d79247b1e3d808c0da14fd8caf1115023a4d12fe3ad8ebe4"
+  url "https://github.com/nalgeon/redka/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "cfccbfc5b4887211146352426efe0c3fcc2adbcfe71ef6b58da3d29cba867bde"
   license "BSD-3-Clause"
   head "https://github.com/nalgeon/redka.git", branch: "main"
 
@@ -24,7 +24,6 @@ class Redka < Formula
   def install
     ldflags = "-s -w -X main.version=v#{version}"
     system "go", "build", *std_go_args(ldflags:, output: bin/"redka"), "./cmd/redka"
-    system "go", "build", *std_go_args(ldflags:, output: bin/"redka-cli"), "./cmd/cli"
   end
 
   test do
@@ -37,7 +36,7 @@ class Redka < Formula
     sleep 2
 
     begin
-      output = shell_output("redis-cli -h 127.0.0.1 -p #{port} ping")
+      output = shell_output("#{Formula["valkey"].opt_bin}/valkey-cli -h 127.0.0.1 -p #{port} ping")
       assert_equal "PONG", output.strip
     ensure
       Process.kill("TERM", pid)

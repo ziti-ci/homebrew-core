5125470fb50713154da77bd33fa5a39f8b3f8c60
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-13 02:45:01 +0000

    stormy: add 0.2.1 bottle.

--- Formula/s/stormy.rb
+++ Formula/s/stormy.rb
@@ -6,6 +6,15 @@ class Stormy < Formula
   license "MIT"
   head "https://github.com/ashish0kumar/stormy.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c6e4aae17786f5f2325ffb965a50fe91a09ca073ac3ba88e4a1781440b58fae9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c6e4aae17786f5f2325ffb965a50fe91a09ca073ac3ba88e4a1781440b58fae9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c6e4aae17786f5f2325ffb965a50fe91a09ca073ac3ba88e4a1781440b58fae9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7f954aafbda37ba4ff10bbf277d2d6c68f9b8fe507c96c1c68b4c8a6078886a6"
+    sha256 cellar: :any_skip_relocation, ventura:       "7f954aafbda37ba4ff10bbf277d2d6c68f9b8fe507c96c1c68b4c8a6078886a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5b9f3fc4ef0927bad3f5ecc70a786463f868278799e616dee296c1672e54eab9"
+  end
+
   depends_on "go" => :build
 
   def install

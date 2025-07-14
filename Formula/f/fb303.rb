1f9456d1cc69ac3e3d65c8fb26b7b340d750652c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-14 17:05:57 +0000

    fb303 2025.07.14.00

--- Formula/f/fb303.rb
+++ Formula/f/fb303.rb
@@ -1,8 +1,8 @@
 class Fb303 < Formula
   desc "Thrift functions for querying information from a service"
   homepage "https://github.com/facebook/fb303"
-  url "https://github.com/facebook/fb303/archive/refs/tags/v2025.06.30.00.tar.gz"
-  sha256 "b8c4d8593d0fe450f2d590d9a4296cdc425bb9150328d804b1bacf27817cd572"
+  url "https://github.com/facebook/fb303/archive/refs/tags/v2025.07.14.00.tar.gz"
+  sha256 "2fb7279e4d1023aa80eb4420051810aede315cb764e0cc4d7baa66554e652552"
   license "Apache-2.0"
   head "https://github.com/facebook/fb303.git", branch: "main"
 

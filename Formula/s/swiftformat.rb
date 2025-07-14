5e76fa0d67ac9ef9b850c07583727660a9e03dc5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-14 02:58:11 +0000

    swiftformat: update 0.57.0 bottle.

--- Formula/s/swiftformat.rb
+++ Formula/s/swiftformat.rb
@@ -7,13 +7,13 @@ class Swiftformat < Formula
   head "https://github.com/nicklockwood/SwiftFormat.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2af61335aa61d6650d43c7fc8c44c6bd379a78dd23442950d0d6bd6c3339680d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8998287c9941652bccf88d34800403a6a82fe0d2bfad4c821906f4dafc97d71d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "30d6c1e51d6e69b4aa380b8404499326155fa6a580fa93e06e7e9c3e5819fcda"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f2997f32d76167ba0bb0efd0770f2422e9dee6a1750de67895befe47ea4fb540"
-    sha256 cellar: :any_skip_relocation, ventura:       "88410246f58f51c5b12e3131b194eaf8cf5f50e63bb732deb09801b4c5028506"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "aedb74df45987bbcfbb1d7ea51e2e5803867f422956803b3be4f9e042067f2e3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1b645b9faab6b968d87af27f9b8cf31e5e83d7d316aef4b238e41ed298b82cc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71629b58d803952cef1a31bd11c6d1a2e82903065786e802eb89da3dd61eb6c8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0d72e24e2eacf52cae5236426ba43762c1518e610d924b0345b639db340e711a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ae63524d50917cbe0dcb00fb2f015829ef95750652e24d3ccc63d2ccd0101bb3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "05bc1a758f63489015bac07b06f0fd51d81b7a28c3384dc6a5bdfae4287e22c5"
+    sha256 cellar: :any_skip_relocation, ventura:       "fe48bbb417bdce1d86882eaea4c26a88229cc80434bf072b0f53a391e859169a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8a90e18a8f8aed7d4600c531496589067a60841188b7aadc2b8ae00165146b29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0dfca9279c81efd21ea33396b4ad62d2762b30eebad7d36fde09778d4cabde9a"
   end
 
   depends_on xcode: ["10.1", :build]

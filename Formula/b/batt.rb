8219da2e4151813be90e5dbe2d6ec45afde2af4e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-14 10:53:45 +0000

    batt 0.4.1

--- Formula/b/batt.rb
+++ Formula/b/batt.rb
@@ -2,8 +2,8 @@ class Batt < Formula
   desc "Control and limit battery charging on Apple Silicon MacBooks"
   homepage "https://github.com/charlie0129/batt"
   url "https://github.com/charlie0129/batt.git",
-      tag:      "v0.4.0",
-      revision: "34d7c1b7507089832a15d7bf0933c5d8606e5c3d"
+      tag:      "v0.4.1",
+      revision: "3f6595f59f88d746e7a3f37a99039e00d6efa587"
   license "GPL-2.0-only"
   head "https://github.com/charlie0129/batt.git", branch: "master"
 

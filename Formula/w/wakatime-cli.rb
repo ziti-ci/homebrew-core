4f014327672643f913e72bf00390802454d00216
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-14 03:12:07 +0000

    wakatime-cli 1.118.0

--- Formula/w/wakatime-cli.rb
+++ Formula/w/wakatime-cli.rb
@@ -2,8 +2,8 @@ class WakatimeCli < Formula
   desc "Command-line interface to the WakaTime api"
   homepage "https://wakatime.com/"
   url "https://github.com/wakatime/wakatime-cli.git",
-      tag:      "v1.115.6",
-      revision: "c6b2e313b68923c849c13f1b899d1ad4222e6b12"
+      tag:      "v1.118.0",
+      revision: "677c1c90bf3b2e052efd447ef2800a5ac3f1246c"
   license "BSD-3-Clause"
   version_scheme 1
 

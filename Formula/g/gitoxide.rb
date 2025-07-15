af81cf456a27cea42c1da90d43209bd284baf980
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-15 05:56:49 +0000

    gitoxide: update 0.45.0 bottle.

--- Formula/g/gitoxide.rb
+++ Formula/g/gitoxide.rb
@@ -12,13 +12,13 @@ class Gitoxide < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "86304e974bd41ea30521c61b8404ec459fa0228f986a22c09368cd89ffbe2ce0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9b5c854272b3182e3abd2d555b1153a817328e9dad78ba991ecd7b8aa61a7dbc"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "1d85afbabe8fb9f0ff09e2d5f7208645663e1d766897c7c9d3a2935813c222d9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "589c23d80b049a8ae104186fcf4b047b5c87102f44e9c1b4764dd3c78069b1b8"
-    sha256 cellar: :any_skip_relocation, ventura:       "85ae6e0efb80512f985345886c98c7117a40621a97597852d322c2efd3d22e56"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5336edbf824817628bc5900db57cda81f5fbba455ea79509f3fa9fdc4da551b7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b245e918ee10027e0e9c70f3cbfb7b5af9419856573e8b3a0bd9f74dfd1230c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "835b9c1e14b69b7cfb9219ecdd571043755e1330642803c7c2b717fb6b12b89b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "afb305e654733f1089ae53aa33991e01a88597171478428f5b589ee9fc36f1ab"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "3686c91b28f4150d6aa84370997d54513b8754ad88f2e6c36b52fe9b09081d1c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7988f0b34958d5b489f798c7709614b132b7dc241c9daee1e9023d9bbab19bb0"
+    sha256 cellar: :any_skip_relocation, ventura:       "018c07fa0fc87d460618f3f9e30ebd6a3f19a56e2a71e65f81b490f1d77ac414"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bc7b90aaa939936c2c81564f1bb99642f941e17f5e911e16ae4ef3a59b0a0da8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "956a2adab3aae85930db685da3a1e3d2df78a80756efb7f62dc958718ace1d56"
   end
 
   depends_on "pkgconf" => :build

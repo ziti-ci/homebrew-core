c97836c579d5cb71d8c94e09965419e2db2a089a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-16 10:35:00 +0000

    jenv: update 0.5.9 bottle.

--- Formula/j/jenv.rb
+++ Formula/j/jenv.rb
@@ -9,7 +9,7 @@ class Jenv < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0854f8b2d434e292fc9b63a9d59e3bc2643cb7638c5b710db7b0ac2fc5a55128"
+    sha256 cellar: :any_skip_relocation, all: "b454f9bfe4726a30422faed10c3fa103050a5dcb0fa39f366691bda94269cd9f"
   end
 
   def install

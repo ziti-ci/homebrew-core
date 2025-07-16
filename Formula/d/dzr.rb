0f24db399807da1d3307504ae8dcbe023b457454
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-16 17:03:03 +0000

    dzr 250716

--- Formula/d/dzr.rb
+++ Formula/d/dzr.rb
@@ -1,8 +1,8 @@
 class Dzr < Formula
   desc "Command-line Deezer.com player"
   homepage "https://github.com/yne/dzr"
-  url "https://github.com/yne/dzr/archive/refs/tags/250612.tar.gz"
-  sha256 "b535b455170e4bbb1ef6b5d58d119477808765b3573ea3cba1c133625d4d1294"
+  url "https://github.com/yne/dzr/archive/refs/tags/250716.tar.gz"
+  sha256 "01f224efb50e4e4864d44a4b54da53743e2e28ec5c000ed816fc5f22e06c4021"
   license "Unlicense"
   head "https://github.com/yne/dzr.git", branch: "master"
 

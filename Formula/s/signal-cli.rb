31ddc9988b6c3e91428a58888e01f201f3c1b926
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-16 20:18:42 +0000

    signal-cli 0.13.18
    signal-cli: update resource
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/s/signal-cli.rb
+++ Formula/s/signal-cli.rb
@@ -1,8 +1,8 @@
 class SignalCli < Formula
   desc "CLI and dbus interface for WhisperSystems/libsignal-service-java"
   homepage "https://github.com/AsamK/signal-cli"
-  url "https://github.com/AsamK/signal-cli/archive/refs/tags/v0.13.17.tar.gz"
-  sha256 "40bb1259ff2e84f97d3b5f3b1d96d30bd71e614ca19b5d5572f4a67e9b944eea"
+  url "https://github.com/AsamK/signal-cli/archive/refs/tags/v0.13.18.tar.gz"
+  sha256 "5040545c43069958bbf914331e39492ae7dde3497dbc2c89e0e4f3ff75fb83ea"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -25,18 +25,17 @@ class SignalCli < Formula
   uses_from_macos "llvm" => :build # For `libclang`, used by `boring-sys` crate
   uses_from_macos "zip" => :build
 
-  # FIXME: Uncomment below when https://github.com/Homebrew/brew/issues/19838 is resolved
-  # FIXME: on_linux do
-  # FIXME:  depends_on arch: :x86_64 # `:libsignal-cli:test` failure, https://github.com/AsamK/signal-cli/issues/1787
-  # FIXME: end
+  on_linux do
+    depends_on arch: :x86_64 # `:libsignal-cli:test` failure, https://github.com/AsamK/signal-cli/issues/1787
+  end
 
   # https://github.com/AsamK/signal-cli/wiki/Provide-native-lib-for-libsignal#determine-the-required-libsignal-client-version
   # To check the version of `libsignal-client`, run:
   # url=https://github.com/AsamK/signal-cli/releases/download/v$version/signal-cli-$version.tar.gz
   # curl -fsSL $url | tar -tz | grep libsignal-client
   resource "libsignal-client" do
-    url "https://github.com/signalapp/libsignal/archive/refs/tags/v0.76.0.tar.gz"
-    sha256 "64a78ff474e102eedeeba25838fc6f3375e6e5dcdd999be6596250bd1419268a"
+    url "https://github.com/signalapp/libsignal/archive/refs/tags/v0.76.3.tar.gz"
+    sha256 "7e474269dfa98929088aaa367e963cd6cab71b60d84cef4da28f97573c24984f"
   end
 
   def install
@@ -55,10 +54,8 @@ class SignalCli < Formula
       res = r.resource
       odie "#{res.name} needs to be updated to #{embedded_jar_version}!" if embedded_jar_version != res.version
 
-      # rm originally-embedded libsignal_jni lib
       system "zip", "-d", libsignal_client_jar, "libsignal_jni_*.so", "libsignal_jni_*.dylib", "signal_jni_*.dll"
 
-      # build & embed library for current platform
       cd "java" do
         inreplace "settings.gradle", "include ':android'", ""
         system "./build_jni.sh", "desktop"
@@ -71,11 +68,9 @@ class SignalCli < Formula
   end
 
   test do
-    # test 1: checks class loading is working and version is correct
     output = shell_output("#{bin}/signal-cli --version")
     assert_match "signal-cli #{version}", output
 
-    # test 2: ensure crypto is working
     begin
       io = IO.popen("#{bin}/signal-cli link", err: [:child, :out])
       sleep 24

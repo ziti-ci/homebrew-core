aa7e648061aef26e8502d607d392bbb57febe470
rgcr <roger.dev@pm.me>
2025-07-18 02:29:34 -0600

    m-cli v2.0.4

--- Formula/m/m-cli.rb
+++ Formula/m/m-cli.rb
@@ -1,10 +1,10 @@
 class MCli < Formula
   desc "Swiss Army Knife for macOS"
   homepage "https://github.com/rgcr/m-cli"
-  url "https://github.com/rgcr/m-cli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "623be61aebf074754b148e725933aebe205fbf2d7d2ea3854a8aa6054ea3307e"
+  url "https://github.com/rgcr/m-cli/archive/refs/tags/v2.0.4.tar.gz"
+  sha256 "03da227d3627811dcc037c184cf338af2fa4b60461ee7bf10ab94effd38132a0"
   license "MIT"
-  head "https://github.com/rgcr/m-cli.git", branch: "master"
+  head "https://github.com/rgcr/m-cli.git", branch: "main"
 
   no_autobump! because: :requires_manual_review
 
@@ -26,23 +26,46 @@ class MCli < Formula
 
   def install
     prefix.install Dir["*"]
+
     inreplace prefix/"m" do |s|
       # Use absolute rather than relative path to plugins.
-      s.gsub!(/^\[ -L.*|^\s+\|\| pushd.*|^popd.*/, "")
-      s.gsub!(/MPATH=.*/, "MPATH=#{prefix}")
+      s.sub!(/^MCLI_PATH=.+$/, "MCLI_PATH=#{prefix}")
       # Disable options "update" && "uninstall", they must be handled by brew
-      s.gsub!(/update_mcli &&.*/, "printf \"Try: brew update && brew upgrade m-cli \\n\" && exit 0")
-      s.gsub!(/uninstall_mcli &&.*/, "printf \"Try: brew uninstall m-cli \\n\" && exit 0")
+      s.sub!(/^\s*update_mcli\s*&&.*/,
+             'printf "\'m-cli\' was installed by brew, try: brew update && brew upgrade m-cli\n" && exit 0')
+      s.sub!(/^\s*uninstall_mcli\s*&&.*/,
+             'printf "\'m-cli\' was installed by brew, try: brew uninstall m-cli\n" && exit 0')
+      s.sub!(/^\s*get_version\s*&&.*/,
+             "printf \"m-cli version: v2.0.4\\n\" && exit 0")
     end
 
+    inreplace prefix/"completions/bash/m" do |s|
+      # Use absolute brew path for bash completion
+      s.sub!(/^\s*local PLUGINS_DIR=.+$/, "local PLUGINS_DIR=#{prefix}/plugins")
+    end
+
+    inreplace prefix/"completions/zsh/_m" do |s|
+      # Use absolute brew path for zsh completion
+      s.sub!(/^\s*local PLUGINS_DIR=.+$/, "local PLUGINS_DIR=#{prefix}/plugins")
+    end
+
+    inreplace prefix/"completions/fish/m.fish" do |s|
+      # Use absolute brew path for fish completion
+      s.gsub!(%r{^\s*set plugins_dir "\$script_dir/plugins"$},
+              "set plugins_dir \"#{prefix}/plugins\"")
+    end
+
+    # Remove the install script, it is not needed
+    (prefix/"install.sh").unlink if (prefix/"install.sh").exist?
+
     bin.install_symlink "#{prefix}/m" => "m"
-    bash_completion.install prefix/"completion/bash/m"
-    zsh_completion.install prefix/"completion/zsh/_m"
-    fish_completion.install prefix/"completion/fish/m.fish"
+    bash_completion.install prefix/"completions/bash/m"
+    zsh_completion.install prefix/"completions/zsh/_m"
+    fish_completion.install prefix/"completions/fish/m.fish"
   end
 
   test do
-    output = pipe_output("#{bin}/m help 2>&1")
+    output = pipe_output("#{bin}/m --help 2>&1")
     refute_match(/.*No such file or directory.*/, output)
     refute_match(/.*command not found.*/, output)
     assert_match(/.*Swiss Army Knife for macOS.*/, output)

dd4f83116c38b14a67e5c9bf928489e5fb8f0308
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-18 11:30:29 +0000

    spacer 0.4.1

--- Formula/s/spacer.rb
+++ Formula/s/spacer.rb
@@ -1,8 +1,8 @@
 class Spacer < Formula
   desc "Small command-line utility for adding spacers to command output"
   homepage "https://github.com/samwho/spacer"
-  url "https://github.com/samwho/spacer/archive/refs/tags/v0.3.9.tar.gz"
-  sha256 "5bbb11699b59f81f74236e1b68ae090dbce5344cda9e386c117747f87d12dd78"
+  url "https://github.com/samwho/spacer/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "ab3542d6fcc5a9eccd21c81491e9cd61394d617893d5ba5f98962dc2f07094f0"
   license "MIT"
 
   bottle do

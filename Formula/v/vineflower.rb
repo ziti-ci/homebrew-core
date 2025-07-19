0aee2982499ae4b8410730030d731bbbad4994b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-19 05:19:36 +0000

    vineflower: add 1.11.1 bottle.

--- Formula/v/vineflower.rb
+++ Formula/v/vineflower.rb
@@ -5,6 +5,15 @@ class Vineflower < Formula
   sha256 "104df4042023190416bff2ad0e2386ddf7669c6aa585f19b9d965a7f5ca5132b"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "62479a14ae3965038932cdf833ee87a48e50b1db0c767362c82d7b5327f62849"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b54dfedf26ee257ef870b6e6a5b96a84ba6fd96955ab16818c093d7b534d653a"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "754e2ce57f9e000ac224811a2fb61c127acb8ca4420e1017faa1d162d4da280d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0db4c09fd07af41914236c4221a59b5e618d3303ea95fc435369813ab8bdda9a"
+    sha256 cellar: :any_skip_relocation, ventura:       "49ee9fd5f4021a9d9eb7d91840c99d111c80644fc0c838e7b83a762934c9f5d3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5ffc032d554d132c2387490e51c8e11a084d5a31d6d97956c69286b999966355"
+  end
+
   depends_on "gradle" => :build
   depends_on "openjdk"
 

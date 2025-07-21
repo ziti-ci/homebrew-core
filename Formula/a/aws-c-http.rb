8412cc2e9111cd25d9986ee857bbf5c3c4ef8121
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-21 22:52:28 +0000

    aws-c-http 0.10.4

--- Formula/a/aws-c-http.rb
+++ Formula/a/aws-c-http.rb
@@ -1,8 +1,8 @@
 class AwsCHttp < Formula
   desc "C99 implementation of the HTTP/1.1 and HTTP/2 specifications"
   homepage "https://github.com/awslabs/aws-c-http"
-  url "https://github.com/awslabs/aws-c-http/archive/refs/tags/v0.10.3.tar.gz"
-  sha256 "04030f1529368e06aebb1ff3c97538b904c6c4b9d97e7acb8f0f26fcd39d5e71"
+  url "https://github.com/awslabs/aws-c-http/archive/refs/tags/v0.10.4.tar.gz"
+  sha256 "dfeeeaa2e84ccda4c8cb0c29f412298df80a57a27003e716f2d3df9794956fc1"
   license "Apache-2.0"
 
   bottle do

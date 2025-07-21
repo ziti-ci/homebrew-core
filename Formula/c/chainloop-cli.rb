e35c127927cdd67a4258809d82019fc89fb9d186
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-21 11:25:56 +0000

    chainloop-cli: update 1.35.0 bottle.

--- Formula/c/chainloop-cli.rb
+++ Formula/c/chainloop-cli.rb
@@ -7,12 +7,12 @@ class ChainloopCli < Formula
   head "https://github.com/chainloop-dev/chainloop.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "68ec005a6c38d05af8ac93d463cd2c42bf77acb00c994c86b90b025a9743ee8b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "be05a5d6cf96350ceb2b12c94fa05a2c09859aa90c74efde4430396544abfb43"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0fca25a59c0a9fa4a46ce42b2ffac39bce08673ff2953685b1b3a9ae71fa9653"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c1c6556506bd4cb8e9060694cef2fc77b270bcf03bd9dd2fbb49741e99e96e30"
-    sha256 cellar: :any_skip_relocation, ventura:       "a86634067eaa131558f6b35fba3255ea3db49d3d83767a5ef161586b99036aa6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c84e1629dc57dd7f5299cd578fead782d98c89fb9b9dc648b2d96f40c0799e4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4a4c8461b3e6d50497435d96729dce76ece503f5cb694116ade5e787dbf5b95e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "80c71e067a317ced935eecf48347c0f29b0d83a0141acdca112cc5f2178406b9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "211bad2131be2a154e8a97e53a7ddb61bd02f5fa442c105be046060132285b1b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7507ea198c87936dc8fd55defd6e8688b5f7ccd7f2a11822847a3376ea5b37a6"
+    sha256 cellar: :any_skip_relocation, ventura:       "314f182f7169e49cc1bb93cb0a88a85870f2797ee51aa5d2721996592a731240"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2e4b59501d6c9b8142095a7b17eec3b9335d45e1fe3e9012254406c6b8ff141"
   end
 
   depends_on "go" => :build

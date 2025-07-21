9d4b0c6b4ae5aec9b66a02beaa61570fb3ef0e55
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-21 03:38:47 +0000

    ccusage: add 15.5.0 bottle.

--- Formula/c/ccusage.rb
+++ Formula/c/ccusage.rb
@@ -5,6 +5,15 @@ class Ccusage < Formula
   sha256 "a5bd1d97160e4a7a8991992700aa3378e7411f7ada54ea2a1ca1969115ff2e2a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73153c3d7be3c4da26169ff48692dde07e0b35ea569660f8f81b9dd9c48b901f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "73153c3d7be3c4da26169ff48692dde07e0b35ea569660f8f81b9dd9c48b901f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "73153c3d7be3c4da26169ff48692dde07e0b35ea569660f8f81b9dd9c48b901f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9ea12ee3d6fb167163e859e7859869a6ab4f46a8e93ddd98a7ffcee9cf40d625"
+    sha256 cellar: :any_skip_relocation, ventura:       "9ea12ee3d6fb167163e859e7859869a6ab4f46a8e93ddd98a7ffcee9cf40d625"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "73153c3d7be3c4da26169ff48692dde07e0b35ea569660f8f81b9dd9c48b901f"
+  end
+
   depends_on "node"
 
   def install

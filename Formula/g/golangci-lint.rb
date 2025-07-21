f3e50d5fccea32ea961635f487ebf5d393f0c6a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-21 17:06:11 +0000

    golangci-lint 2.3.0

--- Formula/g/golangci-lint.rb
+++ Formula/g/golangci-lint.rb
@@ -2,8 +2,8 @@ class GolangciLint < Formula
   desc "Fast linters runner for Go"
   homepage "https://golangci-lint.run/"
   url "https://github.com/golangci/golangci-lint.git",
-      tag:      "v2.2.2",
-      revision: "e9d425110dcd46293652c5981b0a479d091d9f88"
+      tag:      "v2.3.0",
+      revision: "364a4bbe30932b8eb1cd88d077efe1c0a9025856"
   license "GPL-3.0-only"
   head "https://github.com/golangci/golangci-lint.git", branch: "master"
 

506ad5fafed0e58de23416e9d9c592d92c66a736
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-22 23:13:18 +0000

    metabase 0.55.9

--- Formula/m/metabase.rb
+++ Formula/m/metabase.rb
@@ -1,8 +1,8 @@
 class Metabase < Formula
   desc "Business intelligence report server"
   homepage "https://www.metabase.com/"
-  url "https://downloads.metabase.com/v0.55.8/metabase.jar"
-  sha256 "216c17b279a6051fa66dc5cd4018ea81d691dcd4c90907161141b64f43795ec2"
+  url "https://downloads.metabase.com/v0.55.9/metabase.jar"
+  sha256 "0a928008e5635df784d29fba3d3d60c92da18d376f525851298c6363114a75f1"
   license "AGPL-3.0-only"
 
   livecheck do

4f8fea33755c61b755f240d40561350223fc915e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-22 14:14:03 +0000

    logstash 9.0.4

--- Formula/l/logstash.rb
+++ Formula/l/logstash.rb
@@ -1,8 +1,8 @@
 class Logstash < Formula
   desc "Tool for managing events and logs"
   homepage "https://www.elastic.co/products/logstash"
-  url "https://github.com/elastic/logstash/archive/refs/tags/v9.0.3.tar.gz"
-  sha256 "41a4506b69039775c0347d0dbb1544ab380d12ccba17d5c37cb1c3cc4f6896cc"
+  url "https://github.com/elastic/logstash/archive/refs/tags/v9.0.4.tar.gz"
+  sha256 "7ef2f89a55d3d9e5e0854f7949b28b4de18870603b0b40f7083837a9f5cd0e7f"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/elastic/logstash.git", branch: "main"

63fb7a1027f06cdd8ec2cb265b3b7ebb5c6b8bb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-23 17:09:13 +0000

    katana 1.2.0

--- Formula/k/katana.rb
+++ Formula/k/katana.rb
@@ -1,8 +1,8 @@
 class Katana < Formula
   desc "Crawling and spidering framework"
   homepage "https://github.com/projectdiscovery/katana"
-  url "https://github.com/projectdiscovery/katana/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "94d72b9132b536b1125f51c6ee4ddf6b11bf55013a88172fe0fed979799410fc"
+  url "https://github.com/projectdiscovery/katana/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "07f102a34843c4123f7be9b6b6ab2cca93d1caeb6069106fb197396784584303"
   license "MIT"
   head "https://github.com/projectdiscovery/katana.git", branch: "main"
 

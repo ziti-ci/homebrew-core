c751c45962705c9699514acaf290f7a7a9770b1b
Bevan Kay <email@bevankay.me>
2025-07-24 16:41:09 +1000

    dagu 1.17.4 (new formula)

---
+++ Formula/d/dagu.rb
@@ -0,0 +1,39 @@
+class Dagu < Formula
+  desc "Lightweight and powerful workflow engine"
+  homepage "https://dagu.cloud"
+  url "https://github.com/dagu-org/dagu/archive/refs/tags/v1.17.4.tar.gz"
+  sha256 "b1552e6cfcc383880c5fad2448e43f236b0bdb674e17101a7e905b609f4de8cd"
+  license "GPL-3.0-only"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[-s -w -X main.version=#{version}]
+    system "go", "build", *std_go_args(ldflags:), "./cmd"
+  end
+
+  service do
+    run [opt_bin/"dagu", "start-all"]
+    keep_alive true
+    error_log_path var/"log/dagu.log"
+    log_path var/"log/dagu.log"
+    working_dir var
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dagu version 2>&1")
+
+    (testpath/"hello.yaml").write <<~YAML
+      steps:
+        - name: hello
+          command: echo "Hello from Dagu!"
+
+        - name: world
+          command: echo "Running step 2"
+    YAML
+
+    system bin/"dagu", "start", "hello.yaml"
+    shell_output = shell_output("#{bin}/dagu status hello.yaml")
+    assert_match "The DAG completed successfully", shell_output
+  end
+end

84b576d25e362827a55f83b44076b702be33e00c
Thierry Moisan <thierry.moisan@gmail.com>
2025-07-24 18:13:25 -0400

    fastnetmon: revision bump (grpc 1.74.0)

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.8.tar.gz"
   sha256 "d16901b00963f395241c818d02ad2751f14e33fd32ed3cb3011641ab680e0d01"
   license "GPL-2.0-only"
-  revision 7
+  revision 8
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "49de71494f182fa5183947fd6917b59422b714321b20bbc379ae731fd426ee5a"

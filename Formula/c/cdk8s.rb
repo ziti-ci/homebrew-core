2496e3bae135c6276e64c98003ee207653e12ade
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-25 08:20:21 +0000

    cdk8s: update 2.200.141 bottle.

--- Formula/c/cdk8s.rb
+++ Formula/c/cdk8s.rb
@@ -6,13 +6,13 @@ class Cdk8s < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7da0ba108ff33ea5b9ff2ac058e3a25dc0636d0761eae795d4d3f7bfe9553d9a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7da0ba108ff33ea5b9ff2ac058e3a25dc0636d0761eae795d4d3f7bfe9553d9a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7da0ba108ff33ea5b9ff2ac058e3a25dc0636d0761eae795d4d3f7bfe9553d9a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "99923ce3738567b267634c671f1688171d9c423584baeac7df96a8b0e159875a"
-    sha256 cellar: :any_skip_relocation, ventura:       "99923ce3738567b267634c671f1688171d9c423584baeac7df96a8b0e159875a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7da0ba108ff33ea5b9ff2ac058e3a25dc0636d0761eae795d4d3f7bfe9553d9a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7da0ba108ff33ea5b9ff2ac058e3a25dc0636d0761eae795d4d3f7bfe9553d9a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1a42d228df9bb54e1d2f4f5110c07c36d0f5866b5958c74a67b326fe4275c6cb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1a42d228df9bb54e1d2f4f5110c07c36d0f5866b5958c74a67b326fe4275c6cb"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1a42d228df9bb54e1d2f4f5110c07c36d0f5866b5958c74a67b326fe4275c6cb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f4ec3dcff35f3f25e45289e5239bd0753d162092d95ea971eee1874fb31ea3b3"
+    sha256 cellar: :any_skip_relocation, ventura:       "f4ec3dcff35f3f25e45289e5239bd0753d162092d95ea971eee1874fb31ea3b3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1a42d228df9bb54e1d2f4f5110c07c36d0f5866b5958c74a67b326fe4275c6cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1a42d228df9bb54e1d2f4f5110c07c36d0f5866b5958c74a67b326fe4275c6cb"
   end
 
   depends_on "node"

8f0464bebaa4c0c7003793867d21ec235c093fca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-25 16:11:27 +0000

    terramate: update 0.14.1 bottle.

--- Formula/t/terramate.rb
+++ Formula/t/terramate.rb
@@ -7,12 +7,12 @@ class Terramate < Formula
   head "https://github.com/terramate-io/terramate.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "690c3435abc0bc5b01ee49bff5f1990e7932e62d5bff2ad2fb2b1343866b9eec"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "690c3435abc0bc5b01ee49bff5f1990e7932e62d5bff2ad2fb2b1343866b9eec"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "690c3435abc0bc5b01ee49bff5f1990e7932e62d5bff2ad2fb2b1343866b9eec"
-    sha256 cellar: :any_skip_relocation, sonoma:        "299e19291220eeed2baad0fac019d473c0c60a3a9731474ebc6a76daa3669377"
-    sha256 cellar: :any_skip_relocation, ventura:       "299e19291220eeed2baad0fac019d473c0c60a3a9731474ebc6a76daa3669377"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26ebd91f5b34cc55eb3e9e6f9f1da6b41cfa30db9f437fe003765662080e1198"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5cfc310f827677e0414093542050da53c5a9da9e5c6b26a30a8344cc247de6cd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5cfc310f827677e0414093542050da53c5a9da9e5c6b26a30a8344cc247de6cd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5cfc310f827677e0414093542050da53c5a9da9e5c6b26a30a8344cc247de6cd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8fc2d4fcfcad666552f4e69bf0ba907dac28b160c2f6e54e3c97ef1e85a2e414"
+    sha256 cellar: :any_skip_relocation, ventura:       "8fc2d4fcfcad666552f4e69bf0ba907dac28b160c2f6e54e3c97ef1e85a2e414"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8772bb8f3ed859942e9b1a1d61253d9e4703d0701beeef45bc8978f2c99a329e"
   end
 
   depends_on "go" => :build

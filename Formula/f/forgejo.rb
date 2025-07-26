75262eb3153911e8ff585d590deb6665524c2f6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-26 06:48:49 +0000

    forgejo: add 12.0.1 bottle.

--- Formula/f/forgejo.rb
+++ Formula/f/forgejo.rb
@@ -6,6 +6,15 @@ class Forgejo < Formula
   license "GPL-3.0-or-later"
   head "https://codeberg.org/forgejo/forgejo.git", branch: "forgejo"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "247df72c125144050d897bddd0730e5c4be4f3e9f42fb750671bb9f967b82c69"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3ea31cfae57a04706551b2c93971c1ee1d943f8fb06a227c24e36d032455b006"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f929960fe69d51328658bed7a3d483ca6e63b588903c4dfa839fd63d51ba7c88"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ef251e31ba34b75207c8cf64e0b463eb0c7fb367e27d3cea9666f08834e66ec2"
+    sha256 cellar: :any_skip_relocation, ventura:       "08f1141686d352ae9440abddd70c28cb65da35c127c2138518d2c041c1eaf66a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "059e06ff01da10063584fd5a0be92d1f73847fcc1444d894f696d067d64a9ce0"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
 

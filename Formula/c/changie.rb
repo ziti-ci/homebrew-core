c304d42e46035e6f86490f5b79ce36fc70c28822
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-27 20:22:35 +0000

    changie: update 1.22.1 bottle.

--- Formula/c/changie.rb
+++ Formula/c/changie.rb
@@ -7,12 +7,12 @@ class Changie < Formula
   head "https://github.com/miniscruff/changie.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2fb60e9ca2ccef779bd1cb4242d4f6176b58c31d275b88905ef6171b5482827c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2fb60e9ca2ccef779bd1cb4242d4f6176b58c31d275b88905ef6171b5482827c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "2fb60e9ca2ccef779bd1cb4242d4f6176b58c31d275b88905ef6171b5482827c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bd8de99d840ec8b80a3d98cebbd77a4a374730e8ed0f0d129a36ebe4f2111fd9"
-    sha256 cellar: :any_skip_relocation, ventura:       "bd8de99d840ec8b80a3d98cebbd77a4a374730e8ed0f0d129a36ebe4f2111fd9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9243c53cb73b0e2b7830be72ceb885ba525bca820aee22c00d6db7453db9409"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0c2054b5806ebaa4c334630133fc4dfd9c4220a86580236d18e4b047b6eef5e3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0c2054b5806ebaa4c334630133fc4dfd9c4220a86580236d18e4b047b6eef5e3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0c2054b5806ebaa4c334630133fc4dfd9c4220a86580236d18e4b047b6eef5e3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5368c47524415aa9e4e26e54957479396c530ddefd197b9b0b1d40718e80ff03"
+    sha256 cellar: :any_skip_relocation, ventura:       "5368c47524415aa9e4e26e54957479396c530ddefd197b9b0b1d40718e80ff03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b73b899d74730298d81313dd97bbc2e68a6ca234bcd10c0781023b9d5cf683be"
   end
 
   depends_on "go" => :build

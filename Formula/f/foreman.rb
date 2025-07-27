a07f33fd090116bc9e877cf37d953a16fcd80f50
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-27 11:42:19 +0000

    foreman: update 0.90.0 bottle.

--- Formula/f/foreman.rb
+++ Formula/f/foreman.rb
@@ -6,13 +6,13 @@ class Foreman < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "77ed065a7aff5e0a13b9645db33d36916f925d9b5943abd9c84b4d7f434bc329"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "77ed065a7aff5e0a13b9645db33d36916f925d9b5943abd9c84b4d7f434bc329"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "77ed065a7aff5e0a13b9645db33d36916f925d9b5943abd9c84b4d7f434bc329"
-    sha256 cellar: :any_skip_relocation, sonoma:        "77ed065a7aff5e0a13b9645db33d36916f925d9b5943abd9c84b4d7f434bc329"
-    sha256 cellar: :any_skip_relocation, ventura:       "77ed065a7aff5e0a13b9645db33d36916f925d9b5943abd9c84b4d7f434bc329"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "933a0892b69de4221ed745cd8ef0258d78b49c3e4bc63e4d920789600d4071bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "933a0892b69de4221ed745cd8ef0258d78b49c3e4bc63e4d920789600d4071bf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "961597e37878c33f9903db7e51dc9be14d5fe11434e179527f9c25a045408d7c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "961597e37878c33f9903db7e51dc9be14d5fe11434e179527f9c25a045408d7c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "961597e37878c33f9903db7e51dc9be14d5fe11434e179527f9c25a045408d7c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "961597e37878c33f9903db7e51dc9be14d5fe11434e179527f9c25a045408d7c"
+    sha256 cellar: :any_skip_relocation, ventura:       "961597e37878c33f9903db7e51dc9be14d5fe11434e179527f9c25a045408d7c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "23abef518736e00d017b564a9f78b2063a0fd93825d81bf79885ff6a909cf061"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23abef518736e00d017b564a9f78b2063a0fd93825d81bf79885ff6a909cf061"
   end
 
   uses_from_macos "ruby"

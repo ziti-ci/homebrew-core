7fa20724da6cfd5c0617f386e8cbd42aef674fb6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-27 08:13:15 +0000

    brush: add 0.2.20 bottle.

--- Formula/b/brush.rb
+++ Formula/b/brush.rb
@@ -11,6 +11,15 @@ class Brush < Formula
     regex(/brush-shell[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8794d2936afc5d7ebbbf75ee50bfc4ccaa5f2c94117c460c1772932152a177d2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "257a0e4dfdd157153d99a9e625500e53bb16e7b72ab7d492a59bc43f53ffd606"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "29b632465c3bbb57aa03c857a67fc6495a8293cb98daf15c9ca496a99a7cadd1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ca7987052c3be314b5dd960e5315179d57381dd58431ca88c15da3568d699de0"
+    sha256 cellar: :any_skip_relocation, ventura:       "89337cf3e9fe70ebf7ade3838cdc49687a7b46c6863d817cd6b926d57f7dd6fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b639e0d9477f75a8af7819fe8fb4c03943b7104f91a9340f1f7e7066ba38b486"
+  end
+
   depends_on "rust" => :build
 
   def install

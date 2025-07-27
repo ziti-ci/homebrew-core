50e26da6e80cd496090f04e80f1cbb3c5d143ad3
Rui Chen <rui@chenrui.dev>
2025-07-27 09:02:41 +0000

    brush: update 0.2.20 bottle.

--- Formula/b/brush.rb
+++ Formula/b/brush.rb
@@ -17,6 +17,7 @@ class Brush < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "29b632465c3bbb57aa03c857a67fc6495a8293cb98daf15c9ca496a99a7cadd1"
     sha256 cellar: :any_skip_relocation, sonoma:        "ca7987052c3be314b5dd960e5315179d57381dd58431ca88c15da3568d699de0"
     sha256 cellar: :any_skip_relocation, ventura:       "89337cf3e9fe70ebf7ade3838cdc49687a7b46c6863d817cd6b926d57f7dd6fe"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "247f6a20f2802df922475d8ee124dffa1c62cba886a8222d4392401773294d39"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "b639e0d9477f75a8af7819fe8fb4c03943b7104f91a9340f1f7e7066ba38b486"
   end
 

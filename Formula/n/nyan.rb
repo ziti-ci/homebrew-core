b8b6b513a343ce53b0d089b2787026038e106fc5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-27 07:45:53 +0000

    nyan: add 1.2.0 bottle.

--- Formula/n/nyan.rb
+++ Formula/n/nyan.rb
@@ -6,6 +6,15 @@ class Nyan < Formula
   license "MIT"
   head "https://github.com/toshimaru/nyan.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1721157f2792d7cdf534b614ca15ec4fef853642152430b1bb27ae0f8b64e558"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1721157f2792d7cdf534b614ca15ec4fef853642152430b1bb27ae0f8b64e558"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1721157f2792d7cdf534b614ca15ec4fef853642152430b1bb27ae0f8b64e558"
+    sha256 cellar: :any_skip_relocation, sonoma:        "81016155872fbe67b72378e7ef73e0b0419895b094325bc5ba79cdcee5b328f3"
+    sha256 cellar: :any_skip_relocation, ventura:       "81016155872fbe67b72378e7ef73e0b0419895b094325bc5ba79cdcee5b328f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3de0783be16d777e55002d945c19d916010bc539c85b24883a1e4a20a6a9f832"
+  end
+
   depends_on "go" => :build
 
   def install

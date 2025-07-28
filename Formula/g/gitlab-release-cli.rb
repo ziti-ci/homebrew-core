68738813dab4cedd105010fa5926977dded7f94e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-28 07:41:37 +0000

    gitlab-release-cli: add 0.24.0 bottle.

--- Formula/g/gitlab-release-cli.rb
+++ Formula/g/gitlab-release-cli.rb
@@ -6,6 +6,15 @@ class GitlabReleaseCli < Formula
   license "MIT"
   head "https://gitlab.com/gitlab-org/release-cli.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "876a3ed04ff07b0316bec9ff3f7979b473080ecb754b3751e0210994cd04a9bc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "876a3ed04ff07b0316bec9ff3f7979b473080ecb754b3751e0210994cd04a9bc"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "876a3ed04ff07b0316bec9ff3f7979b473080ecb754b3751e0210994cd04a9bc"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5480d1281a3356e1ddb811a9b4a6634969286682e14366641166a30c150d8202"
+    sha256 cellar: :any_skip_relocation, ventura:       "5480d1281a3356e1ddb811a9b4a6634969286682e14366641166a30c150d8202"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21aed32d1e61d02a7c6b3b5d111df239e576a3f5eeb82795ef150c5f3f8bc741"
+  end
+
   depends_on "go" => :build
 
   def install

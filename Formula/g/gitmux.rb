7154655b6432987ebb7637f91d9724b0bba24c86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-28 06:34:57 +0000

    gitmux: add 0.11.2 bottle.

--- Formula/g/gitmux.rb
+++ Formula/g/gitmux.rb
@@ -5,6 +5,15 @@ class Gitmux < Formula
   sha256 "c62b180415c272743d01531b911091b9c35911be4ec4aae3e7bfceddf5094f6c"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "94802a69e31ec9c017fb332eef1427d91c17e87b657db245817a83f5362b3b21"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "94802a69e31ec9c017fb332eef1427d91c17e87b657db245817a83f5362b3b21"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "94802a69e31ec9c017fb332eef1427d91c17e87b657db245817a83f5362b3b21"
+    sha256 cellar: :any_skip_relocation, sonoma:        "47f6304b35b33a7c5ffd896df4879fe25fba51b51b9701170f63b31d1f420bff"
+    sha256 cellar: :any_skip_relocation, ventura:       "47f6304b35b33a7c5ffd896df4879fe25fba51b51b9701170f63b31d1f420bff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40b685c802a8ccbb422f1e60541c12236b8be7c2c28f275247f2a41d4425e29f"
+  end
+
   depends_on "go" => :build
   depends_on "git" => :test
   depends_on "tmux"

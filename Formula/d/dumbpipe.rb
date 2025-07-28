3576bbbe8d5952c901c5edd2f01618b784e1c85e
Bevan Kay <email@bevankay.me>
2025-07-28 16:53:35 +1000

    dumbpipe 0.28.0 (new formula)

---
+++ Formula/d/dumbpipe.rb
@@ -0,0 +1,46 @@
+class Dumbpipe < Formula
+  desc "Unix pipes between devices"
+  homepage "https://dumbpipe.dev"
+  url "https://github.com/n0-computer/dumbpipe/archive/refs/tags/v0.28.0.tar.gz"
+  sha256 "bb7bd90eacebe505f2c669e4e13dac57c43c9c0eb5eca94dfa1378fd7cdcda84"
+  license any_of: ["MIT", "Apache-2.0"]
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    read, write = IO.pipe
+
+    # We need to capture both stdout and stderr from the listener process
+    # the node ID is output to stderror and the listener data is output to stdout.
+    listener_pid = spawn bin/"dumbpipe", "listen", err: write, out: write
+
+    begin
+      sleep 2
+      node_id = ""
+      while read.wait_readable(1)
+        line = read.gets
+        break if line.nil?
+
+        match = line.match(/dumbpipe connect (\w+)/)
+        next if match.blank?
+
+        node_id = match[1]
+      end
+      odie "No node ID found in listener output" if node_id.blank?
+
+      sender_read, sender_write = IO.pipe
+      sender_pid = spawn bin/"dumbpipe", "connect", node_id, in: sender_read
+      sender_write.puts "foobar"
+      assert_match "foobar", read.gets
+    ensure
+      Process.kill "TERM", sender_pid
+      Process.wait sender_pid
+      Process.kill "TERM", listener_pid
+      Process.wait listener_pid
+    end
+  end
+end

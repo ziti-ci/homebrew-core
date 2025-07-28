ed9b41efd651517b0a5b84f5f7291af44f8ed1b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-28 07:36:33 +0000

    stu: add 0.7.3 bottle.

--- Formula/s/stu.rb
+++ Formula/s/stu.rb
@@ -6,6 +6,15 @@ class Stu < Formula
   license "MIT"
   head "https://github.com/lusingander/stu.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d728f1ea93624c0af13aa6087826f9463f4eb7287294925b60a10e6a53fd3c19"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d1b8b3ed90a089fce6cc3757819ee75b4192b92bd1ea9f51813a111d3ba9c434"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c54544ed4d9ebc82bf9107b9c6d6cb561a3abaf994cdfa28ea506439bdb14ac2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8c4bc78930d12cdc0e0ea6f22a4ad1dc09cac0516cf1adf4fda2907aa0b6136c"
+    sha256 cellar: :any_skip_relocation, ventura:       "f9e2a0932304f3e304e1940c7635a47070da2521c594e533cfb098282e5d56a9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c85289acaaffefc89cb0fb2c5f2f7149a191fd99decc6cf92f47bdf39788d3ae"
+  end
+
   depends_on "rust" => :build
 
   def install

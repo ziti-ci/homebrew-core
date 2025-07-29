e5e819e64a3104cabb55d46ea8e80d69aaf76d5b
Carlo Cabrera <github@carlo.cab>
2025-07-29 10:42:44 +0800

    tree-sitter: split CLI to a separate formula
    The CLI build dependencies are pretty heavy, which is unpleasant if
    you're forced to build `tree-sitter` from source when you're most likely
    only looking for the library.
    
    This also removes `libtree-sitter` and its dependents from the
    dependency trees of `rust` and `node`, which should make the dependent
    tests for those formula just a tiny bit faster.

--- Formula/t/tree-sitter.rb
+++ Formula/t/tree-sitter.rb
@@ -1,9 +1,10 @@
 class TreeSitter < Formula
-  desc "Parser generator tool and incremental parsing library"
+  desc "Incremental parsing library"
   homepage "https://tree-sitter.github.io/"
   url "https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v0.25.8.tar.gz"
   sha256 "178b575244d967f4920a4642408dc4edf6de96948d37d7f06e5b78acee9c0b4e"
   license "MIT"
+  revision 1
   head "https://github.com/tree-sitter/tree-sitter.git", branch: "master"
 
   livecheck do
@@ -21,48 +22,19 @@ class TreeSitter < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "d26190d82f558a4deb06d20031c42c25fc701a9ffa81b707c0b7272b8b3a61ba"
   end
 
-  depends_on "rust" => :build
-  depends_on "node" => :test
-
   def install
     system "make", "install", "AMALGAMATED=1", "PREFIX=#{prefix}"
-    system "cargo", "install", *std_cargo_args(path: "cli")
-    generate_completions_from_executable(bin/"tree-sitter", "complete", shell_parameter_format: :arg)
+  end
+
+  def caveats
+    <<~EOS
+      This formula now installs only the `tree-sitter` library (`libtree-sitter`).
+      To install the CLI tool:
+        brew install tree-sitter-cli
+    EOS
   end
 
   test do
-    # a trivial tree-sitter test
-    assert_equal "tree-sitter #{version}", shell_output("#{bin}/tree-sitter --version").strip
-
-    # test `tree-sitter generate`
-    (testpath/"grammar.js").write <<~JS
-      module.exports = grammar({
-        name: 'YOUR_LANGUAGE_NAME',
-        rules: {
-          source_file: $ => 'hello'
-        }
-      });
-    JS
-    system bin/"tree-sitter", "generate", "--abi=latest"
-
-    # test `tree-sitter parse`
-    (testpath/"test/corpus/hello.txt").write <<~EOS
-      hello
-    EOS
-    parse_result = shell_output("#{bin}/tree-sitter parse #{testpath}/test/corpus/hello.txt").strip
-    assert_equal("(source_file [0, 0] - [1, 0])", parse_result)
-
-    # test `tree-sitter test`
-    (testpath/"test"/"corpus"/"test_case.txt").write <<~EOS
-      =========
-        hello
-      =========
-      hello
-      ---
-      (source_file)
-    EOS
-    system bin/"tree-sitter", "test"
-
     (testpath/"test_program.c").write <<~C
       #include <stdio.h>
       #include <string.h>

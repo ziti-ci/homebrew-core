c583c3d9d167e5ba87597985fd32ed96db90abbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-29 08:11:39 +0000

    dagu: update 1.18.0 bottle.

--- Formula/d/dagu.rb
+++ Formula/d/dagu.rb
@@ -6,12 +6,12 @@ class Dagu < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34881220355da690627b7ee9f96b6cac6499113ce716d95a2042b700bc2f55fd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d93fc93d1a4602b36c97ece9b97d3be24cb76d32c749192e13df19e33744f9e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8d6c81ea3649324a19ad81c91ef631ffc79eac155f34c3050f81f78f00af031d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "247d0b8fe1312b9157f6c65a36adf03ca02888d74b0de3b20c396c25f6461b13"
-    sha256 cellar: :any_skip_relocation, ventura:       "38d9c5ab6db4e1ab06b1aba2b01147ab3e8643fd2c9a207e227e5da5480286f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d16799b507702ca03d0984cda14ecea6ad9388f6f4521aea98a94d45a3cc6861"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "47748be852487b8094c57b2da4e963773e5c60905f84771098a44ca23273e9ce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb7e4c4639812e016e0ddf16cdab8d3a5b89daf5082f027ede206ad894c3af53"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "365e24723a6a5915812b26f21fc6f3492bb52f7ac4c5e92c5a3554b99fbc5c1a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "35f2ab65173359aea2a731c1d171133f495732d4913b8ca32c45d886592188ad"
+    sha256 cellar: :any_skip_relocation, ventura:       "f8efe89dc6e853ba93e4db1935f1cef672fc337b165af5c93a34c61172baab88"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ebeaf95dae027aabe45c7e8bf30b3ebf493735923a6ab3bcd8c432cea77e69b1"
   end
 
   depends_on "go" => :build

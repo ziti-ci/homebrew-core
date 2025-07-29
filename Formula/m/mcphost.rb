1586a034e774af8ead4e4c075a27c55b1a65b2d7
Rui Chen <rui@chenrui.dev>
2025-07-29 19:34:38 -0400

    mcphost 0.26.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/m/mcphost.rb
@@ -0,0 +1,22 @@
+class Mcphost < Formula
+  desc "CLI host for LLMs to interact with tools via MCP"
+  homepage "https://github.com/mark3labs/mcphost"
+  url "https://github.com/mark3labs/mcphost/archive/refs/tags/v0.26.1.tar.gz"
+  sha256 "1c74c8e403ee572a323397032d30b4b3ec2332c92941764714f1bf221b78b022"
+  license "MIT"
+  head "https://github.com/mark3labs/mcphost.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
+
+    generate_completions_from_executable(bin/"mcphost", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mcphost --version")
+    assert_match "Authentication Status", shell_output("#{bin}/mcphost auth status")
+    assert_match "EVENT  MATCHER  COMMAND  TIMEOUT", shell_output("#{bin}/mcphost hooks list")
+  end
+end

4d7e4439645afbaffb902abc9d1a6ea464b17970
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-30 16:11:18 +0000

    dita-ot: update 4.3.4 bottle.

--- Formula/d/dita-ot.rb
+++ Formula/d/dita-ot.rb
@@ -11,13 +11,13 @@ class DitaOt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cd925d8847c43b8a687966b5690a4f7a671526282c630424a5113103ec85e5a8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cd925d8847c43b8a687966b5690a4f7a671526282c630424a5113103ec85e5a8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "cd925d8847c43b8a687966b5690a4f7a671526282c630424a5113103ec85e5a8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f1bdece1b81a2bd1d9c82d605c7921fa107065e1dc99c77ff36eccc2117ffea7"
-    sha256 cellar: :any_skip_relocation, ventura:       "f1bdece1b81a2bd1d9c82d605c7921fa107065e1dc99c77ff36eccc2117ffea7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "55711435f2720e496c03305d15362aa38df8384c3cd178d0a9907a6fd2a60ddc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55711435f2720e496c03305d15362aa38df8384c3cd178d0a9907a6fd2a60ddc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bf827317098a83364824b48f4dabb3ffc9597da35fc2fdb75367f18be79be202"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bf827317098a83364824b48f4dabb3ffc9597da35fc2fdb75367f18be79be202"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "bf827317098a83364824b48f4dabb3ffc9597da35fc2fdb75367f18be79be202"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8a6675abd313544391cd5428cc250f817f815296f0a59ab1d0ae24211cfbd626"
+    sha256 cellar: :any_skip_relocation, ventura:       "8a6675abd313544391cd5428cc250f817f815296f0a59ab1d0ae24211cfbd626"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d59169aa5d2685490469408bb0ea04e69103d141a61dbb84f34fe622c69f85b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d59169aa5d2685490469408bb0ea04e69103d141a61dbb84f34fe622c69f85b6"
   end
 
   depends_on "openjdk"

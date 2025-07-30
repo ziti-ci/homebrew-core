27bc67f14db61bae44223d3b777188b81bf02a77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-30 20:03:19 +0000

    kubectl-ai 0.0.20

--- Formula/k/kubectl-ai.rb
+++ Formula/k/kubectl-ai.rb
@@ -1,8 +1,8 @@
 class KubectlAi < Formula
   desc "AI powered Kubernetes Assistant"
   homepage "https://github.com/GoogleCloudPlatform/kubectl-ai"
-  url "https://github.com/GoogleCloudPlatform/kubectl-ai/archive/refs/tags/v0.0.19.tar.gz"
-  sha256 "892aa6f196c9fd4a2f1231294eb21513a6f30cd30949b981d0f208ce14fc3ac7"
+  url "https://github.com/GoogleCloudPlatform/kubectl-ai/archive/refs/tags/v0.0.20.tar.gz"
+  sha256 "ef0f825c335870e6674b36da62665f7f31c9a4c77ebd8f00cef60875aa21e009"
   license "Apache-2.0"
 
   livecheck do

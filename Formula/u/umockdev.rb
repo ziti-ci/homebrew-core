cc67e14b95c1b005b343a785225d65b909575a9a
botantony <antonsm21@gmail.com>
2025-07-31 16:25:29 +0200

    umockdev 0.19.2

--- Formula/u/umockdev.rb
+++ Formula/u/umockdev.rb
@@ -1,12 +1,10 @@
 class Umockdev < Formula
   desc "Mock hardware devices for creating unit tests and bug reporting"
   homepage "https://github.com/martinpitt/umockdev"
-  url "https://github.com/martinpitt/umockdev/releases/download/0.19.1/umockdev-0.19.1.tar.xz"
-  sha256 "2cece0e8e366b89b4070be74f3389c9f7fa21aca56d8a5357e96e30cd8d4f426"
+  url "https://github.com/martinpitt/umockdev/releases/download/0.19.2/umockdev-0.19.2.tar.xz"
+  sha256 "6fdda67544f366c95f1556de0d1f82db13f231bc0362c7df03cc34ba26b29268"
   license "LGPL-2.1-or-later"
   head "https://github.com/martinpitt/umockdev.git", branch: "main"
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     sha256 arm64_linux:  "ca0cdf39df6bd2a6520a838d7c72945674574019b48b7863f31df0e4e5d67234"

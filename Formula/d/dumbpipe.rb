53f38c5754add62af90b2bbd18f574db63aef91d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-31 02:41:03 +0000

    dumbpipe 0.29.0
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/d/dumbpipe.rb
+++ Formula/d/dumbpipe.rb
@@ -1,8 +1,8 @@
 class Dumbpipe < Formula
   desc "Unix pipes between devices"
   homepage "https://dumbpipe.dev"
-  url "https://github.com/n0-computer/dumbpipe/archive/refs/tags/v0.28.0.tar.gz"
-  sha256 "bb7bd90eacebe505f2c669e4e13dac57c43c9c0eb5eca94dfa1378fd7cdcda84"
+  url "https://github.com/n0-computer/dumbpipe/archive/refs/tags/v0.29.0.tar.gz"
+  sha256 "69dadd34b56ff18320a27a69c66cf0591b2f11ee725427b4a281928d269fa905"
   license any_of: ["MIT", "Apache-2.0"]
 
   bottle do
@@ -29,7 +29,7 @@ class Dumbpipe < Formula
     listener_pid = spawn bin/"dumbpipe", "listen", err: write, out: write
 
     begin
-      sleep 2
+      sleep 10
       node_id = while read.wait_readable(1)
         line = read.gets
         break if line.nil?
@@ -38,7 +38,7 @@ class Dumbpipe < Formula
         next if match.blank?
 
         break match[1]
-      end
+      end.to_s
       refute_empty node_id, "No node ID found in listener output"
 
       sender_read, sender_write = IO.pipe
@@ -46,10 +46,10 @@ class Dumbpipe < Formula
       sender_write.puts "foobar"
       assert_match "foobar", read.gets
     ensure
-      Process.kill "TERM", sender_pid
-      Process.kill "TERM", listener_pid
-      Process.wait sender_pid
-      Process.wait listener_pid
+      Process.kill "TERM", sender_pid unless sender_pid.nil?
+      Process.kill "TERM", listener_pid unless listener_pid.nil?
+      Process.wait sender_pid unless sender_pid.nil?
+      Process.wait listener_pid unless listener_pid.nil?
     end
   end
 end

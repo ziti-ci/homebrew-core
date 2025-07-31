0bdb048850f16bced022e39a5b105ec9cd81b249
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-31 08:12:00 +0000

    deck: add 1.7.0 bottle.

--- Formula/d/deck.rb
+++ Formula/d/deck.rb
@@ -6,6 +6,15 @@ class Deck < Formula
   license "MIT"
   head "https://github.com/k1LoW/deck.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6fee021bc25c9e4b11b96eb32643b65bd0264121651c132c36246a6acc36f992"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6fee021bc25c9e4b11b96eb32643b65bd0264121651c132c36246a6acc36f992"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "6fee021bc25c9e4b11b96eb32643b65bd0264121651c132c36246a6acc36f992"
+    sha256 cellar: :any_skip_relocation, sonoma:        "43771b2949a9290dfe45c8880b54983a3c82a6d6341f755567726bd05eaa8d74"
+    sha256 cellar: :any_skip_relocation, ventura:       "43771b2949a9290dfe45c8880b54983a3c82a6d6341f755567726bd05eaa8d74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bbdd67b70dfc709288fcd44f804ad38f481df4e1f37a288ad7736116f47ba6d6"
+  end
+
   depends_on "go" => :build
 
   def install

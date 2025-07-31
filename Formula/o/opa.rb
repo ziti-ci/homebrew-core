fdf50eb8cca941559d5c016ef6e9474220ac4bbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-31 23:14:33 +0000

    opa 1.7.1

--- Formula/o/opa.rb
+++ Formula/o/opa.rb
@@ -1,8 +1,8 @@
 class Opa < Formula
   desc "Open source, general-purpose policy engine"
   homepage "https://www.openpolicyagent.org"
-  url "https://github.com/open-policy-agent/opa/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "a98386cf64eaff62003d844244e8e1587edf36b2306c5395d221b6b5b62cf824"
+  url "https://github.com/open-policy-agent/opa/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "f426516ee9e09b19d4135441753a44346664f5769ee8c83b52095b0cded54828"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 

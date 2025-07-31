0d38aa63f89147468b9d387180065c991644eac1
Rui Chen <rui@chenrui.dev>
2025-07-31 14:47:53 -0400

    prefligit 0.0.13 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/prefligit.rb
@@ -0,0 +1,21 @@
+class Prefligit < Formula
+  desc "Pre-commit re-implemented in Rust"
+  homepage "https://github.com/j178/prefligit"
+  url "https://github.com/j178/prefligit/archive/refs/tags/v0.0.13.tar.gz"
+  sha256 "f5dce3a675e6ec5e0257d88cb989f05a337d18604d14a3eb61a74a6609317329"
+  license "MIT"
+  head "https://github.com/j178/prefligit.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/prefligit --version")
+
+    output = shell_output("#{bin}/prefligit sample-config")
+    assert_match "See https://pre-commit.com for more information", output
+  end
+end

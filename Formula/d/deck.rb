2c76d51e74a846483d7f0c0d64f3031ae812e347
TATSUNO “Taz” Yasuhiro <ytatsuno.jp@gmail.com>
2025-07-31 10:02:16 +0900

    deck 1.7.0 (new formula)

---
+++ Formula/d/deck.rb
@@ -0,0 +1,21 @@
+class Deck < Formula
+  desc "Creates slide deck using Markdown and Google Slides"
+  homepage "https://github.com/k1LoW/deck"
+  url "https://github.com/k1LoW/deck/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "7136eba6db276237898099a38f62e6094b205be34748a539d6a6f8e95c19b6c5"
+  license "MIT"
+  head "https://github.com/k1LoW/deck.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/deck"
+
+    generate_completions_from_executable(bin/"deck", "completion", shells: [:zsh, :bash, :fish])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/deck --version")
+    assert_match "presentation ID is required", shell_output("#{bin}/deck export 2>&1", 1)
+  end
+end

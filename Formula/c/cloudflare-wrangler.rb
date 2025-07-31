e9ad3140cf9445f5d6810c75a8ae74bc57f7d6dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-31 16:59:29 +0000

    cloudflare-wrangler 4.27.0

--- Formula/c/cloudflare-wrangler.rb
+++ Formula/c/cloudflare-wrangler.rb
@@ -1,8 +1,8 @@
 class CloudflareWrangler < Formula
   desc "CLI tool for Cloudflare Workers"
   homepage "https://github.com/cloudflare/workers-sdk"
-  url "https://registry.npmjs.org/wrangler/-/wrangler-4.26.1.tgz"
-  sha256 "1fc6f703f01d3a0c7d47f3bd286c83ad4ab02c3e26ba3c1062a4c63a8f25d5ad"
+  url "https://registry.npmjs.org/wrangler/-/wrangler-4.27.0.tgz"
+  sha256 "62342e7fd6e894e91df0cdf7724094235899a30aa6d711dc1d097f063474b1af"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do

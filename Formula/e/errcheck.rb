33bd65be85ef6ae4f28919e5df7cacf4bc043db2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-01 18:37:39 +0000

    errcheck: add 1.9.0 bottle.

--- Formula/e/errcheck.rb
+++ Formula/e/errcheck.rb
@@ -5,6 +5,15 @@ class Errcheck < Formula
   sha256 "f8b9c864c0bdc8e56fbd709fb97a04b43b989815641b8bd9aae2e5fbc43b6930"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b0c68d0627e96f81d5c9908e475a382997f174292480d7e401b9611684ccdd1f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b0c68d0627e96f81d5c9908e475a382997f174292480d7e401b9611684ccdd1f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b0c68d0627e96f81d5c9908e475a382997f174292480d7e401b9611684ccdd1f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c9d6b69062cdbb9c8b5edc8129eb5e397807321f74f6b14a359bf33b9aff6fed"
+    sha256 cellar: :any_skip_relocation, ventura:       "c9d6b69062cdbb9c8b5edc8129eb5e397807321f74f6b14a359bf33b9aff6fed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9ec9399bb316f7bd3d51f053ec6f918a09a7c209427c75192d812767135fa6ec"
+  end
+
   depends_on "go" => [:build, :test]
 
   def install

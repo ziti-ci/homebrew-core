b2064a8c7a36d6cf78242a632613227925a83908
junglerobba <junglerobba@jngl.one>
2025-08-01 08:33:40 +0000

    tmux-sessionizer 0.5.0

--- Formula/t/tmux-sessionizer.rb
+++ Formula/t/tmux-sessionizer.rb
@@ -1,8 +1,8 @@
 class TmuxSessionizer < Formula
   desc "Tool for opening git repositories as tmux sessions"
   homepage "https://github.com/jrmoulton/tmux-sessionizer/"
-  url "https://github.com/jrmoulton/tmux-sessionizer/archive/refs/tags/v0.4.5.tar.gz"
-  sha256 "37cceae77bad373452d08b990065e7d1e8ed7b038a0af126aa4403332364530e"
+  url "https://github.com/jrmoulton/tmux-sessionizer/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "c3205764f70c8e7f94a1b32eccbc22e402cd9ab28c54d06b405073cae185bdd8"
   license "MIT"
 
   bottle do
@@ -15,41 +15,18 @@ class TmuxSessionizer < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "a588098f0d380b6fd1ca53a697760e9d4d56a9d828bcd1e7a21041d488c18ff3"
   end
 
-  depends_on "pkgconf" => :build
   depends_on "rust" => :build
-  depends_on "libgit2"
-  depends_on "libssh2"
-  depends_on "openssl@3"
 
   uses_from_macos "zlib"
 
   def install
-    # Ensure that the `openssl` crate picks up the intended library.
-    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
-    ENV["OPENSSL_NO_VENDOR"] = "1"
-
-    ENV["LIBGIT2_NO_VENDOR"] = "1"
-    ENV["LIBSSH2_SYS_USE_PKG_CONFIG"] = "1"
-
     system "cargo", "install", *std_cargo_args
 
     generate_completions_from_executable(bin/"tms", shell_parameter_format: :clap)
   end
 
   test do
-    require "utils/linkage"
-
     assert_match "Configuration has been stored", shell_output("#{bin}/tms config -p /dev/null")
     assert_match version.to_s, shell_output("#{bin}/tms --version")
-
-    [
-      Formula["libgit2"].opt_lib/shared_library("libgit2"),
-      Formula["libssh2"].opt_lib/shared_library("libssh2"),
-      Formula["openssl@3"].opt_lib/shared_library("libssl"),
-      Formula["openssl@3"].opt_lib/shared_library("libcrypto"),
-    ].each do |library|
-      assert Utils.binary_linked_to_library?(bin/"tms", library),
-             "No linkage with #{library.basename}! Cargo is likely using a vendored version."
-    end
   end
 end

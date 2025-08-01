127514a264aeadf34e69ec491549299fdb735b1f
botantony <antonsm21@gmail.com>
2025-08-01 09:29:41 +0200

    jdtls 1.49.0
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/j/jdtls.rb
+++ Formula/j/jdtls.rb
@@ -3,9 +3,9 @@ class Jdtls < Formula
 
   desc "Java language specific implementation of the Language Server Protocol"
   homepage "https://github.com/eclipse-jdtls/eclipse.jdt.ls"
-  url "https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.48.0/jdt-language-server-1.48.0-202506271502.tar.gz"
-  version "1.48.0"
-  sha256 "b0a7fa1240e2caf1296d59ea709c525d4a631fbefda49e7182e07e00c1de62c9"
+  url "https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.49.0/jdt-language-server-1.49.0-202507311558.tar.gz"
+  version "1.49.0"
+  sha256 "a3f9fb5921f5273d0f8fe4365b363fbad1bdc2e86991db3149b2d76f1265bcd7"
   license "EPL-2.0"
   version_scheme 1
 

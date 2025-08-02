9334183d688c44b7c31bb403cc309ad96849e622
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-02 10:06:01 +0000

    prefligit: update 0.0.15 bottle.

--- Formula/p/prefligit.rb
+++ Formula/p/prefligit.rb
@@ -7,13 +7,13 @@ class Prefligit < Formula
   head "https://github.com/j178/prefligit.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "abd29d15c5fe30a416e72b0e1e9b0ecc92f8a3d82ecbdbd740de2c9b13e0cfb6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "391947b8f7c942c3f8044fb980eaee915b872d4fa15cfa12c56fed77d6c68c2f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a61c2231732eff1f48a93c0964a3da4d81aaf6e34ebc782240edea59ce09b90e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6ece93d1218a7fe8b870f90a8c4c64b0803e30ee040b408d5ffa7d629bae9706"
-    sha256 cellar: :any_skip_relocation, ventura:       "1d36ee986537e5b250afeaf3acf111578c0f4a0b0331bd78fadf4797227efc86"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ef8a81428a6c489e09817f670244dcec3d8643e635e9e03118b4a8253542918c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b143640d84b8dbcf068d87169ebaa100e18c695467c3a112cf43f0ef1a3b42e0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f74593a948bb5c1d5f28d5a9a4bb78984f292a6a63892a64d915d5a2223d3118"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4140c57f4f5f67d3f0706b0856477e6b9bc7e19863684e048b30086d9d75a4bd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "39e2b41148062e5cd90b1840ec0ce5af974e898442bf9faafb342f3adb23d42e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c210e071de39327ab40cc03f3c61b497a132db8ffc0c51e512fa0f76591f7721"
+    sha256 cellar: :any_skip_relocation, ventura:       "4a2b25f767473e687adb172d1bbfc40d8e2251191eff5c14f1de35f92f998d66"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a2193674d2eda73cde48f63f0031538883d64d113a3aef9eaee3e336c3c2c6ed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2cc012eeb34bf04a9e151b08a101c507bc1a1fac517b86bbf69cae1557a3d7ed"
   end
 
   depends_on "rust" => :build

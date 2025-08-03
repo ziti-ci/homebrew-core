79f6b0177add180f89271b09ebc7f1f03e741980
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-03 17:00:59 +0000

    detox 3.0.0

--- Formula/d/detox.rb
+++ Formula/d/detox.rb
@@ -1,8 +1,8 @@
 class Detox < Formula
   desc "Utility to replace problematic characters in filenames"
   homepage "https://detox.sourceforge.net/"
-  url "https://github.com/dharple/detox/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "b7924153e3503a2844f111a7d142ebc557d2f03ca2bce8834b8cdb87dc688754"
+  url "https://github.com/dharple/detox/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "409c25875da37137a390d29a65d0cadcf99c4f6fe524fdb76bc1fb7e987ab799"
   license "BSD-3-Clause"
 
   bottle do

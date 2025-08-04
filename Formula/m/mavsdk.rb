63d530da054950db9894b6899abf26a704bdebb4
Carlo Cabrera <github@carlo.cab>
2025-08-04 19:14:19 +0800

    mavsdk: revision bump (abseil 20250127.1)

--- Formula/m/mavsdk.rb
+++ Formula/m/mavsdk.rb
@@ -5,7 +5,7 @@ class Mavsdk < Formula
       tag:      "v3.7.2",
       revision: "faf36edc6fda478a2c013698c78a42dfc663ef23"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable

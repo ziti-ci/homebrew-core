dc1e95a35202b27ee02a2343f35c7b64828f8f1c
calvinit <calvc@qq.com>
2025-08-04 10:56:47 +0800

    apache-flink@1 1.20.2

--- Formula/a/apache-flink@1.rb
+++ Formula/a/apache-flink@1.rb
@@ -1,10 +1,10 @@
 class ApacheFlinkAT1 < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.20.1/flink-1.20.1-bin-scala_2.12.tgz"
-  mirror "https://archive.apache.org/dist/flink/flink-1.20.1/flink-1.20.1-bin-scala_2.12.tgz"
-  version "1.20.1"
-  sha256 "5fc4551cd11aee83a9569392339c43fb32a60847db456e1cb4fa64c8daae0186"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.20.2/flink-1.20.2-bin-scala_2.12.tgz"
+  mirror "https://archive.apache.org/dist/flink/flink-1.20.2/flink-1.20.2-bin-scala_2.12.tgz"
+  version "1.20.2"
+  sha256 "76923477b2d12a992f1713954c03611a37a293349ae9e21e163790c1fd6a676a"
   license "Apache-2.0"
 
   no_autobump! because: :requires_manual_review

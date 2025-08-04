3e415d70a78c80f6145e5f0e318736c8b5569927
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-04 17:16:54 +0000

    kubevpn 2.9.1

--- Formula/k/kubevpn.rb
+++ Formula/k/kubevpn.rb
@@ -1,8 +1,8 @@
 class Kubevpn < Formula
   desc "Offers a Cloud-Native Dev Environment that connects to your K8s cluster network"
   homepage "https://www.kubevpn.dev"
-  url "https://github.com/kubenetworks/kubevpn/archive/refs/tags/v2.9.0.tar.gz"
-  sha256 "cda33c46988e9ff00b8f51f3b4d666dfde39e27a51c67c68dd2badeb1f70f211"
+  url "https://github.com/kubenetworks/kubevpn/archive/refs/tags/v2.9.1.tar.gz"
+  sha256 "391ace74dfbadca957fdc980078fee31f370d768248ce87cac37006ed3e999a6"
   license "MIT"
   head "https://github.com/kubenetworks/kubevpn.git", branch: "master"
 

de280fee83ef29d90304d2fd72a67bb73414b714
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-04 12:01:53 +0000

    newrelic-cli: update 0.101.0 bottle.

--- Formula/n/newrelic-cli.rb
+++ Formula/n/newrelic-cli.rb
@@ -12,13 +12,13 @@ class NewrelicCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2e5cbbb2f859df272a37dd0db914878367f0db444d8f9c520c8bfafb8ed6f10f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bde89659d211c766601c02b6b8db873b60cf1a6f6a6531bf5dad20cdbaf9db51"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "448fd6538fb6a150e19636c0514963bdb8069fa41183fd7d95817e160cf1653a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bc44951fb886a5b6515859fcdf1024b6dfc5a45902b320ce750cde504d5c92c5"
-    sha256 cellar: :any_skip_relocation, ventura:       "d076bab053f4fadd87a145490ec2988001818fb99f1f963b73fdfe6c8b64c4e3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "07918fe1433cdd80de74f572f8347beed8078cbd6562be348a12478868546bcb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "75d0810107ff8bc8cf7379f64f0465616a1d678dd1548026c2fc35c5524bb878"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f046a49ea66353f683f8be65666036806668ac56c26443e29db9d98916db87ec"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "46145dce3f1c7d3ad5729f6613e9ed1b17d6c61206ac1e0e01bd99c5c29e2689"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "af177810ab6e46280e872714db29be2fc798209de8d33a3688e4816e9a0ad064"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8295ef8aa4881eabb2b2dbb41c501e0d193a4ecf6422c82e6fdf996948f296b3"
+    sha256 cellar: :any_skip_relocation, ventura:       "33084e80faf59bd48f9fe81b3952a4e2c875c21186d41ee33ba95d8f81acf0a0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "33cc67f527d19a438d6310ea2dab6fd56993db3c933e20b35dc2e8fae9b121fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ce206c2945ceec3defd16d4d5c844a438b92a27fd3e86e1d211d9ab113be55f"
   end
 
   depends_on "go" => :build

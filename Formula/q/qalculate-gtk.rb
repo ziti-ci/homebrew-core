69e208b7dd99005a2fd405af479fb8947edc746f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-04 14:20:46 +0000

    qalculate-gtk 5.7.0

--- Formula/q/qalculate-gtk.rb
+++ Formula/q/qalculate-gtk.rb
@@ -1,8 +1,8 @@
 class QalculateGtk < Formula
   desc "Multi-purpose desktop calculator"
   homepage "https://qalculate.github.io/"
-  url "https://github.com/Qalculate/qalculate-gtk/releases/download/v5.6.0/qalculate-gtk-5.6.0.tar.gz"
-  sha256 "3dabd3b1ed981222b7fe7d2c14d47926715b8ae13098b0a4bdaa9dce96eee36d"
+  url "https://github.com/Qalculate/qalculate-gtk/releases/download/v5.7.0/qalculate-gtk-5.7.0.tar.gz"
+  sha256 "dcb3663b36abafdfe32e943644bf4fc64bd685c0225f944a3f1c4a85e70db3b5"
   license "GPL-2.0-or-later"
 
   bottle do

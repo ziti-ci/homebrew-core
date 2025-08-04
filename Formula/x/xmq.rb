9dacce02487c11a2f190318a09662d0228e1702e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-04 15:39:27 +0000

    xmq: update 4.0.1 bottle.

--- Formula/x/xmq.rb
+++ Formula/x/xmq.rb
@@ -6,13 +6,13 @@ class Xmq < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "6d7da8d0ceb2639bc2ac307f045d8647080af15c81125f30a811c1ee90455ad2"
-    sha256 cellar: :any,                 arm64_sonoma:  "72150d9356fc5173eed941e0517624564b764ba54379181a198ee1e28a43ffa4"
-    sha256 cellar: :any,                 arm64_ventura: "7acebce89ff42528c5b3d8353a6162d34902a3c56d0e82cecaa86bdf8e96db71"
-    sha256 cellar: :any,                 sonoma:        "5035028909fc2705baa474d6816dcf781bc8581512f86eade88caa670dd267b4"
-    sha256 cellar: :any,                 ventura:       "edfdc4e650ca9d7b83425fbbcf60e027bab6f70897cb1fce7913abfb7186e867"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b9f6ad29b2af0989d72d0eb5086839e9192107c8c3650998e104f12038e99eca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "00eb32cfcaaadf3cefb1336cbe140dc5ed22de1ea28be36ed44b271e407dc5dd"
+    sha256 cellar: :any,                 arm64_sequoia: "42095874ba74ca331ada2c067d2fbcd6e19efe70413c5f718bb4b09aef313e5d"
+    sha256 cellar: :any,                 arm64_sonoma:  "d57deede2a93c04f0f14906dd26b227cf4a155f3cbbbfec8f8907058bf0c5fc3"
+    sha256 cellar: :any,                 arm64_ventura: "8aca1bdd68bd3920d5849df45614c55c38af154ee1dd40e28cd4828ca614eb6a"
+    sha256 cellar: :any,                 sonoma:        "bf5db137f33a6b09d401804efd10ce28710a2143d8adb482a56dc24b275e26ad"
+    sha256 cellar: :any,                 ventura:       "12e55f6db9480dc671c5d23f603b88c9adac0051391b9fcbdc37116974f84918"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "165b86a489c050cdfedb74e27c16ef77d3bfb90c0f1d8e73cde376b6552531e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "abaff1f72a7abb52ae8a84f576fae750c4322345327209ad996c363709fc3363"
   end
 
   head do

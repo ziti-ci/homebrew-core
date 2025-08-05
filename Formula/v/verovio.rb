db21b599ca62888f891db6075e1b6b0ed255a7a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 11:26:14 +0000

    verovio 5.5.1

--- Formula/v/verovio.rb
+++ Formula/v/verovio.rb
@@ -1,8 +1,8 @@
 class Verovio < Formula
   desc "Command-line MEI music notation engraver"
   homepage "https://www.verovio.org"
-  url "https://github.com/rism-digital/verovio/archive/refs/tags/version-5.5.0.tar.gz"
-  sha256 "6916b0c379f99070b8db3d18df0093db33beec4cea58dbdc3f802a19a6646601"
+  url "https://github.com/rism-digital/verovio/archive/refs/tags/version-5.5.1.tar.gz"
+  sha256 "e27deafa8ae07052649a85478a1476aa83dcf69bb0b84280c5fa3d810d5f2360"
   license "LGPL-3.0-only"
   head "https://github.com/rism-digital/verovio.git", branch: "develop"
 

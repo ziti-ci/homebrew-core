46a923559b162bd669e1da4b057ac6567ca1f34f
Rui Chen <rui@chenrui.dev>
2025-08-05 16:50:32 +0000

    pyrefly: update 0.26.1 bottle.

--- Formula/p/pyrefly.rb
+++ Formula/p/pyrefly.rb
@@ -12,6 +12,7 @@ class Pyrefly < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "cfb1a49c5e7b7aaa4b7c485d41cc5318fa9511f123b5426579c1d1ca7b93e99a"
     sha256 cellar: :any_skip_relocation, sonoma:        "1b2b7965dd8053150ebdbca809ff39efe877a3f8aee7755f313e1a96f947fd80"
     sha256 cellar: :any_skip_relocation, ventura:       "4774d8e2a6192454aba94785f57abbd8028b3373da129432a835f43bce49fe72"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c7274ddd1b856908300fa5bd9c59753dbb46e66af93a643ee1a85f91a6528570"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "92bac047a9c209c9cf0346038757cfc2b78bab57e9c80d27b2972b2523cd9617"
   end
 

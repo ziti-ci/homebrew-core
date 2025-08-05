bd76b38edb53e39a6df2c7042ec46e2327353fa6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 11:13:44 +0000

    neo4j 2025.07.1

--- Formula/n/neo4j.rb
+++ Formula/n/neo4j.rb
@@ -1,8 +1,8 @@
 class Neo4j < Formula
   desc "Robust (fully ACID) transactional property graph database"
   homepage "https://neo4j.com/"
-  url "https://neo4j.com/artifact.php?name=neo4j-community-2025.07.0-unix.tar.gz"
-  sha256 "88e1405ba86177d2d21e061707f5ec42da6cdeec9bc3312be9fcef2220d6dc9c"
+  url "https://neo4j.com/artifact.php?name=neo4j-community-2025.07.1-unix.tar.gz"
+  sha256 "09a0bca676b2b4c2b539d9fe4736dadc9dd844f566b50912da918fa14da8416e"
   license "GPL-3.0-or-later"
 
   livecheck do

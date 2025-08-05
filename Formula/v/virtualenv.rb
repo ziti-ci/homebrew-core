39d1533f91998951eebc1b0823dd36eeb103da9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 18:53:35 +0000

    virtualenv: update 20.33.1 bottle.

--- Formula/v/virtualenv.rb
+++ Formula/v/virtualenv.rb
@@ -9,13 +9,13 @@ class Virtualenv < Formula
   head "https://github.com/pypa/virtualenv.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b8255617a62e6d07a0cf0c80a3e0ae2a4fea702b69474edc43731954b3bc4c85"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b8255617a62e6d07a0cf0c80a3e0ae2a4fea702b69474edc43731954b3bc4c85"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "b8255617a62e6d07a0cf0c80a3e0ae2a4fea702b69474edc43731954b3bc4c85"
-    sha256 cellar: :any_skip_relocation, sonoma:        "84dfdd423e13c2c8930525c142c2aa9810171e69a1ab7ccc4f5dfa15b85e3b03"
-    sha256 cellar: :any_skip_relocation, ventura:       "84dfdd423e13c2c8930525c142c2aa9810171e69a1ab7ccc4f5dfa15b85e3b03"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7966c0f293fce47a6e372edd76253b2e75eb8f062b1f57e0329626d468d29fe5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7966c0f293fce47a6e372edd76253b2e75eb8f062b1f57e0329626d468d29fe5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0835bd9e7533f158fb74e39fc3d9e3d1abf418b42f411d9f8d6e535f94d60e80"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0835bd9e7533f158fb74e39fc3d9e3d1abf418b42f411d9f8d6e535f94d60e80"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0835bd9e7533f158fb74e39fc3d9e3d1abf418b42f411d9f8d6e535f94d60e80"
+    sha256 cellar: :any_skip_relocation, sonoma:        "18d2a0244cb3cfd13eed9864dfc5c4582624c1bfbd5725d81e622b84101d8797"
+    sha256 cellar: :any_skip_relocation, ventura:       "18d2a0244cb3cfd13eed9864dfc5c4582624c1bfbd5725d81e622b84101d8797"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "654829fa500b94f31d798f828e69143aa6a4d978b6527ac79b0a950171f629d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "654829fa500b94f31d798f828e69143aa6a4d978b6527ac79b0a950171f629d6"
   end
 
   depends_on "python@3.13"

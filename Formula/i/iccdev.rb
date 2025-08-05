f1421bc66ba07d8d0a5bcd239c6c24308b59ddc3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 18:03:04 +0000

    iccdev: add 2.2.50 bottle.

--- Formula/i/iccdev.rb
+++ Formula/i/iccdev.rb
@@ -5,6 +5,15 @@ class Iccdev < Formula
   sha256 "f48fc2e3f4cc80f9c73df27bf48fca3de7d0a81266d7084df544941e61b37eb2"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "f722a5bbdb050d4c8fc3a98ddb44ee416bf5b3dcef1ff97a7a4d7337c78e3626"
+    sha256 cellar: :any,                 arm64_sonoma:  "cbf8e8773fb606ad43f4eeef01be4531b03e8671a200fc930d16269184b1cfed"
+    sha256 cellar: :any,                 arm64_ventura: "320314d7aa7f677efe22cdc29b815205c4a8e44acc41bf2e269f2e7f49182d03"
+    sha256 cellar: :any,                 sonoma:        "7d8888b48a3fabea1b4b425ce45c0022cfe1aefab7ef197fc96af928027f2cbc"
+    sha256 cellar: :any,                 ventura:       "19822d5ff8e4213013e005cf41b52046c2822b2ecafc08f6da0109bba4b45e08"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1096b2b99784ab6d08d1590ab5f3dcd3f2b9145429b97c4e5182328a85877145"
+  end
+
   depends_on "cmake" => :build
   depends_on "nlohmann-json" => :build
   depends_on "jpeg"

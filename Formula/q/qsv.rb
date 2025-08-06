7e6eacac5e94b0140d7f04641b85362bcebcd50b
Mike McQuaid <mike@mikemcquaid.com>
2025-08-06 17:02:24 +0100

    qsv: use ENV.append_to_rustflags

--- Formula/q/qsv.rb
+++ Formula/q/qsv.rb
@@ -35,7 +35,7 @@ class Qsv < Formula
   def install
     # Use explicit CPU target instead of "native" to avoid brittle behavior
     # see discussion at https://github.com/briansmith/ring/discussions/2528#discussioncomment-13196576
-    ENV["RUSTFLAGS"] = "-C target-cpu=apple-m1" if OS.mac? && Hardware::CPU.arm?
+    ENV.append_to_rustflags "-C target-cpu=apple-m1" if OS.mac? && Hardware::CPU.arm?
 
     system "cargo", "install", *std_cargo_args, "--features", "apply,lens,luau,feature_capable"
     bash_completion.install "contrib/completions/examples/qsv.bash" => "qsv"

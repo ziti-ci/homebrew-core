16d8f93ff44c106a3e6d1f9f086d83d883c55e9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-06 22:00:27 +0000

    sf: update 2.100.3 bottle.

--- Formula/s/sf.rb
+++ Formula/s/sf.rb
@@ -13,13 +13,13 @@ class Sf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3b81cdaac1f8c1c7ba98fc0270ff01d795f5b385c990fdecfd63ce77c12fb39f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3b81cdaac1f8c1c7ba98fc0270ff01d795f5b385c990fdecfd63ce77c12fb39f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3b81cdaac1f8c1c7ba98fc0270ff01d795f5b385c990fdecfd63ce77c12fb39f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a38c44f58a2b60ec1ee0835113be2e709789c806b200daa980f28f1b57deadeb"
-    sha256 cellar: :any_skip_relocation, ventura:       "a38c44f58a2b60ec1ee0835113be2e709789c806b200daa980f28f1b57deadeb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3b81cdaac1f8c1c7ba98fc0270ff01d795f5b385c990fdecfd63ce77c12fb39f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3b81cdaac1f8c1c7ba98fc0270ff01d795f5b385c990fdecfd63ce77c12fb39f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "24f863af9367abf3b72500944c0ce1bfdef7190d53d546e43422d6abd0c71a9d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "24f863af9367abf3b72500944c0ce1bfdef7190d53d546e43422d6abd0c71a9d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "24f863af9367abf3b72500944c0ce1bfdef7190d53d546e43422d6abd0c71a9d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "62f7bd00f5c2f1d3ec1711d3cc47c0f4feb60cb9ee86c6f8fd42422faba1f3b3"
+    sha256 cellar: :any_skip_relocation, ventura:       "62f7bd00f5c2f1d3ec1711d3cc47c0f4feb60cb9ee86c6f8fd42422faba1f3b3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "24f863af9367abf3b72500944c0ce1bfdef7190d53d546e43422d6abd0c71a9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "24f863af9367abf3b72500944c0ce1bfdef7190d53d546e43422d6abd0c71a9d"
   end
 
   depends_on "node"

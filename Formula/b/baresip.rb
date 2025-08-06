1c6c03e049a13adc2bae08186b92a20509598660
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-06 05:01:36 +0000

    baresip 4.0.0

--- Formula/b/baresip.rb
+++ Formula/b/baresip.rb
@@ -1,8 +1,8 @@
 class Baresip < Formula
   desc "Modular SIP useragent"
   homepage "https://github.com/baresip/baresip"
-  url "https://github.com/baresip/baresip/archive/refs/tags/v3.24.0.tar.gz"
-  sha256 "a6dc7d76714236cbf64b9f58ece742befcd9e40bc787b0173370d52fd0447eb4"
+  url "https://github.com/baresip/baresip/archive/refs/tags/v4.0.0.tar.gz"
+  sha256 "481db747f9946c0304dab584f5b64dd05bfb847a701b2263aa0346d76dab503c"
   license "BSD-3-Clause"
 
   bottle do

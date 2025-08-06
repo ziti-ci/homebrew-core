fb215c68a690e0f5009f13f846fdc63631d79781
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-06 17:40:28 +0000

    balena-cli: update 22.2.2 bottle.

--- Formula/b/balena-cli.rb
+++ Formula/b/balena-cli.rb
@@ -13,13 +13,13 @@ class BalenaCli < Formula
   end
 
   bottle do
-    sha256                               arm64_sequoia: "1c0ef4f97bb535e5bf58f5c0eefb12c4eb93707d897ab30cec6050dc2e1e14b0"
-    sha256                               arm64_sonoma:  "d21388b67fdb0a645829508405918771b52b2c6aa7d18df68e031e1539e60161"
-    sha256                               arm64_ventura: "fbf2c95f6451fccbfc34b9231d92ca1d41491c9576f5f8ac01d9cf99568719f5"
-    sha256                               sonoma:        "a5e10af4308b92cfbe8a7449d3ecb9e20c9ff8236b45df4779b83d2f21d299d3"
-    sha256                               ventura:       "cad08d77475db783162bbc5759ca3677cbf68b915a3029e2059ec6650af05c5b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f0dc254178c3b45d40a4ee1d9e72462605fef322621cc41d1641a75367aba0b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5fa1bc114d7ef8719fc1bf24f0d81f2142999280c316c9ce841691b1432944ad"
+    sha256                               arm64_sequoia: "946a0b17bed973010c1d85f237056623cf10c02980d00a25a4092c42e46ff2a6"
+    sha256                               arm64_sonoma:  "688231262f911d6aa252d48af1064aeb8948c5c5e85d60e52438f3436297657c"
+    sha256                               arm64_ventura: "ff9e84dda19c669aa62e84127d5c9f74c5bbadee90e06361e602a8072604a111"
+    sha256                               sonoma:        "da70c9550c4c8c976ee953698d1719808ac7a7629ee93d2430c30e270c808eb4"
+    sha256                               ventura:       "28d33cd9ba79519cbb1680cc1280be000a0aeea2a84c0ff7504fb3e82eac96ad"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "beb381395d67b7664c36811fbc3eb8164a5d87d5436411e23ac34f2ada991e6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "17c2ae4bcc6d5b786dd9d1bf85220235f175591bf6b3653199a3d7e9f389be93"
   end
 
   # align with upstream, https://github.com/balena-io/balena-cli/blob/master/.github/actions/publish/action.yml#L21

d85d60af5d9076c364d03ede47906b8d0500ef00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 17:48:21 +0000

    goresym: update 3.1.1 bottle.

--- Formula/g/goresym.rb
+++ Formula/g/goresym.rb
@@ -7,13 +7,13 @@ class Goresym < Formula
   head "https://github.com/mandiant/GoReSym.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "77a9fb3d32d9b3005befae33e51fc76749d458e8ad80590c96cabd0d360bb4b3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "77a9fb3d32d9b3005befae33e51fc76749d458e8ad80590c96cabd0d360bb4b3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "77a9fb3d32d9b3005befae33e51fc76749d458e8ad80590c96cabd0d360bb4b3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c696147556ba2c0a077910777245cb9d2756dbd3ab0673d152f83740a9f8b864"
-    sha256 cellar: :any_skip_relocation, ventura:       "c696147556ba2c0a077910777245cb9d2756dbd3ab0673d152f83740a9f8b864"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3170bc3433caf14959d719a86f4a3115ab9a97bd80130ad76b0b683da56aa8a6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6d53da90f5f41a6decb0b9a8eecddf9c5c868792eb68fcee81bf790899abcdfe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d95afffe8cdc77c33259ecdcef922c01cede46ec5997d965548925186e30f89f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d95afffe8cdc77c33259ecdcef922c01cede46ec5997d965548925186e30f89f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d95afffe8cdc77c33259ecdcef922c01cede46ec5997d965548925186e30f89f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4ddd7523694d96f7ca9cef349b2f2abd92a04a1a08049b17b5777cadf0de5ef5"
+    sha256 cellar: :any_skip_relocation, ventura:       "4ddd7523694d96f7ca9cef349b2f2abd92a04a1a08049b17b5777cadf0de5ef5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "895c128646081f43a159810ff8f71922181a1e49c50e56ecd9ec7aeed46b9ebf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9916b4b8cc7cc28cdc90bd9e86ba2eb9d6e2992e862bfa7e83946751e5128406"
   end
 
   depends_on "go" => :build

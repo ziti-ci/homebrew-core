c1356ce6fc2028ac92c8ccef0bdce491494d8354
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 20:22:41 +0000

    prek 0.0.24

--- Formula/p/prek.rb
+++ Formula/p/prek.rb
@@ -1,8 +1,8 @@
 class Prek < Formula
   desc "Pre-commit re-implemented in Rust"
   homepage "https://github.com/j178/prek"
-  url "https://github.com/j178/prek/archive/refs/tags/v0.0.21.tar.gz"
-  sha256 "e4122150b44d39f7693ca8c2676fe07fa1f2607cf42302c8b8447619aafa311e"
+  url "https://github.com/j178/prek/archive/refs/tags/v0.0.24.tar.gz"
+  sha256 "f47105cfa938896b59e5afce0fac862e2eb00989c9d93d1e3625cf9fe6cc2cc2"
   license "MIT"
   head "https://github.com/j178/prek.git", branch: "master"
 
@@ -10,7 +10,7 @@ class Prek < Formula
 
   def install
     system "cargo", "install", *std_cargo_args
-    generate_completions_from_executable(bin/"prek", "generate-shell-completion")
+    generate_completions_from_executable(bin/"prek", shell_parameter_format: :clap)
   end
 
   test do

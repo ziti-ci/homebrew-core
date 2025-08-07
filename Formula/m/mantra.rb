94c5b845d6b9f98ab4f4ccad296319b78119bd3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 03:40:41 +0000

    mantra: update 3.1 bottle.

--- Formula/m/mantra.rb
+++ Formula/m/mantra.rb
@@ -7,14 +7,12 @@ class Mantra < Formula
   head "https://github.com/brosck/mantra.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "7c1c351dc805308006e2e3d5d0f4da28603a711b3044dc76813c63b5d0794a0d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "6a53d93d65ced3c59f29695d6480b91651e866b01a7858aa6f748389e02fd0e4"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8a0d0e35be8c8c9bb772cbd486fa108fcf4454861c6a025ad0db15273dc57c73"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bc5e2eca313a3583f9ce1453b229b46b01ede7d97a984c258fab50e195039814"
-    sha256 cellar: :any_skip_relocation, sonoma:         "d4c9bb02500093b2a41ffce55b75a6fdfdd3b522366c4e4f6dada9d94cbc5b04"
-    sha256 cellar: :any_skip_relocation, ventura:        "0b5d253645f863d03554c0fe4ed3fb2d1bc8f1b8248289e7cff6d28c542464b9"
-    sha256 cellar: :any_skip_relocation, monterey:       "9bc954f6399c3ebc4cbe352c295c0a92a2bb3c649edf98fa965bbb2dc5297eac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4a18404491b344866ef5a92dca510b02a79da2be1f87f6627d180da3e2bc3456"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7b327b97eb7cd1bed9e3cbbb8523501568762c1ab23b3b6f96a1004574d250ea"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b327b97eb7cd1bed9e3cbbb8523501568762c1ab23b3b6f96a1004574d250ea"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7b327b97eb7cd1bed9e3cbbb8523501568762c1ab23b3b6f96a1004574d250ea"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ad0307f0e619dcdeb51cf5ae48aa906e7cb1d97133beb064e506ebc47cf4aa0f"
+    sha256 cellar: :any_skip_relocation, ventura:       "ad0307f0e619dcdeb51cf5ae48aa906e7cb1d97133beb064e506ebc47cf4aa0f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c37cc06e99d712dc61fcc3b708660da350183a0e3bb83bf1273c2c822ca324a"
   end
 
   depends_on "go" => :build

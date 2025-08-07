cfdde6d533b4de63ac4ccd23c13c16f57c72354c
Mintsuki <mintsuki@protonmail.com>
2025-08-07 04:26:20 +0200

    limine 9.6.0 (new formula)

---
+++ Formula/l/limine.rb
@@ -0,0 +1,38 @@
+class Limine < Formula
+  desc "Modern, advanced, portable, multiprotocol bootloader and boot manager"
+  homepage "https://limine-bootloader.org"
+  url "https://github.com/limine-bootloader/limine/releases/download/v9.6.0/limine-9.6.0.tar.gz"
+  sha256 "715f9b2d507cc06553e2127e5a45da41034bcd66a86663176b5a993da1990271"
+  license "BSD-2-Clause"
+
+  # The reason to have LLVM and LLD as dependencies here is because building the
+  # bootloader is essentially decoupled from building any other normal host program;
+  # the compiler, LLVM tools, and linker are used similarly as any other generator
+  # creating any other non-program/library data file would be.
+  # Adding LLVM and LLD ensures they are present and that they are at their most
+  # updated version (unlike the host macOS LLVM which usually is not).
+  depends_on "lld" => :build
+  depends_on "llvm" => :build
+  depends_on "mtools" => :build
+  depends_on "nasm" => :build
+
+  def install
+    # Homebrew LLVM is not in path by default. Get the path to it, and override the
+    # build system's defaults for the target tools.
+    llvm_bins = Formula["llvm"].opt_bin
+
+    system "./configure", *std_configure_args, "--enable-all"
+    system "make",
+           "TOOLCHAIN_FOR_TARGET=#{llvm_bins}/llvm-",
+           "CC_FOR_TARGET=#{llvm_bins}/clang",
+           "LD_FOR_TARGET=ld.lld"
+    system "make", "install"
+  end
+
+  test do
+    bytes = 8 * 1024 * 1024 # 8M in bytes
+    (testpath/"test.img").write("\0" * bytes)
+    output = shell_output("#{bin}/limine bios-install #{testpath}/test.img 2>&1")
+    assert_match "installed successfully", output
+  end
+end

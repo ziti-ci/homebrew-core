fb3a801960f02b50aeb54da875a93728cdf87fac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 20:08:57 +0000

    jbang 0.129.0

--- Formula/j/jbang.rb
+++ Formula/j/jbang.rb
@@ -1,8 +1,8 @@
 class Jbang < Formula
   desc "Tool to create, edit and run self-contained source-only Java programs"
   homepage "https://jbang.dev/"
-  url "https://github.com/jbangdev/jbang/releases/download/v0.128.7/jbang-0.128.7.zip"
-  sha256 "bd2fb359d7f94c5e0c745a21664d777dd8dda5a78bc2daa696868a7f623fd647"
+  url "https://github.com/jbangdev/jbang/releases/download/v0.129.0/jbang-0.129.0.zip"
+  sha256 "e9b9000bf490065329f8279e13ef47d1d57f1ef16ecd7c3dc62e070c530417f3"
   license "MIT"
 
   livecheck do

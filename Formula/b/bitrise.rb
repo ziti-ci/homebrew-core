ad20a322fe7471c9b830df8b4b6b3b952b04e4e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 13:00:05 +0000

    bitrise: update 2.33.0 bottle.

--- Formula/b/bitrise.rb
+++ Formula/b/bitrise.rb
@@ -12,12 +12,12 @@ class Bitrise < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d8be298885e782adafc33734924a8967043a722583a1b1faf060ddc53a044ac3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d8be298885e782adafc33734924a8967043a722583a1b1faf060ddc53a044ac3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "d8be298885e782adafc33734924a8967043a722583a1b1faf060ddc53a044ac3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2a6b9d7c9c149397eee710966008fa97a2db0c5b7f00852ea96389fafeeb5066"
-    sha256 cellar: :any_skip_relocation, ventura:       "2a6b9d7c9c149397eee710966008fa97a2db0c5b7f00852ea96389fafeeb5066"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc655d7e820cbf0c2e526056241ecc86171720177da4c7aa9947ed585a4c885a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "227a2ca7d4eb576804a8d3b5830632d36ad0d82e291049ce883c72ceecb4d987"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "227a2ca7d4eb576804a8d3b5830632d36ad0d82e291049ce883c72ceecb4d987"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "227a2ca7d4eb576804a8d3b5830632d36ad0d82e291049ce883c72ceecb4d987"
+    sha256 cellar: :any_skip_relocation, sonoma:        "42dbadf3e9787dbcdec2d79908330107549a202d48315b07d87ba448497d9caa"
+    sha256 cellar: :any_skip_relocation, ventura:       "42dbadf3e9787dbcdec2d79908330107549a202d48315b07d87ba448497d9caa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "657934598d10e72f183d17c8ad4c6e5273867dd02b32af0809aec0dae8798705"
   end
 
   depends_on "go" => :build

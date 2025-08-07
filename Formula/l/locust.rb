507d73b3d69bbd674cf02a361363ce029d18c158
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 13:00:41 +0000

    locust: update 2.38.0 bottle.

--- Formula/l/locust.rb
+++ Formula/l/locust.rb
@@ -8,13 +8,13 @@ class Locust < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "b17a005df9926b598115c89bbbdac3793cf244d382f7d41476d6a4c2e034c7fe"
-    sha256 cellar: :any,                 arm64_sonoma:  "7ec0030f44b0e9abf5cd9873e734e7acbbfbae34a1787aba7adaea4be11e7719"
-    sha256 cellar: :any,                 arm64_ventura: "44323db01090d949a1e0739f3bb1bac7c68f7e2ccc9f770f49282a7556690813"
-    sha256 cellar: :any,                 sonoma:        "f9b047bea59ef628cc4e5c0e63081a53e7bd688709e72f48581581c4c769a0ad"
-    sha256 cellar: :any,                 ventura:       "a20057076815d32f3d2ae5307a77e4950cf4c1ced1f36cec7e7b7ceb07ec2249"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d3425191ed9bcc816ff85e3c1efb869fe6116613520f0dd0cdbf5632ff0c8b6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "79a3a3e9a31f41159595201180cf69c574adaa786a84f7817b2130b4c1221fe6"
+    sha256 cellar: :any,                 arm64_sequoia: "90151c3c0b51179054fc192870929761b5680c1b0a82d7214739f6ae4aabe95d"
+    sha256 cellar: :any,                 arm64_sonoma:  "4c1b20f30e04dc210b656774b9b36249d70d8ac2a708f671562ae82f2a57b53d"
+    sha256 cellar: :any,                 arm64_ventura: "bdd8a916985eb33734db24201cc2fa859970e241403089b098a651c791207ac9"
+    sha256 cellar: :any,                 sonoma:        "ae767029007fe31d2a09e56d1b0a5cb1cf70a5fb2875cd1bbf19512b1f76bce8"
+    sha256 cellar: :any,                 ventura:       "7a1eb5ecb19820f37498603654dda3f5273edb563c34c8c0c2a365644a4b00f8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bd314e1ba5ece6ea102968446a3ba75a763a9e13b0d6edde4dd8117da1bd2d72"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e38c42b8385c1875ee0bc8ef075540e6e568da58c59073b4483c2789299e51da"
   end
 
   depends_on "cmake" => :build # for pyzmq

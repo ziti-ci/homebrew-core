cbc9fefe95ac573f39e3cdb5379a0833565b561a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-08 20:04:38 +0000

    grype 0.97.2

--- Formula/g/grype.rb
+++ Formula/g/grype.rb
@@ -1,8 +1,8 @@
 class Grype < Formula
   desc "Vulnerability scanner for container images and filesystems"
   homepage "https://github.com/anchore/grype"
-  url "https://github.com/anchore/grype/archive/refs/tags/v0.97.1.tar.gz"
-  sha256 "c5019e568378f851bb45b90fc53af470ec7be0886b3d62f404dc51630487b510"
+  url "https://github.com/anchore/grype/archive/refs/tags/v0.97.2.tar.gz"
+  sha256 "bea921f3470e3d8db0552be8273112c0571d44828b202177dd52963d38eb6539"
   license "Apache-2.0"
   head "https://github.com/anchore/grype.git", branch: "main"
 

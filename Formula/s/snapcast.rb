a99b339f5259f3b3e7332d7e5819a950cfb43f87
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-08 17:22:28 +0000

    snapcast 0.32.3

--- Formula/s/snapcast.rb
+++ Formula/s/snapcast.rb
@@ -1,8 +1,8 @@
 class Snapcast < Formula
   desc "Synchronous multiroom audio player"
   homepage "https://github.com/badaix/snapcast"
-  url "https://github.com/badaix/snapcast/archive/refs/tags/v0.32.2.tar.gz"
-  sha256 "881173321f5fc929319d31b5063d91a25187e61e984cdb85e57266ea5e2b3f7f"
+  url "https://github.com/badaix/snapcast/archive/refs/tags/v0.32.3.tar.gz"
+  sha256 "e53a62872d03521c7ce261378792f203d4073d769b362116ab02c98aa7c64008"
   license "GPL-3.0-or-later"
 
   bottle do

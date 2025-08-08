e14cc6b484bad2951ceaa98e6ebbdd9041d291d1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-08 16:52:06 +0000

    dish: update 1.12.0 bottle.

--- Formula/d/dish.rb
+++ Formula/d/dish.rb
@@ -7,12 +7,13 @@ class Dish < Formula
   head "https://github.com/thevxn/dish.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a5898415f5408e0f6e6518c899447db33e8948bbf63c38b831393b836deb74ff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a5898415f5408e0f6e6518c899447db33e8948bbf63c38b831393b836deb74ff"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a5898415f5408e0f6e6518c899447db33e8948bbf63c38b831393b836deb74ff"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ba62cbf941427e3887632c23ca8618f83228701f3291405c751a1f1f65aa744b"
-    sha256 cellar: :any_skip_relocation, ventura:       "ba62cbf941427e3887632c23ca8618f83228701f3291405c751a1f1f65aa744b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "75845779ff76860ac8d689de90b8058834c4e343644462ae5fd9807ba488cd82"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "19198cee1c3db608abf9fd1ac9481db5a4468f620a4cf7e74bb5034dafd90cca"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "19198cee1c3db608abf9fd1ac9481db5a4468f620a4cf7e74bb5034dafd90cca"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "19198cee1c3db608abf9fd1ac9481db5a4468f620a4cf7e74bb5034dafd90cca"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9bb4679a8537a8fe7f7c305273573950a648e2378ada306bdc9d5e0b86297948"
+    sha256 cellar: :any_skip_relocation, ventura:       "9bb4679a8537a8fe7f7c305273573950a648e2378ada306bdc9d5e0b86297948"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "edbcb986db8daaacaad96da7f82e9bdfd1efac3a7d50a4df42fbeaa9cc385042"
   end
 
   depends_on "go" => :build

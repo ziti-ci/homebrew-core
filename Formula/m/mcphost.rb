2d2c8fbce7f931b57f606d9f700f996343350ea4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-08 06:10:36 +0000

    mcphost: update 0.27.1 bottle.

--- Formula/m/mcphost.rb
+++ Formula/m/mcphost.rb
@@ -7,12 +7,12 @@ class Mcphost < Formula
   head "https://github.com/mark3labs/mcphost.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "013fabfbb5e403593f3e6ab4ab9be3aadde0f30c2274b0a42e131d881162e16d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "013fabfbb5e403593f3e6ab4ab9be3aadde0f30c2274b0a42e131d881162e16d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "013fabfbb5e403593f3e6ab4ab9be3aadde0f30c2274b0a42e131d881162e16d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8a2712f5477c0a6a0bbcc72cbfeadcff686c7eead0b51f1ddbeba9bb06490c49"
-    sha256 cellar: :any_skip_relocation, ventura:       "8a2712f5477c0a6a0bbcc72cbfeadcff686c7eead0b51f1ddbeba9bb06490c49"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "600072d0ce5ff91403d9edebcb2929fa3ad2cc8462c2709d99274b165983e131"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6734a419e180d73b5b8e9c36f0972b9d47e4b15b401086c2abc38049f7b4519f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6734a419e180d73b5b8e9c36f0972b9d47e4b15b401086c2abc38049f7b4519f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "6734a419e180d73b5b8e9c36f0972b9d47e4b15b401086c2abc38049f7b4519f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d3de4d2e87da73c0b3845ebef10170482a05fc06d78de86c5dfe330575fa0c90"
+    sha256 cellar: :any_skip_relocation, ventura:       "d3de4d2e87da73c0b3845ebef10170482a05fc06d78de86c5dfe330575fa0c90"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bba0d092c9903b1df803e97a6548b707a49721f764db3ed938bcddaf9184637f"
   end
 
   depends_on "go" => :build

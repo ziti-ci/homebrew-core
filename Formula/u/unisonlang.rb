32103470dad34029387c9516c623fa6dfe5ab82f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-08 04:36:15 +0000

    unisonlang: update 0.5.45 bottle.

--- Formula/u/unisonlang.rb
+++ Formula/u/unisonlang.rb
@@ -24,12 +24,12 @@ class Unisonlang < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83e245b75c2b0972fdc064f7fa298b5739ecc2cbd92e9d516825b4c427908f99"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3e54bf7badbf8a597c6fadd7584c2a7e1141e683e1ab57f958cdf394a1014504"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "fe00f371db67d4d50023e4243f03cdb4c9c89ab19a460f08ad50ae47cb376ac4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a323494ee601645269feee35cbec8925d8eb2f518d3866d612fce11d4962272c"
-    sha256 cellar: :any_skip_relocation, ventura:       "8ef4f0cfeec487efa69c92b0a902d5c99dc599f0ea4821ba6d5c56b9f529b153"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cdd99044a9406cf86695a55b7811566eeedcfd7e892b1cdf9b8cbf1a831be4c6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "945d09f7fb0c635556f2ad86c2da4179879578feaa4d0f9a218b4963724e6f49"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d0936bfea54838f0c03d19afcfbc79218df232a61e334cfe7ddc416e940ce6a1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9ccdf9d99dc0d1d8d8f344163ba10ef97af2d98986ba016659c0afc75468c61a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ef44ab9ea8c2d7edf6458ffb0019f5fe1ba149d23b343532c49a5bb8197bb444"
+    sha256 cellar: :any_skip_relocation, ventura:       "480af87b319f5a88ddc47fc45e08f79c1bc26b0dae7bfa7a9efa2d574b8c1b4b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b45d7ae6a36d3eff6601bb999cbdbfa7fbfb5b87150c89ead37c7ce56be9209"
   end
 
   head do

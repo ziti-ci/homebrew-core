827e9cf263b32a05207f1be201c4e1534b79d12b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-08 00:37:50 +0000

    pie: update 1.1.0 bottle.

--- Formula/p/pie.rb
+++ Formula/p/pie.rb
@@ -6,13 +6,13 @@ class Pie < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3df4fe0b33fa3f262fdc3a24d5b17c07c0ee5e507717edc3396ce1ec2041f4c3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3df4fe0b33fa3f262fdc3a24d5b17c07c0ee5e507717edc3396ce1ec2041f4c3"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3df4fe0b33fa3f262fdc3a24d5b17c07c0ee5e507717edc3396ce1ec2041f4c3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1f1a52b83bcb631dd135d9e74e573e6d74ca4b2114a93c4c2b2e81a07dd8dde2"
-    sha256 cellar: :any_skip_relocation, ventura:       "1f1a52b83bcb631dd135d9e74e573e6d74ca4b2114a93c4c2b2e81a07dd8dde2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "23f6d113f9f79fc032ef075c82723d15a6d77d1791c2e9645cd790945249f4fe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23f6d113f9f79fc032ef075c82723d15a6d77d1791c2e9645cd790945249f4fe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a6d0f318f908bcfce45c353885fd99d2ea80118dcb14a237fb9880a3f6561ce6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a6d0f318f908bcfce45c353885fd99d2ea80118dcb14a237fb9880a3f6561ce6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "a6d0f318f908bcfce45c353885fd99d2ea80118dcb14a237fb9880a3f6561ce6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "814fd72ae28944837b35b464af3590f11c2339976a5448d7be09287c7189043d"
+    sha256 cellar: :any_skip_relocation, ventura:       "814fd72ae28944837b35b464af3590f11c2339976a5448d7be09287c7189043d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "361450fe0243ae79ee9e201d014458ea433a3924822b12f15e551d701cc49543"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "361450fe0243ae79ee9e201d014458ea433a3924822b12f15e551d701cc49543"
   end
 
   depends_on "php"

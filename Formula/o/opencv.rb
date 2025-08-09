71bb177ce8a7cfcc313657a2e3e44e302fca426e
Carlo Cabrera <github@carlo.cab>
2025-08-09 17:04:26 +0800

    opencv: revision bump (abseil 20250512.1, protobuf 31.1)

--- Formula/o/opencv.rb
+++ Formula/o/opencv.rb
@@ -2,7 +2,7 @@ class Opencv < Formula
   desc "Open source computer vision library"
   homepage "https://opencv.org/"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   stable do
     url "https://github.com/opencv/opencv/archive/refs/tags/4.12.0.tar.gz"

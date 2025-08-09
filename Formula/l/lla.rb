97897abacfea4c352453f88170c29aa069652918
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-09 20:11:20 +0000

    lla 0.4.0

--- Formula/l/lla.rb
+++ Formula/l/lla.rb
@@ -1,8 +1,8 @@
 class Lla < Formula
   desc "High-performance, extensible alternative to ls"
   homepage "https://github.com/chaqchase/lla"
-  url "https://github.com/chaqchase/lla/archive/refs/tags/v0.3.11.tar.gz"
-  sha256 "dde64fa4acd90f4ae86ed485576cdfe42aab0de2cb674c88712e471968f8ae0a"
+  url "https://github.com/chaqchase/lla/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "cd357b38c609cde98ccbf31bcca86052443d8678d800eb7c06701e3900d8a6cf"
   license "MIT"
 
   bottle do

27597104d98339a0878fb31c64396c0892b2bd73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-09 03:04:42 +0000

    jd: update 2.2.4 bottle.

--- Formula/j/jd.rb
+++ Formula/j/jd.rb
@@ -7,13 +7,12 @@ class Jd < Formula
   head "https://github.com/josephburnett/jd.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3da935978b2656b746d702e3fb024c6e317708eab643319fe0dcb18f59445972"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3da935978b2656b746d702e3fb024c6e317708eab643319fe0dcb18f59445972"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3da935978b2656b746d702e3fb024c6e317708eab643319fe0dcb18f59445972"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4a8155352942e2e58c176d2f90027a98d2d995e7b185303a8dfb52f6b0e4da26"
-    sha256 cellar: :any_skip_relocation, ventura:       "4a8155352942e2e58c176d2f90027a98d2d995e7b185303a8dfb52f6b0e4da26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9536cec9c0feddf8147504272ce24a723402521666271e28161508fe44ec8db9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d3cf382ecb26b9288dedab8eb85c1464f891c81f8f90cc1f22f96b5cd6774f7b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d3cf382ecb26b9288dedab8eb85c1464f891c81f8f90cc1f22f96b5cd6774f7b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d3cf382ecb26b9288dedab8eb85c1464f891c81f8f90cc1f22f96b5cd6774f7b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c85651e38649266b6b2f7226301f614b67b34603b607d0d71fdfa74507d8a78b"
+    sha256 cellar: :any_skip_relocation, ventura:       "c85651e38649266b6b2f7226301f614b67b34603b607d0d71fdfa74507d8a78b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8300e45ccd04f69d153ef2f0a5c1482ff2f7c03a7507b78dbbf2a08a049d673"
   end
 
   depends_on "go" => :build

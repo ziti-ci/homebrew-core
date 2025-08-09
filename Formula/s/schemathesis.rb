1c801ff604ff2ebef3007eddac4c410292290bb8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-09 12:09:00 +0000

    schemathesis: update 4.0.24 bottle.

--- Formula/s/schemathesis.rb
+++ Formula/s/schemathesis.rb
@@ -8,13 +8,13 @@ class Schemathesis < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "d386805cae5d49815e6fad7d4b41086542071ace5710309eb0893c9c6f7c5270"
-    sha256 cellar: :any,                 arm64_sonoma:  "26dab61285f2ce6c578e62dcf8e3cd449d9079f275ce7f37078ced24db9a5206"
-    sha256 cellar: :any,                 arm64_ventura: "49068ae2785c73cc243c301b8db6b516039e043d60ae0fb0640252921c59fb40"
-    sha256 cellar: :any,                 sonoma:        "9d696412d4687a0cb0807f363b8988a4a5f9b1a479adb45a1a5f1a3fed248612"
-    sha256 cellar: :any,                 ventura:       "c1e9dbd5c127b8e7e5399522b07ea3c88b46f79208335ad0f7612fa78d107c69"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7bb6f5f3a7e77ad12292cbbe7e9f58601e392603711e1c6fdab89d3e1ec97220"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a40855e5f0bf26283098c5d48245e902b6b00c16af899a841a186ede5351f7c"
+    sha256 cellar: :any,                 arm64_sequoia: "39d211cf5c55b174131e81156000ae77900e58bad96b92290a2cf9d8d8990444"
+    sha256 cellar: :any,                 arm64_sonoma:  "241f1d51eb171c5c6f8cf4850c06d63e9b3c99f9880d82a8b82dbe527c7ac3b8"
+    sha256 cellar: :any,                 arm64_ventura: "8a792c697d45b7f308f69b2a4f3cdf939f4ea06d07df959ff6cbc79c67fe500e"
+    sha256 cellar: :any,                 sonoma:        "31b5cd47a9c83b36ac1e87b3b134430d7e9ed7cb07dd233719edf5d1130efeb9"
+    sha256 cellar: :any,                 ventura:       "013f72a85a8b6b20e4f0ea4b2b34bd49d5e6ebffba8404f85145a7139047c13c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "14c70b3e6e5a005b018f79567faefc882f13be0fb5cc48e056849304d8d161db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cba2118a5744bbc8d93c9fd11ba24a9d97bb2fb639b618cb4571f7076a787f3d"
   end
 
   depends_on "rust" => :build # for rpds-py

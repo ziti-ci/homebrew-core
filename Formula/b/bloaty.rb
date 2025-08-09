b53d4426c4312cb5a41cc1428bc8781038c167fe
Carlo Cabrera <github@carlo.cab>
2025-08-09 17:04:24 +0800

    bloaty: revision bump (abseil 20250512.1, protobuf 31.1)

--- Formula/b/bloaty.rb
+++ Formula/b/bloaty.rb
@@ -2,7 +2,7 @@ class Bloaty < Formula
   desc "Size profiler for binaries"
   homepage "https://github.com/google/bloaty"
   license "Apache-2.0"
-  revision 37
+  revision 38
   head "https://github.com/google/bloaty.git", branch: "main"
 
   stable do

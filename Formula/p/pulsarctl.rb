1fe970132d336921b766f8d5f7d20dd59985c187
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-11 03:04:29 +0000

    pulsarctl 4.0.6.1

--- Formula/p/pulsarctl.rb
+++ Formula/p/pulsarctl.rb
@@ -1,8 +1,8 @@
 class Pulsarctl < Formula
   desc "CLI for Apache Pulsar written in Go"
   homepage "https://streamnative.io/"
-  url "https://github.com/streamnative/pulsarctl/archive/refs/tags/v4.0.5.5.tar.gz"
-  sha256 "1afe190c4cdbeaea680e6c3e6b183dcb1a803d362f6bfd292710c5dc82b991d9"
+  url "https://github.com/streamnative/pulsarctl/archive/refs/tags/v4.0.6.1.tar.gz"
+  sha256 "cdcb4483a5d82e672d72ff58c8821854255a3555d9f39f8c86163025112f22bc"
   license "Apache-2.0"
   head "https://github.com/streamnative/pulsarctl.git", branch: "master"
 

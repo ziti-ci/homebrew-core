037faa1ceed452c11339f67a37037f8428daa749
Andrew Kane <andrew@ankane.org>
2025-08-11 12:06:25 -0700

    electric 1.1.0 (new formula)

---
+++ Formula/e/electric.rb
@@ -0,0 +1,57 @@
+class Electric < Formula
+  desc "Real-time sync for Postgres"
+  homepage "https://electric-sql.com"
+  url "https://github.com/electric-sql/electric/archive/refs/tags/@core/sync-service@1.1.0.tar.gz"
+  sha256 "58f28cf698cc7d8de8c780fe831932aa62efb207ec443b26ba46e9a0d6242a41"
+  license "Apache-2.0"
+
+  depends_on "elixir" => :build
+  depends_on "postgresql@17" => :test
+  depends_on "erlang"
+  depends_on "openssl@3"
+
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
+
+  def install
+    ENV["MIX_ENV"] = "prod"
+    ENV["MIX_TARGET"] = "application"
+
+    cd "packages/sync-service" do
+      system "mix", "deps.get"
+      system "mix", "compile"
+      system "mix", "release"
+      libexec.install Dir["_build/application_prod/rel/electric/*"]
+      bin.write_exec_script libexec.glob("bin/*")
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/electric version")
+
+    ENV["LC_ALL"] = "C"
+
+    postgresql = Formula["postgresql@17"]
+    pg_ctl = postgresql.opt_bin/"pg_ctl"
+    port = free_port
+
+    system pg_ctl, "initdb", "-D", testpath/"test"
+    (testpath/"test/postgresql.conf").write <<~EOS, mode: "a+"
+      port = #{port}
+      wal_level = logical
+    EOS
+    system pg_ctl, "start", "-D", testpath/"test", "-l", testpath/"log"
+
+    begin
+      ENV["DATABASE_URL"] = "postgres://#{ENV["USER"]}:@localhost:#{port}/postgres?sslmode=disable"
+      ENV["ELECTRIC_INSECURE"] = "true"
+      ENV["ELECTRIC_PORT"] = free_port.to_s
+      spawn bin/"electric", "start"
+
+      output = shell_output("curl -s --retry 5 --retry-connrefused localhost:#{ENV["ELECTRIC_PORT"]}/v1/health")
+      assert_match "active", output
+    ensure
+      system pg_ctl, "stop", "-D", testpath/"test"
+    end
+  end
+end

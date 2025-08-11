609fcff041322ad5dcb08c8ad8699db4b739aac5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-11 19:38:28 +0000

    storj-uplink: update 1.135.3 bottle.

--- Formula/s/storj-uplink.rb
+++ Formula/s/storj-uplink.rb
@@ -18,12 +18,12 @@ class StorjUplink < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f1421b622d75f1ec48f0b6355c9ae65dd184eb804c42414fd284c0cd9a493cc9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f1421b622d75f1ec48f0b6355c9ae65dd184eb804c42414fd284c0cd9a493cc9"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "f1421b622d75f1ec48f0b6355c9ae65dd184eb804c42414fd284c0cd9a493cc9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "66edc40ffcbe2c05dfc46bcf56789f7139db47544ef32b19b7173c46644184b8"
-    sha256 cellar: :any_skip_relocation, ventura:       "66edc40ffcbe2c05dfc46bcf56789f7139db47544ef32b19b7173c46644184b8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e149efeb60f045efaf08742c8810303ae166ecfc07d7f412b2f7c194fd612262"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0f364d06c79f749f76b1eaee8c4d8aaa206ab255e1a9c15c6cf58cf5a96cedad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0f364d06c79f749f76b1eaee8c4d8aaa206ab255e1a9c15c6cf58cf5a96cedad"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0f364d06c79f749f76b1eaee8c4d8aaa206ab255e1a9c15c6cf58cf5a96cedad"
+    sha256 cellar: :any_skip_relocation, sonoma:        "7f80cfa82a54b526da4230f921bd94dbec6edaa2bf92792d9c5771e477a9f620"
+    sha256 cellar: :any_skip_relocation, ventura:       "7f80cfa82a54b526da4230f921bd94dbec6edaa2bf92792d9c5771e477a9f620"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eaf54d1b2bbb7a277ab4617b71620a9ef91ca7170a635aa25f5138a1c1d327f9"
   end
 
   depends_on "go" => :build

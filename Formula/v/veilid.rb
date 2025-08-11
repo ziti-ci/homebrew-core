8597d073ce3ed838d0f7d622aa81f565f856f940
Mike McQuaid <mike@mikemcquaid.com>
2025-08-11 13:36:30 +0000

    veilid: update 0.4.7 bottle.

--- Formula/v/veilid.rb
+++ Formula/v/veilid.rb
@@ -7,13 +7,14 @@ class Veilid < Formula
   head "https://gitlab.com/veilid/veilid.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "32b2828d8b246267a4d35fb0aaf36f4f890edd6df535705753e08f7a2d48bafd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "44c214b57c8b84bcbf2ac61d17529181ca9cb6c9969139c99f2f0fd424a08f00"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "c0b1bd89ac7a12c5141995f66923e4450aee26ee17f146d000de335081c716ab"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bc4601ac764c6fb4ece4aac02f1790084a6394a5368219854a14950ca8c2146a"
-    sha256 cellar: :any_skip_relocation, ventura:       "f715e4fd93f6afbc3ded77efef8baf173610a148892bc7255ad7060bb11435d0"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c585f4fd38d6a3b511680ca4d2bbd43138b60d7d33a80a82768c501b3014cbe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "20e5d7f32d54285919c50a51a32e5b87918bdbf6e0ded3d36437126efdeac78c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0dfdd88564f4c28c64587507cd3c0582e8101a2b9e59a379c83e5b1847b5b1c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e496dcb522e3488234d4601c8f41ae5e1baddf820dc7b1841ba68ee9bc55c52f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "61a62594593bad1a2c49bb964b3609c31be5f0f45c488a749894b0da24dfcc20"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fb5957790aa9013587e83cff154328203222d4ac0b0e78ab442350ce63ecf16b"
+    sha256 cellar: :any_skip_relocation, ventura:       "7bf67d2351447abc43bfd388cf97219d557490cab49946edeff1b47f2af0be9a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b1e0e37857a00f7925f8755543301be83603695e1b9eed62c36d55808b1db28e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "141bf959d448b699cecdbf875cfcee61c502ae8049435ff4ad93565e3d7353f8"
   end
 
   depends_on "cmake" => :build

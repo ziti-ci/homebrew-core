c3cb39c0387ac59fedf21715fbf033ca77bfe0d1
botantony <antonsm21@gmail.com>
2025-08-11 15:08:22 +0200

    logcheck 1.4.6

--- Formula/l/logcheck.rb
+++ Formula/l/logcheck.rb
@@ -1,16 +1,14 @@
 class Logcheck < Formula
   desc "Mail anomalies in the system logfiles to the administrator"
   homepage "https://packages.debian.org/sid/logcheck"
-  url "https://deb.debian.org/debian/pool/main/l/logcheck/logcheck_1.4.5.tar.xz"
-  sha256 "7a75311d85617620d02cc027427463c7f9af2041e02fc93f5bc25d6ba3ccadf1"
+  url "https://deb.debian.org/debian/pool/main/l/logcheck/logcheck_1.4.6.tar.xz"
+  sha256 "1c038ac8bfce551e84d7be5022bfd56482f2d70ee6a8cb7a4499227f318b627d"
   license "GPL-2.0-only"
 
   livecheck do
     url "https://packages.debian.org/unstable/logcheck"
     regex(/href=.*?logcheck[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "ded893b2d0d1835e7dbb571986d842ebd1135ceac94c902bf5ce57beb4466fcc"

8685ecf77ac09fc2ae7bf6df295a101357a444bc
Patrick Linnane <patrick@linnane.io>
2025-08-12 11:41:54 -0700

    fancy-cat: revision bump (mupdf 1.26.5)

--- Formula/f/fancy-cat.rb
+++ Formula/f/fancy-cat.rb
@@ -4,7 +4,7 @@ class FancyCat < Formula
   url "https://github.com/freref/fancy-cat/archive/refs/tags/v0.4.1.tar.gz"
   sha256 "d264dbaf05f8713a4c52ce0c74a8d5e900989ec815fac1bbfec7d7b385bc1dd5"
   license "AGPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "40dab164f64cc880acbfed31805d0026c2f3f8e2e644e7c1f87b70a5a7776ccc"

3fda833a7f27edde0049af5a4273b775656b2b7a
Andrew Kane <andrew@ankane.org>
2025-08-11 18:34:36 -0700

    nessie 0.104.3 (new formula)

---
+++ Formula/n/nessie.rb
@@ -0,0 +1,36 @@
+class Nessie < Formula
+  desc "Transactional Catalog for Data Lakes with Git-like semantics"
+  homepage "https://projectnessie.org"
+  url "https://github.com/projectnessie/nessie/archive/refs/tags/nessie-0.104.3.tar.gz"
+  sha256 "67ed6221ebceb3d55ed332e5461842bc3ca36273fd164d02d55498fce6af13ef"
+  license "Apache-2.0"
+
+  depends_on "gradle@8" => :build
+  # The build fails with more recent JDKs
+  # See: https://github.com/projectnessie/nessie/issues/11145
+  depends_on "openjdk@21"
+
+  def install
+    ENV["JAVA_HOME"] = Language::Java.java_home("21")
+    system "gradle", ":nessie-quarkus:assemble"
+    libexec.install Dir["servers/quarkus-server/build/quarkus-app/*"]
+    bin.write_jar_script libexec/"quarkus-run.jar", "nessie"
+  end
+
+  service do
+    run [opt_bin/"nessie"]
+    keep_alive true
+    error_log_path var/"log/nessie.log"
+    log_path var/"log/nessie.log"
+  end
+
+  test do
+    port = free_port
+    ENV["QUARKUS_HTTP_PORT"] = free_port.to_s
+    ENV["QUARKUS_MANAGEMENT_PORT"] = port.to_s
+    spawn bin/"nessie"
+
+    output = shell_output("curl -s --retry 5 --retry-connrefused localhost:#{port}/q/health")
+    assert_match "UP", output
+  end
+end

dcbf3db6922c62762bf712531daa124068295934
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-12 17:16:10 +0000

    icloudpd 1.29.4

--- Formula/i/icloudpd.rb
+++ Formula/i/icloudpd.rb
@@ -5,8 +5,8 @@ class Icloudpd < Formula
   homepage "https://github.com/icloud-photos-downloader/icloud_photos_downloader"
   # We use a git checkout as scripts/patch_version runs git commands to update SHA
   url "https://github.com/icloud-photos-downloader/icloud_photos_downloader.git",
-      tag:      "v1.29.3",
-      revision: "a0ef4d4024018b90c4272d5122287618106542b4"
+      tag:      "v1.29.4",
+      revision: "7483c4e53e01723bcbc19b4a857ebef0680d2724"
   license "MIT"
   head "https://github.com/icloud-photos-downloader/icloud_photos_downloader.git", branch: "master"
 

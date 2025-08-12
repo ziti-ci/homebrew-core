2752f1d5d175ca45ff69a9d6ed326cf00a1792a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-12 08:19:53 +0000

    localai 3.4.0

--- Formula/l/localai.rb
+++ Formula/l/localai.rb
@@ -1,10 +1,8 @@
 class Localai < Formula
-  include Language::Python::Virtualenv
-
   desc "OpenAI alternative"
   homepage "https://localai.io"
-  url "https://github.com/mudler/LocalAI/archive/refs/tags/v3.3.2.tar.gz"
-  sha256 "e8c10990e41f4e1a89fecfa5eb26a5ad555f904bbb08867171a05c5dd715c642"
+  url "https://github.com/mudler/LocalAI/archive/refs/tags/v3.4.0.tar.gz"
+  sha256 "a3393c91b777c93bfcbe04557e823a4d52dc130669e505c2dba2633db37adef0"
   license "MIT"
   head "https://github.com/mudler/LocalAI.git", branch: "master"
 
@@ -17,34 +15,13 @@ class Localai < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "d2248f7b16ac7ec5f69c546d5a1c77548c11a4ea266f44d6e9e6b3f54ca0975b"
   end
 
-  depends_on "abseil" => :build
-  depends_on "cmake" => :build
   depends_on "go" => :build
-  depends_on "go-rice" => :build
-  depends_on "grpc" => :build
   depends_on "protobuf" => :build
   depends_on "protoc-gen-go" => :build
   depends_on "protoc-gen-go-grpc" => :build
-  depends_on "python@3.13" => :build
-
-  resource "grpcio-tools" do
-    url "https://files.pythonhosted.org/packages/90/c8/bca79cb8c14bb63027831039919c801db9f593c7504c09433934f5dff6a4/grpcio_tools-1.74.0.tar.gz"
-    sha256 "88ab9eb18b6ac1b4872add6b394073bd8d44eee7c32e4dc60a022e25ffaffb95"
-  end
-
-  def python3
-    which("python3.13")
-  end
 
   def install
-    # Fix to CMake Error at encodec.cpp/ggml/CMakeLists.txt:1 (cmake_minimum_required):
-    ENV["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
-
     ENV["SDKROOT"] = MacOS.sdk_path if OS.mac?
-
-    venv = virtualenv_create(buildpath/"venv", python3)
-    venv.pip_install resources
-    ENV.prepend_path "PATH", venv.root/"bin"
 
     system "make", "build", "VERSION=#{version}"
     bin.install "local-ai"

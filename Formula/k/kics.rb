31cdb8c9665af26198e4a70ce4133d3aed1e132c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-12 14:20:57 +0000

    kics 2.1.13

--- Formula/k/kics.rb
+++ Formula/k/kics.rb
@@ -1,8 +1,8 @@
 class Kics < Formula
   desc "Detect vulnerabilities, compliance issues, and misconfigurations"
   homepage "https://kics.io/"
-  url "https://github.com/Checkmarx/kics/archive/refs/tags/v2.1.12.tar.gz"
-  sha256 "b0ed31d1a5055fa52fb346beb309c3084794219ad38400aee5c17e61046ac5ae"
+  url "https://github.com/Checkmarx/kics/archive/refs/tags/v2.1.13.tar.gz"
+  sha256 "f3d775755d8ca0de8dfc7bef23aa11e46a5e269c438f8f732a30805ca9eba08f"
   license "Apache-2.0"
   head "https://github.com/Checkmarx/kics.git", branch: "master"
 

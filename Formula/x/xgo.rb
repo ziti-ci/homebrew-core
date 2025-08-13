826bb68a38801a499d3e229c98303feec64d07b9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-13 05:45:37 +0000

    xgo 1.5.1

--- Formula/x/xgo.rb
+++ Formula/x/xgo.rb
@@ -1,8 +1,8 @@
 class Xgo < Formula
   desc "AI-native programming language that integrates software engineering"
   homepage "https://xgo.dev/"
-  url "https://github.com/goplus/xgo/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "e7d80a6760f794ffb4f30c89097b8903f8a1e8c6b60706673d88bcdc304f05c8"
+  url "https://github.com/goplus/xgo/archive/refs/tags/v1.5.1.tar.gz"
+  sha256 "838bb0a6145f7fd475908378adce274a6d5f7ee43ec78496738302aa89a1044a"
   license "Apache-2.0"
   head "https://github.com/goplus/xgo.git", branch: "main"
 

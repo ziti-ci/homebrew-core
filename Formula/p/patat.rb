48750a885b59cf5d5885eb3099aa24eff7abde3e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-13 05:31:10 +0000

    patat 0.15.2.0

--- Formula/p/patat.rb
+++ Formula/p/patat.rb
@@ -1,8 +1,8 @@
 class Patat < Formula
   desc "Terminal-based presentations using Pandoc"
   homepage "https://github.com/jaspervdj/patat"
-  url "https://hackage.haskell.org/package/patat-0.15.1.0/patat-0.15.1.0.tar.gz"
-  sha256 "390b15031d20f8bd9d03e51e11bd9248af00aa437930300be34668b458be1ed9"
+  url "https://hackage.haskell.org/package/patat-0.15.2.0/patat-0.15.2.0.tar.gz"
+  sha256 "d1f182ecdf145b8db1aacee1c4d46731d197b192e6ef855c3505067c1cea2b65"
   license "GPL-2.0-or-later"
   head "https://github.com/jaspervdj/patat.git", branch: "main"
 

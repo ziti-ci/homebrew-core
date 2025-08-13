1f6afaa7a406bdbee381d615802b1ea89d30db9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-13 20:18:40 +0000

    apify-cli: update 1.1.0 bottle.

--- Formula/a/apify-cli.rb
+++ Formula/a/apify-cli.rb
@@ -8,13 +8,7 @@ class ApifyCli < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c999d3036f54eda5b753f86fc5e43ecea275982b44a58b4cd86443d5c092186f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c999d3036f54eda5b753f86fc5e43ecea275982b44a58b4cd86443d5c092186f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "c999d3036f54eda5b753f86fc5e43ecea275982b44a58b4cd86443d5c092186f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7d5cf7b00015b1e67d9ae7615a2ead1682ece9b4f18ef50db8125bdf64de3f74"
-    sha256 cellar: :any_skip_relocation, ventura:       "7d5cf7b00015b1e67d9ae7615a2ead1682ece9b4f18ef50db8125bdf64de3f74"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8c7c2a860806bc3aa3b27f751a0f50e914c65964eef40a536925e0160f34339c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c7c2a860806bc3aa3b27f751a0f50e914c65964eef40a536925e0160f34339c"
+    sha256 cellar: :any_skip_relocation, all: "d575f7afa26af8a331af00b1834a442c64684cd9df3b92d7548f46fa3279e855"
   end
 
   depends_on "node"

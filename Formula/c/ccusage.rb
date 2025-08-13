baa768a752ff139722c8e0575ef16aea23b69a63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-13 11:18:54 +0000

    ccusage: update 15.9.5 bottle.

--- Formula/c/ccusage.rb
+++ Formula/c/ccusage.rb
@@ -6,13 +6,13 @@ class Ccusage < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7e857dd287ef706bef881d9b5cde8b6262d7fdcd0e0d63c6c22be40c78d7e995"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7e857dd287ef706bef881d9b5cde8b6262d7fdcd0e0d63c6c22be40c78d7e995"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "7e857dd287ef706bef881d9b5cde8b6262d7fdcd0e0d63c6c22be40c78d7e995"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e5a352f8cf8bb5c2808f9384e15ef5aa4f43fc182ec6ca13fb5d797af8ee7b49"
-    sha256 cellar: :any_skip_relocation, ventura:       "e5a352f8cf8bb5c2808f9384e15ef5aa4f43fc182ec6ca13fb5d797af8ee7b49"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7e857dd287ef706bef881d9b5cde8b6262d7fdcd0e0d63c6c22be40c78d7e995"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e857dd287ef706bef881d9b5cde8b6262d7fdcd0e0d63c6c22be40c78d7e995"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f3cfb442f2e2c567e8868764c0b130efc6b35c44ce6a8baedac5734ffcaa3461"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f3cfb442f2e2c567e8868764c0b130efc6b35c44ce6a8baedac5734ffcaa3461"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f3cfb442f2e2c567e8868764c0b130efc6b35c44ce6a8baedac5734ffcaa3461"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5337c3f2e0327edc97385ab02df7df7e377a54df6a6ead6fb3b64916c3cc04a2"
+    sha256 cellar: :any_skip_relocation, ventura:       "5337c3f2e0327edc97385ab02df7df7e377a54df6a6ead6fb3b64916c3cc04a2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f3cfb442f2e2c567e8868764c0b130efc6b35c44ce6a8baedac5734ffcaa3461"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3cfb442f2e2c567e8868764c0b130efc6b35c44ce6a8baedac5734ffcaa3461"
   end
 
   depends_on "node"

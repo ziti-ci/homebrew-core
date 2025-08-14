810edec58ca4d572e57b4c80f6e33aafd91ba419
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-14 23:19:28 +0000

    bosh-cli: update 7.9.9 bottle.

--- Formula/b/bosh-cli.rb
+++ Formula/b/bosh-cli.rb
@@ -7,12 +7,12 @@ class BoshCli < Formula
   head "https://github.com/cloudfoundry/bosh-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "05057d47330b5a0022899e2f87d05a1beedc70324f85dc7d9a20db9ed4e04b8f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "05057d47330b5a0022899e2f87d05a1beedc70324f85dc7d9a20db9ed4e04b8f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "05057d47330b5a0022899e2f87d05a1beedc70324f85dc7d9a20db9ed4e04b8f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5e63149ade9a7425be5b0f11a2155291435f61eb92e4e881660d3c9f1b8f51a3"
-    sha256 cellar: :any_skip_relocation, ventura:       "5e63149ade9a7425be5b0f11a2155291435f61eb92e4e881660d3c9f1b8f51a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "998f063c25c4986e4f517c4ae075ef88c209f36f029c3024275f94d4c3e11315"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "015c7ab7ecff6f256196a7937daf3b6b1e3054a43a1a6f79937ac94d67771cd2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "015c7ab7ecff6f256196a7937daf3b6b1e3054a43a1a6f79937ac94d67771cd2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "015c7ab7ecff6f256196a7937daf3b6b1e3054a43a1a6f79937ac94d67771cd2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "54f2293f7cecdecf17fb3ef4437b58925e7f1eeb22198328268bab773dacf2e4"
+    sha256 cellar: :any_skip_relocation, ventura:       "54f2293f7cecdecf17fb3ef4437b58925e7f1eeb22198328268bab773dacf2e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a58dda27bc00447698186fc2a7b8d4be7281637716e6a822372dba6618bb2e6a"
   end
 
   depends_on "go" => :build

ff47142ce60d2f2207b08e9b75ef1d6fb5b0f0a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-14 04:36:00 +0000

    gromacs: update 2025.2 bottle.

--- Formula/g/gromacs.rb
+++ Formula/g/gromacs.rb
@@ -11,13 +11,13 @@ class Gromacs < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "bf9e1f90f23301105b419d8b5a9e13f76c7ae11a80803ae31a258ac6eb0eb76a"
-    sha256 arm64_sonoma:  "9be83042cdc1753bc52e215d78bd571a3257265f5befb8a0d28316003bfa9046"
-    sha256 arm64_ventura: "542b7cc88c9bee42687dbe6a9e07e9ab3c89222402f2101cc3d31464a6d4b560"
-    sha256 sonoma:        "67b0c64976ae384d3043565a7137f086c9d7997cce11f0ec79ba18d8787c7b5e"
-    sha256 ventura:       "b48883b24262326dba96ad2392bd1bb9a2b221eea6a671bb1e4e067ead3a5a06"
-    sha256 arm64_linux:   "c8a2701c95dd18c8e208961a8490dac0faa57d77ee3d605b5bb1be93b3ecea53"
-    sha256 x86_64_linux:  "c6614ae480f0635f9ec6f1cfaa8baf37e813aaa737e95ad585ce2e6a08d0c136"
+    sha256 arm64_sequoia: "280c30eef948fc0e30ed6d46c9799e89036f1308fbf0f2b1be73c852b91dea73"
+    sha256 arm64_sonoma:  "10e8472fcbe3888c61f80d3ecc51ff4aa1162a8956362191be160b94232dc4a4"
+    sha256 arm64_ventura: "b0f17f247bec1b24026a87c8cadf9eb313e1acdd10c49588c2c8c9e3ac945832"
+    sha256 sonoma:        "635b9934811e55ca1be5e22abdf94ae5c47fe81b24cc5b8efef8f7d6f012b3f9"
+    sha256 ventura:       "184a9226beada9b7e8116fc658030af7baa89ca30cb4fe0a15c470de7a19b174"
+    sha256 arm64_linux:   "20c104aa1ee12c0f0181ba37bd38ed97d16859dd0764cfb1929d75a00439cdff"
+    sha256 x86_64_linux:  "0cbebc9d0041f0ff51839ba8d202177a11ac79d475679e7fd0624abff06a896d"
   end
 
   depends_on "cmake" => :build

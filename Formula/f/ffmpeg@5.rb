f0480c16024b29de33bbfa787c7c779277323d0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-14 02:43:13 +0000

    ffmpeg@5 5.1.7

--- Formula/f/ffmpeg@5.rb
+++ Formula/f/ffmpeg@5.rb
@@ -1,12 +1,11 @@
 class FfmpegAT5 < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-5.1.6.tar.xz"
-  sha256 "f4fa066278f7a47feab316fef905f4db0d5e9b589451949740f83972b30901bd"
+  url "https://ffmpeg.org/releases/ffmpeg-5.1.7.tar.xz"
+  sha256 "27d87965c5b0ab857a0092aeb9f55d975becb7126d83aefe39ae24102492180b"
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
-  revision 7
 
   livecheck do
     url "https://ffmpeg.org/download.html"

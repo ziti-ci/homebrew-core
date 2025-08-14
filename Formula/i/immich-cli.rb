6e1882eb75f21277e87940f16620ce4d59399b5a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-14 20:06:02 +0000

    immich-cli 2.2.78

--- Formula/i/immich-cli.rb
+++ Formula/i/immich-cli.rb
@@ -1,8 +1,8 @@
 class ImmichCli < Formula
   desc "Command-line interface for self-hosted photo manager Immich"
   homepage "https://immich.app/docs/features/command-line-interface"
-  url "https://registry.npmjs.org/@immich/cli/-/cli-2.2.77.tgz"
-  sha256 "d06e8e8ca9118a022b637b88704a5e0b02d04e2d7fc4c2e89cf3682abf027f08"
+  url "https://registry.npmjs.org/@immich/cli/-/cli-2.2.78.tgz"
+  sha256 "a65bf17ac0879fd6710f4e5eb5f3f7cfab2b01d5643fa153c336d7f75071569e"
   license "AGPL-3.0-only"
 
   bottle do

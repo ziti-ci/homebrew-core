6a0794332c174b38d2d7b42bf411822b076fd093
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-15 11:04:05 +0000

    karmadactl 1.14.3

--- Formula/k/karmadactl.rb
+++ Formula/k/karmadactl.rb
@@ -1,8 +1,8 @@
 class Karmadactl < Formula
   desc "CLI for Karmada control plane"
   homepage "https://karmada.io/"
-  url "https://github.com/karmada-io/karmada/archive/refs/tags/v1.14.2.tar.gz"
-  sha256 "fa338d62bfdec97065ef5c53c09c35b5ea59b8faa72df79e5e42605219cbe5b6"
+  url "https://github.com/karmada-io/karmada/archive/refs/tags/v1.14.3.tar.gz"
+  sha256 "e472876ea528042838f431ec94a1186b2bf4dc9c8ca3303e26e8830c9e32d83b"
   license "Apache-2.0"
   head "https://github.com/karmada-io/karmada.git", branch: "master"
 

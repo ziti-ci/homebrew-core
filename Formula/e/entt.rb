a7ec1dc712c655a43ce5448c621f79120d36c334
botantony <antonsm21@gmail.com>
2025-08-15 03:44:37 +0200

    entt 3.15.0 (new formula)
    Signed-off-by: botantony <antonsm21@gmail.com>

---
+++ Formula/e/entt.rb
@@ -0,0 +1,48 @@
+class Entt < Formula
+  desc "Fast and reliable entity-component system for C++"
+  homepage "https://github.com/skypjack/entt/wiki"
+  url "https://github.com/skypjack/entt/archive/refs/tags/v3.15.0.tar.gz"
+  sha256 "01466fcbf77618a79b62891510c0bbf25ac2804af5751c84982b413852234d66"
+  license "MIT"
+  head "https://github.com/skypjack/entt.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build
+  depends_on "pkgconf" => :test
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DENTT_INSTALL=ON", "-DENTT_BUILD_DOCS=ON", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <iostream>
+      #include <entt/entt.hpp>
+
+      struct Position { float x, y; };
+
+      int main() {
+        entt::registry registry;
+
+        auto entity = registry.create();
+        registry.emplace<Position>(entity, 1.0f, 2.0f);
+
+        auto e2 = registry.create();
+        registry.emplace<Position>(e2, -3.0f, 4.5f);
+
+        auto view = registry.view<Position>();
+        for (auto entity : view) {
+          auto &pos = view.get<Position>(entity);
+          std::cout << int(entity) << std::endl;
+        }
+
+        return 0;
+      }
+    CPP
+    pkg_config_flags = shell_output("pkgconf --cflags --libs entt").chomp.split
+    system ENV.cxx, "-std=c++17", *pkg_config_flags, "test.cpp", "-o", "test"
+    system "./test"
+  end
+end

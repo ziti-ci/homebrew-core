63d8023dc3daed636662bad56241acb109cecafc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-18 18:33:35 +0000

    nova-fairwinds: update 3.11.8 bottle.

--- Formula/n/nova-fairwinds.rb
+++ Formula/n/nova-fairwinds.rb
@@ -7,12 +7,12 @@ class NovaFairwinds < Formula
   head "https://github.com/FairwindsOps/nova.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "52dc1850073c9cf0b7d0df2c3072b4dcca7609f600da0b22fabd6296299b8052"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "52dc1850073c9cf0b7d0df2c3072b4dcca7609f600da0b22fabd6296299b8052"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "52dc1850073c9cf0b7d0df2c3072b4dcca7609f600da0b22fabd6296299b8052"
-    sha256 cellar: :any_skip_relocation, sonoma:        "cad8ba55c5c677aaf0db53b12016b74f66d029384a38f20b2f1920c670990a12"
-    sha256 cellar: :any_skip_relocation, ventura:       "cad8ba55c5c677aaf0db53b12016b74f66d029384a38f20b2f1920c670990a12"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "328d1ae14ca7cf4c0b5b1051b058010394e0d7c18c663985d6a709eb79a6ea10"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0307938adcbe20d13815700636303b9d9472f21a6b7b3988db8dba15dd96932d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0307938adcbe20d13815700636303b9d9472f21a6b7b3988db8dba15dd96932d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0307938adcbe20d13815700636303b9d9472f21a6b7b3988db8dba15dd96932d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "11f78c3a77156aa1e057d30e684cb39c1545173a48a45706ecdf28b356d8ee93"
+    sha256 cellar: :any_skip_relocation, ventura:       "11f78c3a77156aa1e057d30e684cb39c1545173a48a45706ecdf28b356d8ee93"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a73502a71157b399cd366debb4c8bf671fe4fe7c4ad20251677bba8f64f1636"
   end
 
   depends_on "go" => :build

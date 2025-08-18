bec048938239ec835d4b7db6b4cdf2fb21579a5a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-18 17:11:44 +0000

    jetty 12.1.0

--- Formula/j/jetty.rb
+++ Formula/j/jetty.rb
@@ -1,8 +1,8 @@
 class Jetty < Formula
   desc "Java servlet engine and webserver"
   homepage "https://jetty.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-home/12.0.25/jetty-home-12.0.25.tar.gz"
-  sha256 "adb2c23f410f3e2a5a34c3edc9e94b2282bb44c1c067d23a9de57a06919a7167"
+  url "https://search.maven.org/remotecontent?filepath=org/eclipse/jetty/jetty-home/12.1.0/jetty-home-12.1.0.tar.gz"
+  sha256 "c154adbac11446368162887fdfbbceecfbdd3455eb9fbacb71c721586633cfef"
   license any_of: ["Apache-2.0", "EPL-2.0"]
 
   livecheck do

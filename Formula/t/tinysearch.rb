4be5f4da69aaeb408d7c447fdad667d9b3390c65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-18 14:29:17 +0000

    tinysearch: update 0.9.0 bottle.

--- Formula/t/tinysearch.rb
+++ Formula/t/tinysearch.rb
@@ -7,17 +7,13 @@ class Tinysearch < Formula
   head "https://github.com/tinysearch/tinysearch.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "604da29b5763cbd83ee3851adf412ebc0683a39f82c4f6fb5db430c21e5804b8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "22ba810fa21cf5ed9b4b3da171f5c528812ee9a7347ed87f658dd9c6cb930b22"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d3112dab46ff4f6ebfcd130e186cca018d7e72cf503a6a74b8e632dfe30ff147"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e6419da17e1e7c0f7bc74eeefd60d9fec429f9a351bfa6732336207da27ba62e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a3f5895d2f977f0c32c7a80798922f56ea1f628bb6522de0243a74ef9ee6635"
-    sha256 cellar: :any_skip_relocation, sonoma:         "0150d38ad3672b677894cb99071ad87464ee130a5aca00360197fb3cc488afab"
-    sha256 cellar: :any_skip_relocation, ventura:        "149fcc1be09119c692b20381ed2646170c004909a1998ddab8b5e451d19e6c0c"
-    sha256 cellar: :any_skip_relocation, monterey:       "bb5e206aff6a7ffbf2bf1da5dbb9907e6e72873ad507059f53699984080e6fe6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0a256b304d49a871b20915721fde4aea47ef132afa5cd537c6bfba36b8ff24d9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:    "a3357914b084ecdc5935888629a57cea9b210b263ea6f6c9a53f749d220bf410"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "741bdce133c7ac5a2fecddb639fa17b3d7b11c3a2adda9f7998bf69e5b2376ce"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "72284640a7b7d46057e20a316dc5a443e8b9aa1c8a4bcb3ecb769b4bd826dd33"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c07fe020900cdd4873f466b5bd266647d5995fd4f3e97a4613bada184b5a053c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1cf4d0ef6810420b5dff9e7cb17d2527157bce268352a8912ec21f987fa4d4ee"
+    sha256 cellar: :any_skip_relocation, sonoma:        "631d5a84525d68b93da843f511b1747960f3e165afd13bfbf3a201bac07bf880"
+    sha256 cellar: :any_skip_relocation, ventura:       "ba42e3aaa2d14dbd07e47156d5453e67164dd05c92032fde2077fe3368ec46a2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7003b0b7c5f9e72a6ca6b6c3f52efd569aeefa3f31c14278c762895190dc4ac4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed0c139aaab9231c38cad560da0f9a7c44b4a148b440660655607a06d379996b"
   end
 
   depends_on "rust" => :build

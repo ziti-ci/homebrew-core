76f42dc8a89a3106b0f045514fc13adbfc4dee93
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-08-18 15:12:18 +0000

    maeparser: update 1.3.3_1 bottle.

--- Formula/m/maeparser.rb
+++ Formula/m/maeparser.rb
@@ -13,6 +13,7 @@ class Maeparser < Formula
     sha256 cellar: :any,                 arm64_ventura: "465ec71b1618c4c650aaf44b30de39acda2f156652deac0e8961fd02efe64fb7"
     sha256 cellar: :any,                 sonoma:        "8e78db80f19badb63e39b407437e2ffe56b37a29f27257b359e67862a34f803b"
     sha256 cellar: :any,                 ventura:       "c98342065d18d6e1c4040975a8ca4bb911fb0a34b58f67c70f641e271a06d5e2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b9a5c6cfa985befb79739131b65496d5f6bfdead69ed569903168a6a0950a5bd"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "c50f7c93b5fb8354aee48e6952bc1df0170a3a8a1a6911a0582ad9c8091d097a"
   end
 

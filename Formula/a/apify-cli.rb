c150ecb7bbe5512b7faaf882ec77f937ca5fc776
Apify Service Account <apify-service-account@users.noreply.github.com>
2025-08-18 19:16:54 +0000

    apify-cli 1.1.1

--- Formula/a/apify-cli.rb
+++ Formula/a/apify-cli.rb
@@ -1,8 +1,8 @@
 class ApifyCli < Formula
   desc "Apify command-line interface"
   homepage "https://docs.apify.com/cli/"
-  url "https://registry.npmjs.org/apify-cli/-/apify-cli-1.1.0.tgz"
-  sha256 "1a1603110bb65002d155efc208ea6f628157157f02ae533891295a3b2072436b"
+  url "https://registry.npmjs.org/apify-cli/-/apify-cli-1.1.1.tgz"
+  sha256 "00e4827b9d4995263ca008b0ee1e6fa1bad556d5e01e422407c25302a48eb92d"
   license "Apache-2.0"
 
   no_autobump! because: :bumped_by_upstream

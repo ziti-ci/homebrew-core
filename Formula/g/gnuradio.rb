c23666357345380fb6de77db772ea252e5908f4e
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-08-18 13:18:14 +0000

    gnuradio: update 3.10.12.0_2 bottle.

--- Formula/g/gnuradio.rb
+++ Formula/g/gnuradio.rb
@@ -15,12 +15,12 @@ class Gnuradio < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "764d4cd2ae0dfd4d3bdb4fb3b7c7bcb947f7758449ecb38f1daca1383a0ee748"
-    sha256 cellar: :any,                 arm64_sonoma:  "f5d8e2235a7f96d47fa52aadb21bd7f0b5b533368944b8c9d86aff7cda853fce"
-    sha256 cellar: :any,                 arm64_ventura: "ef06ea9e6e2feb630198c086ebadd974a76e1e293cdc1d05ed8ae4030f5c00b9"
-    sha256 cellar: :any,                 sonoma:        "c0d8795ae4992f9b44626822118245016c91312242e778a13b94573cdbeefc3b"
-    sha256 cellar: :any,                 ventura:       "6114da91738bef6577e4e573d11502d7b3d51355cac0ede95433a18a730e6d94"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b0357eed145763df02fc65f861c55754cf5f9f2f9ad6c7cd32f09afbd51bb78b"
+    sha256 cellar: :any,                 arm64_sequoia: "4fd1d52c2df1c3a1ca49301f6d92d8d577be909d0db7349c8714e45a552b2953"
+    sha256 cellar: :any,                 arm64_sonoma:  "fecac36b9ee8a9ea25eaf20a84c775f3f5a80ff80609a79492e26cd99284e4fe"
+    sha256 cellar: :any,                 arm64_ventura: "4d535ea4e010f39c793528e51b23cd75257aed3d25f984d797d55c8006485f70"
+    sha256 cellar: :any,                 sonoma:        "d7ab6a70a8546d2ec175af0bee3ea0ed0ea42286ebb1893ea0c8ada0eea2492b"
+    sha256 cellar: :any,                 ventura:       "c8bffed8f469fca53d642a7464bed852e5cd3178026f3de6a711f5ac8c2c6a1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e41ebb33ebe5d51963b0fd34844e088d10c907b925458e56756a4f73fa9ccfc"
   end
 
   depends_on "cmake" => :build

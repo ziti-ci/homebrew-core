e5172300eaebba11ba5f15ef3d64847d3e96516b
Michael Cho <michael@michaelcho.dev>
2025-08-18 13:05:10 -0400

    s2geometry: revision bump (abseil 20250814.0)

--- Formula/s/s2geometry.rb
+++ Formula/s/s2geometry.rb
@@ -4,7 +4,7 @@ class S2geometry < Formula
   url "https://github.com/google/s2geometry/archive/refs/tags/v0.12.0.tar.gz"
   sha256 "c09ec751c3043965a0d441e046a73c456c995e6063439a72290f661c1054d611"
   license "Apache-2.0"
-  revision 2
+  revision 3
 
   livecheck do
     url :homepage
@@ -25,6 +25,10 @@ class S2geometry < Formula
   depends_on "abseil"
   depends_on "glog"
   depends_on "openssl@3"
+
+  # Backport support for Abseil >= 20250814.0 from
+  # https://github.com/google/s2geometry/commit/9bdfdc0db978fb9d6753a880042a65ed5e83eabe
+  patch :DATA
 
   def install
     args = %W[
@@ -92,3 +96,67 @@ class S2geometry < Formula
     system "./test"
   end
 end
+
+__END__
+diff --git a/src/s2/s2density_tree.cc b/src/s2/s2density_tree.cc
+index b1892be380d15c55866630619405fd3b619a8ac5..c40964740cc49a72e613c1f0023c4a6400f66a8a 100644
+--- a/src/s2/s2density_tree.cc
++++ b/src/s2/s2density_tree.cc
+@@ -911,7 +911,7 @@ const S2DensityTree::Cell* S2DensityTree::DecodedPath::LoadCell(
+   return cell;
+ }
+ 
+-S2DensityTree S2DensityTree::Normalize(absl::Nonnull<S2Error*> error) const {
++S2DensityTree S2DensityTree::Normalize(S2Error* absl_nonnull error) const {
+   *error = S2Error::Ok();
+ 
+   DecodedPath path(this);
+@@ -950,7 +950,7 @@ S2DensityTree S2DensityTree::Normalize(absl::Nonnull<S2Error*> error) const {
+   return tree;
+ }
+ 
+-S2CellUnion S2DensityTree::Leaves(absl::Nonnull<S2Error*> error) const {
++S2CellUnion S2DensityTree::Leaves(S2Error* absl_nonnull error) const {
+   std::vector<S2CellId> leaves;
+ 
+   VisitCells(
+diff --git a/src/s2/s2density_tree.h b/src/s2/s2density_tree.h
+index a6b7a48d8a1b549d16e7d2597ccb207b2e1038bf..249438e00fc6edae01d103885cf8f0068cb03649 100644
+--- a/src/s2/s2density_tree.h
++++ b/src/s2/s2density_tree.h
+@@ -272,11 +273,11 @@ class S2DensityTree {
+   // weight is scaled by (its parent's weight / the sum of weights of the node
+   // and its siblings). This makes the weight of a parent equal to the sum of
+   // its children.
+-  S2DensityTree Normalize(absl::Nonnull<S2Error*> error) const;
++  S2DensityTree Normalize(S2Error* absl_nonnull error) const;
+ 
+   // Returns an S2CellUnion containing the leaves of this tree.  The cell union
+   // is not necessarily normalized.
+-  S2CellUnion Leaves(absl::Nonnull<S2Error*> error) const;
++  S2CellUnion Leaves(S2Error* absl_nonnull error) const;
+ 
+   // The decoded weight and offsets of encoded cells.
+   class Cell {
+diff --git a/src/s2/s2edge_tessellator.h b/src/s2/s2edge_tessellator.h
+index 513afbdc52eb621aa42d8d30b8383950da455dd4..b55d1028c1fff298816e4af94f68590eed2e4a46 100644
+--- a/src/s2/s2edge_tessellator.h
++++ b/src/s2/s2edge_tessellator.h
+@@ -44,7 +45,7 @@ class S2EdgeTessellator {
+   // ------------------|------------------------|-----------------------
+   // AppendProjected   | S2 geodesics           | Planar projected edges
+   // AppendUnprojected | Planar projected edges | S2 geodesics
+-  S2EdgeTessellator(absl::Nonnull<const S2::Projection*> projection,
++  S2EdgeTessellator(const S2::Projection* absl_nonnull projection,
+                     S1Angle tolerance);
+ 
+   // Converts the spherical geodesic edge AB to a chain of planar edges in the
+@@ -95,7 +96,7 @@ class S2EdgeTessellator {
+                        const R2Point& pb, const S2Point& b,
+                        std::vector<R2Point>* vertices) const;
+ 
+-  absl::Nonnull<const S2::Projection*> proj_;
++  const S2::Projection* absl_nonnull proj_;
+ 
+   // The given tolerance scaled by a constant fraction so that it can be
+   // compared against the result returned by EstimateMaxError().

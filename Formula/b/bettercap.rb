c913d0d85f51c9370711f0ab582a991ef91c42b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-18 13:57:04 +0000

    bettercap 2.41.2

--- Formula/b/bettercap.rb
+++ Formula/b/bettercap.rb
@@ -1,8 +1,8 @@
 class Bettercap < Formula
   desc "Swiss army knife for network attacks and monitoring"
   homepage "https://www.bettercap.org/"
-  url "https://github.com/bettercap/bettercap/archive/refs/tags/v2.41.1.tar.gz"
-  sha256 "c00a489110a01b799796bfc5701bbaea882e0a1aa675d16ce2aba25bd0d71ad1"
+  url "https://github.com/bettercap/bettercap/archive/refs/tags/v2.41.2.tar.gz"
+  sha256 "990fe894259b0129ad1e331d020b151f1494f850ef25be3e110e8ddc9525b79f"
   license "GPL-3.0-only"
   head "https://github.com/bettercap/bettercap.git", branch: "master"
 

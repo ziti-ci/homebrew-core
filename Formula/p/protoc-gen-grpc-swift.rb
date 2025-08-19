d6797ad53e139979bb9572aff05b824f00fc4a54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-19 11:17:34 +0000

    protoc-gen-grpc-swift 2.1.0

--- Formula/p/protoc-gen-grpc-swift.rb
+++ Formula/p/protoc-gen-grpc-swift.rb
@@ -1,8 +1,8 @@
 class ProtocGenGrpcSwift < Formula
   desc "Protoc plugin for generating gRPC Swift stubs"
   homepage "https://github.com/grpc/grpc-swift-protobuf"
-  url "https://github.com/grpc/grpc-swift-protobuf/archive/refs/tags/2.0.0.tar.gz"
-  sha256 "308e62a9160603310f055a8fa02484f80e245ad49e094cef4193b520a1736adf"
+  url "https://github.com/grpc/grpc-swift-protobuf/archive/refs/tags/2.1.0.tar.gz"
+  sha256 "4d31d1c96e361a01443cb4037293d3fc586568e9bfca13fd7290bc3e20f0ecb8"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/grpc/grpc-swift-protobuf.git", branch: "main"

932a0ef7136b7838bbc24850ed679a6a26863e9f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-20 15:30:04 +0000

    libdatrie: add 0.2.13 bottle.

--- Formula/lib/libdatrie.rb
+++ Formula/lib/libdatrie.rb
@@ -5,6 +5,15 @@ class Libdatrie < Formula
   sha256 "12231bb2be2581a7f0fb9904092d24b0ed2a271a16835071ed97bed65267f4be"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "f864dccadb35cd53fcee3e84a369d0b7ca10c842b8771a1c465d44fb03b71a84"
+    sha256 cellar: :any,                 arm64_sonoma:  "66daae07645d7b488dfdd19120830ea7226934de6434370f3a174d6b49a6c16c"
+    sha256 cellar: :any,                 arm64_ventura: "541b03e5526b7ff6814697f7d343827d120cbeb42e83b441e4264b5f72bc8f1a"
+    sha256 cellar: :any,                 sonoma:        "cae5e0af929b08ab076d3669675a659a974ae1a860d6a57cce0f75728c22037d"
+    sha256 cellar: :any,                 ventura:       "83b886b9fc24c2f45cd98584c91ab3eeb14c3838c456026216886d9151811f93"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1a04b4f1c659bb58c95db895643c02d3b197439c3ae2b0530bde75ea898a7d4b"
+  end
+
   depends_on "pkgconf" => :build
 
   def install

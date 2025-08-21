07b29cb2398c0c9454a89f3d185a069866ac3c07
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 08:52:28 +0000

    libudfread: add 1.2.0 bottle.

--- Formula/lib/libudfread.rb
+++ Formula/lib/libudfread.rb
@@ -10,6 +10,15 @@ class Libudfread < Formula
     regex(/href=.*?libudfread[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "a563689c17ca21d204a228773ec9380b5b0880bd3b0ab322170f8596be7f844a"
+    sha256 cellar: :any,                 arm64_sonoma:  "a5818ef3abbd4de631f60ba8abfd0450585c789c57fd5ffa4129a6d2aa446e37"
+    sha256 cellar: :any,                 arm64_ventura: "d5f039fc1b4b415f554391bd3500cd47c3f7129baa8729d302f9f31d9bb2c939"
+    sha256 cellar: :any,                 sonoma:        "f48224450840a89fd3c0941f0fdfcca6e25900174a2dc0529b5b741794ebdc1a"
+    sha256 cellar: :any,                 ventura:       "dcfc7c2ae1e18c03814140a7fe31507166ad7174c105b99d1a6db3afdec9f51c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d739ff8b7b3989919e494baae8ae502b249e61afe81587064d675ab7271744f"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
 

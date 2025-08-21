e3f15c1d43a9028bdc61e921bc8754d5b0717a8b
Anselm Hahn <Anselm.Hahn@gmail.com>
2025-08-21 23:19:50 +0200

    nuxi 3.28.0 (new formula)
    Adds the `nuxi` formula for the Nuxt CLI, enabling users to create and manage Nuxt projects.
    
    Includes:
    - Installation via npm.
    - Version and help testing.
    - Project initialization tests.
    
    apply review feedback

---
+++ Formula/n/nuxi.rb
@@ -0,0 +1,32 @@
+class Nuxi < Formula
+  desc "Nuxt CLI (nuxi) for creating and managing Nuxt projects"
+  homepage "https://github.com/nuxt/cli"
+  url "https://registry.npmjs.org/nuxi/-/nuxi-3.28.0.tgz"
+  sha256 "2f4872aedf175d81390f273ac892aa0c045b2cd911e7c10893da556f6ca285d9"
+  license "MIT"
+  head "https://github.com/nuxt/cli.git", branch: "main"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    # Both aliases should be present and report the same version
+    assert_match version.to_s, shell_output("#{bin}/nuxi --version")
+    assert_match version.to_s, shell_output("#{bin}/nuxt --version")
+
+    # Perform a minimal project initialization in the temporary testpath
+    ENV["CI"] = "1"
+    target = testpath/"nuxi-tmp"
+    output = shell_output(
+      "#{bin}/nuxt init . --cwd #{target} -f --no-install --packageManager npm --gitInit -M --preferOffline",
+    )
+    assert_predicate target, :directory?
+    assert_predicate target/".git", :directory?
+    assert_path_exists target/"package.json"
+    assert_match "npm run dev", output
+  end
+end

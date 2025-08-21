b2986124c1f2c661033a99091559404fc650b833
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 19:57:25 +0000

    kubernetes-mcp-server: add 0.0.49 bottle.

--- Formula/k/kubernetes-mcp-server.rb
+++ Formula/k/kubernetes-mcp-server.rb
@@ -6,6 +6,15 @@ class KubernetesMcpServer < Formula
   license "Apache-2.0"
   head "https://github.com/containers/kubernetes-mcp-server.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6cf228e13f0f087c5e8314adce18102956a93646323d618c65b38fa3a9264e9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b20a59c82aa2d46a46d212f3c822a8d8179ae7629eeac25d62cf98bb36aaddf"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f44ce5a76d9bb75238f7fced459b03f4e9efde7a56dff669cb9b87fc835efc4d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bae35f8000c0171da36db0cdeda59c4370b843f40d0653043c6234977fa03b41"
+    sha256 cellar: :any_skip_relocation, ventura:       "415b4a9e262a89fec153189f43c17b6bc662f27b1f033bd49a430963e5008ed1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f11e52086ceba11623ff06c555042531dd9603086e5f612228b01f52a0c443a"
+  end
+
   depends_on "go" => :build
 
   def install

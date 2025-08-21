da3b6dc6a5be5dbac7190c505c60264cdcb8ae51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 11:27:38 +0000

    jfrog-cli: update 2.78.5 bottle.

--- Formula/j/jfrog-cli.rb
+++ Formula/j/jfrog-cli.rb
@@ -15,12 +15,12 @@ class JfrogCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a20e587ce28f44f86ae2105d5db61bd27a6eea2ed825da39c60d931d8f49555e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a20e587ce28f44f86ae2105d5db61bd27a6eea2ed825da39c60d931d8f49555e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a20e587ce28f44f86ae2105d5db61bd27a6eea2ed825da39c60d931d8f49555e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f3b7a2f00cd35bd2575a1ad9eda0c2ac5e3b52800b5ce181fad98f18e45958df"
-    sha256 cellar: :any_skip_relocation, ventura:       "c96487dbd70491053906f57ec685574bdda04d345883336350adceac0db0127b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c0235423f51dbbcf0997173b7351d743b510cddb0d2f441525cae3705930b3d8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c08a636705be89f3549960fb5f8187a4607848f06c285cb3fc6f29ca924ab1a6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c08a636705be89f3549960fb5f8187a4607848f06c285cb3fc6f29ca924ab1a6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c08a636705be89f3549960fb5f8187a4607848f06c285cb3fc6f29ca924ab1a6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0699d040c4875d4bd3406f53809821c539b2796be86cf7f419f20abc8fa8f8e3"
+    sha256 cellar: :any_skip_relocation, ventura:       "4186ff0379cc3082d348bd6a951440b4cd376ba6bc15daef05370dede4caeb64"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3682d6f8873174978a551618ea75e8cd4c04c28bd74a8761cc376b7b89f3e24c"
   end
 
   depends_on "go" => :build

8bc35b3bbde3cdba64f492eb5c190d4bd4856918
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 14:01:04 +0000

    flyway 11.11.2

--- Formula/f/flyway.rb
+++ Formula/f/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://www.red-gate.com/products/flyway/community/"
-  url "https://github.com/flyway/flyway/releases/download/flyway-11.11.1/flyway-commandline-11.11.1.tar.gz"
-  sha256 "91c8834325d8adf891aa5a05f90d991b8f893878f5a516b35f32d5447acd5a82"
+  url "https://github.com/flyway/flyway/releases/download/flyway-11.11.2/flyway-commandline-11.11.2.tar.gz"
+  sha256 "54c8a2331a58884e41b8ed3e22396d60d304321c1931540bb48d630740bb8213"
   license "Apache-2.0"
 
   bottle do

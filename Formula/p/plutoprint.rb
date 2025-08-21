b5225dc5e523d90cc9a810946f0edfeceef0d78b
Bevan Kay <email@bevankay.me>
2025-08-21 19:46:48 +1000

    plutoprint 0.9.0 (new formula)

---
+++ Formula/p/plutoprint.rb
@@ -0,0 +1,62 @@
+class Plutoprint < Formula
+  include Language::Python::Virtualenv
+
+  desc "Generate PDFs and Images from HTML"
+  homepage "https://github.com/plutoprint/plutoprint"
+  url "https://files.pythonhosted.org/packages/c0/35/aca0b163ae7a201b8b9c27f1c5bc5020b0badc93c7019020288f2099ca21/plutoprint-0.9.0.tar.gz"
+  sha256 "05c3c425dd57dbd5db0c26449e17e9e8cbc4eb041320697fa7c3f042dd3c1960"
+  license "MIT"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "plutobook"
+  depends_on "python@3.13"
+
+  on_macos do
+    depends_on "llvm" if DevelopmentTools.clang_build_version <= 1499
+  end
+
+  on_ventura do
+    depends_on "llvm"
+  end
+
+  on_linux do
+    depends_on "patchelf" => :build
+  end
+
+  fails_with :clang do
+    build 1499
+    cause "Requires C++20 support"
+  end
+
+  fails_with :gcc do
+    version "9"
+    cause "requires GCC 10+"
+  end
+
+  def python3
+    "python3.13"
+  end
+
+  def install
+    if OS.mac? && (MacOS.version == :ventura || DevelopmentTools.clang_build_version <= 1499)
+      ENV.llvm_clang
+      llvm = Formula["llvm"]
+      ENV.append "LDFLAGS", "-L#{llvm.opt_lib}/c++ -L#{llvm.opt_lib}/unwind -lunwind"
+    end
+
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/plutoprint --version")
+
+    (testpath/"test.html").write <<~HTML
+      <h1>Hello World!</h1>
+    HTML
+
+    system bin/"plutoprint", "test.html", "test.pdf"
+    assert_path_exists testpath/"test.pdf"
+  end
+end

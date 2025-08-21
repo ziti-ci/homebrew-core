12487275a72088343fb4b3b8d098f363d38d7187
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 08:10:08 +0000

    nano 8.6

--- Formula/n/nano.rb
+++ Formula/n/nano.rb
@@ -1,8 +1,8 @@
 class Nano < Formula
   desc "Free (GNU) replacement for the Pico text editor"
   homepage "https://www.nano-editor.org/"
-  url "https://www.nano-editor.org/dist/v8/nano-8.5.tar.xz"
-  sha256 "000b011d339c141af9646d43288f54325ff5c6e8d39d6e482b787bbc6654c26a"
+  url "https://www.nano-editor.org/dist/v8/nano-8.6.tar.xz"
+  sha256 "f7abfbf0eed5f573ab51bd77a458f32d82f9859c55e9689f819d96fe1437a619"
   license "GPL-3.0-or-later"
 
   livecheck do

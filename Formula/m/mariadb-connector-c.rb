ba2cd67082cd851672b1b70684f2e35d1c8148ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-22 11:14:46 +0000

    mariadb-connector-c 3.4.7

--- Formula/m/mariadb-connector-c.rb
+++ Formula/m/mariadb-connector-c.rb
@@ -2,9 +2,9 @@ class MariadbConnectorC < Formula
   desc "MariaDB database connector for C applications"
   homepage "https://mariadb.org/download/?tab=connector&prod=connector-c"
   # TODO: Remove backward compatibility library symlinks on breaking version bump
-  url "https://archive.mariadb.org/connector-c-3.4.5/mariadb-connector-c-3.4.5-src.tar.gz"
-  mirror "https://fossies.org/linux/misc/mariadb-connector-c-3.4.5-src.tar.gz/"
-  sha256 "b17e193816cb25c3364c2cc92a0ad3f1d0ad9f0f484dc76b8e7bdb5b50eac1a3"
+  url "https://archive.mariadb.org/connector-c-3.4.7/mariadb-connector-c-3.4.7-src.tar.gz"
+  mirror "https://fossies.org/linux/misc/mariadb-connector-c-3.4.7-src.tar.gz/"
+  sha256 "33bb61168380af706b9e9531396cca223179ac2f9fe72edeae69e4546d7b7a2b"
   license "LGPL-2.1-or-later"
   head "https://github.com/mariadb-corporation/mariadb-connector-c.git", branch: "3.4"
 

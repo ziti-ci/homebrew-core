84bada73d0361c88d20ccb292e51a1fec5236238
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-22 02:56:04 +0000

    gollama: update 1.36.0 bottle.

--- Formula/g/gollama.rb
+++ Formula/g/gollama.rb
@@ -7,12 +7,12 @@ class Gollama < Formula
   head "https://github.com/sammcj/gollama.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5cd0906313cfec7c676899ad62cf411c4b3d09cc0641ff7c013c2dc288649f1f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "61d7a3cd8527e8d5333927e459acc098b46742852d96d8e38c8139c2b0686e3b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "c0d820c9b4682dab74920e4027a4a99dfedf6e79cb484ef2f3be6b03e4008f18"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5178d1222b8f03f7f81cc0a91ad881ff5a578570e85edeac783abd185d06404b"
-    sha256 cellar: :any_skip_relocation, ventura:       "734d5d601f7db2e3d38fc65a8689feac03518c7db4d7eb7815b88ea5e6419982"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a4776a3cde29c8040e6508ae1f92c1feeeb1ea10b4c38894e2c619e29b963708"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "409a3bcc489c5d5d647d735d7a6764c81b16494cddf30de6ee4cfaf4b2b4bb25"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f20ce130b2db9b05495ca873f08237daeb193a3d0ec51f50a9df29538adb3e8c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0d73db706368ebb6dbb932ceab8fb66d1f52dbdba41381b7d299c5195386826b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "36f705107682512afc9de1aadb8d3772792a013cd5c7e4bc81c3a86cc3f3eb1e"
+    sha256 cellar: :any_skip_relocation, ventura:       "f772a875390b2680b0ce42462acb5dd31216bf647e1eae93f052afe4ff79c077"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6929185695bfc7bd7da89990d014d9fb22f0c2537dadfa3f967c7e65954a9c64"
   end
 
   depends_on "go" => :build

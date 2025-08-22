b511f077ab4a73dc7abc7b475e874b5415ad9a5f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-22 08:13:25 +0000

    doctl: update 1.141.0 bottle.

--- Formula/d/doctl.rb
+++ Formula/d/doctl.rb
@@ -7,12 +7,12 @@ class Doctl < Formula
   head "https://github.com/digitalocean/doctl.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bc3fffcad4eea7cdb01804a5286b6d9d767d292c7acfb36bc2de3ff2c7b5ad8e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bc3fffcad4eea7cdb01804a5286b6d9d767d292c7acfb36bc2de3ff2c7b5ad8e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "bc3fffcad4eea7cdb01804a5286b6d9d767d292c7acfb36bc2de3ff2c7b5ad8e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "988cb2676b377bab47ce51a84f857b5b3b7c735723999989d4d1abafbddd32b8"
-    sha256 cellar: :any_skip_relocation, ventura:       "988cb2676b377bab47ce51a84f857b5b3b7c735723999989d4d1abafbddd32b8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "95d24bd5349fc1e6f17318354dc35225a145e5f11335218f794d2245029632ee"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f124dd5a460f3fc004a00f771713ab56ee60e9df98856b9c1499d65753f0ab60"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f124dd5a460f3fc004a00f771713ab56ee60e9df98856b9c1499d65753f0ab60"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f124dd5a460f3fc004a00f771713ab56ee60e9df98856b9c1499d65753f0ab60"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d8fbcef107e57a192da0ca1934f3647ff3442fd62971be61abaf9aff0335de92"
+    sha256 cellar: :any_skip_relocation, ventura:       "d8fbcef107e57a192da0ca1934f3647ff3442fd62971be61abaf9aff0335de92"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c78997fd1b807294b10371d350791b0e35e3eb08716bb335168d87b271f78e32"
   end
 
   depends_on "go" => :build

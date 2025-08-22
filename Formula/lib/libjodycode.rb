1af5ce41c3b3af38feecfd425a675ccab399f2fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-22 08:49:42 +0000

    libjodycode: update 4.0.1 bottle.

--- Formula/lib/libjodycode.rb
+++ Formula/lib/libjodycode.rb
@@ -6,13 +6,13 @@ class Libjodycode < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "a2a821f7c73f07a4ea420aa4f414d879646b374b8c12c94ab383b7b0a3584ca1"
-    sha256 cellar: :any,                 arm64_sonoma:  "e23b641ca33e5c082349f9ad05ac3c914c00526c4f56359784353284ac9d0bb7"
-    sha256 cellar: :any,                 arm64_ventura: "1ff41309b7c181eb3e8649678b20e9d10695faca435d3307c9c20ddcee1c1f3c"
-    sha256 cellar: :any,                 sonoma:        "17c072e6319675767fbf6024f20b9d0b01fca0ab70246ce412f9d8d4ee21dccf"
-    sha256 cellar: :any,                 ventura:       "7ce39f917672aae1a349b6ddaf067c189e40100810986ded0410a8ff139308ab"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "62cfb8f7e91afcb30c339aaa93cc154f734e951c29a54a2d5bd7ef444fc786b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ce72b93d5b11ffa01bfe57b7bedbb63c33959b0e50a4f18a8c2fdcf2bc73266"
+    sha256 cellar: :any,                 arm64_sequoia: "6d3eef0d94ad65f2c78d585d0c0f1e2a74ce6d929c20db6cef2e96df576aff22"
+    sha256 cellar: :any,                 arm64_sonoma:  "b8d20edda315be556fd5c6bb18a4482b5b41c5e7c6a706cdf3522ef677e4cd7f"
+    sha256 cellar: :any,                 arm64_ventura: "84c2bea836781ba9fcfcca0c99f74e955b01cc71335305c1287fbf1497963f38"
+    sha256 cellar: :any,                 sonoma:        "7193d489f1cbbbbc6bb95ffb76665a19d4409496c9a660e87b64c7986f739404"
+    sha256 cellar: :any,                 ventura:       "b48b1fcead2e9a95606d2ed2a74bb1b3afee0b90b1da86fae015a2f1dca553a7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c6b3dc1d84377b4d9e2361a9da9cf0bdbd84148b42575f9c02dafc7366b216b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "87b5e80463eb0fc77580954d45094da4935e3005674463492be4c9891bf15891"
   end
 
   # These files used to be distributed as part of the jdupes formula

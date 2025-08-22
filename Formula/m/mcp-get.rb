1e0fee41fba1db1913e9abe83aee9e97bb2d68cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-22 00:10:41 +0000

    mcp-get: add 1.0.115 bottle.

--- Formula/m/mcp-get.rb
+++ Formula/m/mcp-get.rb
@@ -5,6 +5,10 @@ class McpGet < Formula
   sha256 "15b58431832df7c8038faa22dd09110f0daa5da6be7b208afd1455eace428cf9"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "40ebc222469a196d53cfbed45e0a14156358e76ea72ac9bc43ba85e49a2bfc3e"
+  end
+
   depends_on "node"
 
   def install

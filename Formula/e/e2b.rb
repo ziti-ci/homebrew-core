96d7c6f456be66f63b9b33aff6bbf558d3911d2b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-22 10:59:35 +0000

    e2b 2.0.1

--- Formula/e/e2b.rb
+++ Formula/e/e2b.rb
@@ -1,8 +1,8 @@
 class E2b < Formula
   desc "CLI to manage E2B sandboxes and templates"
   homepage "https://e2b.dev"
-  url "https://registry.npmjs.org/@e2b/cli/-/cli-2.0.0.tgz"
-  sha256 "4b110a77398ae9acb4f6f2a288b1a5874b5cb2de872165f66dc2fae117cda69d"
+  url "https://registry.npmjs.org/@e2b/cli/-/cli-2.0.1.tgz"
+  sha256 "e73d37d72724f59504c7d9661e2565d50c9fc1d067c9029bb9cc8315c9675b5b"
   license "Apache-2.0"
 
   bottle do

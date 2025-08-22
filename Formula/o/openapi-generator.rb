4217eb475000c794272bd7327910b3b3e4c12172
William Cheng <wing328hk@gmail.com>
2025-08-22 18:42:11 +0800

    openapi-generator 7.15.0

--- Formula/o/openapi-generator.rb
+++ Formula/o/openapi-generator.rb
@@ -1,8 +1,8 @@
 class OpenapiGenerator < Formula
   desc "Generate clients, server & docs from an OpenAPI spec (v2, v3)"
   homepage "https://openapi-generator.tech/"
-  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/7.14.0/openapi-generator-cli-7.14.0.jar"
-  sha256 "e03186835022ca02da4aa95e3967b6a3b6d44c2e5f7606e6d5c22466f519c757"
+  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/7.15.0/openapi-generator-cli-7.15.0.jar"
+  sha256 "4da1a7cdb78c3a43b1eab0648891135e8c3547d2eedba0dd69daf377f865f366"
   license "Apache-2.0"
 
   livecheck do

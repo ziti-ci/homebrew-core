cd67f9626e32eb372cc111c0e64386ad9369309f
Carlo Cabrera <github@carlo.cab>
2025-08-22 17:38:03 +0800

    nmap: restore `ndiff`
    `ndiff` was removed in #223085, but this makes parsing the (often
    verbose) output of `nmap` much harder.
    
    Let's add it back and add a test that it works correctly.
    
    Also, remove the `caveats`, because they no longer apply.

--- Formula/n/nmap.rb
+++ Formula/n/nmap.rb
@@ -21,12 +21,14 @@ class Nmap < Formula
     sha256 x86_64_linux:  "59e03f71b2993561a93f7cc0604a6006ff89ba380e3f7237d866ee28b356cd2f"
   end
 
+  depends_on "python-setuptools" => :build
   depends_on "liblinear"
   depends_on "libssh2"
   # Check supported Lua version at https://github.com/nmap/nmap/tree/master/liblua.
   depends_on "lua"
   depends_on "openssl@3"
   depends_on "pcre2"
+  depends_on "python@3.13" # for ndiff
 
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
@@ -65,19 +67,14 @@ class Nmap < Formula
     system "make" # separate steps required otherwise the build fails
     system "make", "install"
 
+    # Install `ndiff` separately so that we can use `pip` and `setuptools`.
+    system "python3", "-m", "pip", "install", *std_pip_args, "./ndiff"
     bin.glob("uninstall_*").map(&:unlink) # Users should use brew uninstall.
   end
 
-  def caveats
-    on_macos do
-      <<~EOS
-        If using `ndiff` returns an error about not being able to import the ndiff module, try:
-          chmod go-w #{HOMEBREW_CELLAR}
-      EOS
-    end
-  end
-
   test do
-    system bin/"nmap", "-p80,443", "google.com"
+    system bin/"nmap", "-p80,443", "-oX", "scan1.xml", "google.com"
+    cp "scan1.xml", "scan2.xml"
+    system bin/"ndiff", "scan1.xml", "scan2.xml"
   end
 end

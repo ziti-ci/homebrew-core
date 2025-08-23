b66605c17a82844d2035ce4d0123667e23e420b3
Rui Chen <rui@chenrui.dev>
2025-08-23 19:50:44 -0400

    ggc 3.2.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/g/ggc.rb
@@ -0,0 +1,24 @@
+class Ggc < Formula
+  desc "Modern Git CLI"
+  homepage "https://github.com/bmf-san/ggc"
+  url "https://github.com/bmf-san/ggc/archive/refs/tags/3.2.1.tar.gz"
+  sha256 "93f0f3f74c90fcb52ae222956a6c454c70b39abf0b6ef1b95337541c271ab8ef"
+  license "MIT"
+  head "https://github.com/bmf-san/ggc.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user}"
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ggc version")
+
+    # `vim` not found in `PATH`
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    assert_equal "main", shell_output("#{bin}/ggc config get default.branch").chomp
+  end
+end

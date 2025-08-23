858d75698557ead9c68196f1164a5c9f994b614c
FX Coudert <fxcoudert@gmail.com>
2025-08-23 08:16:42 +0000

    kokkos: add 4.6.02 bottle.

--- Formula/k/kokkos.rb
+++ Formula/k/kokkos.rb
@@ -11,6 +11,15 @@ class Kokkos < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "26666fe1198bcea56d20c8f858aa5ecf3e4dcc31766910e74c39bf25ade28e82"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9eaccb23aaafea38ab764a05ef4054da8155a881e62e0ba19ae4f26d9c5702fd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ce8f2d69483623f162650c4586a5a6ccacfb3ecbc2f23864c0506aaa8569316a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a0d37f57f6a027d359db8217801860e66b6f90662fd0e3483bf8c2b1122b1fa0"
+    sha256 cellar: :any_skip_relocation, ventura:       "3c2927bd676c1481e81755ba450ca58258bc5716ebe931f67406e2df9b1bd6ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fcbe74aa9490d0f03ee94eb016711405690a11ad69ed0d2080a37f7dbee70f4a"
+  end
+
   depends_on "cmake" => :build
 
   on_macos do

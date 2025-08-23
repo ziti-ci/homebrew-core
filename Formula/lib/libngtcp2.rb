fd388906275100ad2b9523da3c66a69ba3f22383
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-23 05:13:24 +0000

    libngtcp2 1.15.0

--- Formula/lib/libngtcp2.rb
+++ Formula/lib/libngtcp2.rb
@@ -1,9 +1,9 @@
 class Libngtcp2 < Formula
   desc "IETF QUIC protocol implementation"
   homepage "https://nghttp2.org/ngtcp2/"
-  url "https://github.com/ngtcp2/ngtcp2/releases/download/v1.14.0/ngtcp2-1.14.0.tar.xz"
-  mirror "http://fresh-center.net/linux/www/ngtcp2-1.14.0.tar.xz"
-  sha256 "d1fbf9eae92921bfd33154dab2574bc4b7d7936f486396d6c78bfff90ed5b35d"
+  url "https://github.com/ngtcp2/ngtcp2/releases/download/v1.15.0/ngtcp2-1.15.0.tar.xz"
+  mirror "http://fresh-center.net/linux/www/ngtcp2-1.15.0.tar.xz"
+  sha256 "30a29827b49ffbdf31dadcdeba703df63676526c93724ef741c72dc73c05e3a4"
   license "MIT"
   head "https://github.com/ngtcp2/ngtcp2.git", branch: "main"
 

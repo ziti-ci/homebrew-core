83d7a672cce6ab4e017c744dc5dfd0ba7f259c3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 20:20:30 +0000

    parallel 20250822

--- Formula/p/parallel.rb
+++ Formula/p/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftpmirror.gnu.org/gnu/parallel/parallel-20250722.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/parallel/parallel-20250722.tar.bz2"
-  sha256 "91a81ff4129cdf5ad3c3c45ec033e75f2bbea5447f4b6813a0d8cfe8e5c7843b"
+  url "https://ftpmirror.gnu.org/gnu/parallel/parallel-20250822.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/parallel/parallel-20250822.tar.bz2"
+  sha256 "019d328722867cffe918c449364308c0df048456c69299b91451a3e6fac9167a"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
@@ -25,19 +25,6 @@ class Parallel < Formula
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
     bash_completion.install share/"bash-completion/completions/parallel"
-
-    inreplace_files = [
-      bin/"parallel",
-      doc/"parallel.texi",
-      doc/"parallel_design.texi",
-      doc/"parallel_examples.texi",
-      man1/"parallel.1",
-      man7/"parallel_design.7",
-      man7/"parallel_examples.7",
-    ]
-
-    # Ignore `inreplace` failures when building from HEAD or not building a bottle.
-    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX, audit_result: build.stable? && build.bottle?
   end
 
   def caveats

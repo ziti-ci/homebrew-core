8dc3f9122ebe46b1d57d42ff3786d447a9334d85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-26 18:20:00 +0000

    falco: update 1.20.0 bottle.

--- Formula/f/falco.rb
+++ Formula/f/falco.rb
@@ -7,12 +7,12 @@ class Falco < Formula
   head "https://github.com/ysugimoto/falco.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5aa9a71b492dfebddd297ec103552dae8127310890f114235d37111982114f95"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5aa9a71b492dfebddd297ec103552dae8127310890f114235d37111982114f95"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "5aa9a71b492dfebddd297ec103552dae8127310890f114235d37111982114f95"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a436b3a8d8047f4ad246799d86830c409df998351bd4afa11e4627d39c529550"
-    sha256 cellar: :any_skip_relocation, ventura:       "a436b3a8d8047f4ad246799d86830c409df998351bd4afa11e4627d39c529550"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2281396b92d779f41e2e420fab5ce709e4162127d7d9e166aedfa3a31a409a7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2121dc08b2cc2e6c793eba15ee0a2f5c47738ffe5868a46f81375a8cd51ceff4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2121dc08b2cc2e6c793eba15ee0a2f5c47738ffe5868a46f81375a8cd51ceff4"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2121dc08b2cc2e6c793eba15ee0a2f5c47738ffe5868a46f81375a8cd51ceff4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "36e7f914791e3c758460a2f4c42460205e0b139c66923deade23828e04a4da11"
+    sha256 cellar: :any_skip_relocation, ventura:       "36e7f914791e3c758460a2f4c42460205e0b139c66923deade23828e04a4da11"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e23061f82fc96e1c79362b91044a743e4f965fe51d52ef8ff82610a9631c1864"
   end
 
   depends_on "go" => :build

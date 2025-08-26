084371ef287d5a75fbfea9e30285976863179768
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-26 11:21:03 +0000

    sttr 0.2.27

--- Formula/s/sttr.rb
+++ Formula/s/sttr.rb
@@ -1,8 +1,8 @@
 class Sttr < Formula
   desc "CLI to perform various operations on string"
   homepage "https://github.com/abhimanyu003/sttr"
-  url "https://github.com/abhimanyu003/sttr/archive/refs/tags/v0.2.26.tar.gz"
-  sha256 "d59a4f25c2ad4478699585aff16d3b99b9b1fddfb894bdf072705d6342aee59a"
+  url "https://github.com/abhimanyu003/sttr/archive/refs/tags/v0.2.27.tar.gz"
+  sha256 "72e1c173843e42b3e719843f2825bf1d2a20e3167016c5962158365969e38df2"
   license "MIT"
   head "https://github.com/abhimanyu003/sttr.git", branch: "main"
 

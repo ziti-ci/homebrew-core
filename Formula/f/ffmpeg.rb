6c849c2b31a2124e0413a3dfaf16cd2464ab0b49
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-08-26 19:49:04 +0000

    ffmpeg: update 8.0 bottle.

--- Formula/f/ffmpeg.rb
+++ Formula/f/ffmpeg.rb
@@ -14,13 +14,13 @@ class Ffmpeg < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "721d9dc1e805b3e6a6ba10262c2780c2b18f1e71d9eb625fd563a6f38cd31ce3"
-    sha256 arm64_sonoma:  "a1ceebda4bfafda166680b82db820285f9685fdb6d28c2595b212883d219b879"
-    sha256 arm64_ventura: "e72aa58bdbfc1632cafd89d3a078fbdc56d21079ba32db6d8487cfa4d4368725"
-    sha256 sonoma:        "d95a9d5b291b5058470d388db660951b9ea2da270eb7912002960d6c0b79f3a2"
-    sha256 ventura:       "62b872b80be68c8b006dffd2a003ade2e7a4827165f9f27acafe3d50f491d06b"
-    sha256 arm64_linux:   "2d45408fdcb3e2a237397f9ec4c3de15378350dab9f337f1afc5873ed6dff253"
-    sha256 x86_64_linux:  "6239b6da895491f564960043da7fe6918e98ffd1ac0c172c7869949e823714db"
+    sha256 arm64_sequoia: "a3c8c7d17e361171186713958b25b90a59805eff9ef6348b841249e4411ba63e"
+    sha256 arm64_sonoma:  "76310dc20a609f07064f77915e40e070b90769f1aeb76f2adebc57d186e91fc6"
+    sha256 arm64_ventura: "92325bbf93378bfc4cdfcc3fceabf4f3636a96562cd0f747026e55876e28f527"
+    sha256 sonoma:        "489500b48040b02e61d58385278feac226cc8cc38da46f0612da6c4d82c96453"
+    sha256 ventura:       "dab39c01c0a58c58f4a81abe01627671a4bdeeef24fbd338ff41a4c7f4ddc084"
+    sha256 arm64_linux:   "cc2c521c60d093f6648c12346ecfdd9e1d3fbc9cd836164a69053486a0f25cb0"
+    sha256 x86_64_linux:  "b8a35131d5142ccf5bc3925476fc2283550a697981034ef51ad76aaaade31a3a"
   end
 
   depends_on "pkgconf" => :build

fd77214596434259776506b0defb26804e9373fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-26 17:15:45 +0000

    npm-check-updates 18.0.3

--- Formula/n/npm-check-updates.rb
+++ Formula/n/npm-check-updates.rb
@@ -1,8 +1,8 @@
 class NpmCheckUpdates < Formula
   desc "Find newer versions of dependencies than what your package.json allows"
   homepage "https://github.com/raineorshine/npm-check-updates"
-  url "https://registry.npmjs.org/npm-check-updates/-/npm-check-updates-18.0.2.tgz"
-  sha256 "6030b0e96c51bc762116f3b9a2401d9162f3d6a20cd51f05ef68a22e2caa3e9c"
+  url "https://registry.npmjs.org/npm-check-updates/-/npm-check-updates-18.0.3.tgz"
+  sha256 "bbb0d62cb62b4890022bace748335e3355ca421dd6af05ca82d884d2dd44d578"
   license "Apache-2.0"
 
   bottle do

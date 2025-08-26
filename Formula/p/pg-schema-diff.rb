cc44fff1e117b6615d09e0176c83ce3fed54755e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-26 01:46:55 +0000

    pg-schema-diff: add 1.0.0 bottle.

--- Formula/p/pg-schema-diff.rb
+++ Formula/p/pg-schema-diff.rb
@@ -6,6 +6,15 @@ class PgSchemaDiff < Formula
   license "MIT"
   head "https://github.com/stripe/pg-schema-diff.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b52583156ba63fd768aca26c12bbc5a3db5c9f2bf32da24fb544c35ee0f5e207"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b52583156ba63fd768aca26c12bbc5a3db5c9f2bf32da24fb544c35ee0f5e207"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b52583156ba63fd768aca26c12bbc5a3db5c9f2bf32da24fb544c35ee0f5e207"
+    sha256 cellar: :any_skip_relocation, sonoma:        "186ce18c2edfc82f4fa82645ebc88dc04d918a9ab86c501f0a376d34119dedb4"
+    sha256 cellar: :any_skip_relocation, ventura:       "186ce18c2edfc82f4fa82645ebc88dc04d918a9ab86c501f0a376d34119dedb4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13ac73b865e90a83a2d628001e799a94ab1f48ed63e0dbfb864114f0c89170a2"
+  end
+
   depends_on "go" => :build
 
   def install

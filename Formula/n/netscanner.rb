5dc633bba102ef8f06194224ec341a2f838e6dda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-27 04:16:37 +0000

    netscanner: add 0.6.3 bottle.

--- Formula/n/netscanner.rb
+++ Formula/n/netscanner.rb
@@ -6,6 +6,15 @@ class Netscanner < Formula
   license "MIT"
   head "https://github.com/Chleba/netscanner.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b9b676da6d93f469d0818d8a36401f9ea5acaab79871be20503935313ff303aa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e065a383960691a87a2e0cab684c9b96334ed59f9e6e2ee3c34bb8305fbf8c18"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "457b9ebece7265aef139b9f72f3cc9304cb9da345a0d8a7598732d31c8b24900"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0d8b5c71568111b671f317a4c8e1f676c2d6bce3736fe620e7e0f35b5c39ca19"
+    sha256 cellar: :any_skip_relocation, ventura:       "d75f758710f6a73e9ec214a11e20c1bd3aa091c1042dd82ba16a098c57c9dc9a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b9e389005e065ed119a75aeefc77f513408bcd380ba199a8b55e6e7d80a6eca"
+  end
+
   depends_on "rust" => :build
 
   def install

ee65dfebcd50689760b1c4021032ee57e51c193e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-27 14:08:46 +0000

    kea 3.0.1
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>

--- Formula/k/kea.rb
+++ Formula/k/kea.rb
@@ -1,9 +1,9 @@
 class Kea < Formula
   desc "DHCP server"
   homepage "https://www.isc.org/kea/"
-  url "https://ftp.isc.org/isc/kea/3.0.0/kea-3.0.0.tar.xz"
-  mirror "https://dl.cloudsmith.io/public/isc/kea-3-0/raw/versions/3.0.0/kea-3.0.0.tar.xz"
-  sha256 "bf963d1e10951d8c570c6042afccf27c709d45e03813bd2639d7bb1cfc4fee76"
+  url "https://ftp.isc.org/isc/kea/3.0.1/kea-3.0.1.tar.xz"
+  mirror "https://dl.cloudsmith.io/public/isc/kea-3-0/raw/versions/3.0.1/kea-3.0.1.tar.xz"
+  sha256 "ec84fec4bb7f6b9d15a82e755a571e9348eb4d6fbc62bb3f6f1296cd7a24c566"
   license "MPL-2.0"
   head "https://gitlab.isc.org/isc-projects/kea.git", branch: "master"
 
@@ -36,11 +36,21 @@ class Kea < Formula
   depends_on "log4cplus"
   depends_on "openssl@3"
 
+  # Workaround for boost >= 1.89
+  # https://gitlab.isc.org/isc-projects/kea/-/issues/4085
+  patch :DATA
+
   def install
-    # the build system looks for `sudo` to run some commands, but we don't want to use it
-    inreplace "meson.build",
-              "SUDO = find_program('sudo', required: false)",
+    # TODO: We probably also need to `inreplace` the following so they don't install in the prefix:
+    #   - LOCALSTATEDIR_INSTALLED
+    #   - RUNSTATEDIR_INSTALLED
+    #   - SYSCONFDIR_INSTALLED
+    # Report this upstream so they're not forced to be inside the `prefix`.
+    inreplace "meson.build" do |s|
+      # the build system looks for `sudo` to run some commands, but we don't want to use it
+      s.gsub! "SUDO = find_program('sudo', required: false)",
               "SUDO = find_program('', required: false)"
+    end
 
     system "meson", "setup", "build", "-Dcpp_std=c++20", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
@@ -54,3 +64,37 @@ class Kea < Formula
     system sbin/"keactrl", "status"
   end
 end
+
+__END__
+diff --git a/meson.build b/meson.build
+index cedc949773..aed020ebb2 100644
+--- a/meson.build
++++ b/meson.build
+@@ -189,7 +189,10 @@ message(f'Detected system "@SYSTEM@".')
+ 
+ #### Dependencies
+ 
+-boost_dep = dependency('boost', version: '>=1.66', modules: ['system'])
++boost_dep = dependency('boost', version: '>=1.69', required: false)
++if not boost_dep.found()
++    boost_dep = dependency('boost', version: '>=1.66', modules: ['system'])
++endif
+ dl_dep = dependency('dl')
+ threads_dep = dependency('threads')
+ add_project_dependencies(boost_dep, dl_dep, threads_dep, language: ['cpp'])
+diff --git a/src/lib/asiolink/asio_wrapper.h b/src/lib/asiolink/asio_wrapper.h
+index a33c56f2d4..e1ae6e06f6 100644
+--- a/src/lib/asiolink/asio_wrapper.h
++++ b/src/lib/asiolink/asio_wrapper.h
+@@ -74,9 +74,11 @@
+ #pragma GCC push_options
+ #pragma GCC optimize ("O0")
+ #include <boost/asio.hpp>
++#include <boost/asio/deadline_timer.hpp>
+ #pragma GCC pop_options
+ #else
+ #include <boost/asio.hpp>
++#include <boost/asio/deadline_timer.hpp>
+ #endif
+ 
+ #endif // ASIO_WRAPPER_H

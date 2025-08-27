e6c88ae586bb557a67aacdca7661300af3d9ecb1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-27 05:01:34 +0000

    gemini-cli: add 0.2.0 bottle.

--- Formula/g/gemini-cli.rb
+++ Formula/g/gemini-cli.rb
@@ -5,6 +5,15 @@ class GeminiCli < Formula
   sha256 "e1dfacc40cbf8bf14901261376ce4734667decbb809d90fd78afa1daa5711152"
   license "Apache-2.0"
 
+  bottle do
+    sha256                               arm64_sequoia: "05c2f09cc699d7f4095968c7ddec84dd3d94f6a72d5d2089332b9919a9135671"
+    sha256                               arm64_sonoma:  "172837a798fdd4f0bb0035edb9c12eb8f34f10f19f1d894e3bc15e08680d29ae"
+    sha256                               arm64_ventura: "cee8070f9aa082a547b0b6d8be3ac25fd95f441722b9ac2305cbd88b28013b83"
+    sha256                               sonoma:        "5bd93aa38375e81ac530f8823f755b0bff6ef0de31a24a5610e6c9c488d30ca2"
+    sha256                               ventura:       "4df6a9852b01fda0794f6210cf926be20585fbc7b2cfe44484bc36c0254a061b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a5e1adf90984bb586290a6cc6d193034cad043b99db25b60ff4542bcc791d3d"
+  end
+
   depends_on "node"
 
   def install

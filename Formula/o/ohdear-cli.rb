cbaf4cbb9d390b145e29204ca16724e09798dd02
Owen Voke <development@voke.dev>
2025-08-27 08:53:51 +0100

    ohdear-cli 4.5.1

--- Formula/o/ohdear-cli.rb
+++ Formula/o/ohdear-cli.rb
@@ -1,8 +1,8 @@
 class OhdearCli < Formula
   desc "Tool to manage your Oh Dear sites"
   homepage "https://github.com/ohdearapp/ohdear-cli"
-  url "https://github.com/ohdearapp/ohdear-cli/releases/download/v4.4.0/ohdear.phar"
-  sha256 "4d213baba1bc0275a8ec6f458915f9d5f6df5b1c8522476e82289b931360de13"
+  url "https://github.com/ohdearapp/ohdear-cli/releases/download/v4.5.1/ohdear.phar"
+  sha256 "0dc83e762880b67797c00a0a680ada33ab8d23d98ba26cd1ca7f8d92fe532eb8"
   license "MIT"
 
   no_autobump! because: :requires_manual_review

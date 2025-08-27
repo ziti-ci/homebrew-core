80541d0d94b5a745495f662906b65df7f4764781
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-27 02:04:09 +0000

    yapf: update 0.43.0 bottle.

--- Formula/y/yapf.rb
+++ Formula/y/yapf.rb
@@ -9,13 +9,8 @@ class Yapf < Formula
   head "https://github.com/google/yapf.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cf15857b390cc7e51ca27c742c839b71035382d701e298354fcf521318da61b8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cf15857b390cc7e51ca27c742c839b71035382d701e298354fcf521318da61b8"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "cf15857b390cc7e51ca27c742c839b71035382d701e298354fcf521318da61b8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "6b02bec0b509d12c45e027c20377d2cc927156f932a156cb08e75cf3d0a16b0c"
-    sha256 cellar: :any_skip_relocation, ventura:       "6b02bec0b509d12c45e027c20377d2cc927156f932a156cb08e75cf3d0a16b0c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "24a8928518e665fbbdc956986ec490c577990c536de1c0b233590557e0f0cb32"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4bdbca92626b2c3c57566193c8fd2f66ff8ecc670d8ee8e9e495be42f33cf37"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "f18f6eead958807f6b3df31f4a022c850efe1566b585dea3605c27c1b829ca42"
   end
 
   depends_on "python@3.13"

2e720aefb23197769a1554c40a1c0cac4c1792e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-27 11:35:42 +0000

    unciv 4.17.17-patch1

--- Formula/u/unciv.rb
+++ Formula/u/unciv.rb
@@ -1,8 +1,9 @@
 class Unciv < Formula
   desc "Open-source Android/Desktop remake of Civ V"
   homepage "https://github.com/yairm210/Unciv"
-  url "https://github.com/yairm210/Unciv/releases/download/4.17.17/Unciv.jar"
-  sha256 "4889db83ed5b00554ac4495b7f65e0e34f1d6febec1f73173df826865fd3f424"
+  url "https://github.com/yairm210/Unciv/releases/download/4.17.17-patch1/Unciv.jar"
+  version "4.17.17-patch1"
+  sha256 "ec4f27822edaa846dfc4ba66d0e14ead22edac10cf403408d889b6ba7a71d062"
   license "MPL-2.0"
 
   livecheck do

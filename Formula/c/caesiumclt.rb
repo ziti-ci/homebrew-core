9464ff6965025297bd88bf409afe9b7f9248ee8e
Matteo Paonessa <Lymphatus@users.noreply.github.com>
2025-08-27 08:28:42 +0200

    caesiumclt 1.0.0 (new formula)

---
+++ Formula/c/caesiumclt.rb
@@ -0,0 +1,20 @@
+class Caesiumclt < Formula
+  desc "Fast and efficient lossy and/or lossless image compression tool"
+  homepage "https://github.com/Lymphatus/caesium-clt"
+  url "https://github.com/Lymphatus/caesium-clt/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "2ba935780d3798a3047ff87c7b855531ca2472114460704b99191cc069203f07"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system bin/"caesiumclt", "--lossless", "-Q", "--suffix", "_t", "--output", testpath, test_fixtures("test.jpg")
+    assert_path_exists testpath/"test_t.jpg"
+    system bin/"caesiumclt", "-q", "80", "-Q", "--suffix", "_b", "--output", testpath, test_fixtures("test.jpg")
+    assert_path_exists testpath/"test_b.jpg"
+  end
+end

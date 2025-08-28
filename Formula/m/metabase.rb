155a8e3e1e8f0fa92850e3b8edaa4456e157051a
Sam Ford <1584702+samford@users.noreply.github.com>
2025-08-28 17:54:57 -0400

    metabase 0.56.3
    This updates `metabase` to the latest version, 0.56.3.
    
    The existing `livecheck` block for `metabase` is returning an `Unable
    to get versions` error, as the first-party download page for the OSS
    jar only contains an unversioned link now (/latest/metabase.jar). This
    updates the `livecheck` block to use the `GithubLatest` strategy, as
    the GitHub release body text contains a versioned link to the OSS jar
    file.

--- Formula/m/metabase.rb
+++ Formula/m/metabase.rb
@@ -1,13 +1,16 @@
 class Metabase < Formula
   desc "Business intelligence report server"
   homepage "https://www.metabase.com/"
-  url "https://downloads.metabase.com/v0.56.2/metabase.jar"
-  sha256 "2440b3458a1918826ed798636b4ef43d5a81e0a31e3a8b706e51f7e24a44ea8f"
+  url "https://downloads.metabase.com/v0.56.3/metabase.jar"
+  sha256 "4fba1db441d0ff2246311e564bb5a56a815ada735969f18ceb7e9791108fd70b"
   license "AGPL-3.0-only"
 
+  # The first-party download page only provides an unversioned link to the
+  # latest OSS jar file. We check the "latest" GitHub release, as the release
+  # body text contains a versioned link to the OSS jar file.
   livecheck do
-    url "https://www.metabase.com/start/oss/jar.html"
-    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/metabase\.jar}i)
+    url :head
+    strategy :github_latest
   end
 
   bottle do

59a6d5fae936bc172466284714b937ab78482194
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-28 19:53:00 +0000

    chromaprint 1.6.0

--- Formula/c/chromaprint.rb
+++ Formula/c/chromaprint.rb
@@ -1,10 +1,9 @@
 class Chromaprint < Formula
   desc "Core component of the AcoustID project (Audio fingerprinting)"
   homepage "https://acoustid.org/chromaprint"
-  url "https://github.com/acoustid/chromaprint/releases/download/v1.5.1/chromaprint-1.5.1.tar.gz"
-  sha256 "a1aad8fa3b8b18b78d3755b3767faff9abb67242e01b478ec9a64e190f335e1c"
+  url "https://github.com/acoustid/chromaprint/releases/download/v1.6.0/chromaprint-1.6.0.tar.gz"
+  sha256 "9d33482e56a1389a37a0d6742c376139fa43e3b8a63d29003222b93db2cb40da"
   license "LGPL-2.1-or-later"
-  revision 3
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "d074eaa951816006df71b92cf9b94151020a32f787b5ac0e60d7d8303e5cfd4a"
@@ -17,27 +16,7 @@ class Chromaprint < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "ffmpeg@7"
-
-  # Backport support for FFmpeg 5+. Remove in the next release
-  patch do
-    url "https://github.com/acoustid/chromaprint/commit/584960fbf785f899d757ccf67222e3cf3f95a963.patch?full_index=1"
-    sha256 "b9db11db3589c5f4a2999c1a782bd41f614d438f18a6ed3b5167165d0863f9c2"
-  end
-  patch do
-    url "https://github.com/acoustid/chromaprint/commit/8ccad6937177b1b92e40ab8f4447ea27bac009a7.patch?full_index=1"
-    sha256 "47c9cc257c6e5d46840e9b64ba5f1bcee2705eac3d7f5b23ca0fb4aefc6b8189"
-  end
-  patch do
-    url "https://github.com/acoustid/chromaprint/commit/aa67c95b9e486884a6d3ee8b0c91207d8c2b0551.patch?full_index=1"
-    sha256 "f90f5f13a95f1d086dbf98cd3da072d1754299987ee1734a6d62fcda2139b55d"
-  end
-
-  # Backport support for CMake 4. Remove in the next release
-  patch do
-    url "https://github.com/acoustid/chromaprint/commit/1120d825d7d97668f9dc87768641ebe8c174907e.patch?full_index=1"
-    sha256 "c3d5c86a4765e6f942a57bea245fb7f8fcf5ea69e2a4de2d2cd24e8e87cff9a0"
-  end
+  depends_on "ffmpeg"
 
   def install
     system "cmake", "-S", ".", "-B", "build",

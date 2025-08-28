b98f91de98d55738071009e8bb44d5da256888c6
Rui Chen <rui@chenrui.dev>
2025-08-27 20:58:21 -0400

    claudekit 0.8.4 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/claudekit.rb
@@ -0,0 +1,20 @@
+class Claudekit < Formula
+  desc "Intelligent guardrails and workflow automation for Claude Code"
+  homepage "https://github.com/carlrannaberg/claudekit"
+  url "https://registry.npmjs.org/claudekit/-/claudekit-0.8.4.tgz"
+  sha256 "550f0211d84eaadd6478b8f9432bcffc197dbeec74d89c4635dac145d9cf4d13"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/claudekit --version")
+    assert_match "Hooks:", shell_output("#{bin}/claudekit list")
+    assert_match ".claudekit/config.json not found", shell_output("#{bin}/claudekit doctor 2>&1", 1)
+  end
+end

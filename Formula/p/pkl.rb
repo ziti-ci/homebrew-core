c5fe36bae5c0495e4cade96403e0d31dbe653b1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-28 01:12:04 +0000

    pkl: update 0.29.1 bottle.

--- Formula/p/pkl.rb
+++ Formula/p/pkl.rb
@@ -6,14 +6,13 @@ class Pkl < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cf6ce6ea80fab6b7c066e06e63613f4b53a2fef709ef7b8111a1e3cfad5f6ff8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9135ce3476dad9d55bb49f2dcaba2a27b810280a7107f22ab4191a3fac6ac2ad"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "66234b9765b70dc41eef01fb02af8275c7c80bc1d3825688e0574f248884ea31"
-    sha256 cellar: :any_skip_relocation, sonoma:        "cade7519cf009b3231cd8bc8305a48f17477742df0ed8057bca2e1c80dfe99db"
-    sha256 cellar: :any_skip_relocation, ventura:       "dd247654b3e03a76fa269bef790656edfad9d5e3cf976867b1eef47413a9d58b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e3d30d56d4754534faeecac8baee8da7046405d02d8080b7ed3ce8e0550f6f8b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38f4da375191a4cc760f33bdb269faf93052d2d6de4cd23734fa719c9fef9c8d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13d5821b098a5d299fc46879577ae6e9a34ebe05d45f76cb6c36571ed50d8379"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "329a933fca1bbe973f9e231f855e2895b09fc56d8038a3e8b7ac77b413090461"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c00368f1c7d4e8da6ec7270ad03335d3e280ab2f13a11b441d2d58ba6a4418ae"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6b19578d3a1792200a5462555f301b7e919b94f242fb74e3cf6ed0548f96b471"
+    sha256 cellar: :any_skip_relocation, ventura:       "6148f54d2dbaa8854f000f1e32caf29bb62975c15629ee92ced4f72ac4301263"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "01682d4cab65dce2433a14a11129eeda515602a62ce3873a07c70a69f005b78b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "386b15f1ca707e6c4cf1f5c95cc09cfb0f064560f82f8526201ffe942db448c2"
   end
 
   depends_on "gradle@8" => :build

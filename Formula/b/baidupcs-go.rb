2b414c04e4f2f46f59cc7f635769900879de99ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-28 20:11:59 +0000

    baidupcs-go: update 3.9.8 bottle.

--- Formula/b/baidupcs-go.rb
+++ Formula/b/baidupcs-go.rb
@@ -7,12 +7,12 @@ class BaidupcsGo < Formula
   head "https://github.com/qjfoidnh/BaiduPCS-Go.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e16c4c3187374f9874bf0fc7c67363e62d21710665d4724dd7397a9ca9360ed9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e16c4c3187374f9874bf0fc7c67363e62d21710665d4724dd7397a9ca9360ed9"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "e16c4c3187374f9874bf0fc7c67363e62d21710665d4724dd7397a9ca9360ed9"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c4242f6ae0c1e2484069223aebfd992f07eafddd44e77252a42a1734ed99e1c0"
-    sha256 cellar: :any_skip_relocation, ventura:       "c4242f6ae0c1e2484069223aebfd992f07eafddd44e77252a42a1734ed99e1c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9f03fe4dae8654f3365f0bdfbe2841097272c827b5cebe577ccd615901cb6ef"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cd05748c75cf57ed9df636aabe367cbe5d082a33435e5223b431ac24d8086c39"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cd05748c75cf57ed9df636aabe367cbe5d082a33435e5223b431ac24d8086c39"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "cd05748c75cf57ed9df636aabe367cbe5d082a33435e5223b431ac24d8086c39"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5e0776693a793383d809a5864a59dbe74ff23007e7becac3ac6a849670c5890c"
+    sha256 cellar: :any_skip_relocation, ventura:       "5e0776693a793383d809a5864a59dbe74ff23007e7becac3ac6a849670c5890c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a7bb85fc637dcbd78ed9877d8664e1d27ba2652e46f5800ee7369b074f21ca89"
   end
 
   depends_on "go" => :build

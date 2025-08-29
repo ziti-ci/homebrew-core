0b2f77279e83d1724b0d426a5e2f5c3c21a24bdd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-29 15:01:39 +0000

    dnglab: add 0.7.0 bottle.

--- Formula/d/dnglab.rb
+++ Formula/d/dnglab.rb
@@ -5,6 +5,15 @@ class Dnglab < Formula
   sha256 "dffe4dd94913a687184b2a453eeb170c87afbca62ecf3a4bc680e5f5bf22cacc"
   license "LGPL-2.1-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f88419fd19c10e08b35eccb622ba8737b44eb3cd4f475fe5dc46ae9572ce060f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ceabff3e17c704a0012a939a99c4270771de1d6b76617cf376814b93b2f164c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "156a083c9ebb78650ac6e7433297500c36ed4ad5d9f6c2fa47f6addf5e520942"
+    sha256 cellar: :any_skip_relocation, sonoma:        "87090195be4c7ed7b6208ef0ec993811384ddd5672fdd640409d41a653fe5fb9"
+    sha256 cellar: :any_skip_relocation, ventura:       "cbf1fee7f7aac51b50b40906c4810bb211927af587b201977af9cc8118197922"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "509248fe57abfefa18c941b6fac42e28e5682ac0a31c79c81ddc704a53dbb394"
+  end
+
   depends_on "rust" => :build
 
   def install

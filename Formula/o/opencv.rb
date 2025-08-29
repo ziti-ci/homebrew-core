3d80e9690bcdd9e390309515e9474ad05ba02ec5
Michael Cho <michael@michaelcho.dev>
2025-08-29 08:49:05 -0400

    opencv: revision bump (vtk 9.5.1)

--- Formula/o/opencv.rb
+++ Formula/o/opencv.rb
@@ -2,7 +2,7 @@ class Opencv < Formula
   desc "Open source computer vision library"
   homepage "https://opencv.org/"
   license "Apache-2.0"
-  revision 7
+  revision 8
 
   stable do
     url "https://github.com/opencv/opencv/archive/refs/tags/4.12.0.tar.gz"

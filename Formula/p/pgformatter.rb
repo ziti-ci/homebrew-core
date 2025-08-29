e6ecbdfe00b936e3ae0435650e50e3172567163d
Daeho Ro <email@daeho.ro>
2025-08-29 04:00:03 +0000

    pgformatter: update 5.6 bottle.

--- Formula/p/pgformatter.rb
+++ Formula/p/pgformatter.rb
@@ -6,13 +6,8 @@ class Pgformatter < Formula
   license "PostgreSQL"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a96e7ddda61fab03d6dd3ecac2ebf62a0342d1d53b66b24101e012241e4dfe31"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a96e7ddda61fab03d6dd3ecac2ebf62a0342d1d53b66b24101e012241e4dfe31"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "eb7e8053fc84262c6514411081df0468ba5d3b487133bfe6584ee3c0421cbc6e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "490179e8a81e00b47ad1e798df25009fa4b17cd5a0e0bf209a5b05f12933a67f"
-    sha256 cellar: :any_skip_relocation, ventura:       "462fff310c1dabda0ae24884c4bf25c61d8e3d9efe589bdf10581847160234e9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5a5af8f5f719b5c58be11c02afa9eff6dc15e249b7f22431a5118d7005912e1b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5a5af8f5f719b5c58be11c02afa9eff6dc15e249b7f22431a5118d7005912e1b"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "400b39cfd207cb822d428e42ba850bdff6a6c281a63e3a3c25dfd85e4219ee01"
   end
 
   def install

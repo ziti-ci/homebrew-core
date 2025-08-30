3ebee5a3e85314b48e91d96f50faa6060fcb26f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-30 00:42:31 +0000

    bde: update 4.29.0.0 bottle.

--- Formula/b/bde.rb
+++ Formula/b/bde.rb
@@ -11,13 +11,13 @@ class Bde < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f804a3c51a88e86137d31cbd399f0788926b699b18627b55c2524d24acf53762"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1f5d7aa66db39e19778d43af21a901e09d26e10f71261200f1aee58e2375791b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "995bc63d141057796a142343ac769aad34a90e530ef05bb34553b8ed2e2d3b5b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9c98d3269316d9ff282aaea4c5f8b12cc9fc59f0b349d5b7dd118a4cfc1c38e0"
-    sha256 cellar: :any_skip_relocation, ventura:       "dcb9c1c8189e7394b869161bb1c94680ee30d8a2aac17331344505b4f1226813"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6a033b25705d6e7447e60f582841944313ed172d1ee0cd7676e259b9e4bdafc3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc3cb2ca3cc4176fdb6e929ae04afc5cabb87780387e77aa460c4f97d5863d9b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "691f7e0da877b3441e5ea20e8ef576fd52c3aa2660b08ee8f9f72512a11063ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3ba3fe5df396f397d17e19fc233f81a50db35b08935435203e5ae53818fed2cd"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "5bf0ece129c462567c622599ce4acad3552c69d78f9b609153e0be29f94b97f9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "cc8a114d99998e029a70ce224ff6854d824c76947970eac57923db7b60929cb7"
+    sha256 cellar: :any_skip_relocation, ventura:       "c7685008f2ead6437eeb3b14cbaaced6bfab2f9a2a25691661e6d85004da81a7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2528eca3e18ebc4b501c07b3eb36442c287fc2d5a3ea0cc5c4a1522527065172"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8743eb505a9490252ebcefa014252b3ad2ec1c3756babc525585f9fd007b674f"
   end
 
   depends_on "cmake" => :build

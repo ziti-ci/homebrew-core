c8747f98599435cf6492d60afdaa827022f6de5d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-30 01:17:57 +0000

    emmylua_ls: add 0.12.0 bottle.

--- Formula/e/emmylua_ls.rb
+++ Formula/e/emmylua_ls.rb
@@ -7,6 +7,15 @@ class EmmyluaLs < Formula
   license "MIT"
   head "https://github.com/EmmyLuaLs/emmylua-analyzer-rust.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c7d33fa5bbcc8959cf630b2c5542e4f55e65fd5ebf21acb0f19c5f3ad1d018aa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b09fe5713263c6338fc9e9d11cfb3f60cdec018a5afeedf755034ede1f1d983b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "64299086caf7163a978502a485763dbff4fc02c99e83494a46d802ac2fefadbd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e46518bc9e6570c1cbb34297e6a3e6e94a9805f5f4b3d6d1839f43c8de76c64a"
+    sha256 cellar: :any_skip_relocation, ventura:       "4fd90bf76662e118ef5d655bce9a56ae8340b36670424478d6094829c42896c8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6060a05d2a395ae1edefa829908e0a87feb5192bd6377ff6351940f2e48124af"
+  end
+
   depends_on "rust" => :build
 
   def install

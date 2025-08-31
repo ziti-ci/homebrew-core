945e1230d534f14bc213921c32658c8dfedb61d4
botantony <antonsm21@gmail.com>
2025-08-31 20:10:01 +0200

    davmail 6.4.0
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/d/davmail.rb
+++ Formula/d/davmail.rb
@@ -1,9 +1,9 @@
 class Davmail < Formula
   desc "POP/IMAP/SMTP/Caldav/Carddav/LDAP exchange gateway"
   homepage "https://davmail.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/davmail/davmail/6.3.0/davmail-6.3.0-3627.zip"
-  version "6.3.0"
-  sha256 "621eb5647b04305e927212c4c81577ad123ba49fdbbd1d8ae06f14ea3acf6b70"
+  url "https://downloads.sourceforge.net/project/davmail/davmail/6.4.0/davmail-6.4.0-3755.zip"
+  version "6.4.0"
+  sha256 "71882d3503e7cd92698d5afcb176d7a446eb9f56e3b372e27b64e383e7467704"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -11,7 +11,7 @@ class Davmail < Formula
     regex(%r{url=.*?/davmail[._-]v?(\d+(?:\.\d+)+)(?:-\d+)?\.(?:t|zip)}i)
   end
 
-  no_autobump! because: :requires_manual_review
+  no_autobump! because: :incompatible_version_format
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "c48497283b8ffca924a12cbaa5227d0653b3c37990540eeb01593cf7fa7b93fa"

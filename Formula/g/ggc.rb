5e672617550cc33c1b0518bae9d0cc38e1dceb3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-31 04:35:06 +0000

    ggc: update 4.0.0 bottle.

--- Formula/g/ggc.rb
+++ Formula/g/ggc.rb
@@ -7,13 +7,13 @@ class Ggc < Formula
   head "https://github.com/bmf-san/ggc.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9080a919ec9c5aadcb4a826d5e186e1cf32e26cc021a8f80b67f890930dce8e0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9080a919ec9c5aadcb4a826d5e186e1cf32e26cc021a8f80b67f890930dce8e0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "9080a919ec9c5aadcb4a826d5e186e1cf32e26cc021a8f80b67f890930dce8e0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "853f537fa86a3c78b48fb9c61a61f7a4f44d0ae1d881fba8a1f6de8fa60c135b"
-    sha256 cellar: :any_skip_relocation, ventura:       "853f537fa86a3c78b48fb9c61a61f7a4f44d0ae1d881fba8a1f6de8fa60c135b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "96f1e902da1a04804458bff51454e53425535580b3eaa18568b1a5cfedbef0ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acaf23ff1d29b03b2b34832545e30b882d197557ce1b0ff0dd120b543dba0eb0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d5c90d1ce1628856a578dad08808c60f0badf35f2bb68b69cdfeb14e2eec8747"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d5c90d1ce1628856a578dad08808c60f0badf35f2bb68b69cdfeb14e2eec8747"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d5c90d1ce1628856a578dad08808c60f0badf35f2bb68b69cdfeb14e2eec8747"
+    sha256 cellar: :any_skip_relocation, sonoma:        "50461dd8611510ca7afece9e1b7ba59111c219052b9f4061470c079000daa804"
+    sha256 cellar: :any_skip_relocation, ventura:       "50461dd8611510ca7afece9e1b7ba59111c219052b9f4061470c079000daa804"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "85c4badeeb5e2c11988efc1a67d543675c32c541f759bb6abafdd55f527ce00b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c6192cdf939eda59d01d6ff5370b78c79356c074ff00e2b0dc2d1800117c728"
   end
 
   depends_on "go" => :build

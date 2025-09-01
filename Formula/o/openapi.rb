c19b72308bbd133103b2db6617a66c25004e6589
Tristan Cartledge <tristan@speakeasyapi.dev>
2025-09-02 08:19:20 +1000

    openapi 1.6.1 (new formula)

---
+++ Formula/o/openapi.rb
@@ -0,0 +1,31 @@
+class Openapi < Formula
+  desc "CLI tools for working with OpenAPI, Arazzo and Overlay specifications"
+  homepage "https://github.com/speakeasy-api/openapi"
+  url "https://github.com/speakeasy-api/openapi/archive/refs/tags/v1.6.1.tar.gz"
+  sha256 "2d1cb22b79b515ba0faefdb721b2d199f8094d76badb671ead88bd1454e76e10"
+  license "MIT"
+  head "https://github.com/speakeasy-api/openapi.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{stable.specs[:revision]}
+      -X main.date=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/openapi"
+
+    generate_completions_from_executable(bin/"openapi", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/openapi --version")
+
+    system bin/"openapi", "spec", "bootstrap", "test-api.yaml"
+    assert_path_exists testpath/"test-api.yaml"
+
+    system bin/"openapi", "spec", "validate", "test-api.yaml"
+  end
+end

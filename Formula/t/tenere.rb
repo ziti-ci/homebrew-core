61329853beb8e37f2207513d85a316407feb18d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 23:59:06 +0000

    tenere: update 0.11.3 bottle.

--- Formula/t/tenere.rb
+++ Formula/t/tenere.rb
@@ -6,14 +6,13 @@ class Tenere < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "27ab0ab6d18b6c5e4d0e7edad7c2543d158804ec027a5604158419416b876a60"
-    sha256 cellar: :any,                 arm64_sonoma:  "0960f90c6ae608d04793ddb91503109803974b8f3590006655b1d39c411eea59"
-    sha256 cellar: :any,                 arm64_ventura: "aa38fea42e6d5ff2a657d1abbea38712522561bb44a236f0bf2021e4538425fb"
-    sha256 cellar: :any,                 sonoma:        "34cfde676da9db9d63b7d9e153ec5e8da89da08fc49ac53fc0ee99c6034fa9d4"
-    sha256 cellar: :any,                 ventura:       "d928aa81de543a995db52ee06343ea3eeda83027e95e83b392caa9058d40e232"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "90d3fdf2c27e17cb7d24eebc464b5ee2e909193feb891cfde67fce4bace506ad"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "670e6af5f665d27ba5c502aa245ee64175d561c63af071fa7988129c10d25e2f"
+    sha256 cellar: :any,                 arm64_sequoia: "0ef20facf1138986f008cd6fd4b7f0f4ee847c47f8e0f253471044d468222a14"
+    sha256 cellar: :any,                 arm64_sonoma:  "0a7a2b4307ec2afb07dcb5c766c81d7a1454d3d0b668e69e4dc8de25e0f051b5"
+    sha256 cellar: :any,                 arm64_ventura: "f1d6be5362295a0be1bf0e3d2c5e1a89f64f3e14521619e4ddcfef824a1f0af1"
+    sha256 cellar: :any,                 sonoma:        "17cde7da4a41e034a6fa20dcbfb8db9091cd6ee05b4b7fb35ad612b3bc061be7"
+    sha256 cellar: :any,                 ventura:       "3cc90383070bf3d7a567028c64914ce022e8d5a9f55c57b7119c07f512902d4c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6ca836b19fd554f3f8e871363dfbe25ea82ceb2016d2209c9f622d49bddbc37d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ac1b9027c10ec8262887b680068256db66ce69499ea85d2eff4d7814031e07a"
   end
 
   depends_on "pkgconf" => :build

02de955382d3bea3d353c6a62e4896708437d085
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-02 11:20:59 +0000

    somo 1.2.0

--- Formula/s/somo.rb
+++ Formula/s/somo.rb
@@ -1,8 +1,8 @@
 class Somo < Formula
   desc "Human-friendly alternative to netstat for socket and port monitoring"
   homepage "https://github.com/theopfr/somo"
-  url "https://github.com/theopfr/somo/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "8026a39058a0e71cc603cd887b4fd5c0eb8ff310fb5ee1a36ff98ebe90be5878"
+  url "https://github.com/theopfr/somo/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "15b2644ee13e70d9c2ecd1bc3171e1722ef609484e4bac708d1b7d5b0d129a66"
   license "MIT"
 
   bottle do

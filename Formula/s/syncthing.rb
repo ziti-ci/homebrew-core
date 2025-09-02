6dd8bdb6780092d58e4df304f5cd698c9f57e405
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-02 20:34:11 +0000

    syncthing 2.0.5

--- Formula/s/syncthing.rb
+++ Formula/s/syncthing.rb
@@ -1,8 +1,8 @@
 class Syncthing < Formula
   desc "Open source continuous file synchronization application"
   homepage "https://syncthing.net/"
-  url "https://github.com/syncthing/syncthing/archive/refs/tags/v2.0.4.tar.gz"
-  sha256 "682940b141bef4b4c58e42eed3aa05e5586e5b179898967d49b3b12029ca9206"
+  url "https://github.com/syncthing/syncthing/archive/refs/tags/v2.0.5.tar.gz"
+  sha256 "02b8a1d8591a687ca15fbd95267e05bcb33c20607d5287b005ffd379ebfb8d23"
   license "MPL-2.0"
   head "https://github.com/syncthing/syncthing.git", branch: "main"
 

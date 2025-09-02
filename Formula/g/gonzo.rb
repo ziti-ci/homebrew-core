1da8585152b11ddefa57e2dddf6608189775a3c8
Rui Chen <rui@chenrui.dev>
2025-09-01 23:40:01 -0400

    gonzo 0.1.6 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    gonzo: update desc
    
    Co-authored-by: Carlo Cabrera <github@carlo.cab>

---
+++ Formula/g/gonzo.rb
@@ -0,0 +1,35 @@
+class Gonzo < Formula
+  desc "Log analysis TUI"
+  homepage "https://gonzo.controltheory.com/"
+  url "https://github.com/control-theory/gonzo/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "255564415e97322b80db29e947bd8de11699900b9389bfe82f5477973bc011a8"
+  license "MIT"
+  head "https://github.com/control-theory/gonzo.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=#{tap.user}
+      -X main.buildTime=#{time.iso8601}
+      -X main.goVersion=#{Formula["go"].version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/gonzo"
+
+    generate_completions_from_executable(bin/"gonzo", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gonzo --version")
+
+    (testpath/"app.log").write <<~EOS
+      2025-09-01T12:00:00Z INFO app started
+      2025-09-01T12:01:00Z ERROR failed to connect to db
+      2025-09-01T12:02:00Z WARN retrying connection
+    EOS
+    output = shell_output("#{bin}/gonzo --test-mode -f #{testpath}/app.log")
+    assert_match "Test completed successfully", output
+  end
+end

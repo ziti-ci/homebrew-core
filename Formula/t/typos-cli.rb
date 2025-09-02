6300d4d25d774bd60ac376f9b847c6ceb0d6df80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-02 14:31:42 +0000

    typos-cli 1.36.0

--- Formula/t/typos-cli.rb
+++ Formula/t/typos-cli.rb
@@ -1,8 +1,8 @@
 class TyposCli < Formula
   desc "Source code spell checker"
   homepage "https://github.com/crate-ci/typos"
-  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.35.8.tar.gz"
-  sha256 "a354818ad67974cc7a45ed33aee17b51f66d59771f2d6b5a4f15471e94dae948"
+  url "https://github.com/crate-ci/typos/archive/refs/tags/v1.36.0.tar.gz"
+  sha256 "bd5080e4b418e07fc8a6c1d239fc412faf70d102b6585e4c76d15115d6c87c23"
   license any_of: ["Apache-2.0", "MIT"]
 
   livecheck do

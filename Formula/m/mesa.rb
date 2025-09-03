e4b268b67387d1d6372012ccce2afb09b1019e1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-03 14:15:13 +0000

    mesa 25.2.2

--- Formula/m/mesa.rb
+++ Formula/m/mesa.rb
@@ -3,8 +3,8 @@ class Mesa < Formula
 
   desc "Graphics Library"
   homepage "https://www.mesa3d.org/"
-  url "https://archive.mesa3d.org/mesa-25.2.1.tar.xz"
-  sha256 "c124372189d35f48e049ee503029171c68962c580971cb86d968a6771c965ba4"
+  url "https://archive.mesa3d.org/mesa-25.2.2.tar.xz"
+  sha256 "43d7abcd4aa8049d8fd75538344a374104765e81e17b4a6314cee2c0160e4412"
   license all_of: [
     "MIT",
     "Apache-2.0", # include/{EGL,GLES*,vk_video,vulkan}, src/egl/generate/egl.xml, src/mapi/glapi/registry/gl.xml
@@ -20,7 +20,6 @@ class Mesa < Formula
     { "GPL-1.0-or-later" => { with: "Linux-syscall-note" } }, # include/drm-uapi/sync_file.h
     { "GPL-2.0-only" => { with: "Linux-syscall-note" } }, # include/drm-uapi/{d3dkmthk.h,dma-buf.h,etnaviv_drm.h}
   ]
-  revision 1
   head "https://gitlab.freedesktop.org/mesa/mesa.git", branch: "main"
 
   bottle do

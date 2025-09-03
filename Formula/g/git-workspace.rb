47562a9a708f93d5fd06d678284dae32fa4ea1cb
Branch Vincent <branchevincent@gmail.com>
2025-09-02 22:24:03 -0700

    git-workspace: add shell completions

--- Formula/g/git-workspace.rb
+++ Formula/g/git-workspace.rb
@@ -1,11 +1,25 @@
 class GitWorkspace < Formula
   desc "Sync personal and work git repositories from multiple providers"
   homepage "https://github.com/orf/git-workspace"
-  url "https://github.com/orf/git-workspace/archive/refs/tags/v1.9.0.tar.gz"
-  sha256 "d5e2a5a0a568c46b408f82f981ea3672066d4496755fc14837e553e451c69f2d"
   license "MIT"
+  head "https://github.com/orf/git-workspace.git", branch: "master"
 
-  no_autobump! because: :requires_manual_review
+  stable do
+    url "https://github.com/orf/git-workspace/archive/refs/tags/v1.9.0.tar.gz"
+    sha256 "d5e2a5a0a568c46b408f82f981ea3672066d4496755fc14837e553e451c69f2d"
+
+    # Add command to generate shell completions: https://github.com/orf/git-workspace/pull/422
+    patch do
+      url "https://github.com/orf/git-workspace/commit/64a45045aa8297cdbaf081b76e5af8531f7c4364.patch?full_index=1"
+      sha256 "759bbc4ca0ee122758c9eeb0e4ea19bea09d9d5a2ad10c1f26680a441750f0f8"
+    end
+    patch do
+      url "https://github.com/orf/git-workspace/commit/1a1e35a99a783e02c563b531e5eb370cd7e80c9f.patch?full_index=1"
+      sha256 "f6fd8bd2e7fe5ec188883e7d7f19328a48d8c68a39733ff3de4a3639a1addb88"
+    end
+  end
+
+  no_autobump! because: :bumped_by_upstream
 
   bottle do
     rebuild 1
@@ -32,6 +46,8 @@ class GitWorkspace < Formula
     ENV["OPENSSL_NO_VENDOR"] = "1"
 
     system "cargo", "install", *std_cargo_args
+    ENV["GIT_WORKSPACE"] = buildpath
+    generate_completions_from_executable(bin/"git-workspace", "completion")
   end
 
   test do
@@ -40,7 +56,7 @@ class GitWorkspace < Formula
     ENV["GIT_WORKSPACE"] = Pathname.pwd
     ENV["GITHUB_TOKEN"] = "foo"
     system bin/"git-workspace", "add", "github", "foo"
-    assert_match "provider = \"github\"", File.read("workspace.toml")
+    assert_match 'provider = "github"', File.read("workspace.toml")
     output = shell_output("#{bin}/git-workspace update 2>&1", 1)
     assert_match "Error fetching repositories from Github user/org foo", output
 

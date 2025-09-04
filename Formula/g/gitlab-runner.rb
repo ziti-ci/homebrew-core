9b21dadc2804d5cac4f38ed105313876a2727552
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-04 17:31:40 +0000

    gitlab-runner: update 18.3.1 bottle.

--- Formula/g/gitlab-runner.rb
+++ Formula/g/gitlab-runner.rb
@@ -13,12 +13,12 @@ class GitlabRunner < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0496915af6e4484c22c6f798fc2223afc9e209dff4636d61f86364ca808fb43a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0496915af6e4484c22c6f798fc2223afc9e209dff4636d61f86364ca808fb43a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "0496915af6e4484c22c6f798fc2223afc9e209dff4636d61f86364ca808fb43a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7f2ab7c345e2d6714587e298cee9f917c30debbce29249c9c29484d89fb27285"
-    sha256 cellar: :any_skip_relocation, ventura:       "7f2ab7c345e2d6714587e298cee9f917c30debbce29249c9c29484d89fb27285"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b1b93a508f7836bac31fd9bf54ae8ed62ade49435e6eb0fa43eee7a0621b4a5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73f3c6bbedd8563670243a9c1e2577de42d6e421d36fc7b557b0f646c6fa0af6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "73f3c6bbedd8563670243a9c1e2577de42d6e421d36fc7b557b0f646c6fa0af6"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "73f3c6bbedd8563670243a9c1e2577de42d6e421d36fc7b557b0f646c6fa0af6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4761c775fe682a5b629d78773d2ed612413fbde283fa223f3205f39b6eb75cb3"
+    sha256 cellar: :any_skip_relocation, ventura:       "4761c775fe682a5b629d78773d2ed612413fbde283fa223f3205f39b6eb75cb3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eab56ba765cc27f3344e9bec7697d8cfb04d346ca4b9552e8c14e86a08777af6"
   end
 
   depends_on "go" => :build

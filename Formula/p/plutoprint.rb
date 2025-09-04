30fb213c0b7a35fcdc4f6390a2ef14addf9f60d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-04 02:22:20 +0000

    plutoprint: add 0.9.0 bottle.

--- Formula/p/plutoprint.rb
+++ Formula/p/plutoprint.rb
@@ -7,6 +7,15 @@ class Plutoprint < Formula
   sha256 "05c3c425dd57dbd5db0c26449e17e9e8cbc4eb041320697fa7c3f042dd3c1960"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "47b20dcb6d4b4bcc9d631b923e716c115088437a564607dcc91c1cb8ea3d3d7d"
+    sha256 cellar: :any,                 arm64_sonoma:  "8570fc375cdd7d4ec7ab531bb30a6ff4dcf69e3953ce8b658c21ea1b36629a41"
+    sha256 cellar: :any,                 arm64_ventura: "d8f56880b26cd51abdd93fb423959eae3454d603f8fe000c41f411a2da2cbcbc"
+    sha256 cellar: :any,                 sonoma:        "87ac3e7b0be2ddabf7f2fb18bae6d594eafc2f8e9b93adbf8b19946b45ff8213"
+    sha256 cellar: :any,                 ventura:       "1b68863b5c31c3cba94561571b39dd571192c0cd0670d4d853fd220ad48b30bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b289becd2a56ae0d25a6cfe4f82e1d4f45af92a76a81be67b3999692e04d7d5b"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build

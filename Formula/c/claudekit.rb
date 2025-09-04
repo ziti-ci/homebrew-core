7fedad086e4e058305aa9114058cf75e3c76714c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-04 20:32:49 +0000

    claudekit: update 0.8.14 bottle.

--- Formula/c/claudekit.rb
+++ Formula/c/claudekit.rb
@@ -6,7 +6,7 @@ class Claudekit < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "49a924cf6eb3dc17d4ff908d00ed1a4fc76cc9c248e2a8e4f6012d5ea9617036"
+    sha256 cellar: :any_skip_relocation, all: "6a8bfe2d50d3365c37bf4b3ea564750c2aa532655fc582dd2c9a3a768b27b594"
   end
 
   depends_on "node"

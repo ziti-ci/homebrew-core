9522aea7b8438b7191d30c88d4fe9db3319aaa69
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-04 11:43:22 +0000

    gobuster: update 3.8.2 bottle.

--- Formula/g/gobuster.rb
+++ Formula/g/gobuster.rb
@@ -7,12 +7,12 @@ class Gobuster < Formula
   head "https://github.com/OJ/gobuster.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ef43cf91c4fcca6e0e17328f29f8949fe3f35f868277d893fed0c1f84c6749f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3ef43cf91c4fcca6e0e17328f29f8949fe3f35f868277d893fed0c1f84c6749f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3ef43cf91c4fcca6e0e17328f29f8949fe3f35f868277d893fed0c1f84c6749f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c6943c5bfbb03390266b87e0d0eb0ef1b2a46e1484f6b24cb4f1662abd260589"
-    sha256 cellar: :any_skip_relocation, ventura:       "c6943c5bfbb03390266b87e0d0eb0ef1b2a46e1484f6b24cb4f1662abd260589"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a68ad8266129a888db2ba56be5378d402bcc846533311d2001537a566d91e016"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b342220612d825ea80bd1c5569a291868e309ea1b29a9c074a98b6e8ccb6ea19"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b342220612d825ea80bd1c5569a291868e309ea1b29a9c074a98b6e8ccb6ea19"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "b342220612d825ea80bd1c5569a291868e309ea1b29a9c074a98b6e8ccb6ea19"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b5ff54ece6cb45fe81471c6ef9e60ecbd5bdaaded4a76ccb5a47ce7214e4afb9"
+    sha256 cellar: :any_skip_relocation, ventura:       "b5ff54ece6cb45fe81471c6ef9e60ecbd5bdaaded4a76ccb5a47ce7214e4afb9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb7fce23ca43b8a3cad902ca7ee55062492b3412ce79175da1dbd04187a20483"
   end
 
   depends_on "go" => :build

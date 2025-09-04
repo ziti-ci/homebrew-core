37ddd16fc9830db193424281fab5dbf1a4e133fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-04 00:44:52 +0000

    nanoarrow: add 0.7.0 bottle.

--- Formula/n/nanoarrow.rb
+++ Formula/n/nanoarrow.rb
@@ -5,6 +5,15 @@ class Nanoarrow < Formula
   sha256 "bb422ce7ed486abd95eb027a1ac092bbc1b5ed44955e89c098f0a1cb75109d5c"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "bf71bbbdde9c971b89659c9ae196fc56ce3e7b180caacc3640a62cf20b7c62f1"
+    sha256 cellar: :any,                 arm64_sonoma:  "672338d155c58aa60c7fe8107909b6c12c7b39d97581741b6073d6941867bafd"
+    sha256 cellar: :any,                 arm64_ventura: "997e95fcd0948c90c9680f98fe02c74b91dcced1937cc2dc74940711753e8ed4"
+    sha256 cellar: :any,                 sonoma:        "5f4126977b7e17a6701822ec3529b8f550cd39d48c2de275e35d0a5dc0588609"
+    sha256 cellar: :any,                 ventura:       "2c50ca39d9ad131e3bc0ef1583f301a79e41cb8831603b472e7c4bdc8f6a47a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8c7cb7755e696b37ea6bc6a64cc1602f03eddab8e89e285ef17b138e98658d1"
+  end
+
   depends_on "cmake" => :build
 
   def install

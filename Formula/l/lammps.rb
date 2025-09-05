565ec4c48b75c94b675d521f5059918474d99410
botantony <antonsm21@gmail.com>
2025-09-05 03:05:58 +0200

    lammps 20250722-update1
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/l/lammps.rb
+++ Formula/l/lammps.rb
@@ -1,12 +1,12 @@
 class Lammps < Formula
   desc "Molecular Dynamics Simulator"
   homepage "https://docs.lammps.org/"
-  url "https://github.com/lammps/lammps/archive/refs/tags/stable_22Jul2025.tar.gz"
+  url "https://github.com/lammps/lammps/archive/refs/tags/stable_22Jul2025_update1.tar.gz"
   # lammps releases are named after their release date. We transform it to
   # YYYY-MM-DD (year-month-day) so that we get a sane version numbering.
   # We only track stable releases as announced on the LAMMPS homepage.
-  version "20250722"
-  sha256 "38d7ab508433f33a53e11f0502aa0253945ce45d5595baf69665961c0a76da26"
+  version "20250722-update1"
+  sha256 "4ba3648fae360ea1d3106e08bce13e21f856318196f4965f2a09fd812d572928"
   license "GPL-2.0-only"
 
   # The `strategy` block below is used to massage upstream tags into the

d2d3596efa478de6fa9a4884e21079e0d284da17
Liviru <119770778+livfy@users.noreply.github.com>
2025-09-05 23:08:50 +1000

    nextflow 25.04.6 (new formula)
    nextflow: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/n/nextflow.rb
@@ -0,0 +1,51 @@
+class Nextflow < Formula
+  desc "Reproducible scientific workflows"
+  homepage "https://nextflow.io"
+  url "https://github.com/nextflow-io/nextflow/archive/refs/tags/v25.04.6.tar.gz"
+  sha256 "e0891da76f2e17336eaeadac14971a7e69cdc07722d44b605489c4dc6718d6a5"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "gradle" => :build
+  depends_on "openjdk"
+
+  def install
+    # update Foojay plugin for gradle 9 compatibility, upstream pr ref, https://github.com/nextflow-io/nextflow/pull/6388
+    inreplace "settings.gradle", "0.7.0", "1.0.0"
+
+    ENV["BUILD_PACK"] = "1"
+
+    system "gradle", "pack", "--no-daemon", "-x", "test"
+    libexec.install "build/releases/nextflow-#{version}-dist" => "nextflow"
+
+    (bin/"nextflow").write_env_script libexec/"nextflow", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    (testpath/"hello.nf").write <<~EOS
+      process hello {
+        publishDir "results", mode: "copy"
+
+        output:
+        path "hello.txt"
+
+        script:
+        """
+        echo 'Hello!' > hello.txt
+        """
+      }
+      workflow {
+        hello()
+      }
+    EOS
+
+    system bin/"nextflow", "run", "hello.nf"
+
+    assert_path_exists testpath/"results/hello.txt"
+    assert_match "Hello!", (testpath/"results/hello.txt").read
+  end
+end

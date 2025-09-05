e5577159c3c017edba43401a77f1a7627b8e6e58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-05 01:44:37 +0000

    bom: add 0.6.0 bottle.

--- Formula/b/bom.rb
+++ Formula/b/bom.rb
@@ -6,6 +6,15 @@ class Bom < Formula
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/bom.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d2df37362a07cdb818a262c36e8a4af13ec15d97bd6cf20c2e0c56465241b576"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d2df37362a07cdb818a262c36e8a4af13ec15d97bd6cf20c2e0c56465241b576"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d2df37362a07cdb818a262c36e8a4af13ec15d97bd6cf20c2e0c56465241b576"
+    sha256 cellar: :any_skip_relocation, sonoma:        "21138d3335d9e12dacb071ac92897b45293039d592ad063bf16dd14cdcd116c9"
+    sha256 cellar: :any_skip_relocation, ventura:       "21138d3335d9e12dacb071ac92897b45293039d592ad063bf16dd14cdcd116c9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "163a281909261e7063257b791ebc3cc00c630564b78ad0182a6498feb4dc6844"
+  end
+
   depends_on "go" => :build
 
   def install

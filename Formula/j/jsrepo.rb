d2f18020e6600f654906e6c868c6c73df1249984
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-05 17:11:56 +0000

    jsrepo 2.4.6
    jsrepo: remove macos-term-size dependency
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/j/jsrepo.rb
+++ Formula/j/jsrepo.rb
@@ -1,8 +1,8 @@
 class Jsrepo < Formula
   desc "Build and distribute your code"
   homepage "https://jsrepo.dev/"
-  url "https://registry.npmjs.org/jsrepo/-/jsrepo-2.4.5.tgz"
-  sha256 "7b833899ca60a142b338c73a9cc3d216dd0690ffad68ff31c951a3de8269cd56"
+  url "https://registry.npmjs.org/jsrepo/-/jsrepo-2.4.6.tgz"
+  sha256 "e8996c9846ca54f8479c491509e1bd9b7b93846b2b7ef1d863fca6ff8dd54e90"
   license "MIT"
 
   bottle do
@@ -17,23 +17,9 @@ class Jsrepo < Formula
 
   depends_on "node"
 
-  on_macos do
-    depends_on "macos-term-size"
-  end
-
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink libexec.glob("bin/*")
-
-    term_size_vendor_dir = libexec/"lib/node_modules/jsrepo/node_modules/term-size/vendor"
-    rm_r(term_size_vendor_dir) # remove pre-built binaries
-
-    if OS.mac?
-      macos_dir = term_size_vendor_dir/"macos"
-      macos_dir.mkpath
-      # Replace the vendored pre-built term-size with one we build ourselves
-      ln_sf (Formula["macos-term-size"].opt_bin/"term-size").relative_path_from(macos_dir), macos_dir
-    end
   end
 
   test do

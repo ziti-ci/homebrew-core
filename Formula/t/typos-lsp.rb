2abfc20ebe6856fd6989f63a5003d5543c077885
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-05 11:30:15 +0000

    typos-lsp 0.1.43

--- Formula/t/typos-lsp.rb
+++ Formula/t/typos-lsp.rb
@@ -1,8 +1,8 @@
 class TyposLsp < Formula
   desc "Language Server for typos-cli"
   homepage "https://github.com/tekumara/typos-lsp"
-  url "https://github.com/tekumara/typos-lsp/archive/refs/tags/v0.1.42.tar.gz"
-  sha256 "296168e4e9d6db3c457ade75292f734450f65d19aa7c79e1751a7b09367c10b8"
+  url "https://github.com/tekumara/typos-lsp/archive/refs/tags/v0.1.43.tar.gz"
+  sha256 "974c0b30dec38426cfe7526a0e9b14fe88600b6b78a741fbe273edc91eaec8d4"
   license "MIT"
 
   livecheck do

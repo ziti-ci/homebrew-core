4f60a3b83bf6fa03e6b8168ccb9362cb3b87fb4b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-05 11:10:50 +0000

    libtorrent-rakshasa 0.16.0
    libtorrent-rakshasa: needs curl for linux build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libtorrent-rakshasa.rb
+++ Formula/lib/libtorrent-rakshasa.rb
@@ -1,8 +1,8 @@
 class LibtorrentRakshasa < Formula
   desc "BitTorrent library with a focus on high performance"
   homepage "https://github.com/rakshasa/libtorrent"
-  url "https://github.com/rakshasa/libtorrent/archive/refs/tags/v0.15.7.tar.gz"
-  sha256 "4958e49c7aa3f288bbd2e96e1aeb4eb523e1f30c7a47747090d62cba268838b9"
+  url "https://github.com/rakshasa/libtorrent/archive/refs/tags/v0.16.0.tar.gz"
+  sha256 "ca3b96bc478db2cd282ccb87b91b169662d7c9298dbca9934f8556c2935cab73"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -26,6 +26,7 @@ class LibtorrentRakshasa < Formula
   depends_on "pkgconf" => :build
   depends_on "openssl@3"
 
+  uses_from_macos "curl"
   uses_from_macos "zlib"
 
   conflicts_with "libtorrent-rasterbar", because: "both use the same libname"

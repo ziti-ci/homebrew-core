bc064995555f691e57f97992b0baaa7d3a1e657d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-06 16:57:18 +0000

    nextflow: add 25.04.6 bottle.

--- Formula/n/nextflow.rb
+++ Formula/n/nextflow.rb
@@ -10,6 +10,15 @@ class Nextflow < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ee7639b6a849dd151ccc378fd753016d757fbcd433e02279327d18dccc0a6961"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ec9d42f665a30a6a3d19a2ccccef2eef498450762091294f746482a5310670e9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "67a0822a5b9e0ecdf061ee32f1e04ef079a8917b4a6d0e5556cc44619c1e4982"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9eb94ac800c2edb004de5fe9a76007356c9b64504026fdb9669a656d5b7f9b4c"
+    sha256 cellar: :any_skip_relocation, ventura:       "e006b164228bdab66ec70019f56fbc936217f0208960e33b8c52c40817effe02"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b54c5600ff39d4d73ca33d62b2f4d4e19401e788f8e268ad727823d61451af8d"
+  end
+
   depends_on "gradle" => :build
   depends_on "openjdk"
 

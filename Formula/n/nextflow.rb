cae71b8a86170fe1c5ed0528096861734fa7f52c
Rui Chen <rui@chenrui.dev>
2025-09-06 17:45:25 +0000

    nextflow: update 25.04.6 bottle.

--- Formula/n/nextflow.rb
+++ Formula/n/nextflow.rb
@@ -16,6 +16,7 @@ class Nextflow < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "67a0822a5b9e0ecdf061ee32f1e04ef079a8917b4a6d0e5556cc44619c1e4982"
     sha256 cellar: :any_skip_relocation, sonoma:        "9eb94ac800c2edb004de5fe9a76007356c9b64504026fdb9669a656d5b7f9b4c"
     sha256 cellar: :any_skip_relocation, ventura:       "e006b164228bdab66ec70019f56fbc936217f0208960e33b8c52c40817effe02"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0a156caeba1d209af940b9b874ff1b9c95c16e0b6081c2849af80cdc5626d650"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "b54c5600ff39d4d73ca33d62b2f4d4e19401e788f8e268ad727823d61451af8d"
   end
 

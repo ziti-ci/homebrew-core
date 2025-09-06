464af87e449befbc6cc71e30c6ba35a2ad94acd5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-06 20:33:23 +0000

    mise: update 2025.9.4 bottle.

--- Formula/m/mise.rb
+++ Formula/m/mise.rb
@@ -12,13 +12,13 @@ class Mise < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e50b61b3ff571860cac1dbb634c4c1168b656d188615da4cdb430876317f8838"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0355366f3cd7a4f074255b823a07cd3048d27deb444def510fd0932587ca769e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "27b97bf9c1da10e34d385f84cf59e27631da5961471d741bb5684a73810c374f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a56c5d44abb624d0fc4408854ee7ee296f0037e882395c56375733e44001a8c9"
-    sha256 cellar: :any_skip_relocation, ventura:       "363f1f1957cb36a7cd2a731ae1781fa843ef3691818efec228798198ffe16487"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fc3b6806937fd9b5d689b30ae415112073621091392b893c05c2ab3f31aa29e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4275cec7e52bc3d86fa3bc01914081d36d2ee175cd20388cd3e40ec76c6f1f4d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe2c215eaa19ea35ae9b14ea55a99655bbd7895e2ccd2fa43f80aeeb0b60dfe0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d983d521aba94521c00a4e19885336baf57e5b390de2a0a5607351a36f7e6e0f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "1815b61f29ab90ca6c54230602f3d5bc38209ddb76247a5a40fcde1b0a6bdb9c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c163f85b86e6c5b39556cf30c66d3a884dbe177cc1ee7139bc44745025e04674"
+    sha256 cellar: :any_skip_relocation, ventura:       "2e0b98e411cbae928ae43312726c109cc9355e69616afe1e3aa6523371be1cf3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "82b39fe5d6727a654ba0b5750f461580d964364d5f5d8d830d5e84516077bfc5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "09f121f0f9c0e573c1ec852e55cafdc6ed75a7a672d2ed88f95755f6e7ffd40b"
   end
 
   depends_on "pkgconf" => :build

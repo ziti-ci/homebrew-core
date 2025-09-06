4e4d9ecde3417256359f6210b6a27f1836ca8b98
Michael Cho <michael@michaelcho.dev>
2025-09-06 00:05:33 -0400

    pandoc-plot: migrate to GHC 9.12

--- Formula/p/pandoc-plot.rb
+++ Formula/p/pandoc-plot.rb
@@ -16,11 +16,13 @@ class PandocPlot < Formula
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc@9.10" => :build
+  depends_on "ghc" => :build
   depends_on "graphviz" => :test
 
+  depends_on "gmp"
   depends_on "pandoc"
 
+  uses_from_macos "libffi"
   uses_from_macos "zlib"
 
   def install

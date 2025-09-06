af3721164dbf09746bb7cb9bf84815ee236c5657
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-06 08:38:05 +0000

    rulesync: update 0.70.0 bottle.

--- Formula/r/rulesync.rb
+++ Formula/r/rulesync.rb
@@ -6,7 +6,7 @@ class Rulesync < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9f80db9d3e67b42ccd8c3263ac0b7fe33b641bb0ec9b268a2c394f81aacd099e"
+    sha256 cellar: :any_skip_relocation, all: "69079978608b7a0ab3327fc59dc79839819d8a9bc4a300a4652236689791f317"
   end
 
   depends_on "node"

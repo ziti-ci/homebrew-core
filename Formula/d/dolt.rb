bc675141d181cee128a48a2d19fb6a362500dfd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-06 04:56:12 +0000

    dolt 1.59.4

--- Formula/d/dolt.rb
+++ Formula/d/dolt.rb
@@ -1,8 +1,8 @@
 class Dolt < Formula
   desc "Git for Data"
   homepage "https://github.com/dolthub/dolt"
-  url "https://github.com/dolthub/dolt/archive/refs/tags/v1.59.2.tar.gz"
-  sha256 "653f07b42d9433a4fdb4bc72babb76514066782696f1c5bf68bbf6e126839ca4"
+  url "https://github.com/dolthub/dolt/archive/refs/tags/v1.59.4.tar.gz"
+  sha256 "68ff4ce1511b4472c38c503ddc4addc307bb366c2d6e6e1d668d2b9632f9b49d"
   license "Apache-2.0"
   version_scheme 1
   head "https://github.com/dolthub/dolt.git", branch: "main"

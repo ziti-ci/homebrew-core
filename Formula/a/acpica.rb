64cd2b71ac7d896f7716d104d508a51453685a48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-06 11:49:46 +0000

    acpica: update 20250807 bottle.

--- Formula/a/acpica.rb
+++ Formula/a/acpica.rb
@@ -14,13 +14,13 @@ class Acpica < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ea322ed5e903610d9264f0a46378ab8cd867dba115ef74782dce02bcfd51c80"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dc4284d256259dcb212c0151422a3689136ba2fc7c706196c4913128644b621f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3b05e09052930c392a5faebd81fdcf4ac304eb0a52ae04914f0a6f2581396518"
-    sha256 cellar: :any_skip_relocation, sonoma:        "140b8b8f802f2ab3e279d54f6f8fe6d0ab267630576d01485bf26dde264f27d8"
-    sha256 cellar: :any_skip_relocation, ventura:       "68c95840000e3a84257a8e9f8d6ef67619e22a468cb53651a9c67579fc95a439"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b86f20efa2264f377f9b690e5624a0758a7b43219fe1f1fc323d774745a97af9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dae5dceec9bd03b975f711f49017095a38102f7dfa995906842dc0023fa602bf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6f32cc9b31ebe6e473306036117315a4bc227380cebe27fe439d79ad10885aba"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d39e58cdf4e911d1b3db8ce1b967ac592c219fb4d089eacbf30386112a3e9ff"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7a484f87cc8f909f8fcd4ff3bdaca228e4f5417ba0436f7e4275047da7d4986e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "68e1a5a0237c94a34ab064ce828faa9c074cf1328ed26dc151c93b0a688bfd2a"
+    sha256 cellar: :any_skip_relocation, ventura:       "bb5e24b60b08d46aaf15de390cf5edb74225b0f5b8b60ffe7c0f3648671c2287"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "31b5ad741d331ec60b502e9c0f57e8a7ae96a30091ad630802f89b2f33c8c685"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d97900731e708343c719eed7cf25d66cc6c345dc0122a6a32e3b0de4c878d838"
   end
 
   uses_from_macos "bison" => :build

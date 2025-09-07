078c412af8f86a64a4a4a74b0f122b9ad3ad59ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-07 23:58:50 +0000

    lazycontainer: add 0.0.1 bottle.

--- Formula/l/lazycontainer.rb
+++ Formula/l/lazycontainer.rb
@@ -6,6 +6,15 @@ class Lazycontainer < Formula
   license "MIT"
   head "https://github.com/andreybleme/lazycontainer.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0dc4071f1c199b099256ecbfae20165aefbeef9e9df7a0262d485f346039d13c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0dc4071f1c199b099256ecbfae20165aefbeef9e9df7a0262d485f346039d13c"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "0dc4071f1c199b099256ecbfae20165aefbeef9e9df7a0262d485f346039d13c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9ce2b590419deecde11f0a99a3cb3a22591ddc7a80a6074c867394baa7d8da66"
+    sha256 cellar: :any_skip_relocation, ventura:       "9ce2b590419deecde11f0a99a3cb3a22591ddc7a80a6074c867394baa7d8da66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b28e111c6514af466fa079101c6b027f543f7c23193dd3c65056429c7e933122"
+  end
+
   depends_on "go" => :build
 
   def install

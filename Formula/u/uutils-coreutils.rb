267e77f514a1b98a914cde3cea72a565f9a61a33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-07 14:44:52 +0000

    uutils-coreutils: update 0.2.0 bottle.

--- Formula/u/uutils-coreutils.rb
+++ Formula/u/uutils-coreutils.rb
@@ -12,13 +12,13 @@ class UutilsCoreutils < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "efc20adfb97e84ed67eeddd69dfad9600b64d916a30b6ddb37ec1ec86e1b0d65"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f8e49f4ecacb9a3ff50258bc2b33e04eb049b68149bf4bdd362590995ea9dc1a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "72b467ed6f6f15ec60a3cee34fa365bd1121364afb925999c00304b8a33fde62"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c8af817bc30896c6ee0a6d37f0f58345a0c5a9f81ddc1e4f479a9ab79ac5ecdd"
-    sha256 cellar: :any_skip_relocation, ventura:       "b695ba77c5d41cff1f0856d9287c72994b8d94d54de414b0447194675c8d37db"
-    sha256                               arm64_linux:   "5f63af088aaf76c4349dd441b1423d5df8a6c722247c66d1663b8023d03d8feb"
-    sha256                               x86_64_linux:  "a7b684d72256a852a02a9261707c51e1098033aa25bac60b08629d3ae879631a"
+    sha256 cellar: :any,                 arm64_sequoia: "912680e848466669cbb65c3e744a5b96e240da36cb32d516b0d0a0db87ea26c3"
+    sha256 cellar: :any,                 arm64_sonoma:  "cc1830b49fe485cf5c2666bdaca9dc7ed967c6054d803ca4dc9bbfaf007b51bb"
+    sha256 cellar: :any,                 arm64_ventura: "a7725e305b7890270eb1c3b6870ea6524e9b926a58ecf75d630a75dda34fd6a2"
+    sha256 cellar: :any,                 sonoma:        "02f4f2934ebe230f37a86cbc8d296a3472d8db5de26505b0dc7f175e844f4aee"
+    sha256 cellar: :any,                 ventura:       "3b9a44d6cec1f7c06697c328b8c8275b854d04df4ef4b03ff02d979cd8a77cea"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "61f30b0be733a2c05cceb2b16d277612a5d7c1505c1609a886f94ff35019b276"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "baf9bc9639ed4dfffddb9ee58d8fd0ec95e54e1175288e65aed831b650cb774d"
   end
 
   depends_on "make" => :build

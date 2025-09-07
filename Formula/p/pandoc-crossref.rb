31fa5bb56f41e60251a7122fb089fc98113bdbdf
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-09-07 11:09:32 +0900

    pandoc-crossref: revision bump (pandoc 3.8)
    pandoc-crossref: support pandoc 3.8
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    pandoc-crossref: add patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pandoc-crossref.rb
+++ Formula/p/pandoc-crossref.rb
@@ -4,6 +4,7 @@ class PandocCrossref < Formula
   url "https://github.com/lierdakil/pandoc-crossref/archive/refs/tags/v0.3.20.tar.gz"
   sha256 "935d66e4b52323aba625b2bfa90abfea774816ccf4feb959e8271beac6d9b453"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "34a21d6a699b4e8ce6b7a9959a6e5f2afb190df331d06688ecb185883136eac9"
@@ -22,6 +23,10 @@ class PandocCrossref < Formula
 
   uses_from_macos "unzip" => :build
   uses_from_macos "zlib"
+
+  # upstream bug report, https://github.com/lierdakil/pandoc-crossref/issues/482
+  # support new writerHighlightMethod, upstream pr ref, https://github.com/lierdakil/pandoc-crossref/pull/484
+  patch :DATA
 
   def install
     rm("cabal.project.freeze")
@@ -43,3 +48,105 @@ class PandocCrossref < Formula
     refute_match "WARNING: pandoc-crossref was compiled", output
   end
 end
+
+__END__
+diff --git a/package.yaml b/package.yaml
+index eae4b5a..c7455c0 100644
+--- a/package.yaml
++++ b/package.yaml
+@@ -30,7 +30,7 @@ data-files:
+ dependencies:
+   base: ">=4.16 && <5"
+   text: ">=1.2.2 && <2.2"
+-  pandoc: ">=3.7.0.2 && < 3.8"
++  pandoc: ">=3.7.0.2 && < 3.9"
+   pandoc-types: ">= 1.23 && < 1.24"
+ _deps:
+   containers: &containers { containers: ">=0.1 && <0.7" }
+diff --git a/pandoc-crossref.cabal b/pandoc-crossref.cabal
+index 5270aa1..48a796f 100644
+--- a/pandoc-crossref.cabal
++++ b/pandoc-crossref.cabal
+@@ -169,7 +169,7 @@ library
+     , microlens >=0.4.12.0 && <0.5.0.0
+     , microlens-mtl >=0.2.0.1 && <0.3.0.0
+     , mtl >=1.1 && <2.4
+-    , pandoc >=3.7.0.2 && <3.8
++    , pandoc >=3.7.0.2 && <3.9
+     , pandoc-crossref-internal
+     , pandoc-types ==1.23.*
+     , text >=1.2.2 && <2.2
+@@ -223,7 +223,7 @@ library pandoc-crossref-internal
+     , microlens-mtl >=0.2.0.1 && <0.3.0.0
+     , microlens-th >=0.4.3.10 && <0.5.0.0
+     , mtl >=1.1 && <2.4
+-    , pandoc >=3.7.0.2 && <3.8
++    , pandoc >=3.7.0.2 && <3.9
+     , pandoc-types ==1.23.*
+     , syb >=0.4 && <0.8
+     , template-haskell >=2.7.0.0 && <3.0.0.0
+@@ -253,7 +253,7 @@ executable pandoc-crossref
+     , gitrev >=1.3.1 && <1.4
+     , open-browser ==0.2.*
+     , optparse-applicative >=0.13 && <0.19
+-    , pandoc >=3.7.0.2 && <3.8
++    , pandoc >=3.7.0.2 && <3.9
+     , pandoc-crossref
+     , pandoc-types ==1.23.*
+     , template-haskell >=2.7.0.0 && <3.0.0.0
+@@ -283,7 +283,7 @@ test-suite test-integrative
+     , directory >=1 && <1.4
+     , filepath >=1.1 && <1.6
+     , hspec >=2.4.4 && <3
+-    , pandoc >=3.7.0.2 && <3.8
++    , pandoc >=3.7.0.2 && <3.9
+     , pandoc-crossref
+     , pandoc-types ==1.23.*
+     , text >=1.2.2 && <2.2
+@@ -319,7 +319,7 @@ test-suite test-pandoc-crossref
+     , microlens >=0.4.12.0 && <0.5.0.0
+     , microlens-mtl >=0.2.0.1 && <0.3.0.0
+     , mtl >=1.1 && <2.4
+-    , pandoc >=3.7.0.2 && <3.8
++    , pandoc >=3.7.0.2 && <3.9
+     , pandoc-crossref
+     , pandoc-crossref-internal
+     , pandoc-types ==1.23.*
+@@ -348,7 +348,7 @@ benchmark simple
+   build-depends:
+       base >=4.16 && <5
+     , criterion >=1.5.9.0 && <1.7
+-    , pandoc >=3.7.0.2 && <3.8
++    , pandoc >=3.7.0.2 && <3.9
+     , pandoc-crossref
+     , pandoc-types ==1.23.*
+     , text >=1.2.2 && <2.2
+diff --git a/src/ManData.hs b/src/ManData.hs
+index 5e6b6cc06f67a014c753cc149e93acd338573446..21b21cb7a1f51c048f5892af3eef8dbb093db32f 100644
+--- a/src/ManData.hs
++++ b/src/ManData.hs
+@@ -62,7 +62,7 @@ embedManualHtml = do
+           $   P.compileDefaultTemplate "html5"
+   embedManual $ P.writeHtml5String P.def{
+     P.writerTemplate = Just tt
+-  , P.writerHighlightStyle = Just pygments
++  , P.writerHighlightMethod = P.Skylighting pygments
+   , P.writerTOCDepth = 6
+   , P.writerTableOfContents = True
+   }
+diff --git a/test/test-integrative.hs b/test/test-integrative.hs
+index f367265..3218dcc 100644
+--- a/test/test-integrative.hs
++++ b/test/test-integrative.hs
+@@ -42,8 +42,9 @@ m2m dir
+     expect_md <- runIO $ readFile ("test" </> "m2m" </> dir </> "expect.md")
+     let ro = def { readerExtensions = pandocExtensions }
+         wo = def { writerExtensions = disableExtension Ext_raw_html $ disableExtension Ext_raw_attribute pandocExtensions
+-                 , writerHighlightStyle=Just pygments
+-                 , writerListings = dir `elem` listingsDirs }
++                , writerHighlightMethod = if dir `elem` listingsDirs
++                      then IdiomaticHighlighting
++                      else Skylighting pygments }
+     p@(Pandoc meta _) <- runIO $ either (error . show) id <$> P.runIO (readMarkdown ro $ T.pack input)
+     let actual_md = either (fail . show) T.unpack $ runPure $ writeMarkdown wo $ runCrossRef meta (Just $ Format "markdown") defaultCrossRefAction p
+     it "Markdown" $ do

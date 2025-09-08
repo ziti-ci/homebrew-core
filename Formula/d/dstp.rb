116acacb5715613ba26d8fa11b06ae34cfecdf2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 08:46:29 +0000

    dstp: add 0.4.23 bottle.

--- Formula/d/dstp.rb
+++ Formula/d/dstp.rb
@@ -6,6 +6,15 @@ class Dstp < Formula
   license "MIT"
   head "https://github.com/ycd/dstp.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d846454cb98d9ca168c8a2975a2d551a00b444afa2a3a0599d4bc956e24c9b35"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d846454cb98d9ca168c8a2975a2d551a00b444afa2a3a0599d4bc956e24c9b35"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "d846454cb98d9ca168c8a2975a2d551a00b444afa2a3a0599d4bc956e24c9b35"
+    sha256 cellar: :any_skip_relocation, sonoma:        "cb67648c382f56115631967b9d5c7fc76c9209299e524ea22f7d32c48e44f34c"
+    sha256 cellar: :any_skip_relocation, ventura:       "cb67648c382f56115631967b9d5c7fc76c9209299e524ea22f7d32c48e44f34c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7bbbb70466510b3a9d41a5a6479b9a667d7d55deaf87e739a0957201995ded19"
+  end
+
   depends_on "go" => :build
 
   def install

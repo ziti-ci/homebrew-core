5873ee26410c617d4c3f30557458011cff75efa4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 08:53:05 +0000

    changelogen: add 0.6.2 bottle.

--- Formula/c/changelogen.rb
+++ Formula/c/changelogen.rb
@@ -5,6 +5,10 @@ class Changelogen < Formula
   sha256 "cd5e783f11a9496293d2c7790e36574981296192849a9d904dec617e65e257b2"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8f81b6116ef1eb1212acc82b881c4bd1c099d9a3221c76ea12203be07f73f2a9"
+  end
+
   depends_on "node"
 
   def install

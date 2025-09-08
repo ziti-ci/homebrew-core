6553f3db164df935a98392319cbf961b7c9416fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 20:16:01 +0000

    schema-evolution-manager 0.9.56

--- Formula/s/schema-evolution-manager.rb
+++ Formula/s/schema-evolution-manager.rb
@@ -1,8 +1,8 @@
 class SchemaEvolutionManager < Formula
   desc "Manage postgresql database schema migrations"
   homepage "https://github.com/mbryzek/schema-evolution-manager"
-  url "https://github.com/mbryzek/schema-evolution-manager/archive/refs/tags/0.9.55.tar.gz"
-  sha256 "2db470f9d2000b3b11e3cad9e272f4f3d127a4992703338c42c5e9e294c9395d"
+  url "https://github.com/mbryzek/schema-evolution-manager/archive/refs/tags/0.9.56.tar.gz"
+  sha256 "f13870ead84d3f55488d30fb751f17983df0c436dc1aa483ea5dec934fbac5a4"
   license "Apache-2.0"
 
   bottle do

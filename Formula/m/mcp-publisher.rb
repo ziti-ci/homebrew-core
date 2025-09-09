0e6fddf5d1f3635bfa4b2f456cb6c25f91de0bec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-09 02:36:45 +0000

    mcp-publisher: add 1.0.0 bottle.

--- Formula/m/mcp-publisher.rb
+++ Formula/m/mcp-publisher.rb
@@ -6,6 +6,15 @@ class McpPublisher < Formula
   license "MIT"
   head "https://github.com/modelcontextprotocol/registry.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bba32e606e019a19dd1951e46ece9c8f1d1322a98be5c81e3a05fdca487ab96f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bba32e606e019a19dd1951e46ece9c8f1d1322a98be5c81e3a05fdca487ab96f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "bba32e606e019a19dd1951e46ece9c8f1d1322a98be5c81e3a05fdca487ab96f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "45539fa7500424a1e0ff37dfa88505d902db09b452dae7aded5f6566aacbf9ef"
+    sha256 cellar: :any_skip_relocation, ventura:       "45539fa7500424a1e0ff37dfa88505d902db09b452dae7aded5f6566aacbf9ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2591a1d992fc0fba23a8dc49596def0948a71a14a76ef0b72f1e86023afad0aa"
+  end
+
   depends_on "go" => :build
 
   def install

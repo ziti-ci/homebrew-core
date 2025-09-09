26f5b9a6436094ce038d616c1711e3e6d09a463d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-09 00:37:24 +0000

    schema-evolution-manager: update 0.9.56 bottle.

--- Formula/s/schema-evolution-manager.rb
+++ Formula/s/schema-evolution-manager.rb
@@ -6,7 +6,7 @@ class SchemaEvolutionManager < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0dbe51a97fd028ec696093d1dec6260aed53eaaa086cb7bd10bcede4d3e3db8f"
+    sha256 cellar: :any_skip_relocation, all: "4a1918862dfa68167c32ddc7dd929fd9aadb870b7d785c90c3c35967b5b0e633"
   end
 
   uses_from_macos "ruby"

bfc86ad471723d132bc4a0e4dddfc9b8dfce79e1
Rui Chen <rui@chenrui.dev>
2025-09-09 11:13:03 -0400

    shimmy 1.2.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/s/shimmy.rb
@@ -0,0 +1,35 @@
+class Shimmy < Formula
+  desc "Small local inference server with OpenAI-compatible GGUF endpoints"
+  homepage "https://github.com/Michael-A-Kuykendall/shimmy"
+  url "https://github.com/Michael-A-Kuykendall/shimmy/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "0b1cecce9d47010e24230e6844991467f5d24a59423879fe07db68d3c1aeb852"
+  license "MIT"
+  head "https://github.com/Michael-A-Kuykendall/shimmy.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build # for llama-cpp-sys-2
+  depends_on "rust" => :build
+
+  uses_from_macos "llvm" => :build # for libclang
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  service do
+    run [opt_bin/"shimmy", "serve", "--bind", "127.0.0.1:11435"]
+    keep_alive true
+    log_path var/"log/shimmy.log"
+    error_log_path var/"log/shimmy.error.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/shimmy --version")
+    output = shell_output("#{bin}/shimmy list")
+    assert_match "Total available models: 1", output
+  end
+end

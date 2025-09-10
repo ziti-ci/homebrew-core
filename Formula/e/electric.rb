922b6fe72ce4e1dee46989aae544ad0cd03368ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 02:22:45 +0000

    electric 1.1.10

--- Formula/e/electric.rb
+++ Formula/e/electric.rb
@@ -1,8 +1,8 @@
 class Electric < Formula
   desc "Real-time sync for Postgres"
   homepage "https://electric-sql.com"
-  url "https://github.com/electric-sql/electric/archive/refs/tags/@core/sync-service@1.1.9.tar.gz"
-  sha256 "5de384e5857735f659317edaed328f63ea7ca4d04b74fd3fcba88deee8246618"
+  url "https://github.com/electric-sql/electric/archive/refs/tags/@core/sync-service@1.1.10.tar.gz"
+  sha256 "1b235819ab068baf30984e423abe2085a9d6e7da65188bca5d30df2e40490500"
   license "Apache-2.0"
 
   bottle do

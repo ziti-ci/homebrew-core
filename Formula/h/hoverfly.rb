ced392261009a642b836cd383ecddffec385e500
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 21:24:13 +0000

    hoverfly: update 1.12.0 bottle.

--- Formula/h/hoverfly.rb
+++ Formula/h/hoverfly.rb
@@ -7,12 +7,12 @@ class Hoverfly < Formula
   head "https://github.com/SpectoLabs/hoverfly.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce2056dedd36396ebecc48b10892174de50b8ac36386b7a5d7eafe060b04dae6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ce2056dedd36396ebecc48b10892174de50b8ac36386b7a5d7eafe060b04dae6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ce2056dedd36396ebecc48b10892174de50b8ac36386b7a5d7eafe060b04dae6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1d15dd03eebe265603f8ff1d6d0ad0fb865a107a4fae2760b27514fce82357ca"
-    sha256 cellar: :any_skip_relocation, ventura:       "1d15dd03eebe265603f8ff1d6d0ad0fb865a107a4fae2760b27514fce82357ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "43704d42d48b516ef14b83ec9716c71d10e7c7733949ef6208fc93d83e7ca9f4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f20d4d6207a3fde3c9e7e40fdd93a6c68dfcc9e035d5ce91da10b354f9f4cea1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f20d4d6207a3fde3c9e7e40fdd93a6c68dfcc9e035d5ce91da10b354f9f4cea1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "f20d4d6207a3fde3c9e7e40fdd93a6c68dfcc9e035d5ce91da10b354f9f4cea1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8fafff4289bb6b87854f57d5dbe5fc209e6b93a1aa632a37aeb814afe5b6c95e"
+    sha256 cellar: :any_skip_relocation, ventura:       "8fafff4289bb6b87854f57d5dbe5fc209e6b93a1aa632a37aeb814afe5b6c95e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f01e86a6fd2704d077dd36536e7cd55ed3e1f96f1289e7d72f2e91cbc168660"
   end
 
   depends_on "go" => :build

a7e003da1975f80e79425062b5cd1fb11e5ecc7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 17:25:54 +0000

    zns: update 0.4.0 bottle.

--- Formula/z/zns.rb
+++ Formula/z/zns.rb
@@ -7,12 +7,12 @@ class Zns < Formula
   head "https://github.com/znscli/zns.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6b44e23dcd5f00ee7363edded2f65a8cff2e348a89af0ed97c267278625e285d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6b44e23dcd5f00ee7363edded2f65a8cff2e348a89af0ed97c267278625e285d"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6b44e23dcd5f00ee7363edded2f65a8cff2e348a89af0ed97c267278625e285d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "9fbfa115c982914deef04f605327e1c320967d436718a3d2c0389156713ece26"
-    sha256 cellar: :any_skip_relocation, ventura:       "9fbfa115c982914deef04f605327e1c320967d436718a3d2c0389156713ece26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18f3eb0b09363071f64391d43668b0437106910ec44340a3afd00dc27116bd92"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7cc41244bcfbf7df964ebcb5ac7171abbeee3e9935107ed37e008cfc1ec1103b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7cc41244bcfbf7df964ebcb5ac7171abbeee3e9935107ed37e008cfc1ec1103b"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "7cc41244bcfbf7df964ebcb5ac7171abbeee3e9935107ed37e008cfc1ec1103b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "039f992e91d42aab450a339cd8ab8426f7f3873d423b06fac2a14a13ff9cd882"
+    sha256 cellar: :any_skip_relocation, ventura:       "039f992e91d42aab450a339cd8ab8426f7f3873d423b06fac2a14a13ff9cd882"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7497871ad502dca1485cce5d0e5654bd2e989080dd6914ba2efe880df81a707f"
   end
 
   depends_on "go" => :build

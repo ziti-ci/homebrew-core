a91fd7deba744bbd3289968ffdbeb1ab32377073
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 00:28:42 +0000

    plutobook: update 0.8.0 bottle.

--- Formula/p/plutobook.rb
+++ Formula/p/plutobook.rb
@@ -6,13 +6,13 @@ class Plutobook < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_sequoia: "1ca3e3159e11d28e421bcc5203d3321a2e14b63b90b1cc8f1f1a8a4400ea44f8"
-    sha256 cellar: :any, arm64_sonoma:  "9d7bec20c8fb07c813c38364dea512b36ef7a37939c203c931e385518f76d7e8"
-    sha256 cellar: :any, arm64_ventura: "bb819b4c2de899d3e0ee1c600830de3151ea86284d13d754080ce5513856dc9e"
-    sha256 cellar: :any, sonoma:        "5445fa9a5c5871165846b93db5a53ff5d3d55fe7aa9eadc9d41bd9228dbf1cad"
-    sha256 cellar: :any, ventura:       "7f3d14be468c6d71a128b67f65914ec817c293a831b810195e7f9fdcb4f813b8"
-    sha256               arm64_linux:   "e9a1e3b91decdbb1fca4f61a579b8f10d11395f246bc603ecf7cdf7e7b269e61"
-    sha256               x86_64_linux:  "95f3995698d369a7f6ecc75820c9d919fadcc5311010d34de15f78fa584b5f76"
+    sha256 cellar: :any, arm64_sequoia: "97bec7b8391ef9df32f955c2360c932fb602842086c23c2a985dd8cb48ea021c"
+    sha256 cellar: :any, arm64_sonoma:  "c9857958665f5c1e39f6c2a3bfa24c0e3f1836fa42415dd78a490d8fd58bbd67"
+    sha256 cellar: :any, arm64_ventura: "00a280132ef37a3393bcd50b8406329bba28862cb400d6e0a48c3709d1d1deac"
+    sha256 cellar: :any, sonoma:        "1e6fa58302781f99633a65e3f2ffd8aef519be3c894be1cae27228fbf30ac915"
+    sha256 cellar: :any, ventura:       "1478dfe0f53f682ccdcada3be0f62e35c84afd48317639cb9fa359e2f910d545"
+    sha256               arm64_linux:   "47ce45c4de5ff118a1c7c63bdc9adc7a3256d376cc6d2d4db2b67ebd464f1056"
+    sha256               x86_64_linux:  "3b14e4ad09e9248353912705542ade446a9e3b0b9738d555c753798794959b79"
   end
 
   depends_on "meson" => :build

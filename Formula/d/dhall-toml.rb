f915a64b599001e268d76c32d3a2a06ad08ab0ac
Michael Cho <michael@michaelcho.dev>
2025-09-11 15:31:18 -0400

    dhall-toml: migrate to GHC 9.12

--- Formula/d/dhall-toml.rb
+++ Formula/d/dhall-toml.rb
@@ -17,8 +17,10 @@ class DhallToml < Formula
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc@9.10" => :build
+  depends_on "ghc" => :build
+  depends_on "gmp"
 
+  uses_from_macos "libffi"
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 

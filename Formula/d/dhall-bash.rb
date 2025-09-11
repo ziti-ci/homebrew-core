025e6ea303f2b109a1260f1314b92b35ba87943d
Michael Cho <michael@michaelcho.dev>
2025-09-11 15:34:34 -0400

    dhall-bash: migrate to GHC 9.12

--- Formula/d/dhall-bash.rb
+++ Formula/d/dhall-bash.rb
@@ -28,8 +28,10 @@ class DhallBash < Formula
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc@9.10" => :build
+  depends_on "ghc" => :build
+  depends_on "gmp"
 
+  uses_from_macos "libffi"
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 

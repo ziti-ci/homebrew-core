62479a243cd5298d424b9554ad237c6fabc424cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 14:59:37 +0000

    libsepol: add 3.9 bottle.

--- Formula/lib/libsepol.rb
+++ Formula/lib/libsepol.rb
@@ -5,6 +5,13 @@ class Libsepol < Formula
   sha256 "ba630b59e50c5fbf9e9dd45eb3734f373cf78d689d8c10c537114c9bd769fa2e"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "694496f68034e506cc74f711234a738bdc86fe6862c50c4d94056e7992c891e6"
+    sha256 cellar: :any,                 arm64_sonoma:  "4acda6a9f6bb64c0054b960c8c89c0b99ae8efb2f4cf826d02945da75d230c0f"
+    sha256 cellar: :any,                 sonoma:        "45d2a4183e993e5bff4bba540d7bc371008d9de8677f6bd47fdaa6a30e54a5de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c3d5fb345b92690961611fcf1894419a137c4b370af1151fe6cd251d20f5d32"
+  end
+
   depends_on "rpm2cpio" => :test
 
   uses_from_macos "flex" => :build

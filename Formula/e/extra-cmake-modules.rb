bd54396a21d7a73a17bbe200468439d26183fbf6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 17:06:33 +0000

    extra-cmake-modules 6.18.0

--- Formula/e/extra-cmake-modules.rb
+++ Formula/e/extra-cmake-modules.rb
@@ -1,8 +1,8 @@
 class ExtraCmakeModules < Formula
   desc "Extra modules and scripts for CMake"
   homepage "https://api.kde.org/frameworks/extra-cmake-modules/html/index.html"
-  url "https://download.kde.org/stable/frameworks/6.17/extra-cmake-modules-6.17.0.tar.xz"
-  sha256 "dfecb17d0238f4de1dd3485b92a6606137d4a9c67b9e4ce40407fe0f2aec0a40"
+  url "https://download.kde.org/stable/frameworks/6.18/extra-cmake-modules-6.18.0.tar.xz"
+  sha256 "b2d2222cfe5ace502c60bd8a77a79bc0230f48cee83265c89dcc2618b960e293"
   license all_of: ["BSD-2-Clause", "BSD-3-Clause", "MIT"]
   head "https://invent.kde.org/frameworks/extra-cmake-modules.git", branch: "master"
 
@@ -27,10 +27,6 @@ class ExtraCmakeModules < Formula
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-
-    # Ensure uniform bottles.
-    inreplace_files = %w[prefix.sh.cmake prefix.sh.fish.cmake].map { |f| share/"ECM/kde-modules"/f }
-    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX
   end
 
   test do

eda892899c6c80baf4aea797f5d5e8822a3e8bee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 00:45:44 +0000

    render: update 2.2.0 bottle.

--- Formula/r/render.rb
+++ Formula/r/render.rb
@@ -7,12 +7,12 @@ class Render < Formula
   head "https://github.com/render-oss/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ab409bbfe8a5179bfa089534324fb6a5f388e3f9912a5262563ec253376b212e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ab409bbfe8a5179bfa089534324fb6a5f388e3f9912a5262563ec253376b212e"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "ab409bbfe8a5179bfa089534324fb6a5f388e3f9912a5262563ec253376b212e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "33f01cd063d3df17425d6541d451f6f964c4c4c33938749796357204252dded0"
-    sha256 cellar: :any_skip_relocation, ventura:       "33f01cd063d3df17425d6541d451f6f964c4c4c33938749796357204252dded0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eede78cefe5d245fd30ba34ee36c77bff79509ee17dbf18fd0fdc728d7bb5ae9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c34ede29423cc4a513148ba4aea56e19dc68ce074222f8825824956e7a9d6772"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c34ede29423cc4a513148ba4aea56e19dc68ce074222f8825824956e7a9d6772"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "c34ede29423cc4a513148ba4aea56e19dc68ce074222f8825824956e7a9d6772"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a5496cd6e026220b0c249ea7279d4bfd9dade3ec4697b14b98fcc717ff61fa4e"
+    sha256 cellar: :any_skip_relocation, ventura:       "a5496cd6e026220b0c249ea7279d4bfd9dade3ec4697b14b98fcc717ff61fa4e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2abcd3473f73f9fc8e5581d15c556b658ad22af683ab207a14a9e1f38f9c848a"
   end
 
   depends_on "go" => :build

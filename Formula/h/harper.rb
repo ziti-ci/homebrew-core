133372ae0425ebdba77ab330ae8bbfcc1888f3e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 20:40:47 +0000

    harper: update 0.63.0 bottle.

--- Formula/h/harper.rb
+++ Formula/h/harper.rb
@@ -12,14 +12,11 @@ class Harper < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0204ff6707bb12cfbba767d6cb78445bd4f9be63fe0bbf8f0b27b74a867e29b4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "efadd023eefa139ef967afc10cb6286ebd2396681cb600ecdab23b275303f373"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c4a4a9fb694718341dbe6b0d260ab65a6c8f9e5b4f9cb87e9726e6920a4e4f6c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "a4c2798d903ae136f529bc4aa9f47e1be01480b8a3934ee8c179fd43775ae3c5"
-    sha256 cellar: :any_skip_relocation, sonoma:        "46f970e0e3b86cd8255406b09ec061b58ee4fce4d87ec85f8d99d786d8313988"
-    sha256 cellar: :any_skip_relocation, ventura:       "131594d6f87b6127efbcceb2ccb6ddc0a5576d8a767b8221415a6613906c8343"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "12157bf392eb790ac7240e956208e990767b1493b3b9ddf0d17a2c4871577b63"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "30d850496f0ec4fb33562386fd810c11e7a909535e479bb0c0f53a7e1339119d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6209a41bf28311b4df9988e1942f2788b775d54a0bb7181aab874b42282e5a2e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d9b88bb3519c8ee0da83c26038e7122cc7f58293d02e6a0d5b39047135277155"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bb9326eaed841b756fdeb4fa798fa30c8972bba9c41a9f8d1e416b0f7b189638"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "52629a2b58fb3922f3232638548df4486191881a3245e9d85d4934072f245a26"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "217de8c1f13c22258ca39908e608dea15a8c41c74687d916bb1b13404673a12b"
   end
 
   depends_on "rust" => :build

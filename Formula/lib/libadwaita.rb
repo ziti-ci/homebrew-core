b12f01f29879cdb073051f3586ca9785d789fc0d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 20:06:04 +0000

    libadwaita 1.8.0

--- Formula/lib/libadwaita.rb
+++ Formula/lib/libadwaita.rb
@@ -1,8 +1,8 @@
 class Libadwaita < Formula
   desc "Building blocks for modern adaptive GNOME applications"
   homepage "https://gnome.pages.gitlab.gnome.org/libadwaita/"
-  url "https://download.gnome.org/sources/libadwaita/1.7/libadwaita-1.7.6.tar.xz"
-  sha256 "5eacc5550f0bdbba6a1568aebf25a3d88f5ee07d6b558becc6fd9487579c9a29"
+  url "https://download.gnome.org/sources/libadwaita/1.8/libadwaita-1.8.0.tar.xz"
+  sha256 "2836d2c0c0f46dcdb52f94019477f537810283c710802d8e6671efdebe2637c9"
   license "LGPL-2.1-or-later"
 
   # libadwaita doesn't use GNOME's "even-numbered minor is stable" version

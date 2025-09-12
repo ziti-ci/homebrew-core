9edcf1f5ab32b0d58edab11ba5dc9f92e95c9318
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 00:57:44 +0000

    shimmy: add 1.2.0 bottle.

--- Formula/s/shimmy.rb
+++ Formula/s/shimmy.rb
@@ -11,6 +11,15 @@ class Shimmy < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bdb4caadec2f8eb637a146fb877b0707e9e4ebfa2eedecd4559f34eb4635a149"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c92c710bfbeba92a5a1b3a53e468a435e4779436d2cfa38c10ae9d735bcf3970"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "ad930ea69810d8682a10791aa8dd9fd293296c78c26a130f5d4388957d9483b5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "317b9e0651962e6c1c2717ddef9dcb52b691fd1f812a4e72bbdbfb6943299a7a"
+    sha256 cellar: :any_skip_relocation, ventura:       "d8bf2100b3d23986e8dae8dde12dc6340e1a85358506403393197bc34391b476"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d3f72a1e84bb0dda4ce7667b1ca3320b032504e0957acd8101d8daa80127972"
+  end
+
   depends_on "cmake" => :build # for llama-cpp-sys-2
   depends_on "rust" => :build
 

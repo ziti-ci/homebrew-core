62e6b940b3ac0fe9ea15eb2ba9019d94faede178
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 17:06:33 +0000

    karchive 6.18.0

--- Formula/k/karchive.rb
+++ Formula/k/karchive.rb
@@ -1,8 +1,8 @@
 class Karchive < Formula
   desc "Reading, creating, and manipulating file archives"
   homepage "https://api.kde.org/frameworks/karchive/html/index.html"
-  url "https://download.kde.org/stable/frameworks/6.17/karchive-6.17.0.tar.xz"
-  sha256 "60647c24646a43f67deaf37e20246df9d379fa3f7513fe8536a1dfa330865b40"
+  url "https://download.kde.org/stable/frameworks/6.18/karchive-6.18.0.tar.xz"
+  sha256 "fa24f703aa799e4ff5b9cc2e4a628745912ebfcc9f0c6bb6d92106ff9e02e26f"
   license all_of: [
     "BSD-2-Clause",
     "LGPL-2.0-only",

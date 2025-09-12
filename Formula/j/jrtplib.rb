45fd946b9e5c61e1a7a6ffd2a1bec8c1dfb86ce5
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-12 18:06:40 +0800

    jrtplib: fix build with CMake 4.0+

--- Formula/j/jrtplib.rb
+++ Formula/j/jrtplib.rb
@@ -31,7 +31,9 @@ class Jrtplib < Formula
   depends_on "jthread"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    # Workaround to build with CMake 4
+    args = %w[-DCMAKE_POLICY_VERSION_MINIMUM=3.5]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end

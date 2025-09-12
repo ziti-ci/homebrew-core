3da744832b5f7b59481ede25442a8477a65b8e3b
Rui Chen <rui@chenrui.dev>
2025-09-12 01:22:20 +0000

    shimmy: update 1.2.0 bottle.

--- Formula/s/shimmy.rb
+++ Formula/s/shimmy.rb
@@ -17,6 +17,7 @@ class Shimmy < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura: "ad930ea69810d8682a10791aa8dd9fd293296c78c26a130f5d4388957d9483b5"
     sha256 cellar: :any_skip_relocation, sonoma:        "317b9e0651962e6c1c2717ddef9dcb52b691fd1f812a4e72bbdbfb6943299a7a"
     sha256 cellar: :any_skip_relocation, ventura:       "d8bf2100b3d23986e8dae8dde12dc6340e1a85358506403393197bc34391b476"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7861609e4926eff0d0ad68b79adaac4b6a06aedf20574f5536b0643958b57b4a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d3f72a1e84bb0dda4ce7667b1ca3320b032504e0957acd8101d8daa80127972"
   end
 

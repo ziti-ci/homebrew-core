553bffc899121248e9d7d9950e44c30f788fd1a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 05:04:18 +0000

    hysteria 2.6.3

--- Formula/h/hysteria.rb
+++ Formula/h/hysteria.rb
@@ -1,8 +1,8 @@
 class Hysteria < Formula
   desc "Feature-packed proxy & relay tool optimized for lossy, unstable connections"
   homepage "https://hysteria.network/"
-  url "https://github.com/apernet/hysteria/archive/refs/tags/app/v2.6.2.tar.gz"
-  sha256 "4699431f0bc826da2bbd3939c0a78c4e7bfc02773fc3a62b24615c37ee89b266"
+  url "https://github.com/apernet/hysteria/archive/refs/tags/app/v2.6.3.tar.gz"
+  sha256 "bed1ece93dfaa07fbf709136efadaf4ccb09e0375844de3e28c5644ebe518eb0"
   license "MIT"
   head "https://github.com/apernet/hysteria.git", branch: "master"
 

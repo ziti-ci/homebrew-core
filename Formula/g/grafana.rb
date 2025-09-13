0073d5d46227ad2497ea2087a97803b75399ee08
Michael Cho <michael@michaelcho.dev>
2025-09-12 23:46:39 -0400

    grafana: backport fix for node 22.18.0

--- Formula/g/grafana.rb
+++ Formula/g/grafana.rb
@@ -1,10 +1,19 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/refs/tags/v12.1.0.tar.gz"
-  sha256 "9e2f3f11eff01f8b86c2c232c6a3c3a32fa303589ea9829538ffc867684a4436"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/grafana.git", branch: "main"
+
+  stable do
+    url "https://github.com/grafana/grafana/archive/refs/tags/v12.1.0.tar.gz"
+    sha256 "9e2f3f11eff01f8b86c2c232c6a3c3a32fa303589ea9829538ffc867684a4436"
+
+    # Backport fix for node 22.18.0
+    patch do
+      url "https://github.com/grafana/grafana/commit/20c14c48f4c7f13610b105d17f54b66d0062b212.patch?full_index=1"
+      sha256 "180ecb3eadb594211479875f9340055b6ccd250ca27eaf00b31e8ff08b15ec1b"
+    end
+  end
 
   livecheck do
     url :stable

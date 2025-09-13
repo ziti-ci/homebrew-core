a48eb6a6bfe2cae5c0fdd624401ae6edf376d976
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-14 02:28:02 +0800

    fann: fix build with CMake 4.0+

--- Formula/f/fann.rb
+++ Formula/f/fann.rb
@@ -26,7 +26,9 @@ class Fann < Formula
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    # Workaround to build with CMake 4
+    args = %w[-DCMAKE_POLICY_VERSION_MINIMUM=3.5]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end

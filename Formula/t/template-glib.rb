f1ef09eee7e5884d985b6827ccf5251d66a17715
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-13 02:43:39 +0000

    template-glib 3.38.0

--- Formula/t/template-glib.rb
+++ Formula/t/template-glib.rb
@@ -1,8 +1,8 @@
 class TemplateGlib < Formula
   desc "GNOME templating library for GLib"
   homepage "https://gitlab.gnome.org/GNOME/template-glib"
-  url "https://download.gnome.org/sources/template-glib/3.36/template-glib-3.36.3.tar.xz"
-  sha256 "d528b35b2cf90e07dae50e25e12fbadb0eb048f57fd5151cf9f6e98cce1df20e"
+  url "https://download.gnome.org/sources/template-glib/3.38/template-glib-3.38.0.tar.xz"
+  sha256 "40d00dc223dcf2eb7f2ec422f7dec5a67373a0ca1101abca0f49c62f050cb312"
   license "LGPL-2.1-or-later"
 
   bottle do

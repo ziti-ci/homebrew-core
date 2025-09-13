0ea4833171126134560e0a70cdcab7199ae248b4
botantony <antonsm21@gmail.com>
2025-09-13 17:50:17 +0200

    quickjs 2025-09-13
    Update Formula/q/quickjs.rb
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>

--- Formula/q/quickjs.rb
+++ Formula/q/quickjs.rb
@@ -1,16 +1,14 @@
 class Quickjs < Formula
   desc "Small and embeddable JavaScript engine"
   homepage "https://bellard.org/quickjs/"
-  url "https://bellard.org/quickjs/quickjs-2025-04-26.tar.xz"
-  sha256 "2f20074c25166ef6f781f381c50d57b502cb85d470d639abccebbef7954c83bf"
+  url "https://bellard.org/quickjs/quickjs-2025-09-13.tar.xz"
+  sha256 "6f1f322aea3bb3a90858db85c9fe717013fde4df7dfcafe2f57e78f5bb4b4a0c"
   license "MIT"
 
   livecheck do
     url :homepage
     regex(/href=.*?quickjs[._-]v?(\d+(?:[.-]\d+)+)\.t/i)
   end
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     sha256 arm64_tahoe:   "f5e44acf36cd72ff49f7bbca5d613ba0412080a23b63add6a2bc2591482e3954"
@@ -24,6 +22,10 @@ class Quickjs < Formula
   end
 
   def install
+    # Create dummy JSON file in `examples` path
+    # so that `make install` does not return an error
+    # https://github.com/bellard/quickjs/issues/440
+    (buildpath/"examples/message.json").write "{}"
     system "make", "install", "PREFIX=#{prefix}", "CONFIG_M32="
   end
 

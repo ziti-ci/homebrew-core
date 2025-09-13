a1cc7c99739b453e86401d588d2c8406c152ea69
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-13 22:56:56 +0800

    entityx: fix build with CMake 4.0+

--- Formula/e/entityx.rb
+++ Formula/e/entityx.rb
@@ -32,7 +32,8 @@ class Entityx < Formula
       -DENTITYX_BUILD_SHARED=off
       -DENTITYX_BUILD_TESTING=off
     ]
-
+    # Workaround to build with CMake 4
+    args << "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"

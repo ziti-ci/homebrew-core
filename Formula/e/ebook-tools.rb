2c8d0bccf2691d37dbdd3469eee77c10b0ba4c7b
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-13 20:01:13 +0000

    ebook-tools: update 0.2.2_3 bottle.

--- Formula/e/ebook-tools.rb
+++ Formula/e/ebook-tools.rb
@@ -9,6 +9,7 @@ class EbookTools < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
+    sha256 cellar: :any,                 arm64_tahoe:    "ecdd389cc82aa7712a09a23af8b9fe85cb3aad8f8634101b1933e757de7752a4"
     sha256 cellar: :any,                 arm64_sequoia:  "f5941a8ccff53411313a9126e9414bb6ee069ac5ac3b4ebf819d0b98bc757460"
     sha256 cellar: :any,                 arm64_sonoma:   "62256c5eb6880252f8110c8c35d9c84bd9838bce63de8af4377497d19da21d82"
     sha256 cellar: :any,                 arm64_ventura:  "019f7789541693a154f71bb507db24ab2cd3901f539e08e2ce2e51f53aae48da"

8101b77aa776aa9ff19e81ab36ace1c1a1210299
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-14 01:21:02 +0800

    flif: fix build with CMake 4.0+
    flif: add rpath patch
    
    Co-authored-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/flif.rb
+++ Formula/f/flif.rb
@@ -29,7 +29,12 @@ class Flif < Formula
   depends_on "sdl2"
 
   def install
-    system "cmake", "-S", "src", "-B", "build", *std_cmake_args
+    args = %W[
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+    # Workaround to build with CMake 4
+    args << "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+    system "cmake", "-S", "src", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
     doc.install "doc/flif.pdf"

ff0546bf10651c3eb7c6430cf3869a7b54708130
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-13 09:08:29 +0000

    algol68g: update 3.9.4 bottle.

--- Formula/a/algol68g.rb
+++ Formula/a/algol68g.rb
@@ -11,13 +11,11 @@ class Algol68g < Formula
   end
 
   bottle do
-    sha256 arm64_sequoia: "620ff64ca590c3600da79baa2690cbe8dbc6a9cf1d30aefe49510063c2dd0d02"
-    sha256 arm64_sonoma:  "8c3fb4e68523a369f25c2cbfc355d59fb2d91e86e896f5466366a82aa89d9bf7"
-    sha256 arm64_ventura: "9edee3706e544f1b7b891b4e8f344aec386a54d40c0c6f1e5cc9ed352a152932"
-    sha256 sonoma:        "9e64742227d23e0cfdaa6884f9cf4652d154d06311ba180e705e94c2de21f07b"
-    sha256 ventura:       "f8ec064e494c6c95398f66174c44ec00702a2c4cf3d89b7c0148052958eaba94"
-    sha256 arm64_linux:   "4f7cdf665f3bae2ea4c2ef15943c6c9f08b223c900b08eafaa87cba1699b6985"
-    sha256 x86_64_linux:  "375d621eccdf8d7e0d5b3859bdaec4efce52e4b316f9b592762f93ccd1af7bde"
+    sha256 arm64_sequoia: "7fcaa4ba863f7ec5e10a5d8628603cbe2a2e7ebc52a531273198bb86cf3e6e7b"
+    sha256 arm64_sonoma:  "3140406b51c78466c706146c020289775669c4b82f3d2778744c112a3dd94932"
+    sha256 sonoma:        "e851dd4af5934f7d542b1b0529e98822c6ddb92a9e9c6d0da99b9322379d28fd"
+    sha256 arm64_linux:   "a0e794ef54113013a142b53614b2fdede5ead93f9e2a000173b0523f999c3e3e"
+    sha256 x86_64_linux:  "1cadce599f8cc7e58604dcf8bad939e84bfbae0829e5f4db8fe6d3d8eb6a27d4"
   end
 
   uses_from_macos "ncurses"

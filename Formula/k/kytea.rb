d166b2c8061bf99aff19006656c563afd1b59070
Michael Cho <michael@michaelcho.dev>
2025-09-12 23:29:35 -0400

    kytea: backport fix for newer Clang

--- Formula/k/kytea.rb
+++ Formula/k/kytea.rb
@@ -12,6 +12,12 @@ class Kytea < Formula
       url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
       sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
     end
+
+    # Fix build with newer Clang
+    patch do
+      url "https://github.com/neubig/kytea/commit/eab98ce9c45ccc4a0226a87fa6c40b6d0c5ba82b.patch?full_index=1"
+      sha256 "aabb381b38592432d97f789520c81e6df46808c611ff541aae093357c06921c6"
+    end
   end
 
   livecheck do

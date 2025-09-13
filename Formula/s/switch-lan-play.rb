c25437f31de9e9d1ac9ec60458027f07ea231d68
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-13 17:53:06 +0800

    switch-lan-play: fix build with CMake 4.0+

--- Formula/s/switch-lan-play.rb
+++ Formula/s/switch-lan-play.rb
@@ -30,7 +30,9 @@ class SwitchLanPlay < Formula
   uses_from_macos "libpcap"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    # Workaround to build with CMake 4
+    args = %w[-DCMAKE_POLICY_VERSION_MINIMUM=3.5]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end

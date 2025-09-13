2144f4c035640374276ee2fdfb7dd73a1c6196c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-13 22:56:30 +0000

    dprint 0.50.2
    dprint: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/dprint.rb
+++ Formula/d/dprint.rb
@@ -1,8 +1,8 @@
 class Dprint < Formula
   desc "Pluggable and configurable code formatting platform written in Rust"
   homepage "https://dprint.dev/"
-  url "https://github.com/dprint/dprint/archive/refs/tags/0.50.1.tar.gz"
-  sha256 "85197a9469fe479fc278e77e87ede6eeb55b7d42d0a530e8b828f3ab9b213358"
+  url "https://github.com/dprint/dprint/archive/refs/tags/0.50.2.tar.gz"
+  sha256 "dfba0da97f394e4aa4f372e0013ffd1379215c7353cb56450bea0a2802150d54"
   license "MIT"
   head "https://github.com/dprint/dprint.git", branch: "main"
 
@@ -20,12 +20,6 @@ class Dprint < Formula
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "xz" # required for lzma support
-
-  # update deps, upstream pr ref, https://github.com/dprint/dprint/pull/1003
-  patch do
-    url "https://github.com/dprint/dprint/commit/bb6ddc6034f73adb188fb2c40aa34d0c6a7ec6de.patch?full_index=1"
-    sha256 "ea54bc0c12dbd3057a0c95d4c922fd35459f338112c14eb8dc4fe96eb742a733"
-  end
 
   def install
     ENV.append_to_rustflags "-C link-arg=-Wl,-undefined,dynamic_lookup" if OS.mac?

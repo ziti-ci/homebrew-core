2d1989b024498bb20936148770dd56268ce93fbc
Rui Chen <rui@chenrui.dev>
2025-09-14 16:28:24 +0000

    mermaid-cli: update 11.10.1 bottle.

--- Formula/m/mermaid-cli.rb
+++ Formula/m/mermaid-cli.rb
@@ -6,13 +6,12 @@ class MermaidCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "fed16e2660aaa5ac190a3b6adef1cea7c6f531b11398b1981268e46fb8964104"
-    sha256 cellar: :any,                 arm64_sonoma:  "fed16e2660aaa5ac190a3b6adef1cea7c6f531b11398b1981268e46fb8964104"
-    sha256 cellar: :any,                 arm64_ventura: "fed16e2660aaa5ac190a3b6adef1cea7c6f531b11398b1981268e46fb8964104"
-    sha256 cellar: :any,                 sonoma:        "f61d4d96eeb7230461b4d27c9bebb8d272573ffea0e1da8742e022a03e8c3d63"
-    sha256 cellar: :any,                 ventura:       "f61d4d96eeb7230461b4d27c9bebb8d272573ffea0e1da8742e022a03e8c3d63"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "395fdf1a7e9220510f636d9eba7e0cc6f6fd6fc1bfed02c20f9b797fc5b7c379"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "37a57110fd7faec00784f4ae17210bfebeda522eb0d4acdd0cf587de494a139c"
+    sha256 cellar: :any,                 arm64_tahoe:   "f9f801e76d2335944b5bfe93bce6835046e73c6e33de6dd04f99733caca1781a"
+    sha256 cellar: :any,                 arm64_sequoia: "0657a1ada19ba4742c20499e5c816b57436dad3012f16f5aeebdb92b1d58e32b"
+    sha256 cellar: :any,                 arm64_sonoma:  "0657a1ada19ba4742c20499e5c816b57436dad3012f16f5aeebdb92b1d58e32b"
+    sha256 cellar: :any,                 sonoma:        "40c368028e66d822fa9d67ceebc0a8382b19cb8c052c19e3e180ac53e83e5e73"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f77161bcfbd68dd8b64c863b7e8f1ed056699e731039e777b7700a4bfc582237"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e3ced9cc06277df60faf0b6243e761adaa35ca9b5d70ff1545c540d70123f10"
   end
 
   depends_on "node"

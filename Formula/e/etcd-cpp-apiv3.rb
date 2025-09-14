3079fe1c3077b74189ae2189c320c0bdc79c35d8
Rui Chen <rui@chenrui.dev>
2025-09-13 21:21:48 -0400

    etcd-cpp-apiv3: revision bump (protobuf 32.1)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/e/etcd-cpp-apiv3.rb
+++ Formula/e/etcd-cpp-apiv3.rb
@@ -4,7 +4,7 @@ class EtcdCppApiv3 < Formula
   url "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/archive/refs/tags/v0.15.4.tar.gz"
   sha256 "4516ecfa420826088c187efd42dad249367ca94ea6cdfc24e3030c3cf47af7b4"
   license "BSD-3-Clause"
-  revision 32
+  revision 33
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "5e84282d40c5a66ccb672948574b80b54bb5c208f69d77d8ee720fc0259303f9"

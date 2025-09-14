089d636ca2cb9fd291a09383bc09975efde98c90
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-14 11:28:25 +0000

    gopeed: update 1.8.1 bottle.

--- Formula/g/gopeed.rb
+++ Formula/g/gopeed.rb
@@ -7,6 +7,7 @@ class Gopeed < Formula
   head "https://github.com/GopeedLab/gopeed.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "073b6b7a3ac3903de2faf00f6d399e0ff2187839fe80a7eeec2377afafd4b54a"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "45ad22fac255dda845e4800087d723d4f7fcdb5c4c2d5080cc1953438ed9b7c5"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa967719956a0c2f753268174f91bbe4d97834418912d6b43dab4897defdf1e3"
     sha256 cellar: :any_skip_relocation, sonoma:        "b5fd0a801f6e12e3556bc0803190384bf897f9e25e8fe3b5509254d6fff7e55b"

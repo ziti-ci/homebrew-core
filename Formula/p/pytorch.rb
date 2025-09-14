236baef8f3ed20fe6dcf45d036b9137fea60cd49
Rui Chen <rui@chenrui.dev>
2025-09-13 21:21:51 -0400

    pytorch: revision bump (protobuf 32.1)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pytorch.rb
+++ Formula/p/pytorch.rb
@@ -6,7 +6,7 @@ class Pytorch < Formula
   url "https://github.com/pytorch/pytorch/releases/download/v2.8.0/pytorch-v2.8.0.tar.gz"
   sha256 "c70a2c9488f6f6e8af5982a10d1cc2c37b7df5e6506d839daa5d5e250953d7b5"
   license "BSD-3-Clause"
-  revision 2
+  revision 3
 
   livecheck do
     url :stable

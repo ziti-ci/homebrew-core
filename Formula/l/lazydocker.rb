d1fd960318f7317a75b287155fd7c935815fc2be
Branch Vincent <branchevincent@gmail.com>
2025-09-13 17:07:38 -0700

    lazydocker: build for arm64_linux

--- Formula/l/lazydocker.rb
+++ Formula/l/lazydocker.rb
@@ -19,6 +19,7 @@ class Lazydocker < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = OS.mac? ? "1" : "0"
     ldflags = "-s -w -X main.version=#{version} -X main.date=#{time.iso8601} -X main.buildSource=#{tap.user}"
     system "go", "build", "-mod=vendor", *std_go_args(ldflags:)
   end

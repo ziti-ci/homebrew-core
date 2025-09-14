a608543982bd078013475c3834c95eb299590f13
Rui Chen <rui@chenrui.dev>
2025-09-13 21:21:50 -0400

    protobuf-c: revision bump (protobuf 32.1)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/protobuf-c.rb
+++ Formula/p/protobuf-c.rb
@@ -4,7 +4,7 @@ class ProtobufC < Formula
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.5.2/protobuf-c-1.5.2.tar.gz"
   sha256 "e2c86271873a79c92b58fef7ebf8de1aa0df4738347a8bd5d4e65a80a16d0d24"
   license "BSD-2-Clause"
-  revision 4
+  revision 5
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "b4f0aca3a9b3164f8fab460e21dc4439a04d9fca0ca5a0a7245e7f4c6c41bae7"

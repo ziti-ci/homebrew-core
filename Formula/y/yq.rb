fd9a8a6e97b278c49bcc43738ffbedd213e7f327
Branch Vincent <branchevincent@gmail.com>
2025-09-13 17:35:31 -0700

    yq: build for arm64_linux

--- Formula/y/yq.rb
+++ Formula/y/yq.rb
@@ -27,6 +27,7 @@ class Yq < Formula
   conflicts_with "python-yq", because: "both install `yq` executables"
 
   def install
+    ENV["CGO_ENABLED"] = OS.mac? ? "1" : "0"
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
     # Install shell completions
@@ -40,6 +41,6 @@ class Yq < Formula
 
   test do
     assert_equal "key: cat", shell_output("#{bin}/yq eval --null-input --no-colors '.key = \"cat\"'").chomp
-    assert_equal "cat", pipe_output("#{bin}/yq eval \".key\" -", "key: cat", 0).chomp
+    assert_equal "cat", pipe_output("#{bin}/yq eval .key -", "key: cat", 0).chomp
   end
 end

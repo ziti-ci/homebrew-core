d1e9eaf98ce93b24898bd0fd07a7ed7fe997352e
Rui Chen <rui@chenrui.dev>
2025-09-13 21:21:48 -0400

    bloaty: revision bump (protobuf 32.1)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/bloaty.rb
+++ Formula/b/bloaty.rb
@@ -2,7 +2,7 @@ class Bloaty < Formula
   desc "Size profiler for binaries"
   homepage "https://github.com/google/bloaty"
   license "Apache-2.0"
-  revision 40
+  revision 41
   head "https://github.com/google/bloaty.git", branch: "main"
 
   stable do

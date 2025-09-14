cfb37a01f8e3498e2aa0adbf38b7e73eef501df9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-14 13:49:28 +0000

    base16384: update 2.3.2 bottle.

--- Formula/b/base16384.rb
+++ Formula/b/base16384.rb
@@ -7,13 +7,12 @@ class Base16384 < Formula
   head "https://github.com/fumiama/base16384.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_sequoia: "5b0741d12c7fd27e441000588c8f67d5b25219ec354c30d6f1fb71f0895fd21d"
-    sha256 cellar: :any,                 arm64_sonoma:  "54b2e32ca7f2d38367f81786633fa378511721c6c2a7408a34518c4d3de2082b"
-    sha256 cellar: :any,                 arm64_ventura: "4cedcd31ee0046d2b95a03a3bde145840e61e56eff9c4d8c615ca72f52c293d5"
-    sha256 cellar: :any,                 sonoma:        "b14628debc357e0d0fcc2bd0416294addb0a809f0f5be808c0aecae4f0b69976"
-    sha256 cellar: :any,                 ventura:       "66cd155d2f35568095aa49aaace7f2ab61e0a0a2db380735971e184d85c67487"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5262ae0ad2b26cc424db339d0f1315ef6fa9a51bf38c972896f9525ce768801a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab7546bccd5073612b4c45e233d0e78f4010ddf81ebc280c8a4e566279ed01f5"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sequoia: "deeb2188d7479f513249484059fd1de2adbeeecc433b5017bd352ca29328d9c4"
+    sha256 cellar: :any,                 arm64_sonoma:  "7908aa72a066df28976ac2c33318b3bc14dcdcdb14e03f075aaef4e49f927b0d"
+    sha256 cellar: :any,                 sonoma:        "b9c40a9eeae7fb639505085bf436817583a4bef68ab415966a232facbb1634b8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "292b2d8e8b87a90ee2dbb426a69ed69f8c58945cda95c879478eeab33d38c818"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3010ed072ea82794a4f40a0ec7a11e2bc51a6f0a8d25aa18483be84976b9a2b3"
   end
 
   depends_on "cmake" => :build

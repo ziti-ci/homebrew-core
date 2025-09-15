7f6e7321be8dcff0ecc0b580f5b3b49d29a21f59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-15 14:11:13 +0000

    nghttp2 1.67.1

--- Formula/n/nghttp2.rb
+++ Formula/n/nghttp2.rb
@@ -1,9 +1,9 @@
 class Nghttp2 < Formula
   desc "HTTP/2 C Library"
   homepage "https://nghttp2.org/"
-  url "https://github.com/nghttp2/nghttp2/releases/download/v1.67.0/nghttp2-1.67.0.tar.gz"
-  mirror "http://fresh-center.net/linux/www/nghttp2-1.67.0.tar.gz"
-  sha256 "f61f8b38c0582466da9daa1adcba608e1529e483de6b5b2fbe8a5001d41db80c"
+  url "https://github.com/nghttp2/nghttp2/releases/download/v1.67.1/nghttp2-1.67.1.tar.gz"
+  mirror "http://fresh-center.net/linux/www/nghttp2-1.67.1.tar.gz"
+  sha256 "da8d640f55036b1f5c9cd950083248ec956256959dc74584e12c43550d6ec0ef"
   license "MIT"
 
   bottle do

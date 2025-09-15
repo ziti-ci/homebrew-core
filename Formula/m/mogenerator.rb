99479c274f3b9977f54470348dd08e7c15109ad8
Patrick Linnane <patrick@linnane.io>
2025-09-15 15:00:46 -0700

    mogenerator: remove pre-Catalina bottles
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/m/mogenerator.rb
+++ Formula/m/mogenerator.rb
@@ -20,7 +20,6 @@ class Mogenerator < Formula
     sha256 cellar: :any_skip_relocation, monterey:       "a9907203474f336c731912e28fad5ec2e912a1e7378d5ba527a7bb3d3b160134"
     sha256 cellar: :any_skip_relocation, big_sur:        "415e0e160574b7b16dff3d0395a7e156894675191c911d09cddf59e1d916571b"
     sha256 cellar: :any_skip_relocation, catalina:       "d62cad0cc94a7b05286fb2a8a2f8e4a4cc3a9b46efa9a391aa9fcb00c381e85e"
-    sha256 cellar: :any_skip_relocation, mojave:         "dcb658659b696e44f13e382f553c92199a7ab0be48ff69f33a35ef98ee8a09ac"
   end
 
   depends_on xcode: :build

6458e49f5c8774fd565183aee61709f286c02696
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-15 04:56:43 +0000

    claude-cmd: add 1.1.1 bottle.

--- Formula/c/claude-cmd.rb
+++ Formula/c/claude-cmd.rb
@@ -5,6 +5,10 @@ class ClaudeCmd < Formula
   sha256 "c6b990f7c55ec1281dca603b284d55b468ca7bbdfe217fc8091f5a8f85f16367"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "5ae7ff8473e44498a079bf31fc5072742d7104998ac1ecd499706159d4136fa6"
+  end
+
   depends_on "node"
 
   def install

d0feab8057ed97993130949b15893388abf3156c
Colin Dean <colindean@users.noreply.github.com>
2025-09-15 17:10:25 -0400

    asciinema v3.0.0
    asciinema v3.0.0 was rewritten in Rust.
    
    https://blog.asciinema.org/post/three-point-o/
    
    Sets server envvar for test, otherwise user is prompted.
    
    asciinema: remove `all:` bottle
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/a/asciinema.rb
+++ Formula/a/asciinema.rb
@@ -1,27 +1,21 @@
 class Asciinema < Formula
-  include Language::Python::Virtualenv
-
   desc "Record and share terminal sessions"
   homepage "https://asciinema.org"
-  url "https://files.pythonhosted.org/packages/f1/19/45b405438e90ad5b9618f3df62e9b3edaa2b115b530e60bd4b363465c704/asciinema-2.4.0.tar.gz"
-  sha256 "828e04c36ba622a7b8f8f912c8f0c1329538b6c7ed1c0d1b131bbbfe3a221707"
+  url "https://github.com/asciinema/asciinema/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "f44feaa1bc150e7964635dc4714fd86089a968587fed81dccf860ee7b64617ca"
   license "GPL-3.0-only"
-  revision 1
   head "https://github.com/asciinema/asciinema.git", branch: "develop"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "25d158a0f9a9dcfd554504babc73196b0a0b8cdeea7390c425b7b209bbdb7015"
-  end
-
-  depends_on "python@3.13"
+  depends_on "rust" => :build
 
   def install
-    virtualenv_install_with_resources
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
     ENV["LC_ALL"] = "en_US.UTF-8"
+    ENV["ASCIINEMA_SERVER_URL"] = "https://example.com"
     output = shell_output("#{bin}/asciinema auth 2>&1")
-    assert_match "Open the following URL in a web browser to link your install ID", output
+    assert_match "Open the following URL in a web browser to authenticate this CLI", output
   end
 end

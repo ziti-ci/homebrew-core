8a75231ab304c54bf6380747dd8a4f503460ebab
Patrick Linnane <patrick@linnane.io>
2025-09-15 15:02:14 -0700

    sqtop: remove pre-Catalina bottles
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/s/sqtop.rb
+++ Formula/s/sqtop.rb
@@ -19,10 +19,6 @@ class Sqtop < Formula
     sha256 cellar: :any_skip_relocation, monterey:       "783650fd010e84cba76c1d747d7948fa1ccf0ae17032e797d76920b291ff36a0"
     sha256 cellar: :any_skip_relocation, big_sur:        "236b80d46f0cbc8aedc14b6771b4b58cff6e445eb2e8a115ae14917b307fbab2"
     sha256 cellar: :any_skip_relocation, catalina:       "653be227eb16e647b90f0f034543a490476e3ceaa0e9c165b1963b916b4a9703"
-    sha256 cellar: :any_skip_relocation, mojave:         "27786a7ca63d00ecb47e9f1e3e844a077d38048871cec3c03308831716338dc1"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "fe9a704fdf7f24dadba6b4f7cc20f8f07d93c19450701e01b408ea2f7574ec63"
-    sha256 cellar: :any_skip_relocation, sierra:         "30f51d2886adf914eef22af21dfac92f544c59c88b6e7961972eb6702e48d0c0"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "6d838378cae0971561da60dff1e887bf03b60d1a0ff198a5d468654ef790d9e9"
     sha256 cellar: :any_skip_relocation, arm64_linux:    "02d8d356631939d3201e72c24a96353e5032ddd6abfe1613cd7ac07de0720dd7"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f7bd15bf5407710e19ab7c3bd414c18b163b251274d14e058208a526353a7e0"
   end

32e91661e48288a2b65007b59abfb5e1a15580fc
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-15 21:55:44 +0000

    setweblocthumb: update 1.0.0 bottle.

--- Formula/s/setweblocthumb.rb
+++ Formula/s/setweblocthumb.rb
@@ -8,6 +8,7 @@ class Setweblocthumb < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:    "88745a2bd0a9404e73e38596e27004f33a562aa98a58d47772c70892eb22b026"
     sha256 cellar: :any_skip_relocation, arm64_sequoia:  "7765cc1aed9b92ae0460dfeace83a4c9fee7c63598953caf6e2bbdb581c5395c"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:   "bb99da3db71d9602d3c6ddf5b615a8b9b90ee253fc7561fe02116791050a2376"
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6eb231e7fa24dcebe8d1d863bc1bbbcec86943522f4391dce30c0d0a14a99e7e"

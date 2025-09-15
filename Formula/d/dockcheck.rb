45a23e92ef91987568c49ee7e1ed9ed1224b5f2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-15 11:31:30 +0000

    dockcheck: update 0.7.1 bottle.

--- Formula/d/dockcheck.rb
+++ Formula/d/dockcheck.rb
@@ -7,7 +7,7 @@ class Dockcheck < Formula
   head "https://github.com/mag37/dockcheck.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4e8e24c91b153f7fc8b42588472528977b6ea2f9ff9d3d0a1abba363929a2fbe"
+    sha256 cellar: :any_skip_relocation, all: "661a21b7025a0c32992f5f409514bed696abb4ccc892e8a1a577b6f3d01be91d"
   end
 
   depends_on "jq"

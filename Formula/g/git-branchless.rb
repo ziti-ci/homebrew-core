9083d9ddc1e5438e9c63602834fc3af99b486fda
Rui Chen <rui@chenrui.dev>
2025-09-14 22:35:49 -0400

    git-branchless: update build patch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/git-branchless.rb
+++ Formula/g/git-branchless.rb
@@ -9,8 +9,11 @@ class GitBranchless < Formula
     url "https://github.com/arxanas/git-branchless/archive/refs/tags/v0.10.0.tar.gz"
     sha256 "1eb8dbb85839c5b0d333e8c3f9011c3f725e0244bb92f4db918fce9d69851ff7"
 
-    # patch to use libgit2 1.9, upstream pr ref, https://github.com/arxanas/git-branchless/pull/1485
-    patch :DATA
+    # patch to build with rust 1.89 and bump git2 to 0.20
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/2d47380dce49c6b059369ff3378ab171c48f8fc3/git-branchless/0.10.0-build.patch"
+      sha256 "7624b6fe14a4d471636e4b22433507b2454b7ee311eac13c4d128cb688be234c"
+    end
   end
 
   # Upstream appears to use GitHub releases to indicate that a version is
@@ -65,67 +68,3 @@ class GitBranchless < Formula
     assert linkage_with_libgit2, "No linkage with libgit2! Cargo is likely using a vendored version."
   end
 end
-
-__END__
-diff --git a/Cargo.lock b/Cargo.lock
-index ecd3295..19168b5 100644
---- a/Cargo.lock
-+++ b/Cargo.lock
-@@ -1756,9 +1756,9 @@ dependencies = [
- 
- [[package]]
- name = "git2"
--version = "0.19.0"
-+version = "0.20.0"
- source = "registry+https://github.com/rust-lang/crates.io-index"
--checksum = "b903b73e45dc0c6c596f2d37eccece7c1c8bb6e4407b001096387c63d0d93724"
-+checksum = "3fda788993cc341f69012feba8bf45c0ba4f3291fcc08e214b4d5a7332d88aff"
- dependencies = [
-  "bitflags 2.5.0",
-  "libc",
-@@ -2063,9 +2063,9 @@ checksum = "9c198f91728a82281a64e1f4f9eeb25d82cb32a5de251c6bd1b5154d63a8e7bd"
- 
- [[package]]
- name = "libgit2-sys"
--version = "0.17.0+1.8.1"
-+version = "0.18.0+1.9.0"
- source = "registry+https://github.com/rust-lang/crates.io-index"
--checksum = "10472326a8a6477c3c20a64547b0059e4b0d086869eee31e6d7da728a8eb7224"
-+checksum = "e1a117465e7e1597e8febea8bb0c410f1c7fb93b1e1cddf34363f8390367ffec"
- dependencies = [
-  "cc",
-  "libc",
-diff --git a/Cargo.toml b/Cargo.toml
-index 1c806fa..fa0364a 100644
---- a/Cargo.toml
-+++ b/Cargo.toml
-@@ -69,7 +69,7 @@ git-branchless-smartlog = { version = "0.10.0", path = "git-branchless-smartlog"
- git-branchless-submit = { version = "0.10.0", path = "git-branchless-submit" }
- git-branchless-test = { version = "0.10.0", path = "git-branchless-test" }
- git-branchless-undo = { version = "0.10.0", path = "git-branchless-undo" }
--git2 = { version = "0.19.0", default-features = false }
-+git2 = { version = "0.20.0", default-features = false }
- glob = "0.3.0"
- indexmap = "2.2.6"
- indicatif = { version = "0.17.8", features = ["improved_unicode"] }
-diff --git a/git-branchless-invoke/src/lib.rs b/git-branchless-invoke/src/lib.rs
-index eee43ff..a6cd973 100644
---- a/git-branchless-invoke/src/lib.rs
-+++ b/git-branchless-invoke/src/lib.rs
-@@ -117,12 +117,12 @@ fn install_tracing(effects: Effects) -> eyre::Result<impl Drop> {
- 
- #[instrument]
- fn install_libgit2_tracing() {
--    fn git_trace(level: git2::TraceLevel, msg: &str) {
--        info!("[{:?}]: {}", level, msg);
-+    fn git_trace(level: git2::TraceLevel, msg: &[u8]) {
-+        info!("[{:?}]: {}", level, String::from_utf8_lossy(msg));
-     }
- 
--    if !git2::trace_set(git2::TraceLevel::Trace, git_trace) {
--        warn!("Failed to install libgit2 tracing");
-+    if let Err(err) = git2::trace_set(git2::TraceLevel::Trace, git_trace) {
-+        warn!("Failed to install libgit2 tracing: {err}");
-     }
- }
- 

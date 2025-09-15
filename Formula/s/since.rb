c67555de8a358f22c99e75fb6d7ad7a1c9eee407
Patrick Linnane <patrick@linnane.io>
2025-09-15 15:02:04 -0700

    since: remove pre-Catalina bottles
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/s/since.rb
+++ Formula/s/since.rb
@@ -24,10 +24,6 @@ class Since < Formula
     sha256 cellar: :any_skip_relocation, monterey:       "3db05c5b4e33cc28cf9dd8352dbc6038b139cb6bf8056bc9c5a85bd0db7ee9c2"
     sha256 cellar: :any_skip_relocation, big_sur:        "60c3738e71c6455fa5a7445a21a79695d4644a34de06cbc05743a52c4f5b40f8"
     sha256 cellar: :any_skip_relocation, catalina:       "20b3f4888282ed47021562eb24efe9c37ef3a652ad64164460a5f368260e75d8"
-    sha256 cellar: :any_skip_relocation, mojave:         "6c0290f3500966bb4155352bf277ae127eb341796729dfcc2b9ca968df20b9c4"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "a5b4f42858c41ad5d60850a3a01b8658fb4e58d2473fe2d36938f4ab66eb05c6"
-    sha256 cellar: :any_skip_relocation, sierra:         "ff4ba4b7cad5fa4211bff04d5868521bc21b60995cf40f15bd507abb7c4cbaab"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "ec4898462899cb632329f71dc0b4dd9a13a051aafd6da7dfd22e940e9d1ce01a"
     sha256 cellar: :any_skip_relocation, arm64_linux:    "e0686ce3cedc670157058d0c768236fd2276290197884ab1759aeba4f60789fb"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "db033c09d09fa627dd0d98d52a4b32231a6f696f925c77b0c91d7b8f057ea3f4"
   end

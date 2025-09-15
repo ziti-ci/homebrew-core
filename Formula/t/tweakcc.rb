cdd6aa7fa738688256d010dafe9abcbf8e93eecf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-15 01:17:18 +0000

    tweakcc: add 1.5.3 bottle.

--- Formula/t/tweakcc.rb
+++ Formula/t/tweakcc.rb
@@ -5,6 +5,14 @@ class Tweakcc < Formula
   sha256 "ce35fd5af9eca4c390f88cc4540c70980fe559e6afbe4673c5b583a32f6fa24e"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "517b5dcf7da38acfa1d97d6f973d7c0af78a2113349d071853b26cdea71f4110"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "517b5dcf7da38acfa1d97d6f973d7c0af78a2113349d071853b26cdea71f4110"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "517b5dcf7da38acfa1d97d6f973d7c0af78a2113349d071853b26cdea71f4110"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1d8888ac6ce5391e40d76f7a5c6d9b5a15437849eb257e5cb4630af4337f6725"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d8888ac6ce5391e40d76f7a5c6d9b5a15437849eb257e5cb4630af4337f6725"
+  end
+
   depends_on "node"
 
   def install

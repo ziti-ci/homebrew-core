019285e63860247113de0bc395a04e23903fffbd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-16 16:55:21 +0000

    code2prompt 4.0.0

--- Formula/c/code2prompt.rb
+++ Formula/c/code2prompt.rb
@@ -1,8 +1,8 @@
 class Code2prompt < Formula
   desc "CLI tool to convert your codebase into a single LLM prompt"
   homepage "https://code2prompt.dev/"
-  url "https://github.com/mufeedvh/code2prompt/archive/refs/tags/v3.0.2.tar.gz"
-  sha256 "08e45407b71bf5e5fb89930043b085cf8965a008dc5004d4aa4ac64db0e447e0"
+  url "https://github.com/mufeedvh/code2prompt/archive/refs/tags/v4.0.0.tar.gz"
+  sha256 "8d5958a7ad8f906c6643bdf541404c008d89be6f26102a29f2c4f485a42b911d"
   license "MIT"
   head "https://github.com/mufeedvh/code2prompt.git", branch: "main"
 

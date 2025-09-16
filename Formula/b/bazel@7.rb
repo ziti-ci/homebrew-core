065ea45937957de36ad9a19421babbb53c31768c
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-16 18:49:15 +0000

    bazel@7: update 7.6.1 bottle.

--- Formula/b/bazel@7.rb
+++ Formula/b/bazel@7.rb
@@ -11,6 +11,7 @@ class BazelAT7 < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6c901b27243074fbd633c27c9418db4ac81d6c4c1375d5220a6a6eb45ba1db55"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "7dae7266441ab2beedf3182e5f6a8d2a19ffa7ac14af31c0133abb11f36738f6"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "966ae2fc6bc711139718ce063e31c48c2b627b474d222a25568233ff07298929"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "ff70d65599bbce115caa90cba8efbe7351b6e1341b69c2ae2c18f57059064f02"

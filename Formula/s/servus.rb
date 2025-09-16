e12ca0ec4c410d6cd52ecc1a0837cc7ee48ac0a6
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-16 09:34:24 +0800

    servus: fix build with CMake 4.0+

--- Formula/s/servus.rb
+++ Formula/s/servus.rb
@@ -33,7 +33,9 @@ class Servus < Formula
   end
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    # Workaround to build with CMake 4
+    args = %w[-DCMAKE_POLICY_VERSION_MINIMUM=3.5]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end

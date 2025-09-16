2e282ca8ee4ee83ff20890254c6432c905098fc0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-16 08:37:52 +0000

    zsh-vi-mode 0.12.0

--- Formula/z/zsh-vi-mode.rb
+++ Formula/z/zsh-vi-mode.rb
@@ -1,8 +1,8 @@
 class ZshViMode < Formula
   desc "Better and friendly vi(vim) mode plugin for ZSH"
   homepage "https://github.com/jeffreytse/zsh-vi-mode"
-  url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "03e1b5f0eef89afa834416bd2751584093b929506e11867fbabf9a7f9e57452a"
+  url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.12.0.tar.gz"
+  sha256 "28cdbc1803cf5545e5e5e4b29db075dc4d45b8c498bb6118bfff5c7df1b68622"
   license "MIT"
   head "https://github.com/jeffreytse/zsh-vi-mode.git", branch: "master"
 

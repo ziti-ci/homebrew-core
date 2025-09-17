5554a429595fc01d1012d3913f960f5b3c2feca2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-17 19:55:19 +0000

    web-ext: update 8.10.0 bottle.

--- Formula/w/web-ext.rb
+++ Formula/w/web-ext.rb
@@ -6,14 +6,12 @@ class WebExt < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a9a1364793914df6d1314d3bb33330ada02c91db769806803f1b92913917db45"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3841f0cb6c0f92acc8ac16b33f6009e92059128b1e79fa2f848046489f92a83c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3841f0cb6c0f92acc8ac16b33f6009e92059128b1e79fa2f848046489f92a83c"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3841f0cb6c0f92acc8ac16b33f6009e92059128b1e79fa2f848046489f92a83c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c1d5c298d0399714d03afec2266ca86f9e47dee14fe83343c956f4a837adda75"
-    sha256 cellar: :any_skip_relocation, ventura:       "c1d5c298d0399714d03afec2266ca86f9e47dee14fe83343c956f4a837adda75"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3c725fc7b120bdff7c405044fae2b39b937f159b2301549c9879dd1683b14c96"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c725fc7b120bdff7c405044fae2b39b937f159b2301549c9879dd1683b14c96"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4ac796c0cc872da49cc738c9a8551020c39bf5f31f90f1e8af71b5831e305a4b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4ac796c0cc872da49cc738c9a8551020c39bf5f31f90f1e8af71b5831e305a4b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4ac796c0cc872da49cc738c9a8551020c39bf5f31f90f1e8af71b5831e305a4b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4ac796c0cc872da49cc738c9a8551020c39bf5f31f90f1e8af71b5831e305a4b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a38f5e9b14c8420dbbbb898be65ed77b47a0821a504b4e81efda78e8616b1387"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a38f5e9b14c8420dbbbb898be65ed77b47a0821a504b4e81efda78e8616b1387"
   end
 
   depends_on "node"

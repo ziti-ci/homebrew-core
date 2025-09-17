01ddf0a3eb6a131683b6ac8cf75e0bae8279817e
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-17 19:43:59 +0800

    gradle-profiler: switch to `openjdk@21`
    In preparation for `openjdk` 25 (#243689).

--- Formula/g/gradle-profiler.rb
+++ Formula/g/gradle-profiler.rb
@@ -1,9 +1,11 @@
 class GradleProfiler < Formula
   desc "Profiling and benchmarking tool for Gradle builds"
   homepage "https://github.com/gradle/gradle-profiler/"
+  # TODO: Check if we can use `openjdk` 25+ when bumping the version.
   url "https://search.maven.org/remotecontent?filepath=org/gradle/profiler/gradle-profiler/0.22.0/gradle-profiler-0.22.0.zip"
   sha256 "a269f05861d2682c3b98d050330764200bc322024762295713fab167d133283c"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url "https://search.maven.org/remotecontent?filepath=org/gradle/profiler/gradle-profiler/maven-metadata.xml"
@@ -15,12 +17,12 @@ class GradleProfiler < Formula
     sha256 cellar: :any_skip_relocation, all: "4fdec547a78284921b7ccb437e045ec11fa0d0b16cf4959fbd2ddd8081209498"
   end
 
-  depends_on "openjdk"
+  depends_on "openjdk@21"
 
   def install
     rm(Dir["bin/*.bat"])
     libexec.install %w[bin lib]
-    env = Language::Java.overridable_java_home_env
+    env = Language::Java.overridable_java_home_env("21")
     (bin/"gradle-profiler").write_env_script libexec/"bin/gradle-profiler", env
   end
 

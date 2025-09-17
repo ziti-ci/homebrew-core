6c8695d4f4949948b7490296e039c128b71bcca2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-17 17:09:21 +0000

    greenmask 0.2.14

--- Formula/g/greenmask.rb
+++ Formula/g/greenmask.rb
@@ -1,8 +1,8 @@
 class Greenmask < Formula
   desc "PostgreSQL dump and obfuscation tool"
   homepage "https://www.greenmask.io/"
-  url "https://github.com/GreenmaskIO/greenmask/archive/refs/tags/v0.2.13.tar.gz"
-  sha256 "107b29d94f79dede9d414323f50b9b2c286bd19dc585e5d55dd4cc4d2a415d48"
+  url "https://github.com/GreenmaskIO/greenmask/archive/refs/tags/v0.2.14.tar.gz"
+  sha256 "093e677914f3191de33bca479debd5305fff68420055873b3e331f829748f2c5"
   license "Apache-2.0"
   head "https://github.com/GreenmaskIO/greenmask.git", branch: "main"
 

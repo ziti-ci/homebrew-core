476e1dc38544460a7b138635460047e28b0721dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-17 02:43:53 +0000

    spotbugs 4.9.6

--- Formula/s/spotbugs.rb
+++ Formula/s/spotbugs.rb
@@ -1,8 +1,8 @@
 class Spotbugs < Formula
   desc "Tool for Java static analysis (FindBugs's successor)"
   homepage "https://spotbugs.github.io/"
-  url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/4.9.5/spotbugs-4.9.5.tgz"
-  sha256 "6c55c38f28a76dd11946117c175a520fbbc0abdfe6e62fdc3291fe449e8e3202"
+  url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/4.9.6/spotbugs-4.9.6.tgz"
+  sha256 "55aa9b9e3deef0391be285335dcf134d1ce54aae222bba1da757eaa616108957"
   license "LGPL-2.1-or-later"
 
   livecheck do

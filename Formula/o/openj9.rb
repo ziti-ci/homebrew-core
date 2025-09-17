55dccf99bd99c86b7cbc714f9f3083272dafe142
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-09-18 01:38:26 +0800

    openj9: fix build on Tahoe

--- Formula/o/openj9.rb
+++ Formula/o/openj9.rb
@@ -95,6 +95,13 @@ class Openj9 < Formula
     url "https://github.com/eclipse-openj9/openj9-omr.git",
         tag:      "openj9-0.48.0",
         revision: "d10a4d553a3cfbf35db0bcde9ebccb24cdf1189f"
+
+    # Fix syntax error in OptionFlagArray class definition
+    # Remove when bumped to openj9-0.53.0 or later.
+    patch do
+      url "https://github.com/eclipse-openj9/openj9-omr/commit/29203c807abe45fce56e70b93e80ebaef22b0844.patch?full_index=1"
+      sha256 "044a26ac76fafc536feb8dfc24c521ef26a66c98b78948f4571293a41ede24ca"
+    end
   end
 
   resource "openj9-openjdk-jdk" do
@@ -102,6 +109,10 @@ class Openj9 < Formula
         tag:      "openj9-0.46.1",
         revision: "b77827589c585158319340068dae8497b75322c6"
   end
+
+  # Fix build on Clang 17+. Backport of:
+  # https://github.com/itf/libffi/commit/3065c530d3aa50c2b5ee9c01f88a9c0b61732805
+  patch :DATA
 
   def install
     openj9_files = buildpath.children
@@ -213,3 +224,39 @@ class Openj9 < Formula
     assert_match "Hello, world!", shell_output("#{bin}/java HelloWorld")
   end
 end
+
+__END__
+diff --git a/runtime/libffi/aarch64/sysv.S b/runtime/libffi/aarch64/sysv.S
+index eeaf3f8514..329889cfb3 100644
+--- a/runtime/libffi/aarch64/sysv.S
++++ b/runtime/libffi/aarch64/sysv.S
+@@ -76,8 +76,8 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
+    x5 closure
+ */
+
+-	cfi_startproc
+ CNAME(ffi_call_SYSV):
++	cfi_startproc
+ 	/* Sign the lr with x1 since that is where it will be stored */
+ 	SIGN_LR_WITH_REG(x1)
+
+@@ -268,8 +268,8 @@ CNAME(ffi_closure_SYSV_V):
+ #endif
+
+ 	.align	4
+-	cfi_startproc
+ CNAME(ffi_closure_SYSV):
++	cfi_startproc
+ 	SIGN_LR
+ 	stp     x29, x30, [sp, #-ffi_closure_SYSV_FS]!
+ 	cfi_adjust_cfa_offset (ffi_closure_SYSV_FS)
+@@ -500,8 +500,8 @@ CNAME(ffi_go_closure_SYSV_V):
+ #endif
+
+ 	.align	4
+-	cfi_startproc
+ CNAME(ffi_go_closure_SYSV):
++	cfi_startproc
+ 	stp     x29, x30, [sp, #-ffi_closure_SYSV_FS]!
+ 	cfi_adjust_cfa_offset (ffi_closure_SYSV_FS)
+ 	cfi_rel_offset (x29, 0)

9a6fdf32d96994ca2775a70036a30d9c557be703
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 12:45:13 +0000

    sourcery: update 2.3.0 bottle.

--- Formula/s/sourcery.rb
+++ Formula/s/sourcery.rb
@@ -8,14 +8,12 @@ class Sourcery < Formula
   head "https://github.com/krzysztofzablocki/Sourcery.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d1858c459535b0d6d55b33a4e749e3bbabffbe3d3cd6dd683f0a8a12e39b564b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "492ed39e90b1fa57dfd06fe1232473e57d9b5b7f110453d04387db5a76f19192"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4cef0fe60955af53805a58f3da3d80e6b0849835a8892d7cd31e885ff98afc3f"
-    sha256 cellar: :any,                 arm64_ventura: "7bad173e863423cf66b082d6f1f9e7f34a73fe121aabb3ca29a81be10ed7e707"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8d82c40e46e20b5e60908144e8d58e440798c802a545a2b3d3fbd2db70b87b74"
-    sha256 cellar: :any,                 ventura:       "e621c3cc046ef8c5336b796ad72c36ee2d608b518b5bc746f78d04170c46d2aa"
-    sha256                               arm64_linux:   "5bafe848b77836b034a0c8f0c3c411267feac36e5ee758f184ce7c38197d446d"
-    sha256                               x86_64_linux:  "f0818110b2abe047e9055f761a77c7321f69e5e6563187213461bea738fa46fd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d293720e393e8f1cdb9934480c57c2988197b8687866647d852007cd306bb532"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e9b00239a0776854f00ea97b0f39f7427a0d1918b262a1e9870c0609e8ad3704"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bebaf580a1245a2252b45bfe47f2379fbf1b45a6d9d719519e34c3f5082f1396"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3195f7b0b2da7dc65223012c8780d00967bf4e77eaef7c65b883b1d63d7a2be0"
+    sha256                               arm64_linux:   "6db97b372c4ec3fb2d91b02d84a63344cd812abc10a7f59064204ca30121f977"
+    sha256                               x86_64_linux:  "521ecc8ae4d51e5a43935abaed048f64b7738b0e88291606d0bc38a4eaee0ade"
   end
 
   depends_on xcode: "14.3"

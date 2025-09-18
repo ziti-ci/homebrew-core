a0c5a1f5d482e91587fae0f9452aa28462e9b12f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 01:51:55 +0000

    qt: update 6.9.2 bottle.

--- Formula/q/qt.rb
+++ Formula/q/qt.rb
@@ -24,11 +24,12 @@ class Qt < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_sonoma:  "fc1ad41a8b0ceb5f6047afd936719fb11ae7e253bba51b2b0335635c85b47737"
-    sha256 cellar: :any, arm64_ventura: "9f859faa1731e2ecc5bbb169b3ed89b01f2b6549c7ce6f3b33c4b75eb0316bba"
-    sha256 cellar: :any, sonoma:        "95950612543fe870bae285a67027609f0bee2fc389cacccd036b5a794f1c4ce2"
-    sha256 cellar: :any, ventura:       "67063fcac412aeeda3ae3a911b109f2ad2b031761f264bbe0fdb9ad76a6d1b48"
-    sha256               x86_64_linux:  "e87e7711c808aa3c97e70c44f122bfdbd3d2ce695deb44282540cd67ed223cfc"
+    rebuild 1
+    sha256 cellar: :any, arm64_tahoe:   "21254c8c796c8e2c9a0b29a465f45ea1d2831930847d9de1c30f2333befb9de4"
+    sha256 cellar: :any, arm64_sequoia: "b51bc26874d98507975fb7bb8b5e43f0fa617c503f9b56923d0196016ea1894a"
+    sha256 cellar: :any, arm64_sonoma:  "3e783ecc24d44882a75c8ea62da58428b303d57ace8cd00ce351e8aa5160c25c"
+    sha256 cellar: :any, sonoma:        "e5f574692f7586ef2674b2a4bb5e24f6ccf77ee7cce15a827126a0142c723a22"
+    sha256               x86_64_linux:  "0b405353963f49dabb10fb0697ca5dae05d025ae37efb57d22e1aa66ecc381bc"
   end
 
   depends_on "cmake" => [:build, :test]

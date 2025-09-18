3ee4d05347fee469cf48f43d4ffe8ef872c55c2d
Daeho Ro <email@daeho.ro>
2025-09-18 13:40:00 +0000

    moc: update 2.5.2_10 bottle.

--- Formula/m/moc.rb
+++ Formula/m/moc.rb
@@ -45,6 +45,7 @@ class Moc < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
+    sha256 arm64_tahoe:   "bac52905e18eff309f2c6ba4077a3008f1ca2f449055406f511767d881b3545a"
     sha256 arm64_sequoia: "0c9fa6831440aa41b25064c9f2d93e9827f0d4f1ea0f2e1184def14041366d1b"
     sha256 arm64_sonoma:  "f9698a87a21e1695cfc81523056a86d2a08b3ecb013b978c46b0584a2cff566e"
     sha256 arm64_ventura: "5494731a8b584382705cf06e159d10c42fd869150ab605cf4a096b2112e61d65"

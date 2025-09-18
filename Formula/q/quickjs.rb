fe24defafbe999879561f59a91e18fd9046f5867
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 11:15:18 +0000

    quickjs 2025-09-13-2

--- Formula/q/quickjs.rb
+++ Formula/q/quickjs.rb
@@ -1,8 +1,9 @@
 class Quickjs < Formula
   desc "Small and embeddable JavaScript engine"
   homepage "https://bellard.org/quickjs/"
-  url "https://bellard.org/quickjs/quickjs-2025-09-13.tar.xz"
-  sha256 "6f1f322aea3bb3a90858db85c9fe717013fde4df7dfcafe2f57e78f5bb4b4a0c"
+  url "https://bellard.org/quickjs/quickjs-2025-09-13-2.tar.xz"
+  version "2025-09-13-2"
+  sha256 "996c6b5018fc955ad4d06426d0e9cb713685a00c825aa5c0418bd53f7df8b0b4"
   license "MIT"
 
   livecheck do
@@ -20,10 +21,6 @@ class Quickjs < Formula
   end
 
   def install
-    # Create dummy JSON file in `examples` path
-    # so that `make install` does not return an error
-    # https://github.com/bellard/quickjs/issues/440
-    (buildpath/"examples/message.json").write "{}"
     system "make", "install", "PREFIX=#{prefix}", "CONFIG_M32="
   end
 

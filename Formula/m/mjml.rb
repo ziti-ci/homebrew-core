703d1a2eb6b51885600eee325c6763c04992508b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 20:09:15 +0000

    mjml 4.16.1

--- Formula/m/mjml.rb
+++ Formula/m/mjml.rb
@@ -1,8 +1,8 @@
 class Mjml < Formula
   desc "JavaScript framework that makes responsive-email easy"
   homepage "https://mjml.io"
-  url "https://registry.npmjs.org/mjml/-/mjml-4.16.0.tgz"
-  sha256 "fa0cf10c39b2435442bf85338f15454735ea0abfda894175fc84ca8b869cbb43"
+  url "https://registry.npmjs.org/mjml/-/mjml-4.16.1.tgz"
+  sha256 "39f6b777b88240865abc4ccbbabaeffa85b10621c348fee78b355f4a85090acb"
   license "MIT"
 
   bottle do

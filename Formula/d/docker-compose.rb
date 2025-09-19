a3b150d438f1fc3b82cdd0675001814bde4d1f9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-19 10:59:24 +0000

    docker-compose 2.39.4

--- Formula/d/docker-compose.rb
+++ Formula/d/docker-compose.rb
@@ -1,8 +1,8 @@
 class DockerCompose < Formula
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/refs/tags/v2.39.3.tar.gz"
-  sha256 "3888259a6a212ebbdfd8762f394ae5beafb98cc383142cce46eb27cbc36a5d9f"
+  url "https://github.com/docker/compose/archive/refs/tags/v2.39.4.tar.gz"
+  sha256 "8f3db575b2533dfc0b04e233050593f0d9a435a117cc8b8653b4e89f0813857c"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git", branch: "main"
 

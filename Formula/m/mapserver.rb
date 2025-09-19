9f31930b18d55aefd29a9a76fd3ac858f5f770ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-19 20:06:47 +0000

    mapserver 8.4.1

--- Formula/m/mapserver.rb
+++ Formula/m/mapserver.rb
@@ -1,10 +1,9 @@
 class Mapserver < Formula
   desc "Publish spatial data and interactive mapping apps to the web"
   homepage "https://mapserver.org/"
-  url "https://download.osgeo.org/mapserver/mapserver-8.4.0.tar.gz"
-  sha256 "b0cb3612cd58458cca0808b117c16b9415b3231af39aefb90d668e8b8b188e2c"
+  url "https://download.osgeo.org/mapserver/mapserver-8.4.1.tar.gz"
+  sha256 "fe60bfdbab69437b5f97bb4ca41f2407e245c90edc2a727bf1d4428edb4a240f"
   license "MIT"
-  revision 1
 
   livecheck do
     url "https://mapserver.org/download.html"

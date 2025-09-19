6906d6b8bbf18c77f44455d588e02a52fe19e1e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-19 22:52:02 +0000

    btop 1.4.5

--- Formula/b/btop.rb
+++ Formula/b/btop.rb
@@ -1,8 +1,8 @@
 class Btop < Formula
   desc "Resource monitor. C++ version and continuation of bashtop and bpytop"
   homepage "https://github.com/aristocratos/btop"
-  url "https://github.com/aristocratos/btop/archive/refs/tags/v1.4.4.tar.gz"
-  sha256 "98d464041015c888c7b48de14ece5ebc6e410bc00ca7bb7c5a8010fe781f1dd8"
+  url "https://github.com/aristocratos/btop/archive/refs/tags/v1.4.5.tar.gz"
+  sha256 "0ffe03d3e26a3e9bbfd5375adf34934137757994f297d6b699a46edd43c3fc02"
   license "Apache-2.0"
   head "https://github.com/aristocratos/btop.git", branch: "main"
 

4e5003ac6f91c1b4bc1e944b0a45ba0be0b443e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-19 20:20:47 +0000

    tomee-plus 10.1.2

--- Formula/t/tomee-plus.rb
+++ Formula/t/tomee-plus.rb
@@ -1,9 +1,9 @@
 class TomeePlus < Formula
   desc "Everything in TomEE Web Profile and JAX-RS, plus more"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-10.1.1/apache-tomee-10.1.1-plus.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-10.1.1/apache-tomee-10.1.1-plus.tar.gz"
-  sha256 "5563cffd7777df4e30575d3e3a27cb964fac1b7c010209cb0e23153e49f6f68d"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-10.1.2/apache-tomee-10.1.2-plus.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-10.1.2/apache-tomee-10.1.2-plus.tar.gz"
+  sha256 "d64bcd00029ba3ea51c59263a581eff6da0df5b24f636dfdf83755dc2f765698"
   license "Apache-2.0"
 
   bottle do

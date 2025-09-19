ba86feefddbb201256c03ae108d8b1da8583c72d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-19 10:51:34 +0000

    bb-cli 1.3.4

--- Formula/b/bb-cli.rb
+++ Formula/b/bb-cli.rb
@@ -1,8 +1,8 @@
 class BbCli < Formula
   desc "Bitbucket Rest API CLI written in pure PHP"
   homepage "https://bb-cli.github.io"
-  url "https://github.com/bb-cli/bb-cli/releases/download/1.3.3/bb"
-  sha256 "3a43bd7bc80cde7b2ae8091113d358258b661a63e81a952cbe5111a1aee6e4fd"
+  url "https://github.com/bb-cli/bb-cli/releases/download/1.3.4/bb"
+  sha256 "6c6c5e64b67b113d8d2249bc153303044a94a60c6017e3ed392c3a4087f54c02"
   license "MIT"
 
   bottle do

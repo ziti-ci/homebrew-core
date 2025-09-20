58ac5cd04ff2f7ecbfadf5ae793cd1681b5ae079
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-20 17:06:13 +0000

    nip4 9.0.13

--- Formula/n/nip4.rb
+++ Formula/n/nip4.rb
@@ -1,8 +1,8 @@
 class Nip4 < Formula
   desc "Image processing spreadsheet"
   homepage "https://github.com/jcupitt/nip4"
-  url "https://github.com/jcupitt/nip4/releases/download/v9.0.12/nip4-9.0.12.tar.xz"
-  sha256 "f778f5c7183c30048d1297820aff051a3248d039af6ed96bbd214b5fc7b7a91c"
+  url "https://github.com/jcupitt/nip4/releases/download/v9.0.13/nip4-9.0.13.tar.xz"
+  sha256 "10330eb7f470b8774326d8a05698829e858af9c5f70d4392433abd7f2abfd56e"
   license "GPL-2.0-or-later"
   head "https://github.com/jcupitt/nip4.git", branch: "main"
 

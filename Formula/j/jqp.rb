703474deb8ffd9029799a4ac5f0194cbf1aa79cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-20 23:52:05 +0000

    jqp: add 0.7.0 bottle.

--- Formula/j/jqp.rb
+++ Formula/j/jqp.rb
@@ -5,6 +5,14 @@ class Jqp < Formula
   sha256 "e88b32aeb21b8d000e17619f23a0c00a1eb86219112204031f63fb7cdfafacf0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "92566027d91cd63e3b3f0d31c62381330ac6c8c1c944831a3fdc5308c9532ab7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92566027d91cd63e3b3f0d31c62381330ac6c8c1c944831a3fdc5308c9532ab7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "92566027d91cd63e3b3f0d31c62381330ac6c8c1c944831a3fdc5308c9532ab7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e199de504b616c1b56b8ecbc62e83cd6bdca0ab5e069705dbd141435d721ed91"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "96c9c531b034f344c1c55120312b9664cc09d1aea6364042f84abda6cfe3edd3"
+  end
+
   depends_on "go" => :build
 
   def install

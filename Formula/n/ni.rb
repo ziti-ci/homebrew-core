58ac6450198e8a1d7799b1f055a99d67cb759a53
Alexander Hetzmannseder <git@hetzmannseder.dev>
2025-09-21 00:00:42 +0200

    ni 26.0.1
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Formula/n/ni.rb
@@ -0,0 +1,39 @@
+class Ni < Formula
+  desc "Selects the right Node package manager based on lockfiles"
+  homepage "https://github.com/antfu-collective/ni"
+  url "https://registry.npmjs.org/@antfu/ni/-/ni-26.0.1.tgz"
+  sha256 "bc65d795986d4d86ad5486814c661c509275f42e6b8aba879157ec082f0dfa1b"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ni --version")
+
+    (testpath/"package.json").write <<~JSON
+      {
+        "name": "fake-package",
+        "version": "1.0.0",
+        "description": "Fake package for testing",
+        "license": "MIT"
+      }
+    JSON
+
+    (testpath/"package-lock.json").write <<~JSON
+      {
+        "name": "fake-package",
+        "version": "1.0.0",
+        "lockfileVersion": 3,
+        "packages": {},
+        "dependencies": {}
+      }
+    JSON
+
+    assert_match "up to date, audited 1 package", shell_output(bin/"ni")
+  end
+end

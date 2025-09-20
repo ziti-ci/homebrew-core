d7f89f243e1814067e482fbf869a3b687c733572
Caleb Xu <3536482+alebcay@users.noreply.github.com>
2025-09-20 23:52:34 +0000

    opencode: update 0.10.4 bottle.

--- Formula/o/opencode.rb
+++ Formula/o/opencode.rb
@@ -10,6 +10,7 @@ class Opencode < Formula
     sha256                               arm64_sequoia: "3f88c90cbcee418d9c2f858dc1b8aba8b8df9c22ad3d28582d6f78329f6579b8"
     sha256                               arm64_sonoma:  "3f88c90cbcee418d9c2f858dc1b8aba8b8df9c22ad3d28582d6f78329f6579b8"
     sha256 cellar: :any_skip_relocation, sonoma:        "2efd25621040869ec60bd983c913a38b7cc8c2427dcb5f93b2b814a95e979d9e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d2ee345b91173a85f9ed424e6beb3c06096918c234e29b4ff1089c25c13f08c3"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "b958083c861fe4724a43419766863f643183c18caafa3d93e2b97d9c7ed8bf05"
   end
 

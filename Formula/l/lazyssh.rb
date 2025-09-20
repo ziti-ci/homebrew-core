f04a6ebb19762e7ddc6031cabdc8fa20e1e2010b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-20 18:36:22 +0000

    lazyssh: add 0.2.1 bottle.

--- Formula/l/lazyssh.rb
+++ Formula/l/lazyssh.rb
@@ -6,6 +6,14 @@ class Lazyssh < Formula
   license "Apache-2.0"
   head "https://github.com/Adembc/lazyssh.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f0c63da9b9f68f23c8972fa6b05494629f0adaad4916935df48570e502cfa4f4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f0c63da9b9f68f23c8972fa6b05494629f0adaad4916935df48570e502cfa4f4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f0c63da9b9f68f23c8972fa6b05494629f0adaad4916935df48570e502cfa4f4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e12e64c16498b7389dd64e0b5881a1b049247fd001410411c7b54078e6aaf476"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a0b3f0e05fcca49ef5267526d22d8ad447758073a7d5b7fa29f693dd1db19d8"
+  end
+
   depends_on "go" => :build
 
   def install

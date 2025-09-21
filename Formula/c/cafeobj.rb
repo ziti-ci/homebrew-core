b5c9694ab0ef6b1cb5719cf14506aa2ff1b708ce
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-09-21 23:37:51 +0000

    cafeobj: update 1.6.2 bottle.

--- Formula/c/cafeobj.rb
+++ Formula/c/cafeobj.rb
@@ -18,12 +18,13 @@ class Cafeobj < Formula
   no_autobump! because: :requires_manual_review
 
   bottle do
-    sha256 arm64_sequoia: "91c9ae231c3b44acf6923ac1c9131fc8cbe7edf2f992a617ce791289971dea73"
-    sha256 arm64_sonoma:  "01e514ed674dceb8795c1803057a00efc3eb29b69dc250bee9f843a5c67d7de1"
-    sha256 arm64_ventura: "d965be913974b99253cb094994af1e82bcb6c8736d8658d4e4d80deae69dac93"
-    sha256 sonoma:        "775c475b5a3af142c817148e402fa6de72dd767a537e1d7e5d2892472b5f5059"
-    sha256 ventura:       "b711614790294d992198d166691e66e9e9e78abb1c039fc7e5b30eab1e26183f"
-    sha256 x86_64_linux:  "779108ed08023bc358ac5276e28d7d7f0f4febc0dc99ea67ef04da3ac8d56ae2"
+    sha256 cellar: :any, arm64_tahoe:   "9230aa5cb5c850ca25eed5ff7c65dc9e4ac13ab00713e27e1d385f60de53980f"
+    sha256               arm64_sequoia: "91c9ae231c3b44acf6923ac1c9131fc8cbe7edf2f992a617ce791289971dea73"
+    sha256               arm64_sonoma:  "01e514ed674dceb8795c1803057a00efc3eb29b69dc250bee9f843a5c67d7de1"
+    sha256               arm64_ventura: "d965be913974b99253cb094994af1e82bcb6c8736d8658d4e4d80deae69dac93"
+    sha256               sonoma:        "775c475b5a3af142c817148e402fa6de72dd767a537e1d7e5d2892472b5f5059"
+    sha256               ventura:       "b711614790294d992198d166691e66e9e9e78abb1c039fc7e5b30eab1e26183f"
+    sha256               x86_64_linux:  "779108ed08023bc358ac5276e28d7d7f0f4febc0dc99ea67ef04da3ac8d56ae2"
   end
 
   depends_on "sbcl" => :build

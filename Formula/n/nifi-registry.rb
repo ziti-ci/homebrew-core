28aa3e855ab953596f6bc2d3bcf165a3f5eb0e65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-21 20:41:31 +0000

    nifi-registry: update 2.6.0 bottle.

--- Formula/n/nifi-registry.rb
+++ Formula/n/nifi-registry.rb
@@ -7,7 +7,7 @@ class NifiRegistry < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1fa04b57c6fda1feb4a96a25241d4081907177dc0e06ca83cc8b1cc43d747747"
+    sha256 cellar: :any_skip_relocation, all: "139df7413fd3d51ed9e762e9e4a97cc05ebdeb38ed448cede9e5913f77932525"
   end
 
   depends_on "openjdk"

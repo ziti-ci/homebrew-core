c09abc4ea67224d4bfde9ddc3baf2ccc4b5ecb1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-21 08:05:04 +0000

    musikcube 3.0.5

--- Formula/m/musikcube.rb
+++ Formula/m/musikcube.rb
@@ -1,6 +1,8 @@
 class Musikcube < Formula
   desc "Terminal-based audio engine, library, player and server"
   homepage "https://musikcube.com"
+  url "https://github.com/clangen/musikcube/archive/refs/tags/3.0.5.tar.gz"
+  sha256 "708292a583bb5072a8dbb14e408c2a1f61de9b8c9786d4e53b3e69bef5dad8c5"
   license all_of: [
     "BSD-3-Clause",
     "GPL-2.0-or-later", # src/plugins/supereqdsp/supereq/
@@ -11,32 +13,7 @@ class Musikcube < Formula
     "bcrypt-Solar-Designer", # src/3rdparty/{include,src}/md5.*
     "blessing", # src/3rdparty/{include,src}/sqlite/sqlite3*
   ]
-  revision 2
   head "https://github.com/clangen/musikcube.git", branch: "master"
-
-  stable do
-    url "https://github.com/clangen/musikcube/archive/refs/tags/3.0.4.tar.gz"
-    sha256 "25bb95b8705d8c79bde447e7c7019372eea7eaed9d0268510278e7fcdb1378a5"
-
-    # Backport support for newer asio. Using resource to deal with submodule
-    resource "asio.patch" do
-      url "https://github.com/clangen/musikcube/commit/a5a8a4ba6e21e09185ce10b5ecb48d6bb30f3d07.patch?full_index=1"
-      sha256 "58e4215a6319b625a5c11990732ebabb2622e1dc7a91d5ef48ec791db415b704"
-
-      # Remove submodule modification as `patch` can't handle this
-      patch :DATA
-    end
-
-    # Backport support for FFmpeg 8.0
-    patch do
-      url "https://github.com/clangen/musikcube/commit/a0433606af616b6d1146d10c964195dd81d244c8.patch?full_index=1"
-      sha256 "ba1f480663d28e0d25f84c11e9b60a03600f37976794fd58349e038ac85e2229"
-    end
-    patch do
-      url "https://github.com/clangen/musikcube/commit/1a5887f6dcd8f0c3ed7ddef400a7dc1114721459.patch?full_index=1"
-      sha256 "a04ce7b24631d371ea77373026d617661cfe091b94cd356e349d77561b8bda84"
-    end
-  end
 
   livecheck do
     url :stable
@@ -85,11 +62,6 @@ class Musikcube < Formula
   end
 
   def install
-    if build.stable?
-      resource("asio.patch").stage { buildpath.install Dir["*"].first => "asio.patch" }
-      Patch.create(:p1, File.read("asio.patch")).apply
-    end
-
     # Pretend to be Nix to dynamically link ncurses on macOS.
     ENV["NIX_CC"] = ENV.cc
 
@@ -113,21 +85,3 @@ class Musikcube < Formula
     end
   end
 end
-
-__END__
---- a/a5a8a4ba6e21e09185ce10b5ecb48d6bb30f3d07.patch
-+++ b/a5a8a4ba6e21e09185ce10b5ecb48d6bb30f3d07.patch
-@@ -29,13 +29,6 @@ Subject: [PATCH] Update to asio 1.36.0
-  create mode 100644 src/3rdparty/include/websocketpp/transport/debug/connection.hpp
-  create mode 100644 src/3rdparty/include/websocketpp/transport/debug/endpoint.hpp
- 
--diff --git a/src/3rdparty/asio b/src/3rdparty/asio
--index f693a3eb7fe72a5f19b975289afc4f437d373d9c..231cb29bab30f82712fcd54faaea42424cc6e710 160000
----- a/src/3rdparty/asio
--+++ b/src/3rdparty/asio
--@@ -1 +1 @@
---Subproject commit f693a3eb7fe72a5f19b975289afc4f437d373d9c
--+Subproject commit 231cb29bab30f82712fcd54faaea42424cc6e710
- diff --git a/src/3rdparty/include/websocketpp/roles/server_endpoint.hpp b/src/3rdparty/include/websocketpp/roles/server_endpoint.hpp
- index 9cc652f75ce1c31c597341e5ec2ad47ce17a40be..1967a4733e1a77045f8b5bce6cd0fad335c7a4a5 100644
- --- a/src/3rdparty/include/websocketpp/roles/server_endpoint.hpp

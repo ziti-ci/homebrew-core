abbd0dcc3fd9840ac4e986b69400ef6f5c94afb8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-22 17:06:00 +0000

    flexget 3.18.10

--- Formula/f/flexget.rb
+++ Formula/f/flexget.rb
@@ -3,8 +3,8 @@ class Flexget < Formula
 
   desc "Multipurpose automation tool for content"
   homepage "https://www.flexget.com"
-  url "https://files.pythonhosted.org/packages/ce/f8/c6eafbc06469bb43ca559f01fa981a5bd585e23ae8d01c0d5c2127fea4fa/flexget-3.18.9.tar.gz"
-  sha256 "52369022a19f0cddb39a93fb5d560af76931e425b822f751591217cec2a77b63"
+  url "https://files.pythonhosted.org/packages/f2/64/a52f534de300a4fc6d364c14974c5a903cc2e407ca7b9a5de7bf1d07d063/flexget-3.18.10.tar.gz"
+  sha256 "bacbf7699f96eea249048ea9a4d3eb0b38ac43f5684968e984566ab8a96f633b"
   license "MIT"
 
   bottle do
@@ -68,8 +68,8 @@ class Flexget < Formula
   end
 
   resource "cheroot" do
-    url "https://files.pythonhosted.org/packages/63/e2/f85981a51281bd30525bf664309332faa7c81782bb49e331af603421dbd1/cheroot-10.0.1.tar.gz"
-    sha256 "e0b82f797658d26b8613ec8eb563c3b08e6bd6a7921e9d5089bd1175ad1b1740"
+    url "https://files.pythonhosted.org/packages/f4/01/5ef06df932a974d016ab9d7f93e78740b572c4020016794fd4799cdc09c6/cheroot-11.0.0.tar.gz"
+    sha256 "dd414eda6bdb15140e864bc1d1c9625030375d14cbe0b290092867368924a52f"
   end
 
   resource "cherrypy" do
@@ -78,8 +78,8 @@ class Flexget < Formula
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/60/6c/8ca2efa64cf75a977a0d7fac081354553ebe483345c734fb6b6515d96bbc/click-8.2.1.tar.gz"
-    sha256 "27c491cc05d968d271d5a1db13e3b5a184636d9d930f148c50b038f0d0646202"
+    url "https://files.pythonhosted.org/packages/46/61/de6cd827efad202d7057d93e0fed9294b96952e188f7384832791c7b2254/click-8.3.0.tar.gz"
+    sha256 "e7b8232224eba16f4ebe410c25ced9f7875cb5f3263ffc93cc3e8da705e229c4"
   end
 
   resource "feedparser" do
@@ -238,8 +238,8 @@ class Flexget < Formula
   end
 
   resource "pyparsing" do
-    url "https://files.pythonhosted.org/packages/98/c9/b4594e6a81371dfa9eb7a2c110ad682acf985d96115ae8b25a1d63b4bf3b/pyparsing-3.2.4.tar.gz"
-    sha256 "fff89494f45559d0f2ce46613b419f632bbb6afbdaed49696d322bcf98a58e99"
+    url "https://files.pythonhosted.org/packages/f2/a5/181488fc2b9d093e3972d2a472855aae8a03f000592dbfce716a512b3359/pyparsing-3.2.5.tar.gz"
+    sha256 "2df8d5b7b2802ef88e8d016a2eb9c7aeaa923529cd251ed0fe4608275d4105b6"
   end
 
   resource "pyrss2gen" do

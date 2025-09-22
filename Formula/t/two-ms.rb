b0cc64fb1b282cb4783871ba602853ed528874f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-22 17:37:06 +0000

    two-ms 4.4.0
    two-ms: update build and test
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/two-ms.rb
+++ Formula/t/two-ms.rb
@@ -1,8 +1,8 @@
 class TwoMs < Formula
   desc "Detect secrets in files and communication platforms"
   homepage "https://github.com/Checkmarx/2ms"
-  url "https://github.com/Checkmarx/2ms/archive/refs/tags/v4.1.4.tar.gz"
-  sha256 "7d23eebcbadb9e22b56949c0320ac9a3a6055dc7e8d750bb84583a196fa8d2eb"
+  url "https://github.com/Checkmarx/2ms/archive/refs/tags/v4.4.0.tar.gz"
+  sha256 "edadf136b6d183b9555064de9d4ed2ea1ed229feabd1be04de17a9dac58d2d57"
   license "Apache-2.0"
   head "https://github.com/Checkmarx/2ms.git", branch: "master"
 
@@ -19,12 +19,12 @@ class TwoMs < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X github.com/checkmarx/2ms/v3/cmd.Version=v#{version}"
+    ldflags = "-s -w -X github.com/checkmarx/2ms/v#{version.major}/cmd.Version=v#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags, output: bin/"2ms"), "main.go"
   end
 
   test do
-    version_output = shell_output("#{bin}/2ms --version 2>&1")
+    version_output = shell_output("#{bin}/2ms --version 2>&1", 1)
     assert_match version.to_s, version_output
 
     (testpath/"secret_test.txt").write <<~EOS

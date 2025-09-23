cbcfbeb6c398d72e524014cd3a66466ba8ae8f5c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-23 20:02:17 +0000

    go-feature-flag-relay-proxy 1.46.1

--- Formula/g/go-feature-flag-relay-proxy.rb
+++ Formula/g/go-feature-flag-relay-proxy.rb
@@ -1,8 +1,8 @@
 class GoFeatureFlagRelayProxy < Formula
   desc "Stand alone server to run GO Feature Flag"
   homepage "https://gofeatureflag.org"
-  url "https://github.com/thomaspoignant/go-feature-flag/archive/refs/tags/v1.46.0.tar.gz"
-  sha256 "53fc63c74010912fd7f7484e0f7e470c7578dc3c1bfb0d098949a9dafdd6b85d"
+  url "https://github.com/thomaspoignant/go-feature-flag/archive/refs/tags/v1.46.1.tar.gz"
+  sha256 "a5e58a52ef03dd3567c356247c00fffd73b54a203782d0e8626e9a5070cbfc63"
   license "MIT"
   head "https://github.com/thomaspoignant/go-feature-flag.git", branch: "main"
 

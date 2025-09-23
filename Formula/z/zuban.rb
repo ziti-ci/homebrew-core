2f2ec68d127c019fd542cca3c73186bca090e902
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-23 11:46:43 +0000

    zuban: add 0.0.23 bottle.

--- Formula/z/zuban.rb
+++ Formula/z/zuban.rb
@@ -6,6 +6,14 @@ class Zuban < Formula
   license "AGPL-3.0-only"
   head "https://github.com/zubanls/zuban.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "145058ff0f6d0eabd6ac398eb1e51552c14005ac8dbc2fe43dbc850cf6d23fa0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ffa9d85c76986368b4a88d6f29cadc26f90e543926b2d7a540796a8af023be93"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9dcc918ea36140dd741ff5a0847ca4d6e6dabcafb2e4a296159962c41ba599f8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bb70c749b44932e8a2b72ed0c9c2bc35ce42a42595739c09362d7ca47a5a9429"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a15fe420170c6d0636b7adea3bb889908980cb07d3bb0078e0fba1010f966325"
+  end
+
   depends_on "mypy" => :build
   depends_on "rust" => :build
 

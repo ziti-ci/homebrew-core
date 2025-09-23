f9a0c1b7ec72fbc2db54d6b9e39ff23715667c81
Rui Chen <rui@chenrui.dev>
2025-09-23 18:58:50 -0400

    nanobot 0.0.28 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/n/nanobot.rb
@@ -0,0 +1,30 @@
+class Nanobot < Formula
+  desc "Build MCP Agents"
+  homepage "https://www.nanobot.ai/"
+  url "https://github.com/nanobot-ai/nanobot/archive/refs/tags/v0.0.28.tar.gz"
+  sha256 "cfa778c2b2d82b728799d85c6d5478cdb718c00b37021ab4127dcd7e2e06a92b"
+  license "Apache-2.0"
+  head "https://github.com/nanobot-ai/nanobot.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/nanobot-ai/nanobot/pkg/version.Tag=v#{version}
+      -X github.com/nanobot-ai/nanobot/pkg/version.BaseImage=ghcr.io/nanobot-ai/nanobot:v#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nanobot --version")
+
+    pid = spawn bin/"nanobot", "run"
+    sleep 1
+    assert_path_exists testpath/"nanobot.db"
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end

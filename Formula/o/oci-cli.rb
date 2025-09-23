ca645ebe63b4490a7fc7993fde038fe91e16c25e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-23 11:29:33 +0000

    oci-cli: update 3.66.2 bottle.

--- Formula/o/oci-cli.rb
+++ Formula/o/oci-cli.rb
@@ -9,12 +9,12 @@ class OciCli < Formula
   head "https://github.com/oracle/oci-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "83c953f69d570590cf34e37a6ee4a4c85c9d002fd2f0af02715473a89649e8bf"
-    sha256 cellar: :any,                 arm64_sequoia: "c6271ab1586adb1be313008b9983c42a475f355cd9a4e6238945ebb3e36ddc56"
-    sha256 cellar: :any,                 arm64_sonoma:  "eacefdcddf64268af95a8edb4395e293d1f49705c49c477b9de72c83009126fa"
-    sha256 cellar: :any,                 sonoma:        "86bf440eb0d035198ba77c2883d33cd41504ebec4a54b08aebc1df6291ba6534"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9f7662311e4e097e605ddd11fcdf9fe17a26e63eb34595a9f26a6e4c8bb0e2bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3147f9dac96493015615729abac94186dbc41c381ecd6b098964cfa4b1af4a11"
+    sha256 cellar: :any,                 arm64_tahoe:   "496f19ed11f9369ece419494339ffb2594bcf3286a40683a66bfdd0e44a1fd20"
+    sha256 cellar: :any,                 arm64_sequoia: "2307b0a7be7e7932b546108e4b4f49af111b17b5194e8e35d191903594478873"
+    sha256 cellar: :any,                 arm64_sonoma:  "a1438519f329238301c6d6b610d28e36c9e0d9ab20b076115b8004c99e261671"
+    sha256 cellar: :any,                 sonoma:        "cf44b36808df8629f1ee0392b4ec8908cc4f95ce400e39ef0add8739e90a8fa1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c260890d3afe1c02e7509ec9bf7ec2348512daec265a0d26f22c91c2b0ee7617"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "17fea7b216ce1bb6dd919b529282c648e78c50bd96af2d6d58d95726f75d3e5e"
   end
 
   depends_on "certifi"

37f1cb71ca6401da82d2a396647d3275e4ca0712
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-23 17:22:57 +0000

    shapelib 1.6.2

--- Formula/s/shapelib.rb
+++ Formula/s/shapelib.rb
@@ -1,8 +1,8 @@
 class Shapelib < Formula
   desc "Library for reading and writing ArcView Shapefiles"
   homepage "http://shapelib.maptools.org/"
-  url "https://download.osgeo.org/shapelib/shapelib-1.6.1.tar.gz"
-  sha256 "5da90a60e25440f108f4e8e95732bfa83ede13c8e0c2bcf80ae41006cc8ebc20"
+  url "https://download.osgeo.org/shapelib/shapelib-1.6.2.tar.gz"
+  sha256 "4b74a36ced94e9a7bea401157e664addcc5be251e7df7f88d4674361da012c21"
   license any_of: ["LGPL-2.0-or-later", "MIT"]
 
   livecheck do

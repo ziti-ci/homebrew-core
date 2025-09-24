855ea580c9acbbfae48464edd1765efade1890c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 21:19:19 +0000

    lacework-cli: update 2.8.0 bottle.

--- Formula/l/lacework-cli.rb
+++ Formula/l/lacework-cli.rb
@@ -16,11 +16,11 @@ class LaceworkCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a1ac0ee273a2c173062c95c7dfac9b5f141adedfd1113a20056560a42ad913cd"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a1ac0ee273a2c173062c95c7dfac9b5f141adedfd1113a20056560a42ad913cd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a1ac0ee273a2c173062c95c7dfac9b5f141adedfd1113a20056560a42ad913cd"
-    sha256 cellar: :any_skip_relocation, sonoma:        "195f1a105833f7c6a7e8c35517a682001468295b9cccacb070dfd592bfb7c0a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9cf0ae8fe35d62b9439b5dac9a6a1c344772a7e11b95adfdeb2545ce43e792ad"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e521083572c784e2bce97b260da5d0a93407219ea4583cc793fd55d5d6e18253"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e521083572c784e2bce97b260da5d0a93407219ea4583cc793fd55d5d6e18253"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e521083572c784e2bce97b260da5d0a93407219ea4583cc793fd55d5d6e18253"
+    sha256 cellar: :any_skip_relocation, sonoma:        "09124c001f141794c670bf7ae9ee5580a1716996ae89f48a5e322468ab76aac0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b6a3ccc515cca95dac00477d9c1b33e0892392849568afb3dc78f841e1a04ff9"
   end
 
   depends_on "go" => :build

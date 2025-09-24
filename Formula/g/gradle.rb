f6c23e995feb683a8b9e1362d27fbc78da57fbf9
Bartek Pacia <barpac02@gmail.com>
2025-09-24 22:09:54 +0200

    gradle: depend on openjdk again

--- Formula/g/gradle.rb
+++ Formula/g/gradle.rb
@@ -1,7 +1,6 @@
 class Gradle < Formula
   desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
   homepage "https://www.gradle.org/"
-  # TODO: Switch to `openjdk` 25 when 9.1.0 is released.
   url "https://services.gradle.org/distributions/gradle-9.1.0-all.zip"
   sha256 "b84e04fa845fecba48551f425957641074fcc00a88a84d2aae5808743b35fc85"
   license "Apache-2.0"
@@ -16,12 +15,12 @@ class Gradle < Formula
   end
 
   # https://github.com/gradle/gradle/blob/master/platforms/documentation/docs/src/docs/userguide/releases/compatibility.adoc
-  depends_on "openjdk@21"
+  depends_on "openjdk"
 
   def install
     rm(Dir["bin/*.bat"])
     libexec.install %w[bin docs lib src]
-    env = Language::Java.overridable_java_home_env("21")
+    env = Language::Java.overridable_java_home_env
     (bin/"gradle").write_env_script libexec/"bin/gradle", env
   end
 

073f122a1f6fd9ced7d573b91fa2fd14b8becfc1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-25 15:22:32 +0000

    kubetail: update 0.8.0 bottle.

--- Formula/k/kubetail.rb
+++ Formula/k/kubetail.rb
@@ -12,12 +12,12 @@ class Kubetail < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e041bf90e4073d6ed2d0eeaa751d6e9741b9bcc42f9890e0592f394c6ff62711"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a1cd06c1095d56954fcbfa44db7b672a8b7f6d308a99ed116b739de9e3829dcd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00efa07f1369c49982068145f37e486eb5c170c1ec1694b4997fc89348ede936"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fb59ef6ba28765f9f9ddb92b1782fe5a5fffc5217bfe898c915fd84899a095af"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e88717bb3ce183f96fab954182087b0891d58f403f32345f6a07192498cb2afe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4dd362f1e763c2c8fb157746984ab67e82d64bd5865937be5963bbfceaaff9e0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "701747e24e6051c120331a537e1498b743748202c92656f3da3ed8e5588a0469"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f5109b7a0f5418b6853229d8e64dfad44cfdcb0b37bda509b0e710275006b9dd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "19d6dbc4e3c659cfecbea743357b5c043b4ce00c7c795d8c32b76228543821c8"
+    sha256 cellar: :any_skip_relocation, sonoma:        "85bfa5bd3d472acf0835aad26f32525b376d5404b591c9cd76a6b39db2912648"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "34ae2a582a60a055ac3f8c3a0befdf81bfce9c96936ad0fc083770c59d9728b7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e252b86130d71de0956d8ff350db00952e45e21a9e30cb76bd2a4074a280f4a"
   end
 
   depends_on "go" => :build

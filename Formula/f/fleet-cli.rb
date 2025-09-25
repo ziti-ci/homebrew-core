721e035d6cb9b435542bfb6d73e7c359cdcd979d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-25 15:22:06 +0000

    fleet-cli: update 0.13.3 bottle.

--- Formula/f/fleet-cli.rb
+++ Formula/f/fleet-cli.rb
@@ -12,12 +12,12 @@ class FleetCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ee1d0a23a24f51a2937cbfb6e53cde83ad7a12fa452e3f3c12a41bb2801ade2b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71bcab26cb78c58850afe7d9ee5cb24e61cdafb757b09c0ca4772be428229409"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ae91ed0934571ff44b171bb9158957792a3aa457de68f0ab3a97abd5aa4f625"
-    sha256 cellar: :any_skip_relocation, sonoma:        "39e6f7aa66ccb779c21f1406ea3abd3889b4df3916c5024578f1986e1ea08414"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4229aefe6b042f6a820fadec8692fc32428273add0c1b15d3285619b64a2e391"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb38f6d0d826c3cb5475eece1607d79049c5c52401fd3c6dccc432431394ba00"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "72ea597555050360b2647c45f10cf938f8c5d0a0f09c85b403f1b00b0f5b990e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "60a2d3688774e47e179e3f5593dca1cbf37ffa686490aa2d8b306b365d16b8d9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b21ae96165ff904a5a883641d5b2886f60a86a08b6a21e018940cbc9f7efe0ad"
+    sha256 cellar: :any_skip_relocation, sonoma:        "41d4dfc5af2a4730fd84b522991ddc344eb0a35175d36e33f2e703c27190b5cd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "78f866fc9adc01a4885b46025120eee0163ed69a3dac3b2567e6cd9cddd49a81"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a212ea3563a2ec60b274f0c85a3c22467e0c1784530c67167a5fc3438848e6d8"
   end
 
   depends_on "go" => :build

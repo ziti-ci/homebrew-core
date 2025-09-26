aa4632ba2fff46fa040a22689c65f8028046230b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-26 08:49:53 +0000

    ortp: update 5.4.46 bottle.

--- Formula/o/ortp.rb
+++ Formula/o/ortp.rb
@@ -24,14 +24,12 @@ class Ortp < Formula
   no_autobump! because: "resources cannot be updated automatically"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "07d48e8b8da10f7fe4f7d69781624ccb76701b2e9779425bdfcce688fd85faf6"
-    sha256 cellar: :any,                 arm64_sequoia: "506778639c8e163edeee830bb79a72a5390da6b223c95b0b21b2fa167be4d4d0"
-    sha256 cellar: :any,                 arm64_sonoma:  "0972aa70f7a4026a9617caf15d05dde152e91c17dfe2d9b0d7e9e7a1b0b1b328"
-    sha256 cellar: :any,                 arm64_ventura: "e2e284f968698aa26ff807eb31fed886bcb3da26a2d34714e822e58ee11a9775"
-    sha256 cellar: :any,                 sonoma:        "0a3d046716df631d5042bb1f8d5a5aeac52cb4d713274cf2ce8627736f8eb161"
-    sha256 cellar: :any,                 ventura:       "fc36c64efd399ffd32e8d67cdcd56e1afa02461bd15522ecaa58ac29a851307a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "580c7573dcd8cba79c72f719e6e88bd5fa9b6f3d955b400166120ad0089fac80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "96a0e56ea88cca7905bafae79d6925de68d885a931ea554ea47bc57f93d8e3da"
+    sha256 cellar: :any,                 arm64_tahoe:   "6ad70916f05069074443334f8bc05b3092f6908a95635d13fd9083e3da870383"
+    sha256 cellar: :any,                 arm64_sequoia: "9d05b040fc4738aaa634f573269ec18f58688a3394c3a596635ea8429dfecfb2"
+    sha256 cellar: :any,                 arm64_sonoma:  "3f4cf9a85c0e85e2ac64cc867972fd6d03f80018d5499e7ff6ad00788d78e39c"
+    sha256 cellar: :any,                 sonoma:        "e808b7ce7c8edb94a4f23f283a91bada871dc07e0e5e47983c986895b07d35cb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1c21f14a3d2c29b4984da7866f4e5a28ad782a3e40855a9b7866c5c8bae6e081"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d64a97fbba3657c1f67ababbf9919f70c00c08365c0cae18b79986f18374a27"
   end
 
   head do

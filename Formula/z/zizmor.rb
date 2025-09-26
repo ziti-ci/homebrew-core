93d003dbeeab85d71eb19a05e8ebb543ae9dadc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-26 15:32:53 +0000

    zizmor: update 1.14.0 bottle.

--- Formula/z/zizmor.rb
+++ Formula/z/zizmor.rb
@@ -7,12 +7,12 @@ class Zizmor < Formula
   head "https://github.com/zizmorcore/zizmor.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "881cbf089db8bf0077b576b4db29ca156e93b394d4d274f29df88c880413daf9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "69fd1a3743413769743fc9827e98432b2092f47e0e70b7ba59f81dd4ad47b417"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc59a27d268a633cf6f2d312190512bc27f360e4b7874184ee0f913e1a52947a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0653e8c013f8e8e1c77f1823efa0886997d6fb4852fab9d211acbf45acbd094c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f3896e2edafb5fb4bee9c862df92f218b5c907ca0dfcd6ce5824039d8083216f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eef7bf7274585bcb7bb6ddb75f2ca24800676e3c8c676760b48acb913c016806"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fdc95e0db102bb87eaa1553f0a880344a8ecc710b8457376be6e347f1a57ef9e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ad2495955d5e85e882d3cec277d45660a2c26ae8efe9124a4701bdea1192a2f9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "13c26549c9341f9577a548351091e86c3ffa4d6ebc3cb4c36b04df5f800b54d7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e6154eeabc48e4868012a84096a7ac5207e3c9a312435607ad84b16477ba9400"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1a1c56dbcbe18c6c27160d8704db712d724845d1247650a5e17788329ff1c463"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c5042cd8f183208806a0617a50fa498fa1be9ba92a8854c86596424bead7b16"
   end
 
   depends_on "pkgconf" => :build

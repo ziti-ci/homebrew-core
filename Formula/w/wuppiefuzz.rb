e827d8851879a6ef80b2c8824610c4e98764f60b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-26 12:21:39 +0000

    wuppiefuzz: add 1.3.0 bottle.

--- Formula/w/wuppiefuzz.rb
+++ Formula/w/wuppiefuzz.rb
@@ -5,6 +5,14 @@ class Wuppiefuzz < Formula
   sha256 "45d112673c633684459651976119fcb4d8a7f1a74b974ad05de61481b686f49b"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "31d3ac521302f3eb809fced503becb69569f12d51f23c9eaf772370d2075ee45"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c6fa5391cc83fb00c73315e855bf90da244702f32b9edba1b8f9b74c81df56b9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c4a1279ae32e5a36b8cf01a567cebda90642bdd7ee946507a545aa77807629a6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c5b744520db48662e2e061c6934462fd4e4f509bba4d5b7612ed23c0e0bb3b11"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cbb8158f0378edfb4a61860668181e720fd4366640ec44305929a0b5018e7f87"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "rust" => :build

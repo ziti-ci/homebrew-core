3c193805377d566d14622c7a346d91544a099a9e
Rui Chen <rui@chenrui.dev>
2025-09-26 12:06:03 +0000

    nanobot: update 0.0.30 bottle.

--- Formula/n/nanobot.rb
+++ Formula/n/nanobot.rb
@@ -11,6 +11,7 @@ class Nanobot < Formula
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "62b5c71536c37d0a1c89cce44ec4d0df186dd72f9870cb5d3296f745a28776fd"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d4e569db987d09adb8c7ebce9c7d23ee69ffeb10669952558567044e191b02f8"
     sha256 cellar: :any_skip_relocation, sonoma:        "558f38ddf73e6f60dc6c623390c24c56b565c6a1038ee499a1bd77976eb9fc21"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "14c87155cc561262030da6ab9d4b8546ab49ea3249754685daa0294ee2868988"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "e98e055cf0d0ed95355bb0c3b15c0a7809aad5dc729cc9c18aae387e0d5be53a"
   end
 

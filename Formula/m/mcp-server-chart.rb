cbfe6f78e8d9a29a4df3e8ebc3ced8df0de08514
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-26 06:23:43 +0000

    mcp-server-chart: add 0.9.0 bottle.

--- Formula/m/mcp-server-chart.rb
+++ Formula/m/mcp-server-chart.rb
@@ -5,6 +5,10 @@ class McpServerChart < Formula
   sha256 "fce8f5edc722e7e78da36237fddd0226c37ff4a8a778a84d26d9b0a51a47943d"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7e41e62bba63eca45a9be778f3dce98e8c9bb5d4a20f47b91b55d43f0ad23c1f"
+  end
+
   depends_on "node"
 
   def install

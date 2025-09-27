473e6ec3dc976d9bea25a981a53705be1dab3c1a
Michael Cho <michael@michaelcho.dev>
2025-09-27 10:41:37 -0400

    php-intl 8.4.13
    Remove some duplicate metafiles

--- Formula/p/php-intl.rb
+++ Formula/p/php-intl.rb
@@ -1,9 +1,9 @@
 class PhpIntl < Formula
   desc "PHP internationalization extension"
   homepage "https://www.php.net/manual/en/book.intl.php"
-  url "https://www.php.net/distributions/php-8.4.12.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.4.12.tar.xz"
-  sha256 "c1b7978cbb5054eed6c749bde4444afc16a3f2268101fb70a7d5d9b1083b12ad"
+  url "https://www.php.net/distributions/php-8.4.13.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.4.13.tar.xz"
+  sha256 "b4f27adf30bcf262eacf93c78250dd811980f20f3b90d79a3dc11248681842df"
   license "PHP-3.01"
   head "https://github.com/php/php-src.git", branch: "master"
 
@@ -32,7 +32,7 @@ class PhpIntl < Formula
   def install
     # Keep aligned with PHP formula
     extension_dir = Utils.safe_popen_read(php.bin/"php-config", "--extension-dir").chomp
-    extension_dir = lib/"php/pecl/#{File.basename(extension_dir)}"
+    extension_dir = lib/"php/pecl"/File.basename(extension_dir)
 
     cd "ext/intl" do
       system "phpize"
@@ -40,11 +40,12 @@ class PhpIntl < Formula
       system "make"
       system "make", "install", "EXTENSION_DIR=#{extension_dir}"
     end
+    rm(%w[NEWS README.md])
 
     # Automatically load extension on install and unload on uninstall or unlink
     (prefix/"etc/php/#{version.major_minor}/conf.d/ext-intl.ini").write <<~INI
       [intl]
-      extension=#{extension_dir}/intl.so
+      extension="#{extension_dir}/intl.so"
     INI
   end
 

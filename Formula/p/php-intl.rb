d4433e714a632c7171782efdbe0225278d4ee6eb
Michael Cho <20700669+cho-m@users.noreply.github.com>
2025-09-27 14:01:56 +0000

    php-intl: add 8.4.12 bottle.

--- Formula/p/php-intl.rb
+++ Formula/p/php-intl.rb
@@ -11,6 +11,15 @@ class PhpIntl < Formula
     formula "php"
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "e5cb70e61c1835532f99a01170482bb97067e18d18494fe6cfa32608cc12554f"
+    sha256 cellar: :any,                 arm64_sequoia: "60de5be854ae23ac77b6495361d9a3a4c1f4e16685dee3cd53a7839311c26e10"
+    sha256 cellar: :any,                 arm64_sonoma:  "a3bd8a31456bba69803e1e2ef6472cf9b667a1ed203a610336032c33b2f50747"
+    sha256 cellar: :any,                 sonoma:        "ada2842427a29ab72bad76206c2343949686771ea3135142960e9f0c378bddf0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "84149fbe44505b5c7dc97cc1afc749fa99ed29e1d49731e9e36181dc36c4b9e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "82d390e9bf67059c4a7b5c99ee0917d0544568ea97923065c12d519ae42d2493"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "icu4c@77"
   depends_on "php"

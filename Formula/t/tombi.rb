555031e17ab9476ea0b66a7550af0b77bc63883a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-27 03:14:38 +0000

    tombi: update 0.6.17 bottle.

--- Formula/t/tombi.rb
+++ Formula/t/tombi.rb
@@ -7,12 +7,12 @@ class Tombi < Formula
   head "https://github.com/tombi-toml/tombi.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d0f600ac3a4f8ec661ee3c4a807d554fde10ef1eaf031c73a7a83efbed33cd1f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3f73298a4c25e36a81b7cf7791e83bb54a6976e89db9938042570d04d6ecfd6a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ed0e07c0631e306a6e2e767d1ff5ab68067f8fc8d17cd680e9400ba468f2c147"
-    sha256 cellar: :any_skip_relocation, sonoma:        "294f1b20053c0cbad2f5b88b718921b1fdae847792b57b7e46f498e6dd309f2d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0dbcccca71544f5177d02c5a765d9fd447e873b11521325b684941375444a611"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2db5c03dbfe8a2326f28d7c534969193fd9a8ca105e4a08a3fbe327418e1a18b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e8ca97d316344a9da9abb96c1b48d63a3afe270db8ce432d82c2c65264293f5f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78ed086b9ce2a3a7560db4199335dce4ba188cdc3158d33b59d129dbde0e774e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1ac0f82df91c35834fc4973b5ee2c36238fff49bfd3a8a0de4f0796598f0d963"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3c5100cb97da11834f84943bb16979580cb07c2623efffd7f5def1e708db7e04"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1f14ae305f924f49bc8ce72c83c3cdc48188f2027aa086410bbaaa6325641af2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f5718e5f553348c6ef7cccd6d034049c7174eb26d09fc13e73c909009545cf42"
   end
 
   depends_on "rust" => :build

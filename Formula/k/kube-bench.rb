1343d9eb05e0ede7ac41ec1bc638aa086d2903a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-29 14:18:26 +0000

    kube-bench: update 0.13.0 bottle.

--- Formula/k/kube-bench.rb
+++ Formula/k/kube-bench.rb
@@ -7,13 +7,11 @@ class KubeBench < Formula
   head "https://github.com/aquasecurity/kube-bench.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9af62d701e9161cd4b0288f71258f6c42cff32962cde8b1b6dbdf03f63791714"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bcc9a234c5ef370f6b1cf0e60ec85adbe6ba52639e693b77b28ef5a76134c3f7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bcc9a234c5ef370f6b1cf0e60ec85adbe6ba52639e693b77b28ef5a76134c3f7"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "bcc9a234c5ef370f6b1cf0e60ec85adbe6ba52639e693b77b28ef5a76134c3f7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "f9deb70bbb4d3b75fa18a4ddca74af543e3c6046a29bfb69d2563e4a649793db"
-    sha256 cellar: :any_skip_relocation, ventura:       "f9deb70bbb4d3b75fa18a4ddca74af543e3c6046a29bfb69d2563e4a649793db"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18571d4fa7773debf6679a05a138481c0ddbe45a6723437e0e21902bdb9373bb"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bc8245aed689e3e0e3dd8fcdde3adbf11498309bc67f4aa1331a368d343bf579"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bc8245aed689e3e0e3dd8fcdde3adbf11498309bc67f4aa1331a368d343bf579"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bc8245aed689e3e0e3dd8fcdde3adbf11498309bc67f4aa1331a368d343bf579"
+    sha256 cellar: :any_skip_relocation, sonoma:        "875a28f07dacf2e02540368a29978b925bc505a9602ce84e26529e0e01ef2227"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "60b4e66138c7882ed5a41838bae9560625782262834c88e8e185281e83e368f3"
   end
 
   depends_on "go" => :build

fe4ffb25e7c556a79203b33b11b8807bb890e3dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-29 23:25:49 +0000

    ducker: update 0.5.0 bottle.

--- Formula/d/ducker.rb
+++ Formula/d/ducker.rb
@@ -6,12 +6,12 @@ class Ducker < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4ca9c2329b89a1d8af635a1b1863c60f818b497e1f41064118288a8b12e573df"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1a8fb2799c25bc6ad1952220029146e3d111ad6ab89a9734fa890ad3a7f70ab2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "df92f6dd2b8fd1fb9d01bc8cb047167ced939ebebd3701f331bebc9fecdbf8d2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0c0858bbb06426d91371dc3ea472a517cff535d8ff8151aeed4ea8dd9c8cfee8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d5859e3c7235852fdd7549054b49aec8ab8d8b182c2a685a93f22dc2eb0eb2f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2186bda440f430c4eb012dc8363ba81b33bffcee649c83747f4f7b09d1249d5b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a4cab4b53b4a9abbefef9823eea4564dcb8fbc34ea24f5544c497f226a31fecf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1c2c39e83ad7df61ffe9b600f150a54a4a40a8c2761b09e1b575e1c822b93152"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4a409267956b4f8f92b7e4ae0a15114467d51b11e07a754f25a22a7889e8f08b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f1415266515ef3402a33ffc407079aa7fb4053975822362d31a5e3b214b8e286"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6a3d01854dc827fcab3f2f3bb9ab88739ed528465564183222fa48efe170e2f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b39525b39ab7554115af9622a1972b577ca3f450e76ffc8302274d1b6ffdbdc"
   end
 
   depends_on "rust" => :build

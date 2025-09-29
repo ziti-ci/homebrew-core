c00487cb662002c0e8f58b0f85ee468ef2466e5b
Rui Chen <rui@chenrui.dev>
2025-09-29 09:15:48 -0400

    maxima: revision bump (sbcl 2.5.9)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/maxima.rb
+++ Formula/m/maxima.rb
@@ -4,7 +4,7 @@ class Maxima < Formula
   url "https://downloads.sourceforge.net/project/maxima/Maxima-source/5.48.1-source/maxima-5.48.1.tar.gz"
   sha256 "b0916b5fb37b6eeaae400083175e68e28f80b9a1ab580c106a05448cf1c496b2"
   license "GPL-2.0-only"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable

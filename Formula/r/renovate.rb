60c48c06b1859b0489313275e7a69fb03c0c47ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-29 20:50:43 +0000

    renovate: update 41.132.0 bottle.

--- Formula/r/renovate.rb
+++ Formula/r/renovate.rb
@@ -17,12 +17,12 @@ class Renovate < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9cd2ce29bcb667968dbe91ec3adb0328504238e5b091b6f9c1c71e45b44e47f5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5b3b01a3584bd1207719ff2f1d5a6a8cc51d2876b4543dcfc3bf593ba7c13f06"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "76748df071dc784eb2cc33979cafdda55baba921f7aa242a6db1de3b9db18a86"
-    sha256 cellar: :any_skip_relocation, sonoma:        "646b8c9f1f4bca73bde1cbcee1251e48fab1068b3c93ef3c51ab5f449f091bc7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f8c4f5bae140f5f68a7562c0797ab9781e5e26a5b666e7f7cac13e2aacccc7d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4ee5ddf64ea99025d25f62d57c357634afbbf869cb7eecd10f02ada43b1d387"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c5f4e8f4a282820c3f436a8bac38c5a0293488815b94991af7e5d584003a1e0d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "90045ff3ca116b20c7d8ef0c6938f3147405e540d61c72402332d93b7cf6a563"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6636790be1613b6713d813a5769ca9e8c89a2857f6bd3c363ee5c417ebf13122"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ea025f7243e198943186d50762b711b67c3c0885c7266581a940634f6e040b2c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e4411b3fc008eb2ef106833fc12e4149ec758f10c0e031ee1b999c1e68f8f077"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c808f988c9609b7eabe16ba9577a8930ef1d4d507127f6d95eac35d05f074b57"
   end
 
   depends_on "node@22"

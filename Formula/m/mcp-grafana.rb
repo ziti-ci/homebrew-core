b98065d73070849275b166be4f3dc9cbb25c0ef6
Rui Chen <rui@chenrui.dev>
2025-09-28 23:57:19 -0400

    mcp-grafana 0.7.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/m/mcp-grafana.rb
@@ -0,0 +1,25 @@
+class McpGrafana < Formula
+  desc "MCP server for Grafana"
+  homepage "https://github.com/grafana/mcp-grafana"
+  url "https://github.com/grafana/mcp-grafana/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "b3f909d701f83f751c26f73f84135cab0749c04093c69d25adf3f902d9f95ba1"
+  license "Apache-2.0"
+  head "https://github.com/grafana/mcp-grafana.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/mcp-grafana"
+  end
+
+  test do
+    json = <<~JSON
+      {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2025-03-26"}}
+      {"jsonrpc":"2.0","id":2,"method":"tools/list"}
+    JSON
+
+    output = pipe_output(bin/"mcp-grafana", json, 0)
+    assert_match "This server provides access to your Grafana instance and the surrounding ecosystem", output
+  end
+end

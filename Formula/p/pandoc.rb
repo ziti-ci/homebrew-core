118499d2853830310d5e7449da85ec50dba6eb25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-29 17:13:09 +0000

    pandoc 3.8.1

--- Formula/p/pandoc.rb
+++ Formula/p/pandoc.rb
@@ -1,8 +1,8 @@
 class Pandoc < Formula
   desc "Swiss-army knife of markup format conversion"
   homepage "https://pandoc.org/"
-  url "https://github.com/jgm/pandoc/archive/refs/tags/3.8.tar.gz"
-  sha256 "2d375526e7d26d46ebcdaf107564c8bbfd68e40a7af425906e9612889699bcf6"
+  url "https://github.com/jgm/pandoc/archive/refs/tags/3.8.1.tar.gz"
+  sha256 "1d66d35952e7037cc049a9f170c9ba394693014a71925a84c55dffcaf98ff677"
   license "GPL-2.0-or-later"
   head "https://github.com/jgm/pandoc.git", branch: "main"
 

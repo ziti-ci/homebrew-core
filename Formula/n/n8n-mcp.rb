91957cd740e4763cbcf88d9636cde36ec7e51d16
Rui Chen <rui@chenrui.dev>
2025-09-29 00:05:03 -0400

    n8n-mcp 2.14.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/n/n8n-mcp.rb
@@ -0,0 +1,27 @@
+class N8nMcp < Formula
+  desc "MCP for Claude Desktop, Claude Code, Windsurf, Cursor to build n8n workflows"
+  homepage "https://www.n8n-mcp.com/"
+  url "https://registry.npmjs.org/n8n-mcp/-/n8n-mcp-2.14.1.tgz"
+  sha256 "ca2d0f2e058ac61657b97902cce1826dd24ec6ac066b74ae929ea3aaf9fe6505"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    ENV["N8N_API_URL"] = "https://your-n8n-instance.com"
+    ENV["N8N_API_KEY"] = "your-api-key"
+
+    output_log = testpath/"output.log"
+    pid = spawn bin/"n8n-mcp", testpath, [:out, :err] => output_log.to_s
+    sleep 10
+    assert_match "n8n Documentation MCP Server running on stdio transpor", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end

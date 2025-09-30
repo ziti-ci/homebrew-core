2d7259569a4aa0d1a20fa9d6aa05df2f5af94ff7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 04:57:17 +0000

    mcp-grafana: add 0.7.0 bottle.

--- Formula/m/mcp-grafana.rb
+++ Formula/m/mcp-grafana.rb
@@ -6,6 +6,14 @@ class McpGrafana < Formula
   license "Apache-2.0"
   head "https://github.com/grafana/mcp-grafana.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c0ca8891135475bae553b1c90921c519f477f46b82bd98cda81343211c20f7b2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e66b678018c962ab80caab5023cc7c43139bdc192ee8d022dc98def4c2f2a341"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14afaf80ffcb1262794fa8e35bdc1f43c752d5bce2b128eb5d4908347b27bcc6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ccc34c55ef145c16420eb83e6f0e15e1b2f331ebde2eb93bf59053e3ce656b71"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9cd10a3a45f3d433e2470bddc553b767f9d63a01b64537474ffc4a3560cd1634"
+  end
+
   depends_on "go" => :build
 
   def install

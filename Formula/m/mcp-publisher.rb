f404943940151f0120bc90f5bf016be87e267b62
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 14:39:29 +0000

    mcp-publisher: update 1.2.1 bottle.

--- Formula/m/mcp-publisher.rb
+++ Formula/m/mcp-publisher.rb
@@ -7,12 +7,12 @@ class McpPublisher < Formula
   head "https://github.com/modelcontextprotocol/registry.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f643d1b0baa8fc4083d0e1a8131a4732be4956d96ee895ccdf1343f6cd42b415"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f643d1b0baa8fc4083d0e1a8131a4732be4956d96ee895ccdf1343f6cd42b415"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f643d1b0baa8fc4083d0e1a8131a4732be4956d96ee895ccdf1343f6cd42b415"
-    sha256 cellar: :any_skip_relocation, sonoma:        "827138042550d0b4a9b9a8a3a271c3bb391605ccc9c3bf388ccaa95e99a055e3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4f25003293f93cd20c24e20270d4f875c77881a4147ba42948f6a6a3547b59a6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c04642f1b43c78dc1072389933469f20009963dc9bb226c488768b6b82ac52c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bd1edb64358c4877f88bd2b0323c2377f722d7a52b6575908efdfc1ee9350664"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd1edb64358c4877f88bd2b0323c2377f722d7a52b6575908efdfc1ee9350664"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd1edb64358c4877f88bd2b0323c2377f722d7a52b6575908efdfc1ee9350664"
+    sha256 cellar: :any_skip_relocation, sonoma:        "9ffdfd765bbfee2fb207bad902f276feccc9c066a1782ab5bddc87bcddc3ed3b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "98926ef89a1087cf2c1cf735e5f6a8aee42fb0fcc29767909dbee93963035be6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5dfacbecbeccfcdf5fe7c2204f9fc027f9d7675d375f82c4dfa7a18dc532cf59"
   end
 
   depends_on "go" => :build

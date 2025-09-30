9b74307a9d2b34238edb2b7c8270e13a2280215f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 21:22:16 +0000

    slumber: update 4.1.0 bottle.

--- Formula/s/slumber.rb
+++ Formula/s/slumber.rb
@@ -7,12 +7,12 @@ class Slumber < Formula
   head "https://github.com/LucasPickering/slumber.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3caaf0f83b47b90de2a84e5a39e7b66cc79557876470207f64094eccfe94b495"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "beaecd05158247bdda0be2b606ae94e05457d4582ae1e5e206aad88f79be514d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "76e8030632a8cc3c26ca3e747a8e99810843cd4e599751a9f785678547859130"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8c02c15e45cf73d97d76f93c56d64b375c68407099a40c38da749764bdfa3422"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1df114d38df0e11ef4d74921b1dbde8c7ac811839367320807624e4a95af41ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4602d2b51601b5a4d67ccc216f044659c897fc9d47a152e498e8c57c2c357ebd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0015f3b0862f2bf36e5193e4302fe2ff38ee9bffb290efe3428f6cab30391075"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6b034cf34ea209c628f37487bc73c5fa31ff7ef7c877fd80f61186e32d861eea"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6bee704c2c822953f1d014d086f4a7bc0889275296f1f67fea1115be59b0ae19"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5f6f0b51005182480d4a8c716cea0b2664a6aa2bd80ec12c638ed4e1b0691e1f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "eb556cb519b96e91784612bc16c536987de1aa3cb39c8a25a8f2cca7ef4a4685"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5a6d5eef56c9ad5254604bdf349b5494704b55298c456db64b50e19f78fabe62"
   end
 
   depends_on "rust" => :build

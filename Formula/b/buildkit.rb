9c7e81fc60be3ecb06ca67a6b1b661bcc7b167da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 20:23:47 +0000

    buildkit: update 0.25.0 bottle.

--- Formula/b/buildkit.rb
+++ Formula/b/buildkit.rb
@@ -16,13 +16,11 @@ class Buildkit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c76d4fa977da6189445dff9edf3355913de0283059a81225edeb8f007fb0f775"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da1e9b6d016ff076cb7afe06129279dc874f8e98e04a44ccb1f8d6fed6a57144"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "da1e9b6d016ff076cb7afe06129279dc874f8e98e04a44ccb1f8d6fed6a57144"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "da1e9b6d016ff076cb7afe06129279dc874f8e98e04a44ccb1f8d6fed6a57144"
-    sha256 cellar: :any_skip_relocation, sonoma:        "50a9784b0b7f5ebe2684cac80f2ae96eb1db2be366fa88ebeb3efc443d766a68"
-    sha256 cellar: :any_skip_relocation, ventura:       "50a9784b0b7f5ebe2684cac80f2ae96eb1db2be366fa88ebeb3efc443d766a68"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "be09ce13fc8883a4d25f45a08a7e79af703d5c9b559e9b410448a9974ead2578"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8c7efd0c7fa793e0aaa73f15be13720d619794b848b1ba087abda0c635b824a9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8c7efd0c7fa793e0aaa73f15be13720d619794b848b1ba087abda0c635b824a9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8c7efd0c7fa793e0aaa73f15be13720d619794b848b1ba087abda0c635b824a9"
+    sha256 cellar: :any_skip_relocation, sonoma:        "44801ba46ccce5215671f0861a1f218fa6524d9c6f56a800afcd08a0cc455a63"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18256c2e4665b140eb3ab2dd24eae76165adbb7a98e913c8f6c5312f36e1d7a0"
   end
 
   depends_on "go" => :build

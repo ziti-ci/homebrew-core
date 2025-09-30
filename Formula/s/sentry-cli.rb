69449d62dce874c86d52cbb226c34fc00861084e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 08:50:15 +0000

    sentry-cli: update 2.56.0 bottle.

--- Formula/s/sentry-cli.rb
+++ Formula/s/sentry-cli.rb
@@ -7,12 +7,12 @@ class SentryCli < Formula
   head "https://github.com/getsentry/sentry-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "abbb01df7e26ca1bc45b1dec2af9f2803ef71cbd3519af783a8feb3550d43129"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4b6bb6e0f12c114b39ebdda77aceb174326523d5d690f668d15e65749130dc5c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "853e747e4345b504b62f11982ec88265668540b8d4a5f28d2cc06741e8b6e7a4"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b347d2d1826052b2205dd1760f5b71a1015be88f3d090d227b7a92835a402b2c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b14c88205f835964142d82c202e9a4f1489ebbf9133ec1f2ac078e66aac5a038"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f774880c9b95fc8bc619fece973955971f0ee111a1857a560f2b1bc739ffcdf9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dbeab6947a99338a24db2043c2b49dd9748551c8e997b2089312aea7330e582a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a379c46e511d47261283576441d4c8da00f72ceb7e3566d9aab853a40d4453d8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1ca104f84e6967edeece35adefa9a6dd297bab586d8267c300e0fe5312be3706"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2e02ff7232fb201cca88cc0eaab43d37c8a225c322f5bc8e810f85997faa3912"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1a02a9e09d0cfa5fcaf416cb0c5d200961ca0ec02b2a6e5ad1a86a68b46e6a4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc17e8f97985ee977de6cbce17f34cd0f486e702162c3473a0acbb98408fc64b"
   end
 
   depends_on "pkgconf" => :build

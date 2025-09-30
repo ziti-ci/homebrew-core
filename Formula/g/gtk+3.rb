2b1326547a846c88cc2e8d40a90c20b28abb02b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 05:04:44 +0000

    gtk+3 3.24.51

--- Formula/g/gtk+3.rb
+++ Formula/g/gtk+3.rb
@@ -1,8 +1,8 @@
 class Gtkx3 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-  url "https://download.gnome.org/sources/gtk/3.24/gtk-3.24.50.tar.xz"
-  sha256 "399118a5699314622165a11b769ea9b6ed68e037b6d46d57cfcf4851dec07529"
+  url "https://download.gnome.org/sources/gtk/3.24/gtk-3.24.51.tar.xz"
+  sha256 "0013877c6bd23c2dbe42ad7c70a053d0e449be66736574e37867c49c5f905a4f"
   license "LGPL-2.0-or-later"
 
   livecheck do

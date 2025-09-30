465ac692b585c6373ea75aac610750c5183ad03b
botantony <antonsm21@gmail.com>
2025-09-30 20:36:27 +0200

    djl-serving 0.34.0
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/d/djl-serving.rb
+++ Formula/d/djl-serving.rb
@@ -1,8 +1,8 @@
 class DjlServing < Formula
   desc "This module contains an universal model serving implementation"
   homepage "https://github.com/deepjavalibrary/djl-serving"
-  url "https://publish.djl.ai/djl-serving/serving-0.33.0.tar"
-  sha256 "48326bdeacba973ac54d8f07e22b6be0a0edafe2e1377d7031665d5efe726691"
+  url "https://publish.djl.ai/djl-serving/serving-0.34.0.tar"
+  sha256 "adabd4af596b278ecf890074825b480a11983751a8eaa9e1e22c94d93a18982a"
   license "Apache-2.0"
 
   # `djl-serving` versions aren't considered released until a corresponding
@@ -23,13 +23,14 @@ class DjlServing < Formula
     rm_r(Dir["bin/*.bat"])
     mv "bin/serving", "bin/djl-serving"
     libexec.install Dir["*"]
-    env = { MODEL_SERVER_HOME: "${MODEL_SERVER_HOME:-#{var}}" }
+    env = { MODEL_SERVER_HOME: "${MODEL_SERVER_HOME:-#{var}/djl-serving}" }
     env.merge!(Language::Java.overridable_java_home_env)
     (bin/"djl-serving").write_env_script "#{libexec}/bin/djl-serving", env
   end
 
   service do
     run [opt_bin/"djl-serving", "run"]
+    working_dir var/"djl-serving"
     keep_alive true
   end
 

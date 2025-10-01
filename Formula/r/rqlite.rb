acaccddd2f9f8f62bd46c7cb6d97f9c2af10f1c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-01 14:36:40 +0000

    rqlite 9.1.1

--- Formula/r/rqlite.rb
+++ Formula/r/rqlite.rb
@@ -1,8 +1,8 @@
 class Rqlite < Formula
   desc "Lightweight, distributed relational database built on SQLite"
   homepage "https://www.rqlite.io/"
-  url "https://github.com/rqlite/rqlite/archive/refs/tags/v9.1.0.tar.gz"
-  sha256 "10d6308dd8370b0e705c4fbcd6bcb8a5e81121cf8ab50fd9495613893eaf0360"
+  url "https://github.com/rqlite/rqlite/archive/refs/tags/v9.1.1.tar.gz"
+  sha256 "d4d2387fa046606b53ae34831f07e4960c75a0a4dc54e60a05102fae33ce18bf"
   license "MIT"
   head "https://github.com/rqlite/rqlite.git", branch: "master"
 

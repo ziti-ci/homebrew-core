2f1ca7a6b16454c481a133f29aaa6eefcce26102
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-01 03:02:10 +0000

    specify 0.0.55

--- Formula/s/specify.rb
+++ Formula/s/specify.rb
@@ -3,8 +3,8 @@ class Specify < Formula
 
   desc "Toolkit to help you get started with Spec-Driven Development"
   homepage "https://github.com/github/spec-kit"
-  url "https://github.com/github/spec-kit/archive/refs/tags/v0.0.54.tar.gz"
-  sha256 "e5af711ed18afcb2cd240252dd791bd9847d0cd5c4b6dee59f293e971efe49eb"
+  url "https://github.com/github/spec-kit/archive/refs/tags/v0.0.55.tar.gz"
+  sha256 "2e5864f57d5ef373c732bade35d949f9c742519e5f31e6ec79946ebebff7f7a7"
   license "MIT"
 
   bottle do
@@ -102,14 +102,6 @@ class Specify < Formula
   resource "typing-extensions" do
     url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
     sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
-  end
-
-  # Fix for MarkupError: closing tag '[/bright_black]' at position 80 doesn't match any open tag
-  # Should be removed in the next release
-  # Issue ref: https://github.com/github/spec-kit/issues/593
-  patch do
-    url "https://github.com/github/spec-kit/commit/321edbc62e10e6621bf8b46c443a5c13139d05a1.patch?full_index=1"
-    sha256 "6d79c21869379740e5615b818501964bc754b041620195bd40f02bc5782a16b6"
   end
 
   def install

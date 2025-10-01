ab253705744100f282656f17ee3f2b0ba1cd0542
David Gageot <david.gageot@docker.com>
2025-10-01 10:48:27 +0200

    cagent 1.5.11 (new formula)
    Signed-off-by: David Gageot <david.gageot@docker.com>

---
+++ Formula/c/cagent.rb
@@ -0,0 +1,35 @@
+class Cagent < Formula
+  desc "Agent Builder and Runtime by Docker Engineering"
+  homepage "https://github.com/docker/cagent"
+  url "https://github.com/docker/cagent/archive/refs/tags/v1.5.11.tar.gz"
+  sha256 "9b11261f7c946d591010eb1d799d045f649c4abf7ed57f53a98cac756374336c"
+  license "Apache-2.0"
+  head "https://github.com/docker/cagent.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+    ldflags = %W[
+      -s -w
+      -X github.com/docker/cagent/pkg/version.Version=v#{version}
+      -X github.com/docker/cagent/pkg/version.Commit=#{tap.user}
+    ]
+
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"cagent", "completion")
+  end
+
+  test do
+    (testpath/"agent.yaml").write <<~YAML
+      version: "2"
+      agents:
+        root:
+          model: openai/gpt-4o
+    YAML
+
+    assert_match("cagent version v#{version}", shell_output("#{bin}/cagent version"))
+    assert_match(/must be set.*OPENAI_API_KEY/m, shell_output("#{bin}/cagent exec --dry-run agent.yaml 2>&1", 1))
+  end
+end

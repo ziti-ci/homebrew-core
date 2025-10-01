1739f0c53b65902909a1a46b448ffb2a8102529d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-01 11:25:49 +0000

    xmount 1.2.1

--- Formula/x/xmount.rb
+++ Formula/x/xmount.rb
@@ -1,10 +1,9 @@
 class Xmount < Formula
   desc "Convert between multiple input & output disk image types"
   homepage "https://www.sits.lu/xmount"
-  url "https://code.sits.lu/foss/xmount/-/archive/1.2.0/xmount-1.2.0.tar.bz2"
-  sha256 "07c261e54e1e4cbcd4f7eaaf4f62efcbbbc68c76797ddca704592d99ebed3d10"
+  url "https://code.sits.lu/foss/xmount/-/archive/1.2.1/xmount-1.2.1.tar.bz2"
+  sha256 "14cbbbebcdf1a540d79f64ac24b37882a694292e60afdee1c6923b8baf3eb877"
   license "GPL-3.0-or-later"
-  revision 1
 
   bottle do
     sha256 arm64_linux:  "1de5d61365c208673d84f059724faff5d1930c9807579ac6b05627546717bc8f"

1a8fe42ac85702115b1305b46e6917998047d974
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-02 14:37:48 +0000

    datafusion: update 50.1.0 bottle.

--- Formula/d/datafusion.rb
+++ Formula/d/datafusion.rb
@@ -7,12 +7,12 @@ class Datafusion < Formula
   head "https://github.com/apache/datafusion.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a2da069115b559264382836d682233de451f98dfaf0a8e0b10e5d973da46f218"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2543023332384c0b6ae05ac1e973c23a85a00cb253db3965cb6c2454f9fec2e4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a218b021ad81ca237e26ac6266ac119c8bfdd54f88625fe35da290e9d6671518"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c46f8a54b9389b4c613380df12972b8efaad41f346cf6d02ec99a6946570e428"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "53a0e0690d974a8e26cb663f09288082b2b1603d23d448ff6b973ed883ed533f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32d3b041c07443edf8fed018c8383bbbf2020d2326271bc16a7ef877cc533b02"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0f1dbdd89089220698b4320ee792dc4e78fc0f59ac238bf1b66946aefcca34f5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f3ffb1fbab5d0f044b0b0b8b118a5ace0e79713f01cdaf53af93adc2a76174ac"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2e03c3717495dafd62e9b2ee4ff2388709d2bfd3ad36c1c21e1a249db72219ed"
+    sha256 cellar: :any_skip_relocation, sonoma:        "44ba44b362291b1cea4ea5bccc79286a125fe617682b99d1e596759da645c690"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "316eea80050c4c146a7cddb3243b9c051871a9e6f2935aec94a9de9432a84a35"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "898591a6b40a91ad9f6c8fdc48a17ff32d74971173d2268f76ed6560438646b4"
   end
 
   depends_on "rust" => :build

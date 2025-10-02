3ffd1d06eb0f55ed24e1a680f5ca7898eeddab55
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-02 20:15:19 +0000

    mdz 3.3.5

--- Formula/m/mdz.rb
+++ Formula/m/mdz.rb
@@ -1,8 +1,8 @@
 class Mdz < Formula
   desc "CLI for the mdz ledger Open Source"
   homepage "https://github.com/LerianStudio/midaz"
-  url "https://github.com/LerianStudio/midaz/archive/refs/tags/v3.3.4.tar.gz"
-  sha256 "58d212fceac633daa723e1370f5bfcb4b1a7969cf7d9ffeac8f50b367e2cc341"
+  url "https://github.com/LerianStudio/midaz/archive/refs/tags/v3.3.5.tar.gz"
+  sha256 "cb2715e52a438b47a780ad7d35115ae216e5754c54970b4c484e11531c1fce44"
   license "Apache-2.0"
   head "https://github.com/LerianStudio/midaz.git", branch: "main"
 

f733941637a35964ad08ba2c3d1da265ee8fa9b5
Michael Cho <michael@michaelcho.dev>
2025-10-02 14:31:18 -0400

    qtgraphs 6.9.3 (new formula)

---
+++ Formula/q/qtgraphs.rb
@@ -0,0 +1,92 @@
+class Qtgraphs < Formula
+  desc "Provides functionality for 2D and 3D graphs"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.9/6.9.3/submodules/qtgraphs-everywhere-src-6.9.3.tar.xz"
+  mirror "https://qt.mirror.constant.com/archive/qt/6.9/6.9.3/submodules/qtgraphs-everywhere-src-6.9.3.tar.xz"
+  mirror "https://mirrors.ukfast.co.uk/sites/qt.io/archive/qt/6.9/6.9.3/submodules/qtgraphs-everywhere-src-6.9.3.tar.xz"
+  sha256 "50dc63d055125c30c0bf3a15dd1f71363e474f2fdcb35f927e754042440e048c"
+  license all_of: [
+    "GPL-3.0-only",
+    "BSD-3-Clause", # *.cmake
+  ]
+  head "https://code.qt.io/qt/qtgraphs.git", branch: "dev"
+
+  livecheck do
+    formula "qtbase"
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :test
+
+  depends_on "qtbase"
+  depends_on "qtdeclarative"
+  depends_on "qtquick3d"
+
+  on_macos do
+    depends_on "qtshadertools"
+  end
+
+  def install
+    args = ["-DCMAKE_STAGING_PREFIX=#{prefix}"]
+    args << "-DQT_NO_APPLE_SDK_AND_XCODE_CHECK=ON" if OS.mac?
+
+    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja",
+                    *args, *std_cmake_args(install_prefix: HOMEBREW_PREFIX, find_framework: "FIRST")
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    # Some config scripts will only find Qt in a "Frameworks" folder
+    frameworks.install_symlink lib.glob("*.framework") if OS.mac?
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 4.0)
+      project(test VERSION 1.0.0 LANGUAGES CXX)
+      find_package(Qt6 REQUIRED COMPONENTS Graphs)
+      add_executable(test main.cpp)
+      target_link_libraries(test PRIVATE Qt6::Graphs)
+    CMAKE
+
+    (testpath/"test.pro").write <<~QMAKE
+      QT      += graphs
+      TARGET   = test
+      CONFIG  += console
+      CONFIG  -= app_bundle
+      TEMPLATE = app
+      SOURCES += main.cpp
+    QMAKE
+
+    (testpath/"main.cpp").write <<~CPP
+      #undef QT_NO_DEBUG
+      #include <QLineSeries>
+
+      int main(void) {
+        QLineSeries series;
+        series.append(QList<QPointF>());
+        series.append(0.0,0.0);
+        series.append(QPointF());
+        return 0;
+      }
+    CPP
+
+    ENV["LC_ALL"] = "en_US.UTF-8"
+    ENV["QT_QPA_PLATFORM"] = "minimal" if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    system "cmake", "-S", ".", "-B", "cmake"
+    system "cmake", "--build", "cmake"
+    system "./cmake/test"
+
+    ENV.delete "CPATH" if OS.mac?
+    mkdir "qmake" do
+      system Formula["qtbase"].bin/"qmake", testpath/"test.pro"
+      system "make"
+      system "./test"
+    end
+
+    flags = shell_output("pkgconf --cflags --libs Qt6Graphs").chomp.split
+    system ENV.cxx, "-std=c++17", "main.cpp", "-o", "test", *flags
+    system "./test"
+  end
+end

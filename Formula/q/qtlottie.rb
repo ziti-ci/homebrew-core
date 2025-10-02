b29dd764e110763e84aeea49164e6151b4cc395a
Michael Cho <michael@michaelcho.dev>
2025-10-02 11:16:49 -0400

    qtlottie 6.9.3 (new formula)

---
+++ Formula/q/qtlottie.rb
@@ -0,0 +1,77 @@
+class Qtlottie < Formula
+  desc "Display graphics and animations exported by the Bodymovin plugin"
+  homepage "https://www.qt.io/"
+  url "https://download.qt.io/official_releases/qt/6.9/6.9.3/submodules/qtlottie-everywhere-src-6.9.3.tar.xz"
+  mirror "https://qt.mirror.constant.com/archive/qt/6.9/6.9.3/submodules/qtlottie-everywhere-src-6.9.3.tar.xz"
+  mirror "https://mirrors.ukfast.co.uk/sites/qt.io/archive/qt/6.9/6.9.3/submodules/qtlottie-everywhere-src-6.9.3.tar.xz"
+  sha256 "116e105574f0bb442b80251fa60b88d1c9fe55db64e11b549e8fc2063b90df33"
+  license all_of: [
+    "GPL-3.0-only",
+    "BSD-3-Clause", # *.cmake
+  ]
+  head "https://code.qt.io/qt/qtlottie.git", branch: "dev"
+
+  livecheck do
+    formula "qtbase"
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :test
+
+  depends_on "qtbase"
+  depends_on "qtdeclarative"
+
+  def install
+    args = ["-DCMAKE_STAGING_PREFIX=#{prefix}"]
+    args << "-DQT_NO_APPLE_SDK_AND_XCODE_CHECK=ON" if OS.mac?
+
+    system "cmake", "-S", ".", "-B", "build", "-G", "Ninja",
+                    *args, *std_cmake_args(install_prefix: HOMEBREW_PREFIX, find_framework: "FIRST")
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    # Some config scripts will only find Qt in a "Frameworks" folder
+    frameworks.install_symlink lib.glob("*.framework") if OS.mac?
+  end
+
+  test do
+    resource "rect_rotate.json" do
+      url "https://raw.githubusercontent.com/qt/qtlottie/888fb0ca5d5e18fe5d8b88ed721544bf204ac158/tests/manual/testApp/rect_rotate.json"
+      sha256 "d0d5d2b036c04d4b2c1211d062fe1c123053ea0d4639cd33a2cec724236bf328"
+    end
+    testpath.install resource("rect_rotate.json")
+
+    # Based on https://github.com/qt/qtlottie/blob/dev/tests/manual/testApp/main.qml
+    (testpath/"test.qml").write <<~QML
+      import QtQuick
+      import QtQuick.Window
+      import Qt.labs.lottieqt
+
+      Window {
+        visible: true
+        width: animContainer.width
+        height: animContainer.height
+        title: qsTr("Animation test")
+        color: "black"
+
+        Item {
+          id: animContainer
+          width: childrenRect.width
+          height: childrenRect.height
+
+          LottieAnimation {
+            id: bmAnim
+            quality: LottieAnimation.MediumQuality
+            source: "rect_rotate.json"
+            onFinished: Qt.quit()
+          }
+        }
+      }
+    QML
+
+    ENV["LC_ALL"] = "en_US.UTF-8"
+    ENV["QT_QPA_PLATFORM"] = "minimal" if OS.linux?
+    system Formula["qtdeclarative"].bin/"qml", "test.qml"
+  end
+end

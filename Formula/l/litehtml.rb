7658f8d4a37c46d63595caa0e3e66ecbd29d1c8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-02 19:19:57 +0000

    litehtml: add 0.9 bottle.

--- Formula/l/litehtml.rb
+++ Formula/l/litehtml.rb
@@ -5,6 +5,14 @@ class Litehtml < Formula
   sha256 "ef957307da15b1258a70961942840bcf54225a8d75315dcbc156186eba35b1a7"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "d07e7a815a2fd6e9a44e8f52233ea5a979d41b9dbb24828b0a53508df0ef6e39"
+    sha256 cellar: :any,                 arm64_sequoia: "8e83b6bb6e9576cfc87e68c3e2af8395ade9410909e69fe6b0e7dd0fa84628e6"
+    sha256 cellar: :any,                 arm64_sonoma:  "f523092d1e8b207643978a8fb84d9c3734c5ff123eaf3c8ef3a5ee5dd040b9ef"
+    sha256 cellar: :any,                 sonoma:        "bd159f548833cb3e58a2ccd7637661121d68b20ac24e2023f15d0361d2876b0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b010dde65bdf09f5d5f5bc991512f64d21ed598532966b212123e643e4e1dcd"
+  end
+
   depends_on "cmake" => :build
   depends_on "gumbo-parser"
 

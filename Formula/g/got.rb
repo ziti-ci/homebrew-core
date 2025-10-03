c2b84672b60dcb031f88d6e6a9ac3d5f44ccf53f
botantony <antonsm21@gmail.com>
2025-10-03 09:25:28 +0200

    got 0.120

--- Formula/g/got.rb
+++ Formula/g/got.rb
@@ -1,9 +1,9 @@
 class Got < Formula
   desc "Version control system"
   homepage "https://gameoftrees.org/"
-  url "https://gameoftrees.org/releases/portable/got-portable-0.117.tar.gz"
-  mirror "https://pkg.freebsd.org/ports-distfiles/got-portable-0.117.tar.gz"
-  sha256 "8d53ffd6f5f5749ba474753e47e45e8e53d57de541be8bb3b0ce8e8fc233c141"
+  url "https://gameoftrees.org/releases/portable/got-portable-0.120.tar.gz"
+  mirror "https://pkg.freebsd.org/ports-distfiles/got-portable-0.120.tar.gz"
+  sha256 "b7a60c6761f6dc2810f676606a2b32eb7631c17a96dcc74b8d99b67b91e89f43"
   license "ISC"
 
   livecheck do

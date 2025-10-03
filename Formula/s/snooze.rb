1274e4b15449b21c783181631c337ff5426d84b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-03 21:02:51 +0000

    snooze: add 0.5.1 bottle.

--- Formula/s/snooze.rb
+++ Formula/s/snooze.rb
@@ -5,6 +5,14 @@ class Snooze < Formula
   sha256 "abb0df288e8fe03ae25453d5f0b723b03a03bcc7afa41b9bec540a7a11a9f93e"
   license :public_domain
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b939563950d008b7b55c180cfbca14a881ec02bea69c305d52b6ae9beaa572a2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a242f88d5027b13edbaf3aa5bada7121f8bbe239dcb2aa4200fbb277d017fcd9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "574501024e100924970d083d8e18c99ac857a491513e5c5a3a2ac7328e29b626"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fbeda0b945fd00cd421ff125ad342fde3d205a00251a263ffca5acbc332fe4e3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d1a636a409a86b9d904cf1d8a5e8aa9fc7ee94fc2015308daf907413200e8f63"
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end

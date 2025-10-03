d441966131310422d98224a6ce910f3e18064c51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-03 13:38:35 +0000

    autocorrect: update 2.16.0 bottle.

--- Formula/a/autocorrect.rb
+++ Formula/a/autocorrect.rb
@@ -9,12 +9,12 @@ class Autocorrect < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "946fa2d537749bff4a2179f3a73e1d924e8477d0fdcc6f8c2b1bf080e7ca3e9c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "018cb56bf370671a5eb9b919530988f058d045d8fabbac78be66bf21a390f1fc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c0eefbf393015ea7bddb51e7b80af8afda631a82ece4251d144f7f30b2d3c4ad"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2e0cfccff53a19ddb5433392b6932f3a3ce17c590939ff740a62e85d6d4156d2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b89cffa0a816b5182ecb1b4c11165cc430280b25d758540025f0b118a7dbadbe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "77928733439e82b8a24c00e80af5678dfe69f0595ca7111c5be815a2006e6caf"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4eff2fcecb6ac2419cd4fe9b94846d4a5c0cf9eb2034daede8b46e93325c7fbd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0347e98f52ecb8a040955df02b6ec01416fb77fadb79e91fb540202802213a3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "720632693d85ed3eb37ee23bb4b74244635a6a9a7bd4a195e71e6c4055fe9ac5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aa9210ff9813f627718acdb6cd8d0275d982a2790b896fac232207a63d7a0cd7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "56fa8ed46c7d020f50cd2f4cd711899b2d8d9fd9b7042d9a8e67b4ec5b6c01df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6c6d7e0512b4682eefd063d67cb4e656643426d9df1dfc15de06cfd567de559d"
   end
 
   depends_on "rust" => :build

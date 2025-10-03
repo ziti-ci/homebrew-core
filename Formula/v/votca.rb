a643e1b936493faf2488c5e1a5dec9b84fffc3f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-03 22:22:06 +0000

    votca: update 2025.1 bottle.

--- Formula/v/votca.rb
+++ Formula/v/votca.rb
@@ -6,14 +6,12 @@ class Votca < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "488ca9dcc8766483075398f032bb53dafd5d099a79733097fa4b9fcb5733a3ca"
-    sha256 cellar: :any,                 arm64_sequoia: "49d06defa83fa836439a76328b45c47bd1b5d51a36a9b11588c136768406d5ed"
-    sha256 cellar: :any,                 arm64_sonoma:  "347fe6f8d77363ff23180d1b57b8afe00e29a628df4e736d154dff4e53609ad0"
-    sha256 cellar: :any,                 arm64_ventura: "908abe750c2a90c342c4b25d54f13cb0dd17de2969ea8fc33c5f53cfb96b2ab7"
-    sha256 cellar: :any,                 sonoma:        "977de90ad8d326d316bd95a09180d6fdf2614a2bfb961b3434be02cca404cbdc"
-    sha256 cellar: :any,                 ventura:       "8899fd469c58e43781e37c33ab8879a0c033d9751c6231983fbab2e2c669e122"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a8d8668f5c90d2cdf59790ac441e86a2517241d81772966bd66874e21b6c720d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7bbaa9113e6ed96c12a2fab7442a138ad601c7d8ec52ce07b2cf85fa893463e4"
+    sha256 cellar: :any,                 arm64_tahoe:   "88602abcffa24d80a153c5c1463fcf7ed609e06d6f321a901993034d9a0ccd35"
+    sha256 cellar: :any,                 arm64_sequoia: "54c2de88fd8da7adc51cf1ff4fee9a4155f25a20e7c3184cbb18036362bb340f"
+    sha256 cellar: :any,                 arm64_sonoma:  "7a9c2b5109910aad26a8d1294a3bcdd63dad330f2e136f2ab339e58414838d8d"
+    sha256 cellar: :any,                 sonoma:        "6cc2671cca6ddb98e642b941523c017c20468c59e31d983bfb1d8fac5f1c6e3d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "479e6d8ea7651109c558f90220b609e566ae2388b56beabb0e4b6a6711b2960c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38fad623e875dd3a1016fb85971815877d0529044cb5b81b7aa90ba11684b7f1"
   end
 
   depends_on "cmake" => :build

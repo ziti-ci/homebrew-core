db2861d5dbe8c8796d5debb78b9c5db4b7d72f70
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-04 03:35:23 +0000

    dagu: update 1.22.8 bottle.

--- Formula/d/dagu.rb
+++ Formula/d/dagu.rb
@@ -7,11 +7,12 @@ class Dagu < Formula
   head "https://github.com/dagu-org/dagu.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5f78d6a2d3f8ad41db8ad7b33bc01d1f2499ba038325024dbb3b8ad12640f6cb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "76ab6d0e97f1c10ac3cd6797cbda81473f20463a4fdc98d77b675bd293e78e0d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d891c884f13eeeda1957265458db8b01e6c3c6a129ff0a34541c3a5f3eee8dc0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7bf0142800469cc0fe3f995a8b64352643c3bdbe51b857e2775bbabfe47b92fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e44354619b7c206355edced8f5d70816810ed769edef1b0bd896195f097c8cfa"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4eb88ce44c76b3fde9ed883b522055b315d4ec40a31bdfa81e2401b54ee3e29f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2546d23cd1fb6afd7ab44c7938f0e0d7fa16654f90a4dcd22e1d06142e00deda"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "96d8608f6dc9de0be866e920b55551bf5223f20c298ba2911460983ed547d8e7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "193e2688ab58f94ec94a2666169113ad4cd247ccc072afc78f65bafe6b24f9fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c4f57ffcda977413dc46e01e0ffd3217d4583f1146fcac094131c5d4e09b7f4"
   end
 
   depends_on "go" => :build

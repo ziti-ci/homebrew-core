f34e42d0f40004add97b7b618fb6a690b4a2ea4c
Rui Chen <rui@chenrui.dev>
2025-10-05 14:59:42 +0000

    wassette: update 0.3.0 bottle.

--- Formula/w/wassette.rb
+++ Formula/w/wassette.rb
@@ -11,6 +11,7 @@ class Wassette < Formula
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "b0dfa98b471573e777ee918611b489b689fa101f9a607020a0d3629ca726d19a"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5b5a2ce1eb7ac4ce63f6998be348414a2dd76255fd764172d3495ffb7bea236b"
     sha256 cellar: :any_skip_relocation, sonoma:        "82f9753113c7604c0938ed56ad1d655bd02c4152ede8f12c6c7a69a141f583d3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "00021b3e647beafe909db6ea81f7abbb68e93888c9de24f5fde5f7c3b7637b9d"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf3344168e0d17a624b4307ca548934bca9866991d880ba171a1fbce532206fe"
   end
 

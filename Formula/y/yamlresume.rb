3f3436e6c8b2fc2be5da7fab533acb61060af672
Rui Chen <rui@chenrui.dev>
2025-10-05 11:16:48 -0400

    yamlresume 0.7.5 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/y/yamlresume.rb
@@ -0,0 +1,24 @@
+class Yamlresume < Formula
+  desc "Resumes as code in YAML"
+  homepage "https://github.com/yamlresume/yamlresume"
+  url "https://registry.npmjs.org/yamlresume/-/yamlresume-0.7.5.tgz"
+  sha256 "77a1ff0ffa031a3e335d0eb453c12439c90dd54273889ef3434667b5539e52d3"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/yamlresume --version")
+
+    system bin/"yamlresume", "new"
+    assert_match "YAMLResume provides a builtin schema", (testpath/"resume.yml").read
+
+    output = shell_output("#{bin}/yamlresume validate resume.yml")
+    assert_match "Resume validation passed", output
+  end
+end

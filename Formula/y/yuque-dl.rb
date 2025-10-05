eceda3bf2a39148d487c1da474c49bf78059514a
Rui Chen <rui@chenrui.dev>
2025-10-05 11:26:22 -0400

    yuque-dl 1.0.81 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/y/yuque-dl.rb
@@ -0,0 +1,20 @@
+class YuqueDl < Formula
+  desc "Knowledge base downloader for Yuque"
+  homepage "https://github.com/gxr404/yuque-dl"
+  url "https://registry.npmjs.org/yuque-dl/-/yuque-dl-1.0.81.tgz"
+  sha256 "41039640509fd213938a7c412fec8a43492d52d30bf3fbdcf5f34c905c7a5b8c"
+  license "ISC"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/yuque-dl --version")
+
+    assert_match "Please enter a valid URL", shell_output("#{bin}/yuque-dl test 2>&1", 1)
+  end
+end

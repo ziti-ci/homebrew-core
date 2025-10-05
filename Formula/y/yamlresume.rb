33323fc6bd28b7d68ab0ef8f80541729d2e6f3e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-05 15:33:22 +0000

    yamlresume: add 0.7.5 bottle.

--- Formula/y/yamlresume.rb
+++ Formula/y/yamlresume.rb
@@ -5,6 +5,10 @@ class Yamlresume < Formula
   sha256 "77a1ff0ffa031a3e335d0eb453c12439c90dd54273889ef3434667b5539e52d3"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "da9c94b4ee2b7916a0f249f4fb5207fe14577b9a840f34cf2c1fa7fdbc32910a"
+  end
+
   depends_on "node"
 
   def install

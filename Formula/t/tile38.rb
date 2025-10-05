d0b6097fe5c285c9d056ee12b920d8166fb8208b
Anton Melnikov <antonsm21@gmail.com>
2025-10-05 15:48:03 +0000

    tile38: update 1.36.5 bottle.

--- Formula/t/tile38.rb
+++ Formula/t/tile38.rb
@@ -12,6 +12,7 @@ class Tile38 < Formula
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "ee88a236bf68b49cdfb2543dc88eb95fae293be156b07de8060dbebe107053b7"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "03f270ec38f4b69a51bf8b77db658937c01b805c289589b6dc7e23ed68c2e609"
     sha256 cellar: :any_skip_relocation, sonoma:        "1f0498da1366d75037f12a3f4e2405c496d062930f0b57ef4b2afb230d1f0ddf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c166d5307e57710adfbb4ee57c42eac71e5f6771b60b02b154ff7fdc563a8568"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "c89546c235af2ab22636fd440fde5344e121d36f458ece74a0589601006c8dc0"
   end
 

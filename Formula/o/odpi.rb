e3a9e485392ac5efe52823bf190af430d6b4bb76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 23:11:38 +0000

    odpi 5.6.3

--- Formula/o/odpi.rb
+++ Formula/o/odpi.rb
@@ -1,8 +1,8 @@
 class Odpi < Formula
   desc "Oracle Database Programming Interface for Drivers and Applications"
   homepage "https://oracle.github.io/odpi/"
-  url "https://github.com/oracle/odpi/archive/refs/tags/v5.6.2.tar.gz"
-  sha256 "37c9faaa883df7a3e9e02fb43c33f53e5b6f047361ef51aa1393c34395fd801f"
+  url "https://github.com/oracle/odpi/archive/refs/tags/v5.6.3.tar.gz"
+  sha256 "9efbadd237b2670ed87cedf5bd01b485f645020373c655b69b9a2c1c82ce0d43"
   license any_of: ["Apache-2.0", "UPL-1.0"]
 
   bottle do

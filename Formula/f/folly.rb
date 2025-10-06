bc91df2a9c2d5771a88a89871753f8b73c8f430a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 17:01:41 +0000

    folly 2025.10.06.00

--- Formula/f/folly.rb
+++ Formula/f/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/refs/tags/v2025.09.15.00.tar.gz"
-  sha256 "36002c4ba91a89a59972807cd02e08fabdb0d6d6461040bbb2f0a7938c550060"
+  url "https://github.com/facebook/folly/archive/refs/tags/v2025.10.06.00.tar.gz"
+  sha256 "9ed3d1752c44ac9eb6dfd1ae88d7bc0bf4690152b8966dd0e12b1c0c3d3ca1f2"
   license "Apache-2.0"
   head "https://github.com/facebook/folly.git", branch: "main"
 

375085941cbc8f8fc1be35b427cc11191e1dd96e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-07 12:56:25 +0000

    radvd: add 2.20 bottle.

--- Formula/r/radvd.rb
+++ Formula/r/radvd.rb
@@ -5,6 +5,14 @@ class Radvd < Formula
   sha256 "25d2960fb977ac35c45a8d85b71db22ed8af325db7dbf4a562fb03eab2848dcd"
   license "radvd"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d37d87602c7c0ca6e9b088215c1ca3a7bf01e8b62aa3be7b76ada6a9823f9078"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eec560f303de91f1806f46c6f54f4e51ec080f1dfc229fa569d2c0baab0711e2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54485d998b57f3e9a199f53216530e0ca07e2da6d6a54872f5a2f4d1b4642a1b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a4f4a3af815645871d6eadf2574adad2450da398f767876acdbdd06fe04fb93b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b4e8076563de9d02831b7ebfcedca64b085732111b2363ed46da86faeb382c51"
+  end
+
   head do
     url "https://github.com/radvd-project/radvd.git", branch: "master"
 

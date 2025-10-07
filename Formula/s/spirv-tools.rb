9f9ae91ede64128c3b79c3fa58f0c9856982cba5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-07 13:30:55 +0000

    spirv-tools: update 1.4.328.0 bottle.

--- Formula/s/spirv-tools.rb
+++ Formula/s/spirv-tools.rb
@@ -13,14 +13,12 @@ class SpirvTools < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "0a773817b52358ad902ecd590f9f8321461ba42bcebf6ba3c7a00fc4606f87c1"
-    sha256 cellar: :any,                 arm64_sequoia: "ebb1c57e0f9cfd77dcf9e9a11c09f3775274251b928b0629719f1f7bb51afa5a"
-    sha256 cellar: :any,                 arm64_sonoma:  "1d590b32bf48250371181e59c1ce0168c66d33006f5dbd88198b7fdf7da39353"
-    sha256 cellar: :any,                 arm64_ventura: "48ff490ce54dcc9013f8ba3aa3c4e56098436b53843f51b9f2d85c72f68e9649"
-    sha256 cellar: :any,                 sonoma:        "ef247905d7a6296948ff6bd1342f4033efa5c5bc6ee85dd4535bf86001e04ad3"
-    sha256 cellar: :any,                 ventura:       "e4ab64e171c4b429d3ed2e7fa487a31eae1a15f241dae4e422ebaba3b931ef20"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a9e0642587aea3cf5cf46be903d701fde22809b02d7277ffd5dda8980c0ff01e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bcb5b2b95375978d14f1b470343c4e1c00d159d940a876d356a3fa50e83bb005"
+    sha256 cellar: :any,                 arm64_tahoe:   "3c73443d8a78c83d6eccdf70a83e1a31d1ad53cd0de8ba8552f3313e474bd0a1"
+    sha256 cellar: :any,                 arm64_sequoia: "2efc0617859672df26e9f3790266876d3b10ccb207de0a69c268ca704f3937c7"
+    sha256 cellar: :any,                 arm64_sonoma:  "afb7bd7fc075dd0fb7882648e35bb45ada250d677a97330da5d2dfdc2fe2f7eb"
+    sha256 cellar: :any,                 sonoma:        "01b90d810cb0d32e96d50f5c39e7b70784c7ec484693d436cdc1a0bd078404c3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "447ad7359622dfd433e411d7d1711e4b954d1e5c3f6084689a4152c12b636080"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7bfe93528843c2e75cc3ef2778e1e947398a97fae95c19be7345da8784b4880d"
   end
 
   depends_on "cmake" => :build

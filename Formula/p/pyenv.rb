c5a77ade6679ba41faaca3035dccc3b302bddffe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-07 20:14:01 +0000

    pyenv 2.6.9

--- Formula/p/pyenv.rb
+++ Formula/p/pyenv.rb
@@ -1,8 +1,8 @@
 class Pyenv < Formula
   desc "Python version management"
   homepage "https://github.com/pyenv/pyenv"
-  url "https://github.com/pyenv/pyenv/archive/refs/tags/v2.6.8.tar.gz"
-  sha256 "039121545f2d09bb25c30c452c9487df9b68b0da0096fabf7c540db7516a1e1a"
+  url "https://github.com/pyenv/pyenv/archive/refs/tags/v2.6.9.tar.gz"
+  sha256 "57a0d44a3067de6222255e2046aeca5c1cfed350d92da58e9fc602bb464a53dc"
   license "MIT"
   version_scheme 1
   head "https://github.com/pyenv/pyenv.git", branch: "master"

a01dfea25d8134d731d75b562669747528eb115f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-07 05:16:50 +0000

    openapi 1.7.8

--- Formula/o/openapi.rb
+++ Formula/o/openapi.rb
@@ -1,8 +1,8 @@
 class Openapi < Formula
   desc "CLI tools for working with OpenAPI, Arazzo and Overlay specifications"
   homepage "https://github.com/speakeasy-api/openapi"
-  url "https://github.com/speakeasy-api/openapi/archive/refs/tags/v1.7.7.tar.gz"
-  sha256 "c37d920af829735a68d5ff46f56bf7304f581094a7e7f5fb7b023546685e0254"
+  url "https://github.com/speakeasy-api/openapi/archive/refs/tags/v1.7.8.tar.gz"
+  sha256 "8baed9c5e50f6939e1ff7d1f9de936320412c3a29f2ba7d460474e9e0f9c55cb"
   license "MIT"
   head "https://github.com/speakeasy-api/openapi.git", branch: "main"
 

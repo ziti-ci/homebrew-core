f3df50dd3c9dc031014ca395ba918514056fbfce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-07 11:27:28 +0000

    unixodbc: update 2.3.14 bottle.

--- Formula/u/unixodbc.rb
+++ Formula/u/unixodbc.rb
@@ -12,13 +12,12 @@ class Unixodbc < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_tahoe:   "7c9526b441435001124460466ddd9c036cc494d894b9131a2c1e03f3a0b4dc86"
-    sha256 arm64_sequoia: "e319fd6f83f15c3c96c570c4f9782ad0d964181a98da712905b94f589dddd921"
-    sha256 arm64_sonoma:  "f0363503a99f6f4ffa64b12eb5c99612250af7964c8be8906d3156a798065531"
-    sha256 sonoma:        "b033f17443c5fc20d3af8f904b90a3bf9bdf668efbfb8bdf0bac5b1bfc3ebc64"
-    sha256 arm64_linux:   "151b0f1ec0f4cc526c338542ce1a292ccc923e09c7f3f23bd178794a5b93dcb7"
-    sha256 x86_64_linux:  "5a7ce1eeb9ce55f3a61a9a9ae16123ad02bf67462abdd49d73a42058cab5db01"
+    sha256 arm64_tahoe:   "16b4c2a9d565749bae947c75a100ec4628e57c427af5b542593f67474077b3cb"
+    sha256 arm64_sequoia: "e1522105d20545743c713e90c17bf6393d7a829ec01137ec0d6942bbc04ea2d2"
+    sha256 arm64_sonoma:  "39f022a55a502b3b1d46769705cc46c4aa9f9206b902558ed56cd47aab3d54d8"
+    sha256 sonoma:        "6fc56b53e2abd5b38ed655c2c32caae95753e6b6f5a9fe0a10c4b760a3974d2e"
+    sha256 arm64_linux:   "8e6e9979f6e88ae812222d215688d5b044e25f3182a70e3fbeec3ca07dd9f3d9"
+    sha256 x86_64_linux:  "3f04884aa241e341fea833df1fcb51ec7b181e5a75ff4a7b86ca87dc4c7a24dd"
   end
 
   depends_on "libtool"

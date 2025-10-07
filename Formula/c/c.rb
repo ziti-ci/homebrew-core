33bf8ec95b850de0eb71e6741b9710c8443c891f
botantony <antonsm21@gmail.com>
2025-10-07 10:11:07 +0200

    c 0.15.1

--- Formula/c/c.rb
+++ Formula/c/c.rb
@@ -1,12 +1,10 @@
 class C < Formula
   desc 'Compile and execute C "scripts" in one go'
   homepage "https://github.com/ryanmjacobs/c"
-  url "https://github.com/ryanmjacobs/c/archive/refs/tags/v0.14.tar.gz"
-  sha256 "2b66d79d0d5c60b8e6760dac734b8ec9a7d6a5e57f033b97086821b1985a870b"
+  url "https://github.com/ryanmjacobs/c/archive/refs/tags/v0.15.1.tar.gz"
+  sha256 "ecfad78cb0ab56da44dcfed805f5c261ddefd6dc4a4e57eb2dcfcffa85330605"
   license "MIT"
   head "https://github.com/ryanmjacobs/c.git", branch: "master"
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     rebuild 1

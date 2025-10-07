0f7aa56d8661052044919516c95ca099750cb319
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-07 19:55:36 +0000

    cliproxyapi 6.1.8

--- Formula/c/cliproxyapi.rb
+++ Formula/c/cliproxyapi.rb
@@ -1,8 +1,8 @@
 class Cliproxyapi < Formula
   desc "Wrap Gemini CLI, Codex, Claude Code, Qwen Code as an API service"
   homepage "https://github.com/router-for-me/CLIProxyAPI"
-  url "https://github.com/router-for-me/CLIProxyAPI/archive/refs/tags/v6.1.7.tar.gz"
-  sha256 "e874434fb57eba076a7b490a3a8d8a6c8594299d809a5c535152c745f6b11796"
+  url "https://github.com/router-for-me/CLIProxyAPI/archive/refs/tags/v6.1.8.tar.gz"
+  sha256 "59ea7015e131f10139b16bcf47b4066fb1e1d98e988a779a91877869ec3be9a7"
   license "MIT"
 
   bottle do

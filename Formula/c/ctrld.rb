7e42174d2742153ac14ddd9eb2b00a4e7b6e5325
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-07 07:29:14 +0000

    ctrld: add 1.4.6 bottle.

--- Formula/c/ctrld.rb
+++ Formula/c/ctrld.rb
@@ -5,6 +5,14 @@ class Ctrld < Formula
   sha256 "c29e367304da0d603ce4edd2fae007631656bd3d6b63fa473803dd18449d2d59"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9681e34779e6f8a33d11e18bad9463c4c0f37f63ac4dc904c2287957de97545c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9681e34779e6f8a33d11e18bad9463c4c0f37f63ac4dc904c2287957de97545c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9681e34779e6f8a33d11e18bad9463c4c0f37f63ac4dc904c2287957de97545c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "dc6af0e66111de7fd2df47880a7fbbdd5cee33416563d3273283c562136fa29f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "11d66d7723bdc6ddc3496d35551fdffe9b961f59a14b07fbb5a61de146f5fbc5"
+  end
+
   depends_on "go" => :build
 
   def install

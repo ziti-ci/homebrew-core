3a287bbfcc676476c31c9e9080792f6b7c8389ab
botantony <antonsm21@gmail.com>
2025-10-07 10:14:22 +0200

    libbladerf 2025.10

--- Formula/lib/libbladerf.rb
+++ Formula/lib/libbladerf.rb
@@ -2,10 +2,9 @@ class Libbladerf < Formula
   desc "USB 3.0 Superspeed Software Defined Radio Source"
   homepage "https://nuand.com/"
   url "https://github.com/Nuand/bladeRF.git",
-      tag:      "2023.02",
-      revision: "41ef63460956e833c9b321252245257ab3946055"
+      tag:      "2025.10",
+      revision: "fcf9423325ae49f5fdd2e5b52ab68bfc2576ad47"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later", "MIT"]
-  revision 1
   head "https://github.com/Nuand/bladeRF.git", branch: "master"
 
   # Upstream uses GitHub releases to indicate that a version is released
@@ -15,8 +14,6 @@ class Libbladerf < Formula
     url :stable
     strategy :github_latest
   end
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:    "1e5e5ebc76ee6b91fb1507f9cdbd4701757740664c7270dffa847c12525c90ca"
@@ -36,7 +33,9 @@ class Libbladerf < Formula
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "libusb"
+  depends_on "ncurses"
 
+  uses_from_macos "curl"
   uses_from_macos "libedit"
 
   def install
@@ -44,8 +43,6 @@ class Libbladerf < Formula
     args = %W[
       -DUDEV_RULES_PATH=#{lib}/udev/rules.d
     ]
-    # Workaround to build with CMake 4
-    args << "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
     system "cmake", "-S", "host", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"

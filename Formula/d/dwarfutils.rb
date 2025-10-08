b18711beaafe71cb4a89e14b247fbb4fd76051e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-08 22:58:47 +0000

    dwarfutils 2.2.0

--- Formula/d/dwarfutils.rb
+++ Formula/d/dwarfutils.rb
@@ -1,8 +1,8 @@
 class Dwarfutils < Formula
   desc "Dump and produce DWARF debug information in ELF objects"
   homepage "https://www.prevanders.net/dwarf.html"
-  url "https://www.prevanders.net/libdwarf-2.1.0.tar.xz"
-  sha256 "461bd29cbb9a41c26a25b0e527c3736c772bb7a89f6260d1edb39ab105226e06"
+  url "https://www.prevanders.net/libdwarf-2.2.0.tar.xz"
+  sha256 "54c0abbbeb4190bd1babb5d28574d2b20c2309343ec764cc7ca611e527ee4a42"
   license all_of: ["BSD-2-Clause", "LGPL-2.1-or-later", "GPL-2.0-or-later"]
   version_scheme 1
 

80e340c7d27534171f6358900db0c1825bb35472
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-08 03:15:05 +0000

    rbspy: update 0.38.0 bottle.

--- Formula/r/rbspy.rb
+++ Formula/r/rbspy.rb
@@ -6,12 +6,12 @@ class Rbspy < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6888406b26dc431589a971935aa67b846f0f145cdf1e89d5972da4fa2859e7ca"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1c1f25c42a528ff5665585ff51d5b2fa43b6b3387db825cc6d7f178628d1b71a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bf7afe073b37f743fccc7030dbffe0c51e946943352e195d45074df48a03044d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3e99eb5f15592aa5ca371870fbabacd750785ef9dce28555b3d3c8c1f80556c3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "da2dcb94b7094c8c5e2030feae88642fb5edb7de593c4e8fac19b83b212d803c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "530976d65718ba60467824a509527618625e912ab7aa316e739c45e6c5a02ca0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "40eb4c1f8daa1580f9b64c66b3576ca4fef66b69eb883d079a2287023fddca05"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cbf22daf0250d42519835b3d74f3559514e11fa16dee69810fa895a1bae4e1d3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b3302bb00e089678c3c5ad7f4920d38f73cef6544e356e68dece8ffb5615f6fa"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b8b53ada48fd951fc3b47b34b8bd24fd70e349df50c769ae90f780be31361134"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "650a74b421ab357fb616648776fef55fe3845e97f08582211c3df73ea0ab8f7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "47aa6e47853dda3b48aaab4329d3c2958f4137e8288e3570ce99f0432f08ecfe"
   end
 
   depends_on "rust" => :build

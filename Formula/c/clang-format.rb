1cd2802e1df01f3eefab8999d935cd8b4f9f84ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-08 14:41:48 +0000

    clang-format: update 21.1.3 bottle.

--- Formula/c/clang-format.rb
+++ Formula/c/clang-format.rb
@@ -45,12 +45,12 @@ class ClangFormat < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1352a666412ac474bb832b914b33cb5d06609c027be529c0a01c4f7eb91c46a0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8bc62bab6304951764fb64d7d01eb4b1f27f7ce5172f4265ca7a363fd0451c34"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "84c7a2f60275517b3f77b1e3ec1576b599f255754f609737a1704efb9ccb5128"
-    sha256 cellar: :any_skip_relocation, sonoma:        "5204020b9063d2c967c1d2c977680b5236f5d82f8c2a9102f007f3d17e82f97d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0a69bef8e61b2167409eb0affec43dc17e243fe3eb3d0b52e5ae995177c63c6b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3443ec27012c4476e07bb26d4a749fa6567708311e487237589e64815da5c1ca"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a44f2dbf8165944ad0c6545329af5bc782feeec82e2e7240ce7bda814a4714da"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "68c4bdc00f55d3a5268718990ea70224fcfa9a3bdba2c2c2bf17248d6af1e690"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c82c553fa450645641ab6496c3b6c82bb15c25b12c968550aee4fa7af525eece"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bf25229d5048e12e0932cb0b11d449f35e4e9473640930f431a99c048aefab2b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9c23eeeddc65ad4093ebc62caadac808ae54ca3a2cd964abecac9d2799f641c1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d18713cddbc537583ba11a6e0337b6bc3a4b4546b50d640035f91f746197680"
   end
 
   depends_on "cmake" => :build

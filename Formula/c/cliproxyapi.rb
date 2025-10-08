6ca60f989e4a7e6fd15b449e320f692597eb1699
botantony <antonsm21@gmail.com>
2025-10-08 09:43:37 +0200

    cliproxyapi: disable CGO on Linux Arm
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/c/cliproxyapi.rb
+++ Formula/c/cliproxyapi.rb
@@ -16,6 +16,7 @@ class Cliproxyapi < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
     ldflags = %W[
       -s -w
       -X main.Version=#{version}

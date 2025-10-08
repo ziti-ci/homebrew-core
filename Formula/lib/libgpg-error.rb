2739c68253a26c09a9d5cda148961d65d96810dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-08 02:34:15 +0000

    libgpg-error: update 1.56 bottle.

--- Formula/lib/libgpg-error.rb
+++ Formula/lib/libgpg-error.rb
@@ -11,14 +11,12 @@ class LibgpgError < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "bc4edc6247d8f5257010349424922e4f798dc13c01ce801a46f69363eda88cc9"
-    sha256 arm64_sequoia: "a2d0e274ace59980a81d2b06c445ddef5498c69cbefc85bdf04d3d79d0dfd71d"
-    sha256 arm64_sonoma:  "3ef3920eef9d7f025f528e438ec467d763bfe595e11a5763f0c63fb6dd40aeae"
-    sha256 arm64_ventura: "d0067e8720929efdf81cc5b5dd9b3b05ca7d7dccbdc7e3178e25103f2e85ccdf"
-    sha256 sonoma:        "031f35425ad74efb6299dec9832896c281c630d672c65559bd9dd62c256be76c"
-    sha256 ventura:       "5ad9a39d350da42b905a240de2910ccea43a320b23a56e557d934deafb986c30"
-    sha256 arm64_linux:   "142ea10cc7ccbcfc02f233a3c8f18cb2c90964bb0164da7335a9caa9d08abf1b"
-    sha256 x86_64_linux:  "78119cb448e954ceba1461b93a80700168ecf3b9e330a39f5b3a6f195d162607"
+    sha256 arm64_tahoe:   "c5a04a6d7e3628a9ce018d9acdf269e1a6b5f0eb8a50803ce047da9a73fabf52"
+    sha256 arm64_sequoia: "9dd171019b3b4161374ff8886591ff79270d475f2f000f5b9d6bf9535f75352a"
+    sha256 arm64_sonoma:  "7d5fb8a99b6c9c1fb487da8bb4ecb155ee89e537bc26bc5e7cac3fbbc3420470"
+    sha256 sonoma:        "79fded0b2f1818067dd828aa1c3c9d95d8a22ea31bc1541bc7d2bc6d5cd3a90d"
+    sha256 arm64_linux:   "11d136c0bcd5402f317b4caaf0c904e1da431c0c44e480d2966749453e55d6ba"
+    sha256 x86_64_linux:  "fa8d70d9720645a83a9f0aa84d1fe5dcb631429f724608004c4d18a2d7a19845"
   end
 
   on_macos do

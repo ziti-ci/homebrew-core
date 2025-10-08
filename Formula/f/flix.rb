41d5c6a212d021429d45ff528ce093b0a8b62e77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-08 08:01:27 +0000

    flix 0.65.0
    flix: add openjdk 25/gradle 9.1 patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/flix.rb
+++ Formula/f/flix.rb
@@ -1,10 +1,19 @@
 class Flix < Formula
   desc "Statically typed functional, imperative, and logic programming language"
   homepage "https://flix.dev/"
-  url "https://github.com/flix/flix/archive/refs/tags/v0.64.0.tar.gz"
-  sha256 "7642127a69864d937f90f53b00fd45313b04d4b126cc43b256f522e1749027f1"
   license "Apache-2.0"
   head "https://github.com/flix/flix.git", branch: "master"
+
+  stable do
+    url "https://github.com/flix/flix/archive/refs/tags/v0.65.0.tar.gz"
+    sha256 "10ae39d8582bcbc3e3ed0cad1daa992150e779d07641b248fecd9ade2c48a5d0"
+
+    # Java 25 support
+    patch do
+      url "https://github.com/flix/flix/commit/4df8a6dd7f28273951977de25bae073a2a02f2c4.patch?full_index=1"
+      sha256 "1f71f5323e0de0d674008b2f1374ccb46f858d72ae8b740f659d9ca7facc9add"
+    end
+  end
 
   livecheck do
     url :stable
@@ -17,13 +26,13 @@ class Flix < Formula
 
   depends_on "gradle" => :build
   depends_on "scala" => :build
-  depends_on "openjdk@21"
+  depends_on "openjdk"
 
   def install
-    ENV["JAVA_HOME"] = Language::Java.java_home("21")
+    ENV["JAVA_HOME"] = Language::Java.java_home
     system "gradle", "--no-daemon", "build", "jar", "-x", "test"
-    prefix.install "build/libs/flix-#{version}.jar"
-    bin.write_jar_script prefix/"flix-#{version}.jar", "flix", java_version: "21"
+    prefix.install "build/libs/flix.jar"
+    bin.write_jar_script prefix/"flix.jar", "flix"
   end
 
   test do

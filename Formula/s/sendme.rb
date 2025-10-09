1938bd563be20cf10d7c63c490d5bdf77fdf47cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-09 02:45:43 +0000

    sendme 0.29.0

--- Formula/s/sendme.rb
+++ Formula/s/sendme.rb
@@ -1,8 +1,8 @@
 class Sendme < Formula
   desc "Tool to send files and directories, based on iroh"
   homepage "https://iroh.computer/sendme"
-  url "https://github.com/n0-computer/sendme/archive/refs/tags/v0.28.0.tar.gz"
-  sha256 "a159dc8440deec4801ca95fbf59d242d911c1a5546e15e7ae8dca8e4a058243e"
+  url "https://github.com/n0-computer/sendme/archive/refs/tags/v0.29.0.tar.gz"
+  sha256 "16ab2017a09bd584be4ae803dee7c71cea781eb63627abe6cae35f204775cccb"
   license "MIT"
   head "https://github.com/n0-computer/sendme.git", branch: "main"
 

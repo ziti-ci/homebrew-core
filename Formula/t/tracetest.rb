13f7c14960ed27872ca65c9f8121c0663761ab75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-09 19:32:20 +0000

    tracetest: add 1.7.1 bottle.

--- Formula/t/tracetest.rb
+++ Formula/t/tracetest.rb
@@ -6,6 +6,14 @@ class Tracetest < Formula
   license "MIT" # MIT license for the CLI, TCL license for agent
   head "https://github.com/kubeshop/tracetest.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a4e174c5e1728e5f76c7474244d22351c5c0ec14d02d3f9e0fa9cf0471fd798a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a4e174c5e1728e5f76c7474244d22351c5c0ec14d02d3f9e0fa9cf0471fd798a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4e174c5e1728e5f76c7474244d22351c5c0ec14d02d3f9e0fa9cf0471fd798a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d6b1da02531d87d79eac9d39dfb0ca789d31dfb1f4ee9711b2f65a6031609240"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b93cbf60776d43d78f16f9f6235519ff7c1a3c4546770b680fd44e443f4df7a"
+  end
+
   depends_on "go" => :build
 
   def install

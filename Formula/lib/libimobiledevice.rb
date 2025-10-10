f5da62b231ecfa449299e0c67168093b1cc5e69c
botantony <antonsm21@gmail.com>
2025-10-10 19:42:25 +0200

    libimobiledevice 1.4.0

--- Formula/lib/libimobiledevice.rb
+++ Formula/lib/libimobiledevice.rb
@@ -1,12 +1,10 @@
 class Libimobiledevice < Formula
   desc "Library to communicate with iOS devices natively"
   homepage "https://www.libimobiledevice.org/"
-  url "https://github.com/libimobiledevice/libimobiledevice/releases/download/1.3.0/libimobiledevice-1.3.0.tar.bz2"
-  sha256 "53f2640c6365cd9f302a6248f531822dc94a6cced3f17128d4479a77bd75b0f6"
+  url "https://github.com/libimobiledevice/libimobiledevice/releases/download/1.4.0/libimobiledevice-1.4.0.tar.bz2"
+  sha256 "23cc0077e221c7d991bd0eb02150a0d49199bcca1ddf059edccee9ffd914939d"
   license "LGPL-2.1-or-later"
-  revision 3
-
-  no_autobump! because: :requires_manual_review
+  head "https://github.com/libimobiledevice/libimobiledevice.git", branch: "master"
 
   bottle do
     rebuild 1
@@ -20,29 +18,18 @@ class Libimobiledevice < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "d38d4434dc756c8153d4a0ed23ba0e68c643c7e9e1086cf08c5497700867cdb4"
   end
 
-  # libimobiledevice-glue is required for building future versions
-  # Move outside of HEAD clause when there's a new release.
-  head do
-    url "https://github.com/libimobiledevice/libimobiledevice.git", branch: "master"
-    depends_on "libimobiledevice-glue"
-    depends_on "libtatsu"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkgconf" => :build
+  depends_on "libimobiledevice-glue"
   depends_on "libplist"
   depends_on "libtasn1"
+  depends_on "libtatsu"
   depends_on "libusbmuxd"
   depends_on "openssl@3"
 
   def install
-    # Make libimobiledevice work with libplist 2.3.0
-    # Remove this once libimobiledevice gets a new release
-    inreplace "common/utils.h", "PLIST_FORMAT_XML", "PLIST_FORMAT_XML_" if build.stable?
-    inreplace "common/utils.h", "PLIST_FORMAT_BINARY", "PLIST_FORMAT_BINARY_" if build.stable?
-
     # As long as libplist builds without Cython bindings,
     # so should libimobiledevice as well.
     args = %w[

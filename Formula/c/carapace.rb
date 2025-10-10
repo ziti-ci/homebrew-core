226a435edc94c22daa6c93eed0406cae5b30095a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-10 23:36:06 +0000

    carapace: update 1.5.3 bottle.

--- Formula/c/carapace.rb
+++ Formula/c/carapace.rb
@@ -7,12 +7,12 @@ class Carapace < Formula
   head "https://github.com/carapace-sh/carapace-bin.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "24a606653e01d7a7a6c1238087f3f5802f9100f6334b43f02ad5957053263c4a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "24a606653e01d7a7a6c1238087f3f5802f9100f6334b43f02ad5957053263c4a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "24a606653e01d7a7a6c1238087f3f5802f9100f6334b43f02ad5957053263c4a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "63665a9a90a92a6f3ea2f82612c2603f2b6824ab3afa53fe1f3ab7b8da59c84b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c5c3fd7c420c58173c399195a0c99a024893c5ace63d0dca92b0d9ea9c568a2f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "607377095af37b0ba65ab9350004d7b7d0bb60f54b509deb611fe927f20082e2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d6a4debaf398d6943cedc23d19cd6e4dbafdf3c17e7d447032af9a5af8350572"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d6a4debaf398d6943cedc23d19cd6e4dbafdf3c17e7d447032af9a5af8350572"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d6a4debaf398d6943cedc23d19cd6e4dbafdf3c17e7d447032af9a5af8350572"
+    sha256 cellar: :any_skip_relocation, sonoma:        "eb968189a169ed27b4dbba1aa69b9ddceeaf60d62da48998dfa1c548f9d2f6e9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "74e008027e4e44d19166e22b553df189e59af87615ca309cb973baa174887fd2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5aac3aecd30195ce5e7744211c2a037b73d955221d4af87d420396b5e36dad7d"
   end
 
   depends_on "go" => :build

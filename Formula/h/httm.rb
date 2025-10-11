a1d1ca32e9ec7d9eaec3cc9324e8cc05d776a85f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-11 08:25:29 +0000

    httm: update 0.49.3 bottle.

--- Formula/h/httm.rb
+++ Formula/h/httm.rb
@@ -7,12 +7,12 @@ class Httm < Formula
   head "https://github.com/kimono-koans/httm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1c2e3356d7377a654ba3e0a3e7364f38421c83866d58934c2ad0482dd205d96a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80f028eccf49bcad9a57ba514d11105f1d248916941d5dd7a5430a546b9f8829"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d731b59534176a8ee60fad0cbac9cdc3dd3b56f3c2805216132cb53e21511e59"
-    sha256 cellar: :any_skip_relocation, sonoma:        "476b8c22fb563318c197d1474cc9bd90d2d48918ccb79ffb7e9ed003ff9ef226"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "eedbd352f61b6e02f32081f0efdf4ae913681e9c42dfb7341e8971eab1c533e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bdfa751d71db1aaeeb5ed68efeb3ef418599af5a5f8cab22883569bd7eced4e0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9d87c3f4f29c327cb7f31d0d022ee347bbb1a0a893aff478388f17f92e5610dc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6335e78078bc768eb2978d41021826468c4f3a3a3ed7d9271de127ce15ccea95"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "02f752683556e96dcb1ef38c1794befe0de2099db74b4ad764d81bf0424c3e8f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e7981d86d33ea249c5d8d79e55b957ca0c1a91b9a7b9d7cc9df1f46dd09306c1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6338046111e8fdb9ffd3f3180fb6ffd6c298a2ef66adf3311cfbf3d63ace11e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f5aba1379d2f04fa91d2515716e7720742d22e0e78cb713ddfb53c2725b5386"
   end
 
   depends_on "rust" => :build

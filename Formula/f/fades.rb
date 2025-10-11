81d38bd681d373a55ebfeaf048e780fa8a559b7e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-11 17:00:34 +0000

    fades: update 9.0.2_2 bottle.

--- Formula/f/fades.rb
+++ Formula/f/fades.rb
@@ -8,14 +8,13 @@ class Fades < Formula
   head "https://github.com/PyAr/fades.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8ae23ce3e986aa461efc9dfbb017e62daf3d47bab6650455c64efa609c9a6561"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8ae23ce3e986aa461efc9dfbb017e62daf3d47bab6650455c64efa609c9a6561"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8ae23ce3e986aa461efc9dfbb017e62daf3d47bab6650455c64efa609c9a6561"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "8ae23ce3e986aa461efc9dfbb017e62daf3d47bab6650455c64efa609c9a6561"
-    sha256 cellar: :any_skip_relocation, sonoma:        "21e8405e8b50121bf4d6d0a464265411208e394d764d3982e50a74caddb32176"
-    sha256 cellar: :any_skip_relocation, ventura:       "21e8405e8b50121bf4d6d0a464265411208e394d764d3982e50a74caddb32176"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c86e1370b14aac85b9df22c4d8ec002ebb01c24ee432d9a38efe625c6ae0c96"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c86e1370b14aac85b9df22c4d8ec002ebb01c24ee432d9a38efe625c6ae0c96"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a4ba1f34d9b0ea8d6e49e5ed4e459640eb0889c2ea8bd37fed8def5eb0be8b63"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a4ba1f34d9b0ea8d6e49e5ed4e459640eb0889c2ea8bd37fed8def5eb0be8b63"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4ba1f34d9b0ea8d6e49e5ed4e459640eb0889c2ea8bd37fed8def5eb0be8b63"
+    sha256 cellar: :any_skip_relocation, sonoma:        "55dbc5b431725a5be9f9da921bb903db6ace6f7945db3d007956d94797c1f68e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "333d94af5b6c4482a7778980ffa4b0f5b7b9e0ea45b6f304067a249b17ed2bc2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "333d94af5b6c4482a7778980ffa4b0f5b7b9e0ea45b6f304067a249b17ed2bc2"
   end
 
   depends_on "python@3.14"

c9916bbc806bf5c6fc43fc60d6236fbc17551f1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-11 23:02:51 +0000

    threadweaver 6.19.0

--- Formula/t/threadweaver.rb
+++ Formula/t/threadweaver.rb
@@ -1,8 +1,8 @@
 class Threadweaver < Formula
   desc "Helper for multithreaded programming"
   homepage "https://api.kde.org/threadweaver-index.html"
-  url "https://download.kde.org/stable/frameworks/6.18/threadweaver-6.18.0.tar.xz"
-  sha256 "a6e7f4c90b9b9304ef67a0fffadd77655757c65f7bee00c35b38aefc869e3278"
+  url "https://download.kde.org/stable/frameworks/6.19/threadweaver-6.19.0.tar.xz"
+  sha256 "d8d4d0b6e62b067a8ce4fed7aefeed02ed43a43f97f085db3baedf9210070da1"
   license "LGPL-2.0-or-later"
   head "https://invent.kde.org/frameworks/threadweaver.git", branch: "master"
 

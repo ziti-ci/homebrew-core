414f66c362c67659298c2cdb0c4f7c865dbcd496
Branch Vincent <branchevincent@gmail.com>
2025-10-11 08:37:24 -0700

    pipdeptree: migrate to `python@3.14`

--- Formula/p/pipdeptree.rb
+++ Formula/p/pipdeptree.rb
@@ -11,7 +11,7 @@ class Pipdeptree < Formula
     sha256 cellar: :any_skip_relocation, all: "73d8fc297317a688d08f1bbd9eefa4f19f2c582addd10b6a4aee76ee6e498766"
   end
 
-  depends_on "python@3.13"
+  depends_on "python@3.14"
 
   resource "packaging" do
     url "https://files.pythonhosted.org/packages/a1/d4/1fc4078c65507b51b96ca8f8c3ba19e6a61c8253c72794544580a7b6c24d/packaging-25.0.tar.gz"

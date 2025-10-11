171b6918196f6769e85cb847d7b7f9f72b0fe0ab
Branch Vincent <branchevincent@gmail.com>
2025-10-11 11:17:21 -0700

    repren: migrate to `python@3.14`

--- Formula/r/repren.rb
+++ Formula/r/repren.rb
@@ -11,7 +11,7 @@ class Repren < Formula
     sha256 cellar: :any_skip_relocation, all: "5a82077e5896eea071dea3a44430d1061df24d3d2b0b8cb72b3a6f46ab4846d8"
   end
 
-  depends_on "python@3.13"
+  depends_on "python@3.14"
 
   def install
     virtualenv_install_with_resources

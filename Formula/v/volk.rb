bb302a3b4cc88229c4976540888e17ff6a8b9a07
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-10-11 17:03:18 +0900

    volk: migrate to `python@3.14`

--- Formula/v/volk.rb
+++ Formula/v/volk.rb
@@ -22,11 +22,11 @@ class Volk < Formula
   depends_on "pkgconf" => :build
   depends_on "cpu_features"
   depends_on "orc"
-  depends_on "python@3.13"
+  depends_on "python@3.14"
 
   resource "mako" do
-    url "https://files.pythonhosted.org/packages/5f/d9/8518279534ed7dace1795d5a47e49d5299dd0994eed1053996402a8902f9/mako-1.3.8.tar.gz"
-    sha256 "577b97e414580d3e088d47c2dbbe9594aa7a5146ed2875d4dfa9075af2dd3cc8"
+    url "https://files.pythonhosted.org/packages/9e/38/bd5b78a920a64d708fe6bc8e0a2c075e1389d53bef8413725c63ba041535/mako-1.3.10.tar.gz"
+    sha256 "99579a6f39583fa7e5630a28c3c1f440e4e97a414b80372649c0ce338da2ea28"
   end
 
   resource "markupsafe" do
@@ -35,7 +35,7 @@ class Volk < Formula
   end
 
   def python3
-    "python3.13"
+    "python3.14"
   end
 
   def install

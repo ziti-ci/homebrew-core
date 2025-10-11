2ce54d46f08c247385eaa73e3f427f5cc407b638
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-10-11 15:19:04 +0900

    xdot: migrate to `python@3.14`

--- Formula/x/xdot.rb
+++ Formula/x/xdot.rb
@@ -6,7 +6,7 @@ class Xdot < Formula
   url "https://files.pythonhosted.org/packages/38/76/0503dddc3100e25135d1380f89cfa5d729b7d113a851804aa98dc4f19888/xdot-1.4.tar.gz"
   sha256 "fb029dab92b3c188ad5479108014edccb6c7df54f689ce7f1bd1c699010b7781"
   license "LGPL-3.0-or-later"
-  head "https://github.com/jrfonseca/xdot.py.git", branch: "master"
+  head "https://github.com/jrfonseca/xdot.py.git", branch: "main"
 
   no_autobump! because: "`update-python-resources` cannot determine dependencies"
 
@@ -28,7 +28,7 @@ class Xdot < Formula
   depends_on "numpy"
   depends_on "py3cairo"
   depends_on "pygobject3"
-  depends_on "python@3.13"
+  depends_on "python@3.14"
 
   resource "graphviz" do
     url "https://files.pythonhosted.org/packages/fa/83/5a40d19b8347f017e417710907f824915fba411a9befd092e52746b63e9f/graphviz-0.20.3.zip"
@@ -36,8 +36,8 @@ class Xdot < Formula
   end
 
   resource "packaging" do
-    url "https://files.pythonhosted.org/packages/51/65/50db4dda066951078f0a96cf12f4b9ada6e4b811516bf0262c0f4f7064d4/packaging-24.1.tar.gz"
-    sha256 "026ed72c8ed3fcce5bf8950572258698927fd1dbda10a5e981cdf0ac37f4f002"
+    url "https://files.pythonhosted.org/packages/a1/d4/1fc4078c65507b51b96ca8f8c3ba19e6a61c8253c72794544580a7b6c24d/packaging-25.0.tar.gz"
+    sha256 "d443872c98d677bf60f6a1f2f8c1cb748e8fe762d2bf9d3148b5599295b0fc4f"
   end
 
   def install

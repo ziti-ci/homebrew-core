9ea8d1ac6fc4dee894b412f720d6a8f260bcef7c
botantony <antonsm21@gmail.com>
2025-10-11 21:11:23 +0200

    kea: migrate to `python@3.14`

--- Formula/k/kea.rb
+++ Formula/k/kea.rb
@@ -32,7 +32,7 @@ class Kea < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
-  depends_on "python@3.13" => :build
+  depends_on "python@3.14" => :build
   depends_on "boost"
   depends_on "log4cplus"
   depends_on "openssl@3"

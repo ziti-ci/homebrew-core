b53eadbaafb29b9e6524e1106fc3c2db70ec5657
Branch Vincent <branchevincent@gmail.com>
2025-10-12 11:05:01 -0700

    grc: migrate to `python@3.14`

--- Formula/g/grc.rb
+++ Formula/g/grc.rb
@@ -7,7 +7,7 @@ class Grc < Formula
   sha256 "a7b10d4316b59ca50f6b749f1d080cea0b41cb3b7258099c3eb195659d1f144f"
   license "GPL-2.0-or-later"
   revision 1
-  head "https://github.com/garabik/grc.git", branch: "devel"
+  head "https://github.com/garabik/grc.git", branch: "master"
 
   no_autobump! because: :requires_manual_review
 
@@ -16,7 +16,7 @@ class Grc < Formula
     sha256 cellar: :any_skip_relocation, all: "c53a320fd40831dbfd74a3add881c94b5d8bca074b6826c190f35c1a3316d96d"
   end
 
-  depends_on "python@3.13"
+  depends_on "python@3.14"
 
   def install
     # fix non-standard prefix installs

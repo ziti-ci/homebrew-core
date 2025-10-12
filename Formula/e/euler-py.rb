94f2ef9fe8a95ee066c8e6cfa10517eadb9e3e49
Branch Vincent <branchevincent@gmail.com>
2025-10-12 10:17:53 -0700

    euler-py: migrate to `python@3.14`

--- Formula/e/euler-py.rb
+++ Formula/e/euler-py.rb
@@ -14,7 +14,7 @@ class EulerPy < Formula
     sha256 cellar: :any_skip_relocation, all: "61c7da218aca560f86ce1aae136927319f9d48d07aae107e531c19d3e0350209"
   end
 
-  depends_on "python@3.13"
+  depends_on "python@3.14"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"

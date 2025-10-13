001d06faa9bf87d25991d95403355bf234b5a5f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-13 20:07:54 +0000

    mcp-inspector 0.17.1

--- Formula/m/mcp-inspector.rb
+++ Formula/m/mcp-inspector.rb
@@ -1,8 +1,8 @@
 class McpInspector < Formula
   desc "Visual testing tool for MCP servers"
   homepage "https://modelcontextprotocol.io/docs/tools/inspector"
-  url "https://registry.npmjs.org/@modelcontextprotocol/inspector/-/inspector-0.17.0.tgz"
-  sha256 "4c860c3348919cbbd32b32e7569b5140ffe5e7f021954012cf573de686f10767"
+  url "https://registry.npmjs.org/@modelcontextprotocol/inspector/-/inspector-0.17.1.tgz"
+  sha256 "a4cbb24b1ba1607137ba74785d71e941332f6feabdd68e1c6093d27ddbc74151"
   license "MIT"
 
   bottle do

bc57a89f751a0b70c6b09ef26e41da2bcf20f788
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-13 23:49:23 +0000

    n8n-mcp: update 2.19.5 bottle.

--- Formula/n/n8n-mcp.rb
+++ Formula/n/n8n-mcp.rb
@@ -6,12 +6,12 @@ class N8nMcp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0cf628f2efceb5967259dbd3ed159945f3bdd6e6f2f3a8b082aa4f20aa753005"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f8a8ee3260ffebdf67001b5b1e63aef3ea476cb53b6777e1baab30291999889d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "916ae8ab0beaa6439e50919df60c8a03072e07c624826a0945bfc643c23da95b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ed4e64fbefacbfedf2bc3998028b9adc5318dc6040e09cdb6f46b2e85c1fa769"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "10f3747660e8f3c92ce4f1e9f1ebf35cb79476bd0b85bf82ecda4b13b3d7fc80"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "098809fefc27e295d8a421fc506fb99e9ecdd55f0db04a7f84a983523611712f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8e8c324cebfbf3a0d7416172e451bc0fd529c2ec4c5dd74c7f64e87686715e27"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "431abc3f8f665e3dc15b339372714611c5dfda9a937b9758dd1eccee3920d488"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "180362ea4e4839120c758086b6ea0a469906870eb64d3acf6e6b655088c3bd2f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "84236c3aeae50d18b841c65033dbbd855fadb1c61dfabce67800f3dcd7423176"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b1c5454a8d95a0408e927dcf7c86cbc9374fe821434650e37ea6377b668c3d11"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5e6be435595e89f8a43b814cabdb1bfc73f5b6e24132e143f5058acad625d4c"
   end
 
   depends_on "node"

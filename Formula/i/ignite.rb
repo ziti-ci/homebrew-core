9655e8bc25bdecbaf607a1b766920ec713a7dcfb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-13 11:08:59 +0000

    ignite 29.6.0

--- Formula/i/ignite.rb
+++ Formula/i/ignite.rb
@@ -1,8 +1,8 @@
 class Ignite < Formula
   desc "Build, launch, and maintain any crypto application with Ignite CLI"
   homepage "https://docs.ignite.com/"
-  url "https://github.com/ignite/cli/archive/refs/tags/v29.4.2.tar.gz"
-  sha256 "ff0a1d374551f176dd87ea217abde4ff4b53fafadde2f1c7dd718f2fbd67f576"
+  url "https://github.com/ignite/cli/archive/refs/tags/v29.6.0.tar.gz"
+  sha256 "aa84aefcd349d32bf30e12f2a1c4e9caa83f0c9066e9899439ba5e0080d9be38"
   license "Apache-2.0"
   head "https://github.com/ignite/cli.git", branch: "main"
 

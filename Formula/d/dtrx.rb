a1ac120f806ab8017437a59e9941b38094b822cb
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-10-14 13:47:12 +0900

    dtrx: migrate to `python@3.14`

--- Formula/d/dtrx.rb
+++ Formula/d/dtrx.rb
@@ -14,7 +14,7 @@ class Dtrx < Formula
 
   # Include a few common decompression handlers in addition to the python dep
   depends_on "p7zip"
-  depends_on "python@3.13"
+  depends_on "python@3.14"
   depends_on "xz"
 
   uses_from_macos "zip" => :test

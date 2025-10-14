5d9d7ea4a424386eab4b05ead2eee995bb473434
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-14 03:01:21 +0000

    xmake 3.0.4

--- Formula/x/xmake.rb
+++ Formula/x/xmake.rb
@@ -1,8 +1,8 @@
 class Xmake < Formula
   desc "Cross-platform build utility based on Lua"
   homepage "https://xmake.io/"
-  url "https://github.com/xmake-io/xmake/releases/download/v3.0.3/xmake-v3.0.3.tar.gz"
-  sha256 "49d70671f40f28a1d8125df1a2b318cbd44608a26fa3c60587be3a5ad835b0fb"
+  url "https://github.com/xmake-io/xmake/releases/download/v3.0.4/xmake-v3.0.4.tar.gz"
+  sha256 "b6968dbe266029987bee0a389175f8898042c0bd38f279befc40adaf8e67ce04"
   license "Apache-2.0"
   head "https://github.com/xmake-io/xmake.git", branch: "dev"
 

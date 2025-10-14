5e27dc749361a55552fbccb32cb709adbb0ed589
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-14 10:33:18 +0000

    nicotine-plus: update 3.3.10 bottle.

--- Formula/n/nicotine-plus.rb
+++ Formula/n/nicotine-plus.rb
@@ -11,7 +11,8 @@ class NicotinePlus < Formula
   no_autobump! because: "`update-python-resources` cannot determine dependencies"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "86dd09af957c7be8e3c7559e1c9b67d2536f0c3025cd70bbe08a17d98ad1f89a"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "80edf82b7afa3a964e69c28edab21a4a588144096efaa10f555b567b1c0bfa3e"
   end
 
   depends_on "adwaita-icon-theme"

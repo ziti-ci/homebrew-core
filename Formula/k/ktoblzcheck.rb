37cff85684f406e7876d52ce97b0f439c0930ca1
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-10-15 15:57:19 +0900

    ktoblzcheck: migrate to `python@3.14`

--- Formula/k/ktoblzcheck.rb
+++ Formula/k/ktoblzcheck.rb
@@ -22,7 +22,7 @@ class Ktoblzcheck < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "python@3.13"
+  depends_on "python@3.14"
   depends_on "sqlite"
 
   uses_from_macos "curl"
@@ -44,7 +44,7 @@ class Ktoblzcheck < Formula
   end
 
   def python3
-    "python3.13"
+    "python3.14"
   end
 
   def install

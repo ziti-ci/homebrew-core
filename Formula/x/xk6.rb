05b9087d575bbe664f81a8f14c0238f129d369b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-15 13:07:27 +0000

    xk6: update 1.2.2 bottle.

--- Formula/x/xk6.rb
+++ Formula/x/xk6.rb
@@ -12,12 +12,12 @@ class Xk6 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "77ad0a9679d4d2712b19225c98d95d4fb71811cca042a96af96c07f39be0cc95"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "77ad0a9679d4d2712b19225c98d95d4fb71811cca042a96af96c07f39be0cc95"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "77ad0a9679d4d2712b19225c98d95d4fb71811cca042a96af96c07f39be0cc95"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7cdc3633add03f37d560c3897de2eaa425fa72a7e71846c4314285afa1ca4b8a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b0e053ec126ad23ef73e260849b4011b74cf16399fcad99b2d271fcdd2b45c13"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f881e000c8c544ea55c523a67f1f381fa452a92023b85abc38b71d7648b2980"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "00c3283b257440c4d7ab1bfa8d872d199f4dbd7d330be2675ae7e117218b55a6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "00c3283b257440c4d7ab1bfa8d872d199f4dbd7d330be2675ae7e117218b55a6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00c3283b257440c4d7ab1bfa8d872d199f4dbd7d330be2675ae7e117218b55a6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fc94746c2b56a5a9f7dfa43b3345e0d35f99557bcab622890d5d9773d49139cc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1b2275b0811778e3fb444be87582f382aa5fb872e4a31a75c2a6e7ea34d632bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b288f4c6442ec7613f2eae53d9d8a66f3d06db7b3707df10ad42e944b055cb8"
   end
 
   depends_on "go"

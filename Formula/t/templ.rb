317fe41c135bcf02a77c2a35c618c17850b6ae76
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-15 16:41:16 +0000

    templ: update 0.3.960 bottle.

--- Formula/t/templ.rb
+++ Formula/t/templ.rb
@@ -6,14 +6,12 @@ class Templ < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a05f2a594f7bd8e559c4745bbcc9d33b96c2f67044e2de35d9355207e22c0034"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "29c7d968ad0042f39269e61b8cf64056635302b66124c83fad1cb8a00f800716"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "29c7d968ad0042f39269e61b8cf64056635302b66124c83fad1cb8a00f800716"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "29c7d968ad0042f39269e61b8cf64056635302b66124c83fad1cb8a00f800716"
-    sha256 cellar: :any_skip_relocation, sonoma:        "49a391dd82a9e897aeb19d1b5e95cd17c820e051a9ae4d1500b31acc100ce544"
-    sha256 cellar: :any_skip_relocation, ventura:       "49a391dd82a9e897aeb19d1b5e95cd17c820e051a9ae4d1500b31acc100ce544"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "792bdee53fb88910015ab060fae3eb30490fcc532c2c3d270158d7ad238df2dc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a6965248db6bf03a83addd3ae24c1be14e2e9ec91dae6903558c26a68dab64c8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f5871f3f6a56e89e8cc808917542723963ddc39c60d940356afa51c6cb742057"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f5871f3f6a56e89e8cc808917542723963ddc39c60d940356afa51c6cb742057"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f5871f3f6a56e89e8cc808917542723963ddc39c60d940356afa51c6cb742057"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c0656f2d51b518adf724c8729971db695cdffe560af475ac6a07b37a23035f4b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bbf42e75d9d672142875ee5bd63a9e1ce4027777910d1459befc4ec12a851365"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63d98da9d9d73e96a7451977fdef3b670bf2dd875a521aa45f7a458909322975"
   end
 
   depends_on "go" => :build

927235dd4acc462868e1e87d0809b4a798ecb1f9
Rui Chen <rui@chenrui.dev>
2025-10-15 22:41:52 -0400

    falcosecurity-libs: revision bump (protobuf 33.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/falcosecurity-libs.rb
+++ Formula/f/falcosecurity-libs.rb
@@ -8,7 +8,7 @@ class FalcosecurityLibs < Formula
     { any_of: ["GPL-2.0-only", "MIT"] }, # driver/
     { "GPL-2.0-only" => { with: "Linux-syscall-note" } }, # userspace/libscap/compat/
   ]
-  revision 9
+  revision 10
 
   livecheck do
     url :stable

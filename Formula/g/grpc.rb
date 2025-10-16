98cfb374187cf834a9692d0245c8b14b513494dc
Rui Chen <rui@chenrui.dev>
2025-10-15 22:41:53 -0400

    grpc: revision bump (protobuf 33.0)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/grpc.rb
+++ Formula/g/grpc.rb
@@ -5,6 +5,7 @@ class Grpc < Formula
       tag:      "v1.75.1",
       revision: "9b63ce0d513672c5daad4f28342f03863c5589e5"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/grpc/grpc.git", branch: "master"
 
   # There can be a notable gap between when a version is tagged and a
